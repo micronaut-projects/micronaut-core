9fbaf42afbf475baf9b3706be38ee923a551ecc1
graemerocher <graeme.rocher@gmail.com>
2017-09-28 14:37:24 +0200

    Cleanup

--- inject/src/main/java/org/particleframework/context/Qualifier.java
+++ inject/src/main/java/org/particleframework/context/Qualifier.java
@@ -6,7 +6,10 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
- * Used to qualify which bean to select in the case of multiple possible options
+ * <p>Used to qualify which bean to select in the case of multiple possible options.</p>
+ *
+ * <p>NOTE: When implementing a custom Qualifier you MUST implement {@link #hashCode()} and {@link #equals(Object)}
+ * so that the qualifier can be used in comparisons and equality checks</p>
  *
  * @author Graeme Rocher
  * @since 1.0
