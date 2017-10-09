c290d0167674a6c1fc2cc5d217e4d22118107d07
Dean Wette <wetted@ociweb.com>
2017-10-09 11:53:20 -0500

    Adding ParametrizedFactorySpec test for java-inject

---
+++ java-inject/src/test/groovy/org/particleframework/inject/factory/parameterizedfactory/A.java
@@ -0,0 +1,16 @@
+package org.particleframework.inject.factory.parameterizedfactory;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class A {
+    String name = "A";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ java-inject/src/test/groovy/org/particleframework/inject/factory/parameterizedfactory/C.java
@@ -0,0 +1,11 @@
+package org.particleframework.inject.factory.parameterizedfactory;
+
+public class C {
+    B b;
+    int count;
+
+    public C(B b, int count) {
+        this.b = b;
+        this.count = count;
+    }
+}
