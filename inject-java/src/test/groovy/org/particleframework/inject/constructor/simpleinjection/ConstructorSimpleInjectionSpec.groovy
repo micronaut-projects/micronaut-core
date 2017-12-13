36c41c940af23631789c381a3c80d34326581e6f
jameskleeh <james.kleeh@gmail.com>
2017-12-13 00:41:14 -0500

    Add constructor arguments to the required components

--- inject-java/src/test/groovy/org/particleframework/inject/constructor/simpleinjection/ConstructorSimpleInjectionSpec.groovy
+++ inject-java/src/test/groovy/org/particleframework/inject/constructor/simpleinjection/ConstructorSimpleInjectionSpec.groovy
@@ -2,6 +2,7 @@ package org.particleframework.inject.constructor.simpleinjection
 
 import org.particleframework.context.BeanContext
 import org.particleframework.context.DefaultBeanContext
+import org.particleframework.inject.BeanDefinition
 import spock.lang.Specification
 
 class ConstructorSimpleInjectionSpec extends Specification {
@@ -19,5 +20,15 @@ class ConstructorSimpleInjectionSpec extends Specification {
         b.a != null
         b2.a != null
         b2.a2 != null
+
+        when:
+        BeanDefinition bd = context.getBeanDefinition(B)
+        BeanDefinition bd2 = context.getBeanDefinition(B2)
+
+        then: "The constructor argument is added to the required components"
+        bd.getRequiredComponents().size() == 1
+        bd.getRequiredComponents().contains(A)
+        bd2.getRequiredComponents().size() == 1
+        bd2.getRequiredComponents().contains(A)
     }
 }
