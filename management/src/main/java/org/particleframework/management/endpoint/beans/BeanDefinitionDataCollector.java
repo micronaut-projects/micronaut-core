a9c749e756c924951f9659b01d6f8254566b286c
jameskleeh <james.kleeh@gmail.com>
2017-12-18 00:15:12 -0500

    Implemented beans endpoint

--- management/src/main/java/org/particleframework/management/endpoint/beans/BeanDefinitionDataCollector.java
+++ management/src/main/java/org/particleframework/management/endpoint/beans/BeanDefinitionDataCollector.java
@@ -1,28 +1,35 @@
 /*
  * Copyright 2017 original authors
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
+package org.particleframework.management.endpoint.beans;
+
+import org.particleframework.inject.BeanDefinition;
+
+import java.util.Collection;
 
 /**
- * This package contains classes and interfaces for performing health checks and the health endpoint
+ * <p>Used to respond with bean information used for the {@link BeansEndpoint}</p>
  *
- * @author Graeme Rocher
+ * @author James Kleeh
  * @since 1.0
  */
-@Configuration
-@Requires(endpoint = "endpoints.health")
-package org.particleframework.management.endpoint.health;
+public interface BeanDefinitionDataCollector<T> {
 
-import org.particleframework.context.annotation.Configuration;
-import org.particleframework.context.annotation.Requires;
+    /**
+     * @param beanDefinitions A collection of bean definitions
+     * @return Data representing all of the given bean definitions
+     */
+    T getData(Collection<BeanDefinition<?>> beanDefinitions);
+}
--- management/src/main/java/org/particleframework/management/endpoint/beans/BeansEndpoint.java
+++ management/src/main/java/org/particleframework/management/endpoint/beans/BeansEndpoint.java
@@ -1,29 +1,43 @@
+/*
+ * Copyright 2017 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.particleframework.management.endpoint.beans;
 
 import org.particleframework.context.BeanContext;
-import org.particleframework.inject.BeanDefinition;
-import org.particleframework.inject.qualifiers.Qualifiers;
 import org.particleframework.management.endpoint.Endpoint;
 import org.particleframework.management.endpoint.Read;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+/**
+ * <p>Exposes an {@link Endpoint} to provide information about the beans of the application.</p>
+ *
+ * @author James Kleeh
+ * @since 1.0
+ */
 @Endpoint("beans")
 public class BeansEndpoint {
 
     private BeanContext beanContext;
+    private BeanDefinitionDataCollector beanDefinitionDataCollector;
 
-    public BeansEndpoint(BeanContext beanContext) {
+    public BeansEndpoint(BeanContext beanContext, BeanDefinitionDataCollector beanAggregator) {
         this.beanContext = beanContext;
+        this.beanDefinitionDataCollector = beanAggregator;
     }
 
     @Read
-    public Map<String, Object> getBeans() {
-        Collection<BeanDefinition<?>> definitions = beanContext.getBeanDefinitions(Qualifiers.byType(Object.class));
-        return Collections.emptyMap();
+    public Object getBeans() {
+        return beanDefinitionDataCollector.getData(beanContext.getAllBeanDefinitions());
     }
 }
