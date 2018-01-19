691d732f9e939aa5cb647fefdee56486a46d0c81
Graeme Rocher <graeme.rocher@gmail.com>
2018-01-19 18:22:42 +0000

    Support for JSON stream of POJOs

--- runtime/src/main/java/org/particleframework/jackson/codec/JsonMediaTypeCodec.java
+++ runtime/src/main/java/org/particleframework/jackson/codec/JsonMediaTypeCodec.java
@@ -17,8 +17,10 @@ package org.particleframework.jackson.codec;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.particleframework.core.io.buffer.ByteBuffer;
 import org.particleframework.core.io.buffer.ByteBufferFactory;
 import org.particleframework.core.type.Argument;
@@ -83,6 +85,14 @@ public class JsonMediaTypeCodec implements MediaTypeCodec {
         }
     }
 
+    public <T> T decode(Argument<T> type, JsonNode node) throws CodecException {
+        try {
+            T result = objectMapper.treeToValue(node, type.getType());
+            return result;
+        } catch (IOException e) {
+            throw new CodecException("Error decoding JSON stream for type ["+type.getName()+"]: " + e.getMessage());
+        }
+    }
     @Override
     public <T> T decode(Argument<T> type, ByteBuffer<?> buffer) throws CodecException {
         try {
