4e547ff8b017713eab3854311a8da20a4a31b7ee
graemerocher <graeme.rocher@gmail.com>
2018-02-16 18:24:49 +0100

    Don't block server startup for service registration

--- discovery-client/src/main/java/org/particleframework/discovery/eureka/registration/EurekaAutoRegistration.java
+++ discovery-client/src/main/java/org/particleframework/discovery/eureka/registration/EurekaAutoRegistration.java
@@ -171,7 +171,7 @@ public class EurekaAutoRegistration extends DiscoveryServiceAutoRegistration {
 
             Publisher<HttpStatus> registerPublisher = eurekaClient.register(instanceInfo.getApp(), instanceInfo);
             Observable<HttpStatus> registerObservable = applyRetryPolicy(registration, registerPublisher);
-            performRegistration(EUREKA_SERVICE_NAME, registration, instanceInfo.getApp(), registerObservable);
+            performRegistration(EUREKA_SERVICE_NAME, registration, instance, registerObservable);
         }
     }
 
