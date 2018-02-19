2bde67a24cdc27182397dedbdbce2650fb838075
Sergio del Amo <sergio.delamo@softamo.com>
2018-02-19 10:27:33 +0100

    add more code to sample app

--- examples/petclinic/storefronthtml/src/main/groovy/example/storefront/PetHtmlRendererImpl.groovy
+++ examples/petclinic/storefronthtml/src/main/groovy/example/storefront/PetHtmlRendererImpl.groovy
@@ -1,10 +1,17 @@
 package example.storefront
 
+import example.api.v1.HealthStatus
+import example.api.v1.HealthStatusUtils
 import example.storefront.ui.NavBar
 import example.storefront.ui.PetListViewModel
 import example.storefront.ui.PetViewModel
+import groovy.json.JsonSlurper
 import groovy.transform.CompileStatic
 import io.reactivex.Single
+import io.reactivex.annotations.NonNull
+import io.reactivex.functions.Function
+import org.particleframework.http.HttpResponse
+import org.particleframework.http.client.exceptions.HttpClientException
 
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -19,10 +26,23 @@ class PetHtmlRendererImpl implements PetHtmlRenderer {
     @Inject
     HtmlRenderer htmlRenderer
 
+    @Inject
+    MailClient mailClient
+
     @Override
     Single<String> renderPet(Long id) {
         Single<String> container = fetcher.findById(id)
                 .map { PetViewModel x -> htmlRenderer.renderPet(x) }
+                .concatWith(requestInfoForm(id))
+                .reduce('', { String s, String s2 -> s + s2 })
         htmlRenderer.renderContainer(NavBar.PET, container)
+    }
+
+    Single<String> requestInfoForm(Long petId) {
+        mailClient.health().map { HealthStatus x ->
+                HealthStatusUtils.isUp(x) ? htmlRenderer.renderRequestInfoForm(petId) : ''
+        }.onErrorReturn { throwable ->
+            ''
+        }
     }
 }
