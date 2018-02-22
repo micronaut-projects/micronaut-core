ecd752d675a0defe8107796c00ad26b5940c08a1
Sergio del Amo <sergio.delamo@softamo.com>
2018-02-22 10:16:11 +0100

    PetClinic - remove storefronthtml module

--- examples/petclinic/storefronthtml/src/main/groovy/example/storefront/VendorsHtmlRenderer.groovy
+++
@@ -1,9 +0,0 @@
-package example.storefront
-
-import groovy.transform.CompileStatic
-import io.reactivex.Single
-
-@CompileStatic
-interface VendorsHtmlRenderer {
-    Single<String> renderVendorsTable()
-}
