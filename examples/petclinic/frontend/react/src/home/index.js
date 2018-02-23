7d426567137d065355ed906f67b4c5f4d08c6446
Zachary Klein <zak@silver-chalice.com>
2018-02-22 19:08:09 -0600

    Load server-side events (offer display)

--- examples/petclinic/frontend/react/src/home/index.js
+++ examples/petclinic/frontend/react/src/home/index.js
@@ -1,5 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
+import config from "../config";
+import Offer from "./Offer";
+import Error from "./Error";
 
-const Home = () => <h2>Home</h2>
+let source;
+
+class Home extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      offer: null,
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    this.loadOffers();
+  }
+
+  componentWillUnmount() {
+    source.close();
+  }
+
+
+  loadOffers() {
+    source = new EventSource(`${config.SERVER_URL}/offers`);
+    source.onmessage = (e) => {
+      const offer = JSON.parse(e.data);
+      this.setState({offer, error: null})
+    };
+    source.onerror = () => {
+      this.setState({error: 'Could not load offers', offer: null})
+    }
+  }
+
+  render() {
+    const {offer, error} = this.state;
+
+    return <div>
+      <Offer offer={offer}/>
+      <Error message={error} />
+
+    </div>
+  }
+}
 
 export default Home;
