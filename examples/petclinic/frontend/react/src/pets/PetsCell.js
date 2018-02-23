fbf04afc88cafdefc13d73c14bf1f1a03097e3cd
Zachary Klein <zak@silver-chalice.com>
2018-02-22 19:07:20 -0600

    Pet grid image display, styling

--- examples/petclinic/frontend/react/src/pets/PetsCell.js
+++ examples/petclinic/frontend/react/src/pets/PetsCell.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import config from "../config";
 
 const PetsCell = ({pet}) =>
-    <div className="col-sm">
-        <p>Name: {pet.name}</p>
-        <p>Vendor: {pet.vendor}</p>
-        <p>Type: {pet.type}</p>
+    <div className="col-sm" style={{
+      backgroundImage: `url(${config.SERVER_URL}/images/${pet.image})`,
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      'background-size': 'cover',
+      margin: '10px',
+      minHeight: '300px',
+      color: 'white'}}>
+
+      <div style={{
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        padding: '10px',
+        width: '100%'}}>
+        <h4>{pet.name}</h4>
+      </div>
     </div>
 
 export default PetsCell;
