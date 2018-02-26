2fd2114698f32c6fa9dcb5133603e3c344fae8db
Zachary Klein <zak@silver-chalice.com>
2018-02-26 17:36:32 -0600

    Added branding images, moved inline styles to stylesheet

---
+++ examples/petclinic/frontend/react/src/about/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import about from '../images/about.png'
+
+const About = () => <div className="text-center">
+
+    <h2>Powered By</h2>
+    <img src={about} className='mx-auto d-block about' />
+
+</div>
+
+export default About
