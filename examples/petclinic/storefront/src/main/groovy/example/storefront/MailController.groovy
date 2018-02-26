971b8c22be386aa167ac36149758fd8b2cd5bfb6
Sergio del Amo <sergio.delamo@softamo.com>
2018-02-26 09:10:00 +0100

    Move Email.send logic to service

--- examples/petclinic/storefront/src/main/groovy/example/storefront/MailController.groovy
+++ examples/petclinic/storefront/src/main/groovy/example/storefront/MailController.groovy
@@ -1,10 +1,7 @@
 package example.storefront
 
-import example.api.v1.Email
 import example.api.v1.HealthStatus
-import example.api.v1.Pet
 import example.storefront.client.v1.MailClient
-import example.storefront.client.v1.PetClient
 import groovy.transform.CompileStatic
 import io.reactivex.Single
 import org.particleframework.http.HttpResponse
@@ -13,8 +10,6 @@ import org.particleframework.http.annotation.Body
 import org.particleframework.http.annotation.Controller
 import org.particleframework.http.annotation.Get
 import org.particleframework.http.annotation.Post
-import org.particleframework.http.client.Client
-import org.particleframework.http.client.RxStreamingHttpClient
 
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -25,14 +20,10 @@ import javax.inject.Singleton
 class MailController {
 
     @Inject
+    EmailService emailService
+
+    @Inject
     MailClient mailClient
-
-    private final PetClient petClient
-
-    MailController(PetClient petClient) {
-        this.petClient = petClient
-    }
-
 
     @Get('/health')
     Single<HealthStatus> health() {
@@ -40,23 +31,8 @@ class MailController {
     }
 
     @Post(uri = '/send', consumes = MediaType.APPLICATION_JSON)
-    HttpResponse send(@Body EmailCommand cmd) {
-        petClient.find(cmd.slug).toSingle().subscribe({Pet pet -> sendEmail(cmd, pet)}, {})
-
+    HttpResponse send(@Body('slug') String slug, @Body('email') String email) {
+        emailService.send(slug, email)
         HttpResponse.ok()
     }
-
-    void sendEmail(EmailCommand cmd, Pet pet) {
-        Email email = new Email()
-        email.with {
-            setRecipient cmd.email
-            setSubject "Micronaut Pet Store - Re: ${pet.name} from ${pet.vendor}"
-        }
-        mailClient.send(email)
-    }
-}
-
-class EmailCommand {
-    String email
-    String slug
 }
