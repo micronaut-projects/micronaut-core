aebea76ad4191cef2dae86b7519895f835cf7d41
Zachary Klein <zak@silver-chalice.com>
2018-03-04 09:22:26 -0600

    Removed some inline styles

--- examples/petclinic/frontend/react/src/comments/Thread.js
+++ examples/petclinic/frontend/react/src/comments/Thread.js
@@ -12,17 +12,19 @@ const Thread = ({
                   updateReply
                 }) => <div>
 
-  <div className='card' style={{textAlign: 'left', marginBottom: 20}}>
+  <div className='card comment-card'>
     <Comment comment={thread} expand={expand} close={close} expanded={thread.expanded} isThread={true}/>
 
-    {thread.expanded ? <div className="card" style={{width: '90%', margin: '0 auto', marginBottom: 20}}>
+    {thread.expanded ? <div className="card replies-card">
+      {thread.replies ? <div className='card-title comment-title'><i>Replies</i></div> :
+        <div className='card-title comment-title'><i>Be the first to reply!</i></div>}
       <ul className='list-group list-group-flush'>
-        {thread.replies ? thread.replies.map(r => <li className='list-group-item' style={{backgroundColor: '#f9f9f9'}} key={r.id}>
+        {thread.replies ? thread.replies.map(r => <li className='list-group-item reply-item' key={r.id}>
           <Comment comment={r}/>
         </li>) : null}
 
 
-        <li className='list-group-item' style={{backgroundColor: '#e9ecef'}}><AddComment submit={submitReply} comment={reply} update={updateReply}
+        <li className='list-group-item comment-form-item'><AddComment submit={submitReply} comment={reply} update={updateReply}
                                                     reply={true} expanded={true}/></li>
       </ul>
     </div> : null}
