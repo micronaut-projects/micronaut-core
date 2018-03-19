05ec2a5987cbc5478b1511cddf07a2e7a7662d29
graemerocher <graeme.rocher@gmail.com>
2018-03-19 11:18:39 +0100

    Add annotation processing Hint

--- inject/src/main/java/io/micronaut/context/exceptions/NoSuchBeanException.java
+++ inject/src/main/java/io/micronaut/context/exceptions/NoSuchBeanException.java
@@ -11,11 +11,11 @@ import io.micronaut.context.Qualifier;
 public class NoSuchBeanException extends BeanContextException {
 
     public NoSuchBeanException(Class beanType) {
-        super("No bean of type [" + beanType.getName() + "] exists");
+        super("No bean of type [" + beanType.getName() + "] exists. If you are using Java or Kotlin make sure you have enabled annotation processing.");
     }
 
     public <T> NoSuchBeanException(Class<T> beanType, Qualifier<T> qualifier) {
-        super("No bean of type [" + beanType.getName() + "] exists" + (qualifier != null ? " for the given qualifier: " + qualifier : ""));
+        super("No bean of type [" + beanType.getName() + "] exists" + (qualifier != null ? " for the given qualifier: " + qualifier : "") + ". If you are using Java or Kotlin make sure you have enabled annotation processing.");
     }
 
     protected NoSuchBeanException(String message) {
