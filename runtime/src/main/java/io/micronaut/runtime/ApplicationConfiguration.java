97c20e315abe3633cbc1e07b1d5a010681a2b17f
Iván López <lopez.ivan@gmail.com>
2018-03-22 17:48:01 +0100

    Add javadoc to runtime @ConfigurationProperties classes

--- runtime/src/main/java/io/micronaut/runtime/ApplicationConfiguration.java
+++ runtime/src/main/java/io/micronaut/runtime/ApplicationConfiguration.java
@@ -17,8 +17,6 @@ package io.micronaut.runtime;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
 import io.micronaut.context.annotation.Primary;
-import io.micronaut.context.annotation.ConfigurationProperties;
-import io.micronaut.context.annotation.Primary;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -36,20 +34,38 @@ import java.util.Optional;
 @Primary
 public class ApplicationConfiguration {
 
+    /**
+     * Prefix for Micronaut application settings
+     */
     public static final String PREFIX = "micronaut.application";
+
+    /**
+     * Property name for Micronaut default charset
+     */
     public static final String DEFAULT_CHARSET = PREFIX + ".defaultCharset";
+
+    /**
+     * Property name for Micronaut application name
+     */
     public static final String APPLICATION_NAME = PREFIX + ".name";
 
     private Charset defaultCharset = StandardCharsets.UTF_8;
     private String name;
     private InstanceConfiguration instance = new InstanceConfiguration();
-    @SuppressWarnings("unchecked")
 
     /**
      * @return The default charset to use
      */
+    @SuppressWarnings("unchecked")
     public Charset getDefaultCharset() {
         return defaultCharset;
+    }
+
+    /**
+     * @param defaultCharset Set the default charset to use
+     */
+    public void setDefaultCharset(Charset defaultCharset) {
+        this.defaultCharset = defaultCharset;
     }
 
     /**
@@ -63,30 +79,43 @@ public class ApplicationConfiguration {
     }
 
     /**
+     * @param name Set the application name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
      * @return Configuration for the application instance
      */
     public InstanceConfiguration getInstance() {
         return instance;
     }
 
+    /**
+     * @param instance The instance configuration
+     */
     public void setInstance(InstanceConfiguration instance) {
-        if(instance != null) {
+        if (instance != null) {
             this.instance = instance;
         }
     }
 
-    public void setDefaultCharset(Charset defaultCharset) {
-        this.defaultCharset = defaultCharset;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    /**
+     * Configuration for instance setttings
+     */
     @ConfigurationProperties(InstanceConfiguration.PREFIX)
     public static class InstanceConfiguration {
+        /**
+         * Prefix for Micronaut instance settings
+         */
         public static final String PREFIX = "instance";
+
+        /**
+         * Property name for Micronaut instance id
+         */
         public static final String INSTANCE_ID = ApplicationConfiguration.PREFIX + '.' + PREFIX + ".id";
+
         private String id;
         private String group;
         private String zone;
@@ -101,6 +130,13 @@ public class ApplicationConfiguration {
         }
 
         /**
+         * @param id The instance identifier
+         */
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        /**
          * @return Any metadata to associate with the instance
          */
         public Map<String, String> getMetadata() {
@@ -108,33 +144,38 @@ public class ApplicationConfiguration {
         }
 
         /**
-         * @return The instance group
+         * @param metadata The metadata to associate with the instance
+         */
+        public void setMetadata(Map<String, String> metadata) {
+            this.metadata = metadata;
+        }
+
+        /**
+         * @return The instance auto scaling group
          */
         public Optional<String> getGroup() {
             return Optional.ofNullable(group);
         }
 
         /**
-         * @return The instance zone
+         * @param group The instance auto scaling group
+         */
+        public void setGroup(String group) {
+            this.group = group;
+        }
+
+        /**
+         * @return The instance availability zone. For example it's possible to configure Nexflix Ribbon to load balance between servers only in a particular zone
          */
         public Optional<String> getZone() {
             return Optional.ofNullable(zone);
         }
 
+        /**
+         * @param zone The instance availability zone
+         */
         public void setZone(String zone) {
             this.zone = zone;
-        }
-
-        public void setGroup(String group) {
-            this.group = group;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public void setMetadata(Map<String, String> metadata) {
-            this.metadata = metadata;
         }
     }
 }
