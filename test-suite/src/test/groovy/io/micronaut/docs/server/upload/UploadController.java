712bf40005a0baad33e9e4792b2338bb37961773
Zachary Klein <zak@silver-chalice.com>
2018-03-23 16:01:30 -0500

    Added docs for CompletedFileUpload

--- test-suite/src/test/groovy/io/micronaut/docs/server/upload/UploadController.java
+++ test-suite/src/test/groovy/io/micronaut/docs/server/upload/UploadController.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2017 original authors
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package io.micronaut.docs.server.upload;
 
@@ -28,6 +28,14 @@ import org.reactivestreams.Publisher;
 import java.util.Optional;
 // end::imports[]
 
+// tag::completedImports[]
+import io.micronaut.http.server.netty.multipart.CompletedFileUpload;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+// end::completedImports[]
 /**
  * @author Graeme Rocher
  * @since 1.0
@@ -52,5 +60,20 @@ public class UploadController {
                     });
     }
     // end::upload[]
+
+    // tag::completedUpload[]
+    @Post(value = "/", consumes = MediaType.MULTIPART_FORM_DATA) // <1>
+    public HttpResponse<String> uploadBlocking(CompletedFileUpload file, Optional<String> anotherAttribute) { // <2>
+
+        try {
+            Path path = Paths.get(file.getFilename()); //<3>
+            Files.write(path, file.getBytes());
+
+            return HttpResponse.ok("Uploaded");
+        } catch (IOException exception) {
+            return HttpResponse.badRequest("Upload Failed");
+        }
+    }
+    // end::completedUpload[]
 
 }
