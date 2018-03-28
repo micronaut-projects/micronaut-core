b08ca1993ca00956d1ab257d07dab88684820f8d
Zachary Klein <zak@silver-chalice.com>
2018-03-28 15:55:10 -0500

    Added info endpoint

---
+++ management/src/main/java/io/micronaut/management/endpoint/info/InfoEndpoint.java
@@ -0,0 +1,14 @@
+package io.micronaut.management.endpoint.info;
+
+
+import io.micronaut.management.endpoint.Endpoint;
+
+/**
+ * <p>Exposes an {@link Endpoint} to provide information about the application.</p>
+ *
+ * @author Zachary Klein
+ * @since 1.0
+ */
+@Endpoint("info")
+public class InfoEndpoint {
+}
