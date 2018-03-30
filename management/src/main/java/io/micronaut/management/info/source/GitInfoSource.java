ead68ac98625a5b5ba5b0d117385b98b4b33d146
Zachary Klein <zak@silver-chalice.com>
2018-03-30 17:04:30 -0500

    Added default InfoSource implementations

---
+++ management/src/main/java/io/micronaut/management/info/source/GitInfoSource.java
@@ -0,0 +1,31 @@
+package io.micronaut.management.info.source;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.env.PropertySource;
+import io.micronaut.core.io.ResourceResolver;
+import io.micronaut.management.endpoint.info.InfoEndpoint;
+import org.reactivestreams.Publisher;
+
+import javax.inject.Singleton;
+
+//TODO: @Refreshable
+@Singleton
+@Requires(beans = InfoEndpoint.class)
+@Requires(property = "endpoints.info.git.enabled")//, notEquals = "false")
+public class GitInfoSource implements InfoSource {
+
+    private ResourceResolver resourceResolver;
+
+    public GitInfoSource(ResourceResolver resourceResolver) {
+        this.resourceResolver = resourceResolver;
+    }
+
+    @Override
+    public Publisher<PropertySource> getSource() {
+
+
+        //TODO: Create a property source from git properties
+        //Inject resourceResolver
+        return null;
+    }
+}
