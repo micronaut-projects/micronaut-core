745e5952e202545804b49d214f98baa30d46fc88
Zachary Klein <zak@silver-chalice.com>
2018-03-31 17:54:11 -0500

    Added Java docs

--- management/src/main/java/io/micronaut/management/endpoint/info/InfoSource.java
+++ management/src/main/java/io/micronaut/management/endpoint/info/InfoSource.java
@@ -8,8 +8,19 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
+/**
+ * <p>Describes an source of info that will be retrieved by the {@link InfoEndpoint}. </p>
+ *
+ * @author Zachary Klein
+ * @since 1.0
+ */
 public interface InfoSource extends Ordered {
 
+
+    /**
+     * @return A publisher that returns a {@link PropertySource} containing
+     * data to be added to the endpoint response
+     */
     Publisher<PropertySource> getSource();
 
 }
