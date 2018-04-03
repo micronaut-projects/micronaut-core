b697ad9d419cfb832eabdbc9aadcf77bfaac1925
Iván López <lopez.ivan@gmail.com>
2018-04-03 12:37:05 +0200

    Cleanup and review javadoc & license "http" module

--- http/src/main/java/io/micronaut/http/cookie/CookieFactory.java
+++ http/src/main/java/io/micronaut/http/cookie/CookieFactory.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2017 original authors
- * 
+ * Copyright 2017-2018 original authors
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package io.micronaut.http.cookie;
 
@@ -29,15 +29,16 @@ public interface CookieFactory {
     /**
      * The default {@link CookieFactory} instance
      */
-    CookieFactory INSTANCE = SoftServiceLoader.load(CookieFactory.class)
-                                              .firstOr("io.micronaut.http.netty.cookies.NettyCookieFactory", CookieFactory.class.getClassLoader())
-                                              .map(ServiceDefinition::load)
-                                              .orElse(null);
+    CookieFactory INSTANCE = SoftServiceLoader
+        .load(CookieFactory.class)
+        .firstOr("io.micronaut.http.netty.cookies.NettyCookieFactory", CookieFactory.class.getClassLoader())
+        .map(ServiceDefinition::load)
+        .orElse(null);
 
     /**
      * Create a new cookie
      *
-     * @param name The name
+     * @param name  The name
      * @param value The value
      * @return A Cookie instance
      */
