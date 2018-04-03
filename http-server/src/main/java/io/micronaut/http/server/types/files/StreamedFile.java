e893618c06c40443dd756f467e4708dfa8439248
Iván López <lopez.ivan@gmail.com>
2018-04-03 13:41:54 +0200

    Cleanup and review javadoc & license "http-server" module

--- http-server/src/main/java/io/micronaut/http/server/types/files/StreamedFile.java
+++ http-server/src/main/java/io/micronaut/http/server/types/files/StreamedFile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 original authors
+ * Copyright 2017-2018 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,7 +63,7 @@ public class StreamedFile implements FileCustomizableResponseType {
         String path = url.getPath();
         int idx = path.lastIndexOf(File.separatorChar);
         if (idx > -1) {
-            this.name = path.substring(idx+1);
+            this.name = path.substring(idx + 1);
         } else {
             this.name = path;
         }
