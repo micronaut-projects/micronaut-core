43bc648a9d3c70e60f37925d60cd9cdf812910fa
Iván López <lopez.ivan@gmail.com>
2018-04-03 13:33:04 +0200

    Cleanup and review javadoc & license "http-netty" module

--- http-netty/src/main/java/io/micronaut/http/netty/cookies/NettyCookie.java
+++ http-netty/src/main/java/io/micronaut/http/netty/cookies/NettyCookie.java
@@ -1,35 +1,35 @@
 /*
  * Copyright 2017 original authors
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package io.micronaut.http.netty.cookies;
 
 import io.micronaut.core.annotation.Internal;
 import io.micronaut.http.cookie.Cookie;
 import io.netty.handler.codec.http.cookie.DefaultCookie;
-import io.micronaut.core.annotation.Internal;
-import io.micronaut.http.cookie.Cookie;
 
 import java.util.Objects;
 
 /**
  * A wrapper around a Netty cookie
+ *
  * @author Graeme Rocher
  * @since 1.0
  */
 @Internal
 public class NettyCookie implements Cookie {
+
     private final io.netty.handler.codec.http.cookie.Cookie nettyCookie;
 
     public NettyCookie(io.netty.handler.codec.http.cookie.Cookie nettyCookie) {
