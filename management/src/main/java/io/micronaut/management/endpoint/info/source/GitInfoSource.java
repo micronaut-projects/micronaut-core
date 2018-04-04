8e5b1caa767635d1681ee6ad6995b253b4c55141
Zachary Klein <zak@silver-chalice.com>
2018-04-03 20:57:29 -0500

    Initial implementation/test for GitInfoSource

--- management/src/main/java/io/micronaut/management/endpoint/info/source/GitInfoSource.java
+++ management/src/main/java/io/micronaut/management/endpoint/info/source/GitInfoSource.java
@@ -1,7 +1,9 @@
 package io.micronaut.management.endpoint.info.source;
 
 import io.micronaut.context.annotation.Requires;
-import io.micronaut.context.env.PropertySource;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.context.env.*;
+import io.micronaut.core.async.SupplierUtil;
 import io.micronaut.core.io.ResourceResolver;
 import io.micronaut.management.endpoint.info.InfoEndpoint;
 import io.micronaut.management.endpoint.info.InfoSource;
@@ -9,6 +11,10 @@ import io.reactivex.Flowable;
 import org.reactivestreams.Publisher;
 
 import javax.inject.Singleton;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.util.function.Supplier;
 
 /**
  * <p>An {@link InfoSource} that retrieves info from Git properties. </p>
@@ -22,14 +28,39 @@ import javax.inject.Singleton;
 @Requires(property = "endpoints.info.git.enabled", notEquals = "false")
 public class GitInfoSource implements InfoSource {
 
+    @Value("${endpoints.info.git.location:git.properties}")
+    private String gitPropertiesPath;
+
     private ResourceResolver resourceResolver;
+    private final Supplier<MapPropertySource> supplier;
 
     public GitInfoSource(ResourceResolver resourceResolver) {
         this.resourceResolver = resourceResolver;
+        this.supplier = SupplierUtil.memoized(this::retrieveGitInfo);
     }
 
     @Override
     public Publisher<PropertySource> getSource() {
-        return Flowable.empty();
+        return Flowable.just(supplier.get());
+    }
+
+
+    private MapPropertySource retrieveGitInfo() {
+        Optional<URL> url = resourceResolver.getResource("classpath:" + gitPropertiesPath);
+        if (url.isPresent()) {
+            PropertiesPropertySourceLoader propertySourceLoader = new PropertiesPropertySourceLoader();
+            String propertySourceName = gitPropertiesPath;
+
+            try {
+                if (propertySourceName != null) {
+                    Map<String, Object> properties = propertySourceLoader.read(propertySourceName, url.get().openStream());
+                    return new MapPropertySource("git", new LinkedHashMap<>(properties));
+                }
+            } catch (IOException ex) {
+            }
+
+        }
+        return null;
     }
 }
+
