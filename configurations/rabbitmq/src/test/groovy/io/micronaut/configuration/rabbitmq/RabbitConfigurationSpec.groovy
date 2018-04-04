0eb11cb2e9252935c8f92ac42afb66edd87460bc
benrhine <rhine.ben@gmail.com>
2018-04-04 10:25:11 -0400

    Configuration file to allow rabbitmq client to leverage micronaut properties configurator

--- configurations/rabbitmq/src/test/groovy/io/micronaut/configuration/rabbitmq/RabbitConfigurationSpec.groovy
+++ configurations/rabbitmq/src/test/groovy/io/micronaut/configuration/rabbitmq/RabbitConfigurationSpec.groovy
@@ -9,6 +9,7 @@ import com.rabbitmq.client.DefaultConsumer
 import com.rabbitmq.client.Envelope
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.DefaultApplicationContext
+import io.micronaut.context.env.MapPropertySource
 import spock.lang.Specification
 
 class RabbitConfigurationSpec extends Specification {
@@ -62,24 +63,135 @@ class RabbitConfigurationSpec extends Specification {
             applicationContext.close()
     }
 
-    /**
-     * This test requires a running rabbit instance to be successful. Also while the test always passes you do not always
-     * see the message come through (I have a suspicion that may be due to timing).
-     */
+    void "default rabbit configuration is used when configuration properties are empty"() {
+        given:
+            ApplicationContext applicationContext = new DefaultApplicationContext("test")
+            applicationContext.environment.addPropertySource(MapPropertySource.of(
+                    'test',
+                    ['rabbitmq': [:]]
+            ))
+            applicationContext.start()
+
+        expect: "connection factory bean is available"
+            applicationContext.containsBean(ConnectionFactory)
+
+        when: "when the connection factory is returned and values are overridden"
+            ConnectionFactory cf = applicationContext.getBean(ConnectionFactory)
+
+        then: "default configuration is available"
+            cf.getUsername() == "guest"
+            cf.getPassword() == "guest"
+            cf.getVirtualHost() == "/"
+            cf.getHost() == "localhost"
+            cf.getPort() == 5672
+            cf.getRequestedChannelMax() == 0
+            cf.getRequestedFrameMax() == 0
+            cf.getRequestedHeartbeat() == 60
+            cf.getConnectionTimeout() == 60000
+            cf.getHandshakeTimeout() == 10000
+            cf.getShutdownTimeout() == 10000
+
+        cleanup:
+            applicationContext.close()
+    }
+
+    void "default rabbit configuration is overridden when configuration properties are passed in"() {
+        given:
+            ApplicationContext applicationContext = ApplicationContext.run(
+                    ["rabbitmq.username": "guest1",
+                     "rabbitmq.password": "guest1",
+                     "rabbitmq.virtualHost": "/guest1",
+                     "rabbitmq.host": "guesthost",
+                     "rabbitmq.port": 9999,
+                     "rabbitmq.requestedChannelMax": 50,
+                     "rabbitmq.requestedFrameMax": 50,
+                     "rabbitmq.requestedHeartbeat": 50,
+                     "rabbitmq.connectionTimeout": 50,
+                     "rabbitmq.handshakeTimeout": 50,
+                     "rabbitmq.shutdownTimeout": 50],
+                    "test"
+            )
+
+        expect: "connection factory bean is available"
+            applicationContext.containsBean(ConnectionFactory)
+            applicationContext.containsBean(RabbitConnectionFactoryConfig)
+
+        when: "when the connection factory is returned and values are overridden"
+            ConnectionFactory cf = applicationContext.getBean(RabbitConnectionFactoryConfig)
+
+        then: "default configuration is available"
+            cf.getUsername() == "guest1"
+            cf.getPassword() == "guest1"
+            cf.getVirtualHost() == "/guest1"
+            cf.getHost() == "guesthost"
+            cf.getPort() == 9999
+            cf.getRequestedChannelMax() == 50
+            cf.getRequestedFrameMax() == 50
+            cf.getRequestedHeartbeat() == 50
+            cf.getConnectionTimeout() == 50
+            cf.getHandshakeTimeout() == 50
+            cf.getShutdownTimeout() == 50
+
+        cleanup:
+            applicationContext.close()
+    }
+
+    /** ================================================================================================================
+     * The following tests only run when a working rabbit instance is available
+     * ============================================================================================================== */
+//    void "default rabbit configuration is overridden when configuration properties are passed in and are available from a new connection"() {
+//        given:
+//        ApplicationContext applicationContext = ApplicationContext.run(
+//                ["rabbitmq.username": "guest",
+//                 "rabbitmq.password": "guest",
+//                 "rabbitmq.virtualHost": "/",
+//                 "rabbitmq.host": "localhost",
+//                 "rabbitmq.port": 5672,
+//                 "rabbitmq.requestedChannelMax": 50,
+//                 "rabbitmq.requestedHeartbeat": 50,
+//                 "rabbitmq.connectionTimeout": 50,
+//                 "rabbitmq.handshakeTimeout": 50,
+//                 "rabbitmq.shutdownTimeout": 50],
+//                "test"
+//        )
+//
+//        expect: "connection factory bean is available"
+//        applicationContext.containsBean(ConnectionFactory)
+//        applicationContext.containsBean(RabbitConnectionFactoryConfig)
+//
+//        when: "when the connection factory is returned and values are overridden"
+//        ConnectionFactory cf = applicationContext.getBean(RabbitConnectionFactoryConfig)
+//        Connection c = cf.newConnection()
+//
+//        then: "default configuration is available"
+//        c.getAddress()
+//        c.getPort() == 5672
+//        c.getChannelMax() == 50
+//        c.getHeartbeat() == 50
+//
+//        when:
+//        Channel channel = c.createChannel()
+//
+//        then:
+//        channel
+//
+//        cleanup:
+//        c.close()
+//        applicationContext.close()
+//    }
+//
 //    void "default rabbit configuration with connection"() {
 //        given:
 //            ApplicationContext applicationContext = new DefaultApplicationContext("test")
 //            applicationContext.start()
 //
 //        expect: "connection factory bean is available"
-//            applicationContext.containsBean(ConnectionFactory)
-//            applicationContext.containsBean(Connection)
-//            applicationContext.containsBean(Channel)
+//            applicationContext.containsBean(RabbitConnectionFactoryConfig)
 //
 //        when: "the connection factory is returned"
-//            ConnectionFactory cf = applicationContext.getBean(ConnectionFactory)
-//            Connection publisherConnection = applicationContext.getBean(Connection)
-//            Channel publisherChannel = applicationContext.getBean(Channel)
+//            ConnectionFactory cf = applicationContext.getBean(RabbitConnectionFactoryConfig)
+//            Connection publisherConnection = cf.newConnection()
+//            Channel publisherChannel = publisherConnection.createChannel()
 //
 //        then: "the rabbit beans are available"
 //            cf
@@ -95,15 +207,15 @@ class RabbitConfigurationSpec extends Specification {
 //
 //        when: "a consumer is present"
 //            // TODO: for some reason the output message is only available on the first run or if you edit this block, try adding a println
-//            Connection consumerConnection = applicationContext.getBean(Connection)
+//            Connection consumerConnection = cf.newConnection()
 //            Channel consumerChannel = consumerConnection.createChannel()
 //            consumerChannel.queueDeclare("hello", false, false, false, null)
 //
 //            Consumer consumer = new DefaultConsumer(consumerChannel) {
 //                @Override
 //                void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-//                    String message = new String(body, "UTF-8");
-//                    System.out.println(" [x] Received '" + message + "'");
+//                    String message = new String(body, "UTF-8")
+//                    System.out.println(" [x] Received '" + message + "'")
 //                }
 //            }
 //
