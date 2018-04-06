77672561d961706f60e2f799b1804ffe9970b57f
Zachary Klein <zak@silver-chalice.com>
2018-04-06 14:18:48 -0500

    Added optional `force` parameter to refresh endpoint; added docs

--- management/src/main/java/io/micronaut/management/endpoint/refresh/RefreshEndpoint.java
+++ management/src/main/java/io/micronaut/management/endpoint/refresh/RefreshEndpoint.java
@@ -1,39 +1,37 @@
 /*
  * Copyright 2017 original authors
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package io.micronaut.management.endpoint.refresh;
 
-import io.micronaut.context.env.Environment;
-import io.micronaut.context.event.ApplicationEventPublisher;
 import io.micronaut.context.env.Environment;
 import io.micronaut.context.event.ApplicationEventPublisher;
 import io.micronaut.management.endpoint.Endpoint;
 import io.micronaut.management.endpoint.Write;
 import io.micronaut.runtime.context.scope.refresh.RefreshEvent;
 
+import javax.annotation.Nullable;
 import java.util.Map;
 import java.util.Set;
 
 /**
  * <p>Exposes an {@link Endpoint} to refresh application state via a {@link RefreshEvent}</p>
  *
+ * @author Graeme Rocher
  * @see io.micronaut.runtime.context.scope.refresh.RefreshScope
  * @see RefreshEvent
  * @see io.micronaut.runtime.context.scope.Refreshable
- *
- * @author Graeme Rocher
  * @since 1.0
  */
 @Endpoint("refresh")
@@ -47,13 +45,25 @@ public class RefreshEndpoint {
         this.eventPublisher = eventPublisher;
     }
 
+    /**
+     * Refresh application state only if environment has changed (unless <code>force</code> is set to true)
+     *
+     * @param force optional body property to indicate whether to force all {@link io.micronaut.runtime.context.scope.Refreshable} beans to be refreshed
+     * @return array of change keys if applicable
+     */
     @Write
-    public String[] refresh() {
-        Map<String, Object> changes = environment.refreshAndDiff();
-        if(!changes.isEmpty()) {
-            eventPublisher.publishEvent(new RefreshEvent(changes));
+    public String[] refresh(@Nullable Boolean force) {
+
+        if (force != null && force) {
+            eventPublisher.publishEvent(new RefreshEvent());
+            return new String[0];
+        } else {
+            Map<String, Object> changes = environment.refreshAndDiff();
+            if (!changes.isEmpty()) {
+                eventPublisher.publishEvent(new RefreshEvent(changes));
+            }
+            Set<String> keys = changes.keySet();
+            return keys.toArray(new String[keys.size()]);
         }
-        Set<String> keys = changes.keySet();
-        return keys.toArray(new String[keys.size()]);
     }
 }
