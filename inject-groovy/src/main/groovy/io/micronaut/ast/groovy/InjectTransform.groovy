4e9fe6740314ace2ab2900623451056c6139a689
graemerocher <graeme.rocher@gmail.com>
2018-04-09 18:35:45 +0200

    add temporary build server debug output

--- inject-groovy/src/main/groovy/io/micronaut/ast/groovy/InjectTransform.groovy
+++ inject-groovy/src/main/groovy/io/micronaut/ast/groovy/InjectTransform.groovy
@@ -266,11 +266,9 @@ class InjectTransform implements ASTTransformation, CompilationUnitAware {
                 populateProxyWriterConstructor(node, aopProxyWriter)
                 beanDefinitionWriters.put(node, aopProxyWriter)
                 visitIntroductionTypePublicMethods(aopProxyWriter, node)
-                println "interfaceTypes = ${interfaceTypes.inspect()}"
                 if(ArrayUtils.isNotEmpty(interfaceTypes)) {
                     List<AnnotationNode> annotationNodes = node.annotations
                     Set<ClassNode> interfacesToVisit = []
-                    println "interfacesToVisit = ${interfacesToVisit.inspect()}"
 
                     populateIntroducedInterfaces(annotationNodes, interfacesToVisit)
 
