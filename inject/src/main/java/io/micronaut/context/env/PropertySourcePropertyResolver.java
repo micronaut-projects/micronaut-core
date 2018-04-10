eafeaaa97cab87b8ea87b8cd78ecfb5a37f1b6f1
Zachary Klein <zak@silver-chalice.com>
2018-04-10 00:24:06 -0500

    Merge master

--- inject/src/main/java/io/micronaut/context/env/PropertySourcePropertyResolver.java
+++ inject/src/main/java/io/micronaut/context/env/PropertySourcePropertyResolver.java
@@ -23,15 +23,7 @@ import io.micronaut.core.value.MapPropertyResolver;
 import io.micronaut.core.value.PropertyResolver;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
