93b7c1483748d81ceae47a126c678738a7dd81e5
niravassar <nirav_p_assar@yahoo.com>
2018-04-16 15:08:19 -0500

    checkstyle-function

--- function/src/main/java/io/micronaut/function/FunctionBean.java
+++ function/src/main/java/io/micronaut/function/FunctionBean.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.micronaut.function;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
