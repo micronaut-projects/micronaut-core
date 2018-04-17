b09d44fd702b0bfb055452b48b6e2216c39e82cf
niravassar <nirav_p_assar@yahoo.com>
2018-04-17 13:02:10 -0500

    jdbc checkstyle change in hikari and dbcp

--- configurations/jdbc-hikari/src/main/java/io/micronaut/configuration/jdbc/hikari/package-info.java
+++ configurations/jdbc-hikari/src/main/java/io/micronaut/configuration/jdbc/hikari/package-info.java
@@ -14,14 +14,12 @@
  * limitations under the License.
  */
 /**
- * Configuration for Hikari data sources
+ * Configuration for Hikari data sources.
  */
 @Configuration
 @Requires(classes = HikariDataSource.class)
 package io.micronaut.configuration.jdbc.hikari;
 
 import com.zaxxer.hikari.HikariDataSource;
-import io.micronaut.context.annotation.Configuration;
-import io.micronaut.context.annotation.Requires;
 import io.micronaut.context.annotation.Configuration;
 import io.micronaut.context.annotation.Requires;
