b6b5177e1e88401cda25456f7b8c183426e5e870
niravassar <nirav_p_assar@yahoo.com>
2018-04-18 10:21:25 -0500

    configurations-netflix-checkstyle changes

--- configurations/netflix-archaius/src/main/java/io/micronaut/configurations/archaius1/EnvironmentConfiguration.java
+++ configurations/netflix-archaius/src/main/java/io/micronaut/configurations/archaius1/EnvironmentConfiguration.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.micronaut.configurations.archaius1;
 
 import org.apache.commons.configuration.AbstractConfiguration;
@@ -27,7 +28,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 /**
- * Adapts the {@link Environment} to the {@link AbstractConfiguration} type and fires change events when the application is refreshed
+ * Adapts the {@link Environment} to the {@link AbstractConfiguration} type and fires change events when the application is refreshed.
  *
  * @author graemerocher
  * @since 1.0
@@ -36,6 +37,10 @@ import java.util.Map;
 class EnvironmentConfiguration extends AbstractConfiguration implements ApplicationEventListener<RefreshEvent> {
     private final Environment environment;
 
+    /**
+     * Constructor.
+     * @param environment environment
+     */
     public EnvironmentConfiguration(Environment environment) {
         this.environment = environment;
     }
@@ -80,8 +85,7 @@ class EnvironmentConfiguration extends AbstractConfiguration implements Applicat
             public boolean hasNext() {
                 if(i.hasNext()) {
                     return true;
-                }
-                else {
+                } else {
                     if(propertySourceIterator.hasNext()) {
                         i = propertySourceIterator.next().iterator();
                     }
@@ -103,8 +107,7 @@ class EnvironmentConfiguration extends AbstractConfiguration implements Applicat
             Object value = entry.getValue();
             if(value == null) {
                 fireEvent(EVENT_CLEAR_PROPERTY, entry.getKey(), null, false);
-            }
-            else {
+            } else {
                 fireEvent(EVENT_SET_PROPERTY, entry.getKey(), value, false);
             }
         }
