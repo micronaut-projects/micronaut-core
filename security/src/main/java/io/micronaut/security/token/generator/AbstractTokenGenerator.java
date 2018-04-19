95703e4d68483dd53cbef7e115287b8be0eb3c35
sdelamo <sergio.delamo@softamo.com>
2018-04-19 10:31:53 +0200

    add blank line after license

--- security/src/main/java/io/micronaut/security/token/generator/AbstractTokenGenerator.java
+++ security/src/main/java/io/micronaut/security/token/generator/AbstractTokenGenerator.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.micronaut.security.token.generator;
 
 import com.nimbusds.jose.JOSEException;
