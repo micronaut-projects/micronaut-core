9cf43674c20fb6082d1861e89c67547e671ad4a6
jameskleeh <james.kleeh@gmail.com>
2018-04-20 15:11:27 -0400

    Implement type element visitor support

---
+++ inject-groovy/src/test/groovy/io/micronaut/inject/visitor/CustomVisitorSpec.groovy
@@ -0,0 +1,97 @@
+package io.micronaut.inject.visitor
+
+import io.micronaut.AbstractBeanDefinitionSpec
+import io.micronaut.ast.groovy.utils.AstAnnotationUtils
+
+class CustomVisitorSpec extends AbstractBeanDefinitionSpec {
+
+    void setup() {
+        ControllerGetVisitor.VISITED_ELEMENTS = []
+        AllElementsVisitor.VISITED_ELEMENTS = []
+        AllClassesVisitor.VISITED_ELEMENTS = []
+        InjectVisitor.VISITED_ELEMENTS = []
+        AstAnnotationUtils.invalidateCache()
+    }
+
+    void "test class is visited by custom visitor"() {
+        buildBeanDefinition('test.TestController', '''
+package test
+
+import io.micronaut.http.annotation.*
+import javax.inject.Inject
+
+@Controller("/test")
+class TestController {
+
+    @Inject private String privateField
+    @Inject protected String protectedField  
+    @Inject public String publicField
+    @Inject @groovy.transform.PackageScope String packagePrivateField
+    @Inject String property
+    
+    @Inject
+    TestController(String constructorArg) {}
+    
+    @Inject
+    void setterMethod(String method) {}
+    
+    @Get("/getMethod")
+    String getMethod(String argument) {
+        ""
+    }
+    
+    @Post("/postMethod")
+    String postMethod() {
+        ""
+    }
+
+}
+''')
+        expect:
+        ControllerGetVisitor.VISITED_ELEMENTS == ["test.TestController", "getMethod"]
+        AllElementsVisitor.VISITED_ELEMENTS.toSet() == ["test.TestController", "<init>", "privateField", "protectedField", "publicField", "property", "setterMethod", "getMethod", "postMethod"].toSet()
+        AllClassesVisitor.VISITED_ELEMENTS == ["test.TestController", "getMethod"]
+        InjectVisitor.VISITED_ELEMENTS.toSet() == ["test.TestController", "<init>", "privateField", "protectedField", "publicField", "property", "setterMethod"].toSet()
+    }
+
+    void "test non controller class is not visited by custom visitor"() {
+        buildBeanDefinition('test.TestController', '''
+package test;
+
+import io.micronaut.http.annotation.*
+import javax.inject.Inject
+
+@javax.inject.Singleton
+public class TestController {
+
+    @Inject private String privateField
+    @Inject protected String protectedField  
+    @Inject public String publicField
+    @Inject @groovy.transform.PackageScope String packagePrivateField
+    @Inject String property
+    
+    @Inject
+    TestController(String constructorArg) {}
+    
+    @Inject
+    void setterMethod(String method) {}
+    
+    @Get("/getMethod")
+    String getMethod(String argument) {
+        ""
+    }
+    
+    @Post("/postMethod")
+    String postMethod() {
+        ""
+    }
+
+}
+''')
+        expect:
+        ControllerGetVisitor.VISITED_ELEMENTS == []
+        AllElementsVisitor.VISITED_ELEMENTS == []
+        AllClassesVisitor.VISITED_ELEMENTS == ["test.TestController", "getMethod"]
+        InjectVisitor.VISITED_ELEMENTS.toSet() == ["test.TestController", "<init>", "privateField", "protectedField", "publicField", "property", "setterMethod"].toSet()
+    }
+}
---
+++ inject/src/main/java/io/micronaut/inject/visitor/TypeElementVisitor.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.inject.visitor;
+
+import io.micronaut.core.annotation.AnnotationMetadata;
+
+/**
+ * Provides a hook into the compilation process to allow user
+ * defined functionality to be created at compile time.
+ *
+ * @param <C> The annotation required on the class.
+ * @param <E> The annotation required on the element.
+ * @author James Kleeh
+ * @since 1.0
+ */
+public interface TypeElementVisitor<C, E> {
+
+    /**
+     * Executed when a class is encountered that matches the <C> generic
+     *
+     * @param element The element
+     * @param context The visitor context
+     */
+    void visitClass(ClassElement element, VisitorContext context);
+
+    /**
+     * Executed when a method is encountered that matches the <E> generic
+     *
+     * @param element The element
+     * @param context The visitor context
+     */
+    void visitMethod(MethodElement element, VisitorContext context);
+
+    /**
+     * Executed when a field is encountered that matches the <E> generic
+     *
+     * @param element The element
+     * @param context The visitor context
+     */
+    void visitField(FieldElement element, VisitorContext context);
+}
