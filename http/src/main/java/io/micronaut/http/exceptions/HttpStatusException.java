d2dd8772f804ec9c4493630a02265b380701f6b8
Iván López <lopezi@objectcomputing.com>
2018-04-26 12:09:39 +0200

    Add HttpStatusException and handler

---
+++ http/src/main/java/io/micronaut/http/exceptions/HttpStatusException.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.micronaut.http.exceptions;
+
+import io.micronaut.http.HttpStatus;
+
+/**
+ * Exception thrown to return an specific HttpStatus and an error message.
+ *
+ * @author Iván López
+ * @since 1.0
+ */
+public class HttpStatusException extends HttpException {
+
+    private HttpStatus status;
+
+    /**
+     * @param status  The {@link HttpStatus}
+     * @param message The message
+     */
+    public HttpStatusException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    /**
+     * @return The {@link HttpStatus}
+     */
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
