391f5aac499347b76cde80a6f948633409de812e
graemerocher <graeme.rocher@gmail.com>
2018-05-03 18:15:28 +0200

    Restore and fix test

--- management/src/test/groovy/io/micronaut/management/endpoint/beans/BeansEndpointSpec.groovy
+++ management/src/test/groovy/io/micronaut/management/endpoint/beans/BeansEndpointSpec.groovy
@@ -28,7 +28,6 @@ class BeansEndpointSpec extends Specification {
      * Known failure of the scope. Relies on changes to the annotation
      * metadata to return the correct result.
      */
-    @Ignore
     void "test beans endpoint"() {
         given:
         EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
@@ -43,7 +42,7 @@ class BeansEndpointSpec extends Specification {
         response.code() == HttpStatus.OK.code
         beans["io.micronaut.management.endpoint.beans.\$BeansEndpointDefinition"].dependencies[0] == "io.micronaut.context.BeanContext"
         beans["io.micronaut.management.endpoint.beans.\$BeansEndpointDefinition"].dependencies[1] == "io.micronaut.management.endpoint.beans.BeanDefinitionDataCollector"
-        beans["io.micronaut.management.endpoint.beans.\$BeansEndpointDefinition"].scope == "singleton"
+        beans["io.micronaut.management.endpoint.beans.\$BeansEndpointDefinition"].scope == "endpoint"
         beans["io.micronaut.management.endpoint.beans.\$BeansEndpointDefinition"].type == "io.micronaut.management.endpoint.beans.BeansEndpoint"
 
         cleanup:
