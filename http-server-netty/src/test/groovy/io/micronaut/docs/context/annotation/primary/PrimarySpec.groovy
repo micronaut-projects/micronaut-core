f54df8bad02ef35765c35f9402680cb559561baf
sdelamo <sergio.delamo@softamo.com>
2018-05-03 10:34:13 +0200

    Doc @Primary

---
+++ http-server-netty/src/test/groovy/io/micronaut/docs/context/annotation/primary/PrimarySpec.groovy
@@ -0,0 +1,36 @@
+package io.micronaut.docs.context.annotation.primary
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class PrimarySpec extends Specification {
+
+    @Shared
+    @AutoCleanup
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, [
+            'spec.name': 'primaryspec'
+    ], "test")
+
+    @AutoCleanup
+    @Shared
+    RxHttpClient rxClient = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+
+    def "@Primary annotated beans gets injected in case of a collection"() {
+        expect:
+        embeddedServer.applicationContext.getBeansOfType(ColorPicker.class).size() == 2
+
+        when:
+        HttpResponse<String> rsp = rxClient.toBlocking().exchange(HttpRequest.GET('/test'), String)
+
+        then:
+        rsp.status() == HttpStatus.OK
+        rsp.body() == 'green'
+    }
+}
