fc77934a12539044717fcca6204c49711bf9b14c
graemerocher <graeme.rocher@gmail.com>
2018-05-04 10:11:01 +0200

    Simplify and clarify section on type converters

--- inject-groovy/src/test/groovy/io/micronaut/docs/config/converters/MyConfigurationProperties.groovy
+++ inject-groovy/src/test/groovy/io/micronaut/docs/config/converters/MyConfigurationProperties.groovy
@@ -3,19 +3,14 @@ package io.micronaut.docs.config.converters
 import io.micronaut.context.annotation.ConfigurationProperties
 import java.time.LocalDate
 
-@ConfigurationProperties(BookConfigurationProperties.PREFIX)
-class BookConfigurationProperties {
+// tag::class[]
+@ConfigurationProperties(MyConfigurationProperties.PREFIX)
+class MyConfigurationProperties {
     public static final String PREFIX = "myapp"
-
-    protected List<Book> books
-
     protected LocalDate updatedAt
-
-    List<Book> getBooks() {
-        return this.books
-    }
 
     LocalDate getUpdatedAt() {
         return this.updatedAt
     }
 }
+// end::class[]
