b2994ead37366e22800c7a2d63df900c95609f58
niravassar <assarn@objectcomputing.com>
2018-05-07 15:03:35 -0500

    documentation to accompany the configuration

--- configurations/cassandra/src/test/groovy/io/micronaut/configuration/cassandra/CassandraConfigurationSpec.groovy
+++ configurations/cassandra/src/test/groovy/io/micronaut/configuration/cassandra/CassandraConfigurationSpec.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.configuration.cassandra
 
 import com.datastax.driver.core.Cluster
@@ -24,6 +39,7 @@ class CassandraConfigurationSpec extends Specification {
 
     void "test single cluster connection"() {
         given:
+        // tag::single[]
         ApplicationContext applicationContext = new DefaultApplicationContext("test")
         applicationContext.environment.addPropertySource(MapPropertySource.of(
                 'test',
@@ -34,6 +50,7 @@ class CassandraConfigurationSpec extends Specification {
                  'cassandra.primary.ssl': true]
         ))
         applicationContext.start()
+        // end::single[]
 
         expect:
         applicationContext.containsBean(CassandraConfiguration)
@@ -57,6 +74,7 @@ class CassandraConfigurationSpec extends Specification {
 
     void "test multiple cluster connections"() {
         given:
+        // tag::multiple[]
         ApplicationContext applicationContext = new DefaultApplicationContext("test")
         applicationContext.environment.addPropertySource(MapPropertySource.of(
                 'test',
@@ -66,6 +84,7 @@ class CassandraConfigurationSpec extends Specification {
                  'cassandra.secondary.port': 9043]
         ))
         applicationContext.start()
+        // end::multiple[]
 
         when:
         Cluster primaryCluster = applicationContext.getBean(Cluster, Qualifiers.byName("primary"))
