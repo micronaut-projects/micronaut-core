9a321f962f6ca1c1bf8d2611e9f73e0a3c7e75fe
graemerocher <graeme.rocher@gmail.com>
2018-05-08 13:06:20 +0200

    Progress code samples for HTTP client docs

---
+++ http-client/src/test/groovy/io/micronaut/http/client/docs/basics/HelloController.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.client.docs.basics;
+
+// tag::imports[]
+import static io.micronaut.http.HttpRequest.*;
+import io.micronaut.http.annotation.*;
+import io.micronaut.http.client.*;
+import io.reactivex.Maybe;
+// end::imports[]
+
+/**
+ * @author graemerocher
+ * @since 1.0
+ */
+@Controller("/")
+public class HelloController {
+
+    private final RxHttpClient httpClient;
+
+    public HelloController(@Client("/endpoint") RxHttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    // tag::nonblocking[]
+    @Get("/hello/{name}")
+    Maybe<String> hello(String name) { // <1>
+        return httpClient.retrieve( GET("/endpoint/hello/" + name) )
+                         .firstElement(); // <2>
+    }
+    // end::nonblocking[]
+
+    @Get("/endpoint/hello/{name}")
+    String helloEndpoint(String name) {
+        return "Hello " + name;
+    }
+}
