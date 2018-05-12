2a6958ae2eca8d668524adafbe3f953392987d56
Zachary Klein <zak@silver-chalice.com>
2018-05-11 20:14:09 -0500

    Initial docs on creating endpoints (#107, #108, #109)

---
+++ test-suite/src/test/groovy/io/micronaut/docs/server/endpoints/MessageEndpointSpec.groovy
@@ -0,0 +1,76 @@
+package io.micronaut.docs.server.endpoints
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.Specification
+
+class MessageEndpointSpec extends Specification {
+
+    void "test read message endpoint"() {
+        given:
+        EmbeddedServer server = ApplicationContext.run(EmbeddedServer,
+                ['endpoints.message.enabled': true])
+        RxHttpClient rxClient = server.applicationContext.createBean(RxHttpClient, server.getURL())
+
+        when:
+        def response = rxClient.exchange("/message", String).blockingFirst()
+
+        then:
+        response.code() == HttpStatus.OK.code
+        response.body() == "default message"
+
+        cleanup:
+        server.close()
+    }
+
+    void "test write message endpoint"() {
+        given:
+        EmbeddedServer server = ApplicationContext.run(EmbeddedServer,
+                ['endpoints.message.enabled': true])
+        RxHttpClient rxClient = server.applicationContext.createBean(RxHttpClient, server.getURL())
+
+        when:
+        def response = rxClient.exchange(HttpRequest.POST("/message", [newMessage: "A new message"]), String).blockingFirst()
+
+        then:
+        response.code() == HttpStatus.OK.code
+        response.body() == "Message updated"
+
+        when:
+        response = rxClient.exchange("/message", String).blockingFirst()
+
+        then:
+        response.body() == "A new message"
+
+        cleanup:
+        server.close()
+    }
+
+    void "test delete message endpoint"() {
+        given:
+        EmbeddedServer server = ApplicationContext.run(EmbeddedServer,
+                ['endpoints.message.enabled': true])
+        RxHttpClient rxClient = server.applicationContext.createBean(RxHttpClient, server.getURL())
+
+        when:
+        def response = rxClient.exchange(HttpRequest.DELETE("/message"), String).blockingFirst()
+
+        then:
+        response.code() == HttpStatus.OK.code
+        response.body() == "Message deleted"
+
+        when:
+        rxClient.exchange("/message", String).blockingFirst()
+
+        then:
+        HttpClientResponseException e = thrown(HttpClientResponseException)
+        e.status.code == 404
+
+        cleanup:
+        server.close()
+    }
+}
