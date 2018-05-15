bd27d926685cfb6e7f548c82618995f4176c5bd1
graemerocher <graeme.rocher@gmail.com>
2018-05-15 17:05:14 +0200

    Revert "Handle case where metadata.google.internal doesn't exist"
    This reverts commit 3086cb2a847b3a9f401f0fabf52327d781aef429.

--- runtime/src/main/java/io/micronaut/discovery/cloud/gcp/GoogleComputeInstanceMetadataResolver.java
+++ runtime/src/main/java/io/micronaut/discovery/cloud/gcp/GoogleComputeInstanceMetadataResolver.java
@@ -29,7 +29,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -96,15 +95,13 @@ public class GoogleComputeInstanceMetadataResolver implements ComputeInstanceMet
         try {
             int connectionTimeoutMs = (int) configuration.getConnectTimeout().toMillis();
             int readTimeoutMs = (int) configuration.getReadTimeout().toMillis();
+            JsonNode projectResultJson = readGcMetadataUrl(new URL(configuration.getProjectMetadataUrl() + "?recursive=true"), connectionTimeoutMs, readTimeoutMs);
             JsonNode instanceMetadataJson = readGcMetadataUrl(new URL(configuration.getMetadataUrl() + "?recursive=true"), connectionTimeoutMs, readTimeoutMs);
 
             if (instanceMetadataJson != null) {
-                JsonNode projectResultJson = readGcMetadataUrl(new URL(configuration.getProjectMetadataUrl() + "?recursive=true"), connectionTimeoutMs, readTimeoutMs);
                 GoogleComputeInstanceMetadata instanceMetadata = new GoogleComputeInstanceMetadata();
                 instanceMetadata.instanceId = instanceMetadataJson.findValue(GoogleComputeMetadataKeys.ID.getName()).asText();
-                if (projectResultJson != null) {
-                    instanceMetadata.account = projectResultJson.findValue(GoogleComputeMetadataKeys.PROJECT_ID.getName()).textValue();
-                }
+                instanceMetadata.account = projectResultJson.findValue(GoogleComputeMetadataKeys.PROJECT_ID.getName()).textValue();
                 instanceMetadata.availabilityZone = instanceMetadataJson.findValue(GoogleComputeMetadataKeys.ZONE.getName()).textValue();
                 instanceMetadata.machineType = instanceMetadataJson.findValue(GoogleComputeMetadataKeys.MACHINE_TYPE.getName()).textValue();
                 instanceMetadata.description = instanceMetadataJson.findValue(GoogleComputeMetadataKeys.DESCRIPTION.getName()).textValue();
@@ -173,29 +170,25 @@ public class GoogleComputeInstanceMetadataResolver implements ComputeInstanceMet
      * @throws IOException Failed or interrupted I/O operations while reading from input stream.
      */
     protected JsonNode readGcMetadataUrl(URL url, int connectionTimeoutMs, int readTimeoutMs) throws IOException {
-        try {
-            URLConnection urlConnection = url.openConnection();
+        URLConnection urlConnection = url.openConnection();
 
-            if (url.getProtocol().equalsIgnoreCase("file")) {
-                urlConnection.connect();
-                try (InputStream in = urlConnection.getInputStream()) {
-                    return objectMapper.readTree(in);
-                }
-            } else {
-                HttpURLConnection uc = (HttpURLConnection) urlConnection;
-
-                uc.setConnectTimeout(connectionTimeoutMs);
-                uc.setRequestProperty(HEADER_METADATA_FLAVOR, "Google");
-                uc.setReadTimeout(readTimeoutMs);
-                uc.setRequestMethod(HttpMethod.GET.name());
-                uc.setDoOutput(true);
-                int responseCode = uc.getResponseCode();
-                try (InputStream in = uc.getInputStream()) {
-                    return objectMapper.readTree(in);
-                }
+        if (url.getProtocol().equalsIgnoreCase("file")) {
+            urlConnection.connect();
+            try (InputStream in = urlConnection.getInputStream()) {
+                return objectMapper.readTree(in);
             }
-        } catch (FileNotFoundException e) {
-            return null;
+        } else {
+            HttpURLConnection uc = (HttpURLConnection) urlConnection;
+
+            uc.setConnectTimeout(connectionTimeoutMs);
+            uc.setRequestProperty(HEADER_METADATA_FLAVOR, "Google");
+            uc.setReadTimeout(readTimeoutMs);
+            uc.setRequestMethod(HttpMethod.GET.name());
+            uc.setDoOutput(true);
+            int responseCode = uc.getResponseCode();
+            try (InputStream in = uc.getInputStream()) {
+                return objectMapper.readTree(in);
+            }
         }
     }
 }
