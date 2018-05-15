942e6bc6bb3cab8f1a4200fc48614f4b5ab78f06
Ryan Vanderwerf <vanderwerfr@ociweb.com>
2018-05-15 09:52:29 -0500

    fix test package name

--- discovery-client/src/test/groovy/io/micronaut/discovery/propertystore/AWSParameterStoreClientSpec.groovy
+++ discovery-client/src/test/groovy/io/micronaut/discovery/propertystore/AWSParameterStoreClientSpec.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.micronaut.discovery.propertyStore
+package io.micronaut.discovery.propertystore
 
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder
