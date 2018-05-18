c798ac08f7d91b20f4b422d359cd015960295c1a
graemerocher <graeme.rocher@gmail.com>
2018-05-18 11:06:38 +0200

    Move examples to micronaut-examples

--- examples/petstore/api/src/main/java/example/api/v1/HealthStatusOperation.java
+++
@@ -1,9 +0,0 @@
-package example.api.v1;
-
-import io.reactivex.Single;
-import io.micronaut.http.annotation.Get;
-
-public interface HealthStatusOperation {
-    @Get("/health")
-    Single<HealthStatus> health();
-}
--- examples/petstore/mail/src/main/java/example/mail/EmailService.java
+++
@@ -1,9 +0,0 @@
-package example.mail;
-
-import example.api.v1.Email;
-
-import java.io.IOException;
-
-public interface EmailService {
-    void send(Email email);
-}
