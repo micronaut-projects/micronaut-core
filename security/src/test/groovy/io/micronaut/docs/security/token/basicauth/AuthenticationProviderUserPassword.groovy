3de965df65638a57edd3d3805fd99e19be407a5d
sdelamo <sergio.delamo@softamo.com>
2018-05-21 09:51:13 +0200

    [Doc] Security Improvements

--- security/src/test/groovy/io/micronaut/docs/security/token/basicauth/AuthenticationProviderUserPassword.groovy
+++ security/src/test/groovy/io/micronaut/docs/security/token/basicauth/AuthenticationProviderUserPassword.groovy
@@ -13,28 +13,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.micronaut.docs.security.token.basicauth
+package io.micronaut.docs.security.token.basicauth;
 
-import io.micronaut.context.annotation.Requires
-import io.micronaut.security.authentication.AuthenticationFailed
-import io.micronaut.security.authentication.AuthenticationProvider
-import io.micronaut.security.authentication.AuthenticationRequest
-import io.micronaut.security.authentication.AuthenticationResponse
-import io.micronaut.security.authentication.UserDetails
-import io.reactivex.Flowable
-import org.reactivestreams.Publisher
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.security.authentication.AuthenticationFailed;
+import io.micronaut.security.authentication.AuthenticationProvider;
+import io.micronaut.security.authentication.AuthenticationRequest;
+import io.micronaut.security.authentication.AuthenticationResponse;
+import io.micronaut.security.authentication.UserDetails;
+import io.reactivex.Flowable;
+import org.reactivestreams.Publisher;
 
-import javax.inject.Singleton
+import javax.inject.Singleton;
+import java.util.ArrayList;
 
+// Although this is a Groovy file this is written as close to Java as possible to embedded in the docs
+
+@Requires(property = "spec.name", value = "docsbasicauth")
+//tag::clazz[]
 @Singleton
-@Requires(property = 'spec.name', value = 'docsbasicauth')
-class AuthenticationProviderUserPassword implements AuthenticationProvider {
+public class AuthenticationProviderUserPassword implements AuthenticationProvider {
 
     @Override
-    Publisher<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest) {
-        if ( authenticationRequest.identity == 'user' && authenticationRequest.secret == 'password' ) {
-            return Flowable.just(new UserDetails('user', []))
+    public Publisher<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest) {
+        if ( authenticationRequest.getIdentity().equals("user") && authenticationRequest.getSecret().equals("password") ) {
+            return Flowable.just(new UserDetails("user", new ArrayList<>()));
         }
-        return Flowable.just(new AuthenticationFailed())
+        return Flowable.just(new AuthenticationFailed());
     }
 }
+//end::clazz[]
