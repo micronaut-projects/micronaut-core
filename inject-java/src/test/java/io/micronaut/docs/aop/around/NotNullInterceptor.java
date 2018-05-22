661d5e1facfa8c8e1b9331322864308ad1c12b37
Iván López <lopezi@objectcomputing.com>
2018-05-22 09:30:54 +0200

    Fix some minor typos, links and formatting style

--- inject-java/src/test/java/io/micronaut/docs/aop/around/NotNullInterceptor.java
+++ inject-java/src/test/java/io/micronaut/docs/aop/around/NotNullInterceptor.java
@@ -22,27 +22,27 @@ import io.micronaut.core.type.MutableArgumentValue;
 import javax.inject.Singleton;
 import java.util.*;
 // end::imports[]
+
 /**
  * @author Graeme Rocher
  * @since 1.0
  */
 // tag::interceptor[]
 @Singleton
-public class NotNullInterceptor implements MethodInterceptor<Object,Object> { // <1>
+public class NotNullInterceptor implements MethodInterceptor<Object, Object> { // <1>
     @Override
     public Object intercept(MethodInvocationContext<Object, Object> context) {
         Optional<Map.Entry<String, MutableArgumentValue<?>>> nullParam = context.getParameters()
-                .entrySet()
-                .stream()
-                .filter(entry -> {
-                    MutableArgumentValue<?> argumentValue = entry.getValue();
-                    return Objects.isNull(argumentValue.getValue());
-                })
-                .findFirst(); // <2>
-        if( nullParam.isPresent() ) {
-            throw new IllegalArgumentException("Null parameter ["+nullParam.get().getKey()+"] not allowed"); // <3>
-        }
-        else {
+            .entrySet()
+            .stream()
+            .filter(entry -> {
+                MutableArgumentValue<?> argumentValue = entry.getValue();
+                return Objects.isNull(argumentValue.getValue());
+            })
+            .findFirst(); // <2>
+        if (nullParam.isPresent()) {
+            throw new IllegalArgumentException("Null parameter [" + nullParam.get().getKey() + "] not allowed"); // <3>
+        } else {
             return context.proceed(); // <4>
         }
     }
