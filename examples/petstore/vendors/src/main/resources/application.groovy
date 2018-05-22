5bc19e6a977523efe814d1681242dfaae765eddf
Graeme Rocher <graeme.rocher@gmail.com>
2018-05-22 14:31:06 +0200

    Examples moved to micronaut-examples

--- examples/petstore/vendors/src/main/resources/application.groovy
+++
@@ -1,8 +0,0 @@
-micronaut.application.name="vendors"
-consul.client.defaultZone='${CONSUL_HOST:localhost}:${CONSUL_PORT:8500}'
-vendors.api.version="v1"
-hibernate {
-    hbm2ddl {
-        auto = "create-drop"
-    }
-}
