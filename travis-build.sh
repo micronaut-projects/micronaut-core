d3ddd8e8a5fac15431dc04a502058ee8b8003e46
Graeme Rocher <graeme.rocher@gmail.com>
2018-05-23 17:33:05 +0200

    Only publish snapshot of build passes

--- travis-build.sh
+++ travis-build.sh
@@ -1,67 +1,74 @@
 #!/bin/bash
 set -e
+EXIT_STATUS=0
 ./gradlew --stop
 ./gradlew testClasses
-./gradlew check -x test-suite:test --no-daemon
-./gradlew test-suite:test --no-daemon
+./gradlew check -x test-suite:test --no-daemon || EXIT_STATUS=$?
 
-EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH =~ ^master$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-
-  echo "Publishing archives"
-  ./gradlew --stop
-  if [[ -n $TRAVIS_TAG ]]; then
-      ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
-  else
-      ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
-  fi
-
-  ./gradlew --stop
-  ./gradlew --no-daemon docs || EXIT_STATUS=$?
-
-  git config --global user.name "$GIT_NAME"
-  git config --global user.email "$GIT_EMAIL"
-  git config --global credential.helper "store --file=~/.git-credentials"
-  echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
-
-  git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
-  cd gh-pages
-
-  # If this is the master branch then update the snapshot
-  if [[ $TRAVIS_BRANCH =~ ^master|[12]\..\.x$ ]]; then
-     mkdir -p snapshot
-     cp -r ../build/docs/. ./snapshot/
-
-     git add snapshot/*
-  fi
-
-  # If there is a tag present then this becomes the latest
-  if [[ -n $TRAVIS_TAG ]]; then
-      mkdir -p latest
-      cp -r ../build/docs/. ./latest/
-      git add latest/*
-
-      version="$TRAVIS_TAG"
-      version=${version:1}
-      majorVersion=${version:0:4}
-      majorVersion="${majorVersion}x"
-
-      mkdir -p "$version"
-      cp -r ../build/docs/. "./$version/"
-      git add "$version/*"
-
-      mkdir -p "$majorVersion"
-      cp -r ../build/docs/. "./$majorVersion/"
-      git add "$majorVersion/*"
-
-  fi
-
-  git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" && {
-    git push origin HEAD || true
-  }
-  cd ..
-  rm -rf gh-pages
+if [[ $EXIT_STATUS -eq 0 ]]; then
+    ./gradlew test-suite:test --no-daemon || EXIT_STATUS=$?
 fi
+
+if [[ $EXIT_STATUS -eq 0 ]]; then
+    echo "Publishing archives for branch $TRAVIS_BRANCH"
+    if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH =~ ^master$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+      echo "Publishing archives"
+      ./gradlew --stop
+      if [[ -n $TRAVIS_TAG ]]; then
+          ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
+      else
+          ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
+      fi
+
+      ./gradlew --stop
+      ./gradlew --no-daemon docs || EXIT_STATUS=$?
+
+      git config --global user.name "$GIT_NAME"
+      git config --global user.email "$GIT_EMAIL"
+      git config --global credential.helper "store --file=~/.git-credentials"
+      echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+      git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
+      cd gh-pages
+
+      # If this is the master branch then update the snapshot
+      if [[ $TRAVIS_BRANCH =~ ^master|[12]\..\.x$ ]]; then
+         mkdir -p snapshot
+         cp -r ../build/docs/. ./snapshot/
+
+         git add snapshot/*
+      fi
+
+      # If there is a tag present then this becomes the latest
+      if [[ -n $TRAVIS_TAG ]]; then
+          mkdir -p latest
+          cp -r ../build/docs/. ./latest/
+          git add latest/*
+
+          version="$TRAVIS_TAG"
+          version=${version:1}
+          majorVersion=${version:0:4}
+          majorVersion="${majorVersion}x"
+
+          mkdir -p "$version"
+          cp -r ../build/docs/. "./$version/"
+          git add "$version/*"
+
+          mkdir -p "$majorVersion"
+          cp -r ../build/docs/. "./$majorVersion/"
+          git add "$majorVersion/*"
+
+      fi
+
+      git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" && {
+        git push origin HEAD || true
+      }
+      cd ..
+      rm -rf gh-pages
+    fi
+fi
+
+
 
 exit $EXIT_STATUS
