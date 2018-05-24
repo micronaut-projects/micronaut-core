bc4acc8046c3475f78e7f81fa02d726c935ef2be
sdelamo <sergio.delamo@softamo.com>
2018-05-24 12:41:46 +0200

    use difert bash script for Pull requests

---
+++ travis-build-pr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+EXIT_STATUS=0
+
+./gradlew --stop
+./gradlew testClasses
+./gradlew -Dgeb.env=chromeHeadless check -x test-suite:test --no-daemon || EXIT_STATUS=$?
+
+if [[ $EXIT_STATUS -eq 0 ]]; then
+    ./gradlew test-suite:test --no-daemon || EXIT_STATUS=$?
+fi
+
+exit $EXIT_STATUS
