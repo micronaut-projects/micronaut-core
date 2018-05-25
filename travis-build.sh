d0e063ecc5a9d7c1e33381a388a5ff5508e4e505
Graeme Rocher <graeme.rocher@gmail.com>
2018-05-25 14:16:36 +0200

    Attempt test release

--- travis-build.sh
+++ travis-build.sh
@@ -28,45 +28,6 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
       else
           ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
       fi
-
-      ./gradlew --stop
-      ./gradlew --no-daemon docs || EXIT_STATUS=$?
-
-      cd gh-pages
-
-      # If this is the master branch then update the snapshot
-      if [[ $TRAVIS_BRANCH =~ ^master|[12]\..\.x$ ]]; then
-         mkdir -p snapshot
-         cp -r ../build/docs/. ./snapshot/
-
-         git add snapshot/*
-      fi
-
-      # If there is a tag present then this becomes the latest
-      if [[ -n $TRAVIS_TAG ]]; then
-          mkdir -p latest
-          cp -r ../build/docs/. ./latest/
-          git add latest/*
-
-          version="$TRAVIS_TAG"
-          version=${version:1}
-          majorVersion=${version:0:4}
-          majorVersion="${majorVersion}x"
-
-          mkdir -p "$version"
-          cp -r ../build/docs/. "./$version/"
-          git add "$version/*"
-
-          mkdir -p "$majorVersion"
-          cp -r ../build/docs/. "./$majorVersion/"
-          git add "$majorVersion/*"
-
-      fi
-
-      git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" && {
-        git push origin HEAD || true
-      }
-      cd ..
     fi
 fi
 
