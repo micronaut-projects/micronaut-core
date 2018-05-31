f796166b26122d7966f918d96d87719ad4ff1060
Graeme Rocher <graeme.rocher@gmail.com>
2018-05-31 22:53:30 +0200

    Disable test that is not yet implemented

--- function-web/src/test/groovy/io/micronaut/function/web/TestFunctionFactory.java
+++ function-web/src/test/groovy/io/micronaut/function/web/TestFunctionFactory.java
@@ -8,6 +8,10 @@ import java.util.function.Supplier;
 @Factory
 public class TestFunctionFactory {
 
+    // This should work but is currently not implemented
+    // the reason is because when @FunctionBean is defined on a factory
+    // we do not go through and visit public Executable methods unless
+    // it is an AOP proxy
     @FunctionBean("java/supplier/string")
     Supplier<String> get() {
         return () -> "myvalue";
