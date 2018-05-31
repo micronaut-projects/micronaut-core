9e19f09e5f42d6d100a2c069bd1ce7fdc8e45f2d
Graeme Rocher <graeme.rocher@gmail.com>
2018-05-31 22:38:28 +0200

    Add @NotYetImplemented test with explanation

---
+++ function-web/src/test/groovy/io/micronaut/function/web/JavaLambdaFunctionSpec.groovy
@@ -0,0 +1,27 @@
+package io.micronaut.function.web
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.Specification
+
+class JavaLambdaFunctionSpec extends Specification {
+
+    void "test string supplier"() {
+        given:
+        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+        RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+
+        when:
+        HttpResponse<String> response = client.toBlocking().exchange('/java/supplier/string', String)
+
+        then:
+        response.code() == HttpStatus.OK.code
+        response.body() == 'value'
+
+        cleanup:
+        embeddedServer.stop()
+    }
+}
