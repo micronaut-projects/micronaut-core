9310995c2030f6e91e43c1be6d3d226052d2d033
jameskleeh <james.kleeh@gmail.com>
2018-05-31 11:27:03 -0400

    Remove ignoreif for previous CI environment

--- configurations/mongo-reactive/src/test/groovy/io/micronaut/configuration/mongo/reactive/MongoConfigurationSpec.groovy
+++ configurations/mongo-reactive/src/test/groovy/io/micronaut/configuration/mongo/reactive/MongoConfigurationSpec.groovy
@@ -21,14 +21,12 @@ import com.mongodb.MongoClientOptions
 import io.micronaut.context.ApplicationContext
 import io.micronaut.core.io.socket.SocketUtils
 import org.bson.Document
-import spock.lang.IgnoreIf
 import spock.lang.Specification
 
 /**
  * @author graemerocher
  * @since 1.0
  */
-@IgnoreIf({ System.getenv("JENKINS_URL") })
 class MongoConfigurationSpec extends Specification {
 
     void "test a basic blocking driver connection"() {
