27ba9095af2356c49c097b8bcaa0c5aacc02d0d1
Graeme Rocher <graeme.rocher@gmail.com>
2018-06-15 16:47:13 +0200

    Refactor

--- configurations/kafka/src/test/groovy/io/micronaut/configuration/kafka/KafkaConsumerConfigurationSpec.groovy
+++ configurations/kafka/src/test/groovy/io/micronaut/configuration/kafka/KafkaConsumerConfigurationSpec.groovy
@@ -1,6 +1,5 @@
 package io.micronaut.configuration.kafka
 
-import groovy.transform.CompileStatic
 import io.micronaut.context.ApplicationContext
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
@@ -19,7 +18,7 @@ class KafkaConsumerConfigurationSpec extends Specification {
         Properties props = config.getConfig()
 
         then:
-        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] == KafkaDefaultConfiguration.DEFAULT_BOOTSTRAP_SERVERS
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] == AbstractKafkaConfiguration.DEFAULT_BOOTSTRAP_SERVERS
 
         when:
         KafkaConsumer consumer = applicationContext.createBean(KafkaConsumer, config)
@@ -48,6 +47,36 @@ class KafkaConsumerConfigurationSpec extends Specification {
 
         then:
         props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] == "localhost:1111"
+        props[ConsumerConfig.GROUP_ID_CONFIG] == "mygroup"
+        props[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] == "100"
+
+        when:
+        KafkaConsumer consumer = applicationContext.createBean(KafkaConsumer, config)
+
+        then:
+        consumer != null
+
+        cleanup:
+        consumer.close()
+        applicationContext.close()
+    }
+
+    void "test override consumer default properties"() {
+        given:
+        ApplicationContext applicationContext = ApplicationContext.run(
+                ("kafka.${ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG}".toString()):"localhost:1111",
+                ("kafka.${ConsumerConfig.GROUP_ID_CONFIG}".toString()):"mygroup",
+                ("kafka.consumers.${ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG}".toString()):"localhost:2222",
+                ("kafka.${ConsumerConfig.GROUP_ID_CONFIG}".toString()):"mygroup",
+                ("kafka.consumers.${ConsumerConfig.MAX_POLL_RECORDS_CONFIG}".toString()):"100"
+        )
+
+        when:
+        KafkaConsumerConfiguration config = applicationContext.getBean(KafkaConsumerConfiguration)
+        Properties props = config.getConfig()
+
+        then:
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] == "localhost:2222"
         props[ConsumerConfig.GROUP_ID_CONFIG] == "mygroup"
         props[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] == "100"
 
