995b7f917953d58b0b20593e004651beb54523f0
sdelamo <sergio.delamo@softamo.com>
2018-06-21 12:33:19 +0200

    Allows to specify the secret base64 encoded
    Closes: https://github.com/micronaut-projects/micronaut-core/issues/288

--- security-jwt/src/test/groovy/io/micronaut/docs/base64/JwtBase64Spec.groovy
+++ security-jwt/src/test/groovy/io/micronaut/docs/base64/JwtBase64Spec.groovy
@@ -13,10 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.micronaut.docs.signandencrypt
+package io.micronaut.docs.base64
 
-import com.nimbusds.jwt.EncryptedJWT
-import com.nimbusds.jwt.JWTParser
 import io.micronaut.context.ApplicationContext
 import io.micronaut.docs.YamlAsciidocTagCleaner
 import io.micronaut.http.HttpStatus
@@ -26,17 +24,13 @@ import io.micronaut.inject.qualifiers.Qualifiers
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.security.token.generator.TokenGenerator
 import io.micronaut.security.token.jwt.AuthorizationUtils
-import io.micronaut.security.token.jwt.encryption.EncryptionConfiguration
-import io.micronaut.security.token.jwt.encryption.rsa.RSAEncryption
-import io.micronaut.security.token.jwt.generator.JwtTokenGenerator
 import io.micronaut.security.token.jwt.signature.SignatureConfiguration
-import io.micronaut.security.token.jwt.signature.secret.SecretSignature
 import org.yaml.snakeyaml.Yaml
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 
-class SignSecretEncryptRSASpec extends Specification implements AuthorizationUtils, YamlAsciidocTagCleaner {
+class JwtBase64Spec extends Specification implements AuthorizationUtils, YamlAsciidocTagCleaner {
 
     String yamlConfig = """
 #tag::yamlconfig[]
@@ -49,13 +43,11 @@ micronaut:
         signatures:
           secret:
             generator: 
-              secret: pleaseChangeThisSecretForANewOne #<1>
-              jwsAlgorithm: HS256 # <2>
+              secret: 'cGxlYXNlQ2hhbmdlVGhpc1NlY3JldEZvckFOZXdPbmU=' #<1>
+              base64: true #<2>
+              jwsAlgorithm: HS256
 #end::yamlconfig[]
 """
-
-    @Shared
-    File pemFile = new File('src/test/resources/rsa-2048bit-key-pair.pem')
 
     @Shared
     Map<String, Object> configMap = [
@@ -68,7 +60,8 @@ micronaut:
                                         'signatures': [
                                                 'secret': [
                                                         'generator': [
-                                                                'secret': 'pleaseChangeThisSecretForANewOne',
+                                                                'secret': 'cGxlYXNlQ2hhbmdlVGhpc1NlY3JldEZvckFOZXdPbmU=',
+                                                                'base64': true,
                                                                 'jwsAlgorithm': 'HS256'
                                                         ]
                                                 ]
@@ -82,55 +75,58 @@ micronaut:
     @Shared
     @AutoCleanup
     EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, [
-            'spec.name': 'signandencrypt',
-            'micronaut.security.endpoints.login.enabled': true,
+            'spec.name': 'base64',
             'endpoints.beans.enabled': true,
             'endpoints.beans.sensitive': true,
-            'pem.path': pemFile.absolutePath,
     ] << flatten(configMap), "test")
 
     @Shared
     @AutoCleanup
     RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
 
-    void "test /beans is secured"() {
-        when:
-        get("/beans")
-
-        then:
-        HttpClientResponseException e = thrown(HttpClientResponseException)
-        e.status == HttpStatus.UNAUTHORIZED
-    }
-
-    void "/beans can be accessed if authenticated"() {
+    void "a JWT signed with HS256 with a base 64 encoded secret can be used to acccess a micronaut app secured with the same base64 encoded secret"() {
         expect:
         new Yaml().load(cleanYamlAsciidocTag(yamlConfig)) == configMap
-        embeddedServer.applicationContext.getBean(RSAOAEPEncryptionConfiguration.class)
+        new String(configMap.micronaut.security.token.jwt.signatures.secret.generator.secret.decodeBase64()) == 'pleaseChangeThisSecretForANewOne'
         embeddedServer.applicationContext.getBean(SignatureConfiguration.class)
         embeddedServer.applicationContext.getBean(SignatureConfiguration.class, Qualifiers.byName("generator"))
-        embeddedServer.applicationContext.getBean(EncryptionConfiguration.class, Qualifiers.byName("generator"))
         embeddedServer.applicationContext.getBean(TokenGenerator.class)
 
         when:
-        JwtTokenGenerator tokenGenerator = embeddedServer.applicationContext.getBean(JwtTokenGenerator.class)
+        /*
+        https://jwt.io
+        HEADER:ALGORITHM & TOKEN TYPE
 
-        then:
-        tokenGenerator.getSignatureConfiguration() instanceof SecretSignature
-        tokenGenerator.getEncryptionConfiguration() instanceof RSAEncryption
+                {
+                    "alg": "HS256",
+                    "typ": "JWT"
+                }
+        PAYLOAD:DATA
 
-        when:
-        String token = loginWith(client,'user', 'password')
-
-        then:
-        token
-
-        and:
-        JWTParser.parse(token) instanceof EncryptedJWT
-
-        when:
+                {
+                    "sub": "1234567890",
+                    "name": "John Doe",
+                    "iat": 1516239022
+                }
+        VERIFY SIGNATURE
+        HMACSHA256(
+            base64UrlEncode(header) + "." +
+            base64UrlEncode(payload),
+            cGxlYXNlQ2hhbmdlVGhpc1NlY3JldEZvckFOZXdPbmU=
+        ) secret base64 encoded
+       */
+        String token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.6cD3MnZmX2xyEAWyh-GgGD11TX8SmvmHVLknuAIJ8yE'
         get("/beans", token)
 
         then:
         noExceptionThrown()
+
+        when: 'if the token uses a plain secret instead of the base64 encoded secret in the signature'
+        token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.8YXijqm04ZfCMUKyG3F4TXdMC94bOHYDUzXJwCfB7xE'
+        get("/beans", token)
+
+        then: 'access is not granted'
+        HttpClientResponseException e = thrown(HttpClientResponseException)
+        e.status == HttpStatus.UNAUTHORIZED
     }
 }
