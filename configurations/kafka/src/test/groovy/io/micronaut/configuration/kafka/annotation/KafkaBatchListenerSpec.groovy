a32bb7b3f3847266f595ce1001685dfdd82bb335
Graeme Rocher <graeme.rocher@gmail.com>
2018-06-26 13:32:11 +0200

    Kafka Batch - handle arrays

--- configurations/kafka/src/test/groovy/io/micronaut/configuration/kafka/annotation/KafkaBatchListenerSpec.groovy
+++ configurations/kafka/src/test/groovy/io/micronaut/configuration/kafka/annotation/KafkaBatchListenerSpec.groovy
@@ -17,6 +17,7 @@ class KafkaBatchListenerSpec extends Specification {
 
     public static final String BOOKS_TOPIC = 'KafkaBatchListenerSpec-books'
     public static final String BOOKS_LIST_TOPIC = 'KafkaBatchListenerSpec-books-list'
+    public static final String BOOKS_ARRAY_TOPIC = 'KafkaBatchListenerSpec-books-array'
     public static final String TITLES_TOPIC = 'KafkaBatchListenerSpec-titles'
 
     @Shared @AutoCleanup ApplicationContext context = ApplicationContext.run(
@@ -26,6 +27,7 @@ class KafkaBatchListenerSpec extends Specification {
                     AbstractKafkaConfiguration.EMBEDDED_TOPICS,
                     [ TITLES_TOPIC,
                       BOOKS_LIST_TOPIC,
+                      BOOKS_ARRAY_TOPIC,
                       BOOKS_TOPIC
                     ]
             )
@@ -50,6 +52,26 @@ class KafkaBatchListenerSpec extends Specification {
         }
     }
 
+
+
+    void "test send batch array - blocking"() {
+        given:
+        MyBatchClient myBatchClient = context.getBean(MyBatchClient)
+        BookListener bookListener = context.getBean(BookListener)
+        bookListener.books?.clear()
+        PollingConditions conditions = new PollingConditions(timeout: 30, delay: 0.5)
+
+        when:
+        myBatchClient.sendBooks(new Book(title: "The Stand"), new Book(title: "The Shining"))
+
+        then:
+        conditions.eventually {
+            bookListener.books.size() == 2
+            bookListener.books.contains(new Book(title: "The Stand"))
+            bookListener.books.contains(new Book(title: "The Shining"))
+        }
+    }
+
     @io.micronaut.configuration.kafka.annotation.KafkaClient(batch = true)
     @Topic(KafkaBatchListenerSpec.BOOKS_TOPIC)
     static interface MyBatchClient {
@@ -57,6 +79,7 @@ class KafkaBatchListenerSpec extends Specification {
         @Topic(KafkaBatchListenerSpec.BOOKS_LIST_TOPIC)
         void sendBooks(List<Book> books)
 
+        @Topic(KafkaBatchListenerSpec.BOOKS_ARRAY_TOPIC)
         void sendBooks(Book...books)
 
         Flux<Book> sendBooksFlux(Flux<Book> books)
@@ -65,18 +88,22 @@ class KafkaBatchListenerSpec extends Specification {
 
     }
 
-    @KafkaListener(batch = true, offsetReset = OffsetReset.EARLIEST)
+    @KafkaListener(
+            batch = true,
+            offsetReset = OffsetReset.EARLIEST
+    )
     @Topic(KafkaBatchListenerSpec.BOOKS_TOPIC)
     static class BookListener {
-        List<Book> books
+        List<Book> books = []
 
         @Topic(KafkaBatchListenerSpec.BOOKS_LIST_TOPIC)
         void receiveList(List<Book> books) {
-            this.books = books
+            this.books.addAll books
         }
 
+        @Topic(KafkaBatchListenerSpec.BOOKS_ARRAY_TOPIC)
         void receiveArray(Book...books) {
-            this.books = Arrays.asList(books)
+            this.books.addAll Arrays.asList(books)
         }
 
 
