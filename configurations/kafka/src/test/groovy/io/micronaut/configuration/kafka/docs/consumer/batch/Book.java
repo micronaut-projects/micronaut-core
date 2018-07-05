13637e1b10c3671fb03748ed5f1241405b7fda9e
Graeme Rocher <graeme.rocher@gmail.com>
2018-07-05 11:41:53 +0200

    Progress Kafka docs

---
+++ configurations/kafka/src/test/groovy/io/micronaut/configuration/kafka/docs/consumer/batch/Book.java
@@ -0,0 +1,13 @@
+package io.micronaut.configuration.kafka.docs.consumer.batch;
+
+public class Book {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
