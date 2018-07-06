9ce9ad7004dace195fda6671ec3f9c0a264b0954
Graeme Rocher <graeme.rocher@gmail.com>
2018-07-06 14:20:29 +0200

    Temporarily disable test, site is down

--- http-client/src/test/groovy/io/micronaut/http/client/SslSpec.groovy
+++ http-client/src/test/groovy/io/micronaut/http/client/SslSpec.groovy
@@ -1,10 +1,12 @@
 package io.micronaut.http.client
 
 import io.micronaut.context.ApplicationContext
+import spock.lang.Ignore
 import spock.lang.Specification
 
 class SslSpec extends Specification {
 
+    @Ignore // service down at the moment
     void "test that clients work with self signed certificates"() {
         given:
         ApplicationContext ctx = ApplicationContext.run()
