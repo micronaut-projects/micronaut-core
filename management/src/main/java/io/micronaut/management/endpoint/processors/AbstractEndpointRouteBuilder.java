e1586b935fc84ba977ce0460510366c94efa4b89
sdelamo <sergio.delamo@softamo.com>
2018-07-10 07:53:26 +0200

    Enable Configuration of endpoints base path
    See: https://github.com/micronaut-projects/micronaut-core/issues/369

--- management/src/main/java/io/micronaut/management/endpoint/processors/AbstractEndpointRouteBuilder.java
+++ management/src/main/java/io/micronaut/management/endpoint/processors/AbstractEndpointRouteBuilder.java
@@ -27,6 +27,7 @@ import io.micronaut.http.uri.UriTemplate;
 import io.micronaut.inject.BeanDefinition;
 import io.micronaut.inject.ExecutableMethod;
 import io.micronaut.management.endpoint.Endpoint;
+import io.micronaut.management.endpoint.EndpointDefaultConfiguration;
 import io.micronaut.web.router.DefaultRouteBuilder;
 
 import java.lang.annotation.Annotation;
@@ -51,16 +52,20 @@ abstract class AbstractEndpointRouteBuilder extends DefaultRouteBuilder implemen
 
     private final List<String> nonPathTypes;
 
+    private final EndpointDefaultConfiguration endpointDefaultConfiguration;
+
     /**
      * @param applicationContext The application context
      * @param uriNamingStrategy  The URI naming strategy
      * @param conversionService  The conversion service
      * @param nonPathTypesProviders A list of providers which defines paths not to be used as Path paramters
+     * @param endpointDefaultConfiguration Endpoints default Configuration
      */
     AbstractEndpointRouteBuilder(ApplicationContext applicationContext,
                                  UriNamingStrategy uriNamingStrategy,
                                  ConversionService<?> conversionService,
-                                 Collection<NonPathTypesProvider> nonPathTypesProviders) {
+                                 Collection<NonPathTypesProvider> nonPathTypesProviders,
+                                 EndpointDefaultConfiguration endpointDefaultConfiguration) {
         super(applicationContext, uriNamingStrategy, conversionService);
         this.beanContext = applicationContext;
         nonPathTypes = nonPathTypesProviders.stream()
@@ -68,6 +73,7 @@ abstract class AbstractEndpointRouteBuilder extends DefaultRouteBuilder implemen
                 .flatMap(List::stream)
                 .map(Class::getName)
                 .collect(Collectors.toList());
+        this.endpointDefaultConfiguration = endpointDefaultConfiguration;
     }
 
     /**
@@ -133,13 +139,28 @@ abstract class AbstractEndpointRouteBuilder extends DefaultRouteBuilder implemen
      * @return An {@link UriTemplate}
      */
     protected UriTemplate buildUriTemplate(ExecutableMethod<?, ?> method, String id) {
-        UriTemplate template = new UriTemplate(uriNamingStrategy.resolveUri(id));
+        UriTemplate template = new UriTemplate(resolveUriByRouteId(id));
         for (Argument argument : method.getArguments()) {
             if (isPathParameter(argument)) {
                 template = template.nest("/{" + argument.getName() + "}");
             }
         }
         return template;
+    }
+
+    /**
+     * @param id The route id
+     * @return {@link EndpointDefaultConfiguration#path} + resolved Uri based on UriNamingStrategy
+     */
+    String resolveUriByRouteId(String id) {
+        String uri = uriNamingStrategy.resolveUri(id);
+        if (endpointDefaultConfiguration.getPath().equals("/") && uri.charAt(0) == '/') {
+            return uri;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(endpointDefaultConfiguration.getPath());
+        sb.append(uri);
+        return sb.toString();
     }
 
     /**
