6ef30fc59d1d8958735c36e3fcdb0609611f7873
Graeme Rocher <graeme.rocher@gmail.com>
2018-07-13 17:10:04 +0200

    remove parallel for the moment, causing build problems

--- configurations/kafka/src/main/java/io/micronaut/configuration/kafka/annotation/KafkaListener.java
+++ configurations/kafka/src/main/java/io/micronaut/configuration/kafka/annotation/KafkaListener.java
@@ -36,7 +36,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @MessageListener
-@Parallel
 public @interface KafkaListener {
 
     /**
