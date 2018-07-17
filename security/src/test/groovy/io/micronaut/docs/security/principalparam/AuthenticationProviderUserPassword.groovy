babe9813cdeffa4aafe5cd9dc0fba476775cbb60
sdelamo <sergio.delamo@softamo.com>
2018-07-17 10:07:39 +0200

    Doc - Retrieve the authenticated user
    Close: https://github.com/micronaut-projects/micronaut-core/issues/399

---
+++ security/src/test/groovy/io/micronaut/docs/security/principalparam/AuthenticationProviderUserPassword.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.docs.security.principalparam
+
+import io.micronaut.context.annotation.Requires
+import io.micronaut.security.authentication.AuthenticationFailed
+import io.micronaut.security.authentication.AuthenticationProvider
+import io.micronaut.security.authentication.AuthenticationRequest
+import io.micronaut.security.authentication.AuthenticationResponse
+import io.micronaut.security.authentication.UserDetails
+import io.reactivex.Flowable
+import org.reactivestreams.Publisher
+
+import javax.inject.Singleton
+
+// Although this is a Groovy file this is written as close to Java as possible to embedded in the docs
+
+@Requires(property = "spec.name", value = "principalparam")
+//tag::clazz[]
+@Singleton
+public class AuthenticationProviderUserPassword implements AuthenticationProvider {
+
+    @Override
+    public Publisher<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest) {
+        if (authenticationRequest.getIdentity().equals("user") && authenticationRequest.getSecret().equals("password")) {
+            return Flowable.just(new UserDetails("user", new ArrayList<>()));
+        }
+        return Flowable.just(new AuthenticationFailed());
+    }
+}
+//end::clazz[]
