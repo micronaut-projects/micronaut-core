7319747001230f7f1e026db6d7e24e65a0701a92
Matthew Moss <mossm@objectcomputing.com>
2018-07-18 16:23:52 -0500

    Add concrete LoggersDataCollector (stubbed)

---
+++ management/src/main/java/io/micronaut/management/endpoint/loggers/impl/RxLoggersDataCollector.java
@@ -0,0 +1,23 @@
+package io.micronaut.management.endpoint.loggers.impl;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.management.endpoint.loggers.LoggerConfiguration;
+import io.micronaut.management.endpoint.loggers.LoggersDataCollector;
+import io.micronaut.management.endpoint.loggers.LoggersEndpoint;
+import io.reactivex.Single;
+import org.reactivestreams.Publisher;
+
+import javax.inject.Singleton;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Stream;
+
+@Singleton
+@Requires(beans = LoggersEndpoint.class)
+public class RxLoggersDataCollector implements LoggersDataCollector<Map<String, Object>> {
+
+    public Publisher<Map<String, Object>> getData(Stream<LoggerConfiguration> loggerConfigurations) {
+        return Single.just(Collections.<String, Object>emptyMap()).toFlowable();
+    }
+
+}
