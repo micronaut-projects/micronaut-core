a3c13643598faf540483e5ea67ed7716298a047f
Matthew Moss <mossm@objectcomputing.com>
2018-07-18 22:34:48 -0500

    Change LoggersDataCollector.getData interface to take a LoggingSystem argument (and adjust appropriately where used) Add LoggersDataCollector.getOne and implement in RxLoggersDataCollector Add @Read getLogger implementation to endpoint

--- management/src/main/java/io/micronaut/management/endpoint/loggers/impl/RxLoggersDataCollector.java
+++ management/src/main/java/io/micronaut/management/endpoint/loggers/impl/RxLoggersDataCollector.java
@@ -1,25 +1,21 @@
 package io.micronaut.management.endpoint.loggers.impl;
 
 import io.micronaut.context.annotation.Requires;
-import io.micronaut.management.endpoint.loggers.LogLevel;
-import io.micronaut.management.endpoint.loggers.LoggerConfiguration;
-import io.micronaut.management.endpoint.loggers.LoggersDataCollector;
-import io.micronaut.management.endpoint.loggers.LoggersEndpoint;
+import io.micronaut.management.endpoint.loggers.*;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
 import org.reactivestreams.Publisher;
 
 import javax.inject.Singleton;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Singleton
 @Requires(beans = LoggersEndpoint.class)
 public class RxLoggersDataCollector implements LoggersDataCollector<Map<String, Object>> {
 
     @Override
-    public Publisher<Map<String, Object>> getData(Collection<LoggerConfiguration> configurations) {
-        return Single.zip(getLoggers(configurations), getLogLevels(),
+    public Publisher<Map<String, Object>> getData(LoggingSystem loggingSystem) {
+        return Single.zip(getLoggers(loggingSystem.getLoggers()), getLogLevels(),
                 (loggers, levels) -> {
                     Map<String, Object> data = new HashMap<>(2);
                     data.put("loggers", loggers);
@@ -29,17 +25,23 @@ public class RxLoggersDataCollector implements LoggersDataCollector<Map<String, 
                 .toFlowable();
     }
 
+    @Override
+    public Publisher<Map<String, Object>> getOne(LoggingSystem loggingSystem, String name) {
+        return Single.just(loggingSystem.getLogger(name).getData())
+                .toFlowable();
+    }
+
     /**
      * @param configurations The logger configurations
      * @return A {@link Single} that wraps a Map
      */
     protected Single<Map<String, Object>> getLoggers(Collection<LoggerConfiguration> configurations) {
-        Map<String, Object> loggers = new ConcurrentHashMap<>(configurations.size());
+        Map<String, Object> loggers = new HashMap<>(configurations.size());
 
         return Flowable
                 .fromIterable(configurations)
                 .collectInto(loggers, (map, configuration) ->
-                        map.put(configuration.getName(), configuration)
+                    map.put(configuration.getName(), configuration.getData())
                 );
     }
 
