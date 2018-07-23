cfcd1feff742a4ffb20c11adf32f5290e658f5e2
Graeme Rocher <graeme.rocher@gmail.com>
2018-07-23 15:46:36 +0200

    Fix failing test

--- inject/src/main/java/io/micronaut/inject/DefaultBeanIdentifier.java
+++ inject/src/main/java/io/micronaut/inject/DefaultBeanIdentifier.java
@@ -64,8 +64,12 @@ class DefaultBeanIdentifier implements BeanIdentifier {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         DefaultBeanIdentifier that = (DefaultBeanIdentifier) o;
         return Objects.equals(id, that.id);
     }
