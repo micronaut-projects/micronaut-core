7dfaef95d53845f5dcb6f29db4d29a550342fdaa
sdelamo <sergio.delamo@softamo.com>
2018-07-26 18:28:05 +0200

    Doc - Simple 404 responses with null or Maybe.empty()
    Close: https://github.com/micronaut-projects/micronaut-core/issues/442

---
+++ http-server-netty/src/test/groovy/io/micronaut/docs/respondingnotfound/BooksSpec.groovy
@@ -0,0 +1,55 @@
+package io.micronaut.docs.respondingnotfound
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class BooksSpec extends Specification {
+
+    @Shared
+    @AutoCleanup
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, [
+            'spec.name':'respondingnotfound'
+    ], "test")
+
+    @AutoCleanup
+    @Shared
+    RxHttpClient rxClient = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+
+    def "returning null returns 404"() {
+
+        when:
+        rxClient.toBlocking().exchange(HttpRequest.GET('/books/stock/XXXXX'))
+
+        then:
+        def e = thrown(HttpClientResponseException)
+
+        when:
+        def response = e.response
+
+        then:
+        response.status == HttpStatus.NOT_FOUND
+    }
+
+
+    def "returning Maybe.empty returns 404"() {
+
+        when:
+        rxClient.toBlocking().exchange(HttpRequest.GET('/books/maybestock/XXXXX'))
+
+        then:
+        def e = thrown(HttpClientResponseException)
+
+        when:
+        def response = e.response
+
+        then:
+        response.status == HttpStatus.NOT_FOUND
+    }
+}
