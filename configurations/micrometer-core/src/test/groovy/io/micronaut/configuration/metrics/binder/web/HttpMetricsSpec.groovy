c70c33bfcf9d0fcdc7b3aa3c595c47901b117d79
Graeme Rocher <graeme.rocher@gmail.com>
2018-08-03 11:48:45 +0200

    Support for HTTP client metrics. Fixes #467

---
+++ configurations/micrometer-core/src/test/groovy/io/micronaut/configuration/metrics/binder/web/HttpMetricsSpec.groovy
@@ -0,0 +1,81 @@
+package io.micronaut.configuration.metrics.binder.web
+
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
+import io.micrometer.core.instrument.search.RequiredSearch
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.client.Client
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static io.micronaut.configuration.metrics.micrometer.MeterRegistryFactory.MICRONAUT_METRICS_ENABLED
+
+class HttpMetricsSpec extends Specification {
+
+    void "test client / server metrics"() {
+        when:
+        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+        def context = embeddedServer.getApplicationContext()
+        TestClient client = context.getBean(TestClient)
+
+        then:
+        client.index() == 'ok'
+
+        when:
+        MeterRegistry registry = context.getBean(MeterRegistry)
+
+        Timer serverTimer = registry.get(WebMetricsPublisher.METRIC_HTTP_SERVER_REQUESTS).tags('uri','/test-http-metrics').timer()
+        Timer clientTimer = registry.get(WebMetricsPublisher.METRIC_HTTP_CLIENT_REQUESTS).tags('uri','/test-http-metrics').timer()
+
+
+        then:
+        serverTimer != null
+        serverTimer.count() == 1
+        clientTimer.count() == 1
+
+
+        cleanup:
+        embeddedServer.close()
+
+    }
+
+
+    @Unroll
+    def "test getting the beans #cfg #setting"() {
+        when:
+        ApplicationContext context = ApplicationContext.run([(cfg): setting])
+
+        then:
+        context.findBean(ClientRequestMetricRegistryFilter).isPresent() == setting
+        context.findBean(ServerRequestMeterRegistryFilter).isPresent() == setting
+
+        cleanup:
+        context.close()
+
+        where:
+        cfg                           | setting
+        MICRONAUT_METRICS_ENABLED     | true
+        MICRONAUT_METRICS_ENABLED     | false
+        (WebMetricsPublisher.ENABLED) | true
+        (WebMetricsPublisher.ENABLED) | false
+    }
+
+
+
+    @Client('/test-http-metrics')
+    static interface TestClient {
+        @Get
+        String index()
+    }
+
+    @Controller('/test-http-metrics')
+    static class TestController {
+        @Get
+        String index() {
+            return "ok"
+        }
+    }
+}
