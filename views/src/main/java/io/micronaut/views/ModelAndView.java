412afa46cde45ae1034b2a4f7c4b460e852dde14
sdelamo <sergio.delamo@softamo.com>
2018-08-08 14:42:31 +0200

    Views rendering
    Closes: https://github.com/micronaut-projects/micronaut-core/issues/396

---
+++ views/src/main/java/io/micronaut/views/ModelAndView.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.micronaut.views;
+
+/**
+ * Holder for both Model and View.
+ *
+ * @author Sergio del Amo
+ * @since 1.0
+ */
+public class ModelAndView {
+
+    private String view;
+
+    private Object model;
+
+    /**
+     * Empty constructor.
+     */
+    public ModelAndView() {
+
+    }
+
+    /**
+     * Constructor.
+     * @param view view name to be rendered
+     * @param model Model to be rendered against the view
+     */
+    public ModelAndView(String view, Object model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    /**
+     *
+     * @return view name to be rendered
+     */
+    public String getView() {
+        return view;
+    }
+
+    /**
+     * view Setter.
+     * @param view the view name
+     */
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    /**
+     * @return model to render
+     */
+    public Object getModel() {
+        return model;
+    }
+
+    /**
+     * model Setter.
+     * @param model model to be rendered
+     */
+    public void setModel(Object model) {
+        this.model = model;
+    }
+}
