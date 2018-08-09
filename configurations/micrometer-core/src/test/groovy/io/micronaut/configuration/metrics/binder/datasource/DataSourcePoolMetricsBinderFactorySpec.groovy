00dd69acc04f056745765669a007a3c12ca84446
coestre <christian.oestreich@optum.com>
2018-08-09 14:12:33 -0500

    Metrics - adding more datasource metrics binder tests

--- configurations/micrometer-core/src/test/groovy/io/micronaut/configuration/metrics/binder/datasource/DataSourcePoolMetricsBinderFactorySpec.groovy
+++ configurations/micrometer-core/src/test/groovy/io/micronaut/configuration/metrics/binder/datasource/DataSourcePoolMetricsBinderFactorySpec.groovy
@@ -1,30 +1,64 @@
 package io.micronaut.configuration.metrics.binder.datasource
 
-import io.micrometer.core.instrument.MeterRegistry
-import spock.lang.Ignore
+
+import io.micronaut.jdbc.metadata.DataSourcePoolMetadata
 import spock.lang.Specification
 
 import javax.sql.DataSource
 
 class DataSourcePoolMetricsBinderFactorySpec extends Specification {
 
-    @Ignore
     def "test getting the beans manually"() {
         given:
-        def meterRegistry = Mock(MeterRegistry)
-        def dataSource1 = Mock(DataSource)
-        def dataSource2 = Mock(DataSource)
+        DataSourcePoolMetricsBinderFactory dataSourcePoolMetricsBinderFactory = new DataSourcePoolMetricsBinderFactory()
 
         when:
-        def binder = new DataSourcePoolMetricsBinderFactory([], meterRegistry)
-        binder.dataSourceMeterBinder("default", dataSource1)
-        binder.dataSourceMeterBinder("first", dataSource2)
+        def meterBinder = dataSourcePoolMetricsBinderFactory.dataSourceMeterBinder("foo", new Foo())
 
         then:
-        1 * dataSource2.hashCode() >> 1
-        1 * dataSource1.hashCode() >> 2
-        0 * _._
+        meterBinder
+    }
 
+    class Foo implements DataSourcePoolMetadata {
 
+        @Override
+        DataSource getDataSource() {
+            return null
+        }
+
+        @Override
+        Integer getIdle() {
+            return null
+        }
+
+        @Override
+        Float getUsage() {
+            return null
+        }
+
+        @Override
+        Integer getActive() {
+            return null
+        }
+
+        @Override
+        Integer getMax() {
+            return null
+        }
+
+        @Override
+        Integer getMin() {
+            return null
+        }
+
+        @Override
+        String getValidationQuery() {
+            return null
+        }
+
+        @Override
+        Boolean getDefaultAutoCommit() {
+            return null
+        }
     }
 }
