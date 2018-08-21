ba8f0d10be772a2510b7e1b1c3fc777184eff3b4
Ryan Vanderwerf <rvanderwerf@gmail.com>
2018-08-21 12:52:25 -0500

    checkstyle fixes

--- function-aws-alexa/src/main/java/io/micronaut/function/aws/alexa/package-info.java
+++ function-aws-alexa/src/main/java/io/micronaut/function/aws/alexa/package-info.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * <p>Classes to support exposing {@link io.micronaut.function.FunctionBean} instances for AWS lambda Alexa Skills</p>.
  *
