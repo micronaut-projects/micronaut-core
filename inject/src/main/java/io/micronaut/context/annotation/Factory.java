e64bf4cb9c96c9b4216ec13c52d2c418b6f567f5
Graeme Rocher <graeme.rocher@gmail.com>
2018-08-30 17:23:43 +0200

    doc improvements

--- inject/src/main/java/io/micronaut/context/annotation/Factory.java
+++ inject/src/main/java/io/micronaut/context/annotation/Factory.java
@@ -23,9 +23,38 @@ import java.lang.annotation.RetentionPolicy;
 
 /**
  * <p>A factory is a {@link Singleton} that produces one or many other bean implementations.</p>
- * <p>
+ *
  * <p>Each produced bean is defined by method that is annotated with {@link Bean}</p>
  *
+ * <pre class="code">
+ * &#064;Factory
+ * public class MyFactory {
+ *
+ *     &#064;Bean
+ *     public MyBean myBean() {
+ *         // create the bean
+ *     }
+ * }</pre>
+ *
+ * <p>Methods defined within the body of the class that are annotated with {@link Bean} will be exposed as beans.</p>
+ *
+ * <p>You can use a {@link javax.inject.Scope} annotation to control the scope the bean is exposed within. For example for a
+ * singleton instance you can annotation the method with {@link Singleton}.</p>
+ *
+ * <p>Methods annotated with {@link Bean} can accept arguments and Micronaut will attempt to inject those arguments from existing beans or values. For example:</p>
+ *
+ * <pre class="code">
+ * &#064;Factory
+ * public class MyFactory {
+ *
+ *     &#064;Bean
+ *     public MyBean myBean(&#064;Value("foo.bar") String myValue) {
+ *         // create the bean
+ *     }
+ * }</pre>
+ *
+ * @see Bean
+ * @see Configuration
  * @author Graeme Rocher
  * @since 1.0
  */
