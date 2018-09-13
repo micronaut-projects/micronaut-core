08fceb9e8d885a084c941999cf8c2c5bbc9888f7
Graeme Rocher <graeme.rocher@gmail.com>
2018-09-13 11:34:53 +0200

    Revert tests

--- http-server-netty/src/test/groovy/io/micronaut/http/server/netty/websocket/BinaryWebSocketSpec.groovy
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/websocket/BinaryWebSocketSpec.groovy
@@ -15,7 +15,7 @@ class BinaryWebSocketSpec extends Specification{
     void "test binary websocket exchange"() {
         given:
         EmbeddedServer embeddedServer = ApplicationContext.build('micronaut.server.netty.log-level':'TRACE').run(EmbeddedServer)
-        PollingConditions conditions = new PollingConditions(timeout: 3, delay: 0.5)
+        PollingConditions conditions = new PollingConditions(timeout: 15, delay: 0.5)
 
         when: "a websocket connection is established"
         RxWebSocketClient wsClient = embeddedServer.applicationContext.createBean(RxWebSocketClient, embeddedServer.getURI())
