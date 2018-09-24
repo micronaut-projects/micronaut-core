d48b8a76f39378071a5046a1981151d3a8b46b3f
Graeme Rocher <graeme.rocher@gmail.com>
2018-09-24 14:40:51 +0200

    CLI: Allow generating correct Maven/Kotlin annotation processor paths

--- cli/src/main/groovy/io/micronaut/cli/io/support/BuildTokens.groovy
+++ cli/src/main/groovy/io/micronaut/cli/io/support/BuildTokens.groovy
@@ -1,4 +1,9 @@
 package io.micronaut.cli.io.support
 
 class BuildTokens {
+    final String sourceLanguage, testFramework
+    BuildTokens(String sourceLanguage, String testFramework) {
+        this.sourceLanguage = sourceLanguage
+        this.testFramework = testFramework
+    }
 }
