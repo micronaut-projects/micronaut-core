fbb23a47a557543b76ad2ed106d5c1480ff1fcf0
Graeme Rocher <graeme.rocher@gmail.com>
2018-10-08 10:01:14 +0200

    Revert "Test for #698"
    This reverts commit 5f68109dc20a6a868c5f9a7f1cd5e43ed189dcd8.

--- inject-java/src/test/groovy/io/micronaut/aop/factory/SessionProxySpec.groovy
+++ inject-java/src/test/groovy/io/micronaut/aop/factory/SessionProxySpec.groovy
@@ -1,14 +1,9 @@
 package io.micronaut.aop.factory
 
-import io.micronaut.aop.Intercepted
-import io.micronaut.context.DefaultBeanContext
-import io.micronaut.core.reflect.ReflectionUtils
+
 import io.micronaut.inject.AbstractTypeElementSpec
 import io.micronaut.inject.BeanDefinition
-import io.micronaut.inject.BeanFactory
 import io.micronaut.inject.writer.BeanDefinitionVisitor
-import org.hibernate.Session
-import org.hibernate.SessionFactory
 
 class SessionProxySpec extends AbstractTypeElementSpec {
 
@@ -33,20 +28,7 @@ class AbstractBean {
 }
 
 ''')
-        // make sure all the public method are implemented
-        def clazz = beanDefinition.getBeanType()
-        int count = 1 // proxy methods
-        def interfaces = ReflectionUtils.getAllInterfaces(Session.class)
-        interfaces += Session.class
-        for(i in interfaces) {
-            for(m in i.declaredMethods) {
-                count++
-                assert clazz.getDeclaredMethod(m.name, m.parameterTypes)
-            }
-        }
-
         then:
         beanDefinition.isProxy()
-
     }
 }
