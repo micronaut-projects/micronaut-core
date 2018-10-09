2ebf9a1f261d02192d898bdda460cb4e635d33de
sdelamo <sergio.delamo@softamo.com>
2018-10-09 11:41:32 +0200

    Add upload empty file test
    See: https://github.com/micronaut-projects/micronaut-core/issues/701

---
+++ test-suite/src/test/groovy/io/micronaut/upload/browser/UploadBrowserSpec.groovy
@@ -0,0 +1,43 @@
+package io.micronaut.upload.browser
+
+import geb.spock.GebSpec
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.Environment
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.AutoCleanup
+import spock.lang.IgnoreIf
+import spock.lang.Shared
+
+class UploadBrowserSpec extends GebSpec {
+    @Shared
+    @AutoCleanup
+    ApplicationContext context = ApplicationContext.run(
+            [
+                    'spec.name': UploadBrowserSpec.simpleName,
+            ], Environment.TEST)
+
+    @Shared
+    @AutoCleanup
+    EmbeddedServer embeddedServer = context.getBean(EmbeddedServer).start()
+
+    @IgnoreIf({ !sys['geb.env'] })
+    def "submitting a form without a file triggers an error"() {
+        given:
+        browser.baseUrl = "http://localhost:${embeddedServer.port}"
+
+        when:
+        go '/image/create'
+
+        then:
+        at CreatePage
+
+        when:
+        CreatePage page = browser.page CreatePage
+        page.upload()
+
+        then:
+        at FileEmptyPage
+
+
+    }
+}
