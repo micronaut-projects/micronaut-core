da8a4578591288260807dd8ace07ec8cfa49735b
Sergio del Amo <sergio.delamo@softamo.com>
2018-11-12 11:33:45 +0100

    Refactor CookieHttpSessionStrategy (#890)
    To ease debugging and replacement.
    
    See: https://github.com/micronaut-projects/micronaut-core/issues/882

--- session/src/main/java/io/micronaut/session/http/CookieHttpSessionStrategy.java
+++ session/src/main/java/io/micronaut/session/http/CookieHttpSessionStrategy.java
@@ -23,10 +23,11 @@ import io.micronaut.http.cookie.Cookie;
 import io.micronaut.http.cookie.Cookies;
 import io.micronaut.session.Session;
 import io.micronaut.session.SessionSettings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
@@ -40,9 +41,10 @@ import java.util.Map;
 @Requires(property = SessionSettings.HTTP_COOKIE_STRATEGY, notEquals = "false")
 public class CookieHttpSessionStrategy implements HttpSessionIdStrategy {
 
-    private final boolean base64Decode;
-    private final String prefix;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CookieHttpSessionStrategy.class);
+
     private final HttpSessionConfiguration configuration;
+    private final CookieHttpSessionIdGenerator cookieHttpSessionIdGenerator;
 
     /**
      * Constructor.
@@ -50,27 +52,32 @@ public class CookieHttpSessionStrategy implements HttpSessionIdStrategy {
      * @param configuration The HTTP session configuration
      */
     public CookieHttpSessionStrategy(HttpSessionConfiguration configuration) {
+        this(configuration, new CookieHttpSessionIdGenerator(configuration));
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param configuration The HTTP session configuration
+     * @param cookieHttpSessionIdGenerator Cookie HTTP Session Id generator
+     */
+    public CookieHttpSessionStrategy(HttpSessionConfiguration configuration, CookieHttpSessionIdGenerator cookieHttpSessionIdGenerator) {
         this.configuration = configuration;
-        this.base64Decode = configuration.isBase64Encode();
-        this.prefix = configuration.getPrefix().orElse(null);
+        this.cookieHttpSessionIdGenerator = cookieHttpSessionIdGenerator;
     }
 
     @Override
     public List<String> resolveIds(HttpRequest<?> message) {
         Cookies cookies = message.getCookies();
         List<String> resolvedIds = new ArrayList<>();
-        String cookieName = configuration.getCookieName();
+        String cookieName = getConfiguration().getCookieName();
         for (Map.Entry<String, Cookie> entry : cookies) {
             String name = entry.getKey();
             if (cookieName.equalsIgnoreCase(name)) {
                 Cookie cookie = entry.getValue();
-                String id = cookie.getValue();
-                if (base64Decode) {
-                    id = new String(Base64.getDecoder().decode(id));
-                }
-                int len = id.length();
-                if (prefix != null && len < prefix.length()) {
-                    id = id.substring(prefix.length());
+                String id = cookieHttpSessionIdGenerator.sessionIdFromCookie(cookie);
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.debug("path {}, session id: {}", id, message.getPath());
                 }
                 resolvedIds.add(id);
             }
@@ -85,29 +92,34 @@ public class CookieHttpSessionStrategy implements HttpSessionIdStrategy {
                          Session session) {
         Cookie cookie;
         if (session.isExpired()) {
-            cookie = Cookie.of(configuration.getCookieName(), "")
+            cookie = Cookie.of(getConfiguration().getCookieName(), "")
                 .maxAge(0);
         } else {
-            String id = session.getId();
-            if (prefix != null) {
-                id = prefix + id;
+            String cookieValue = cookieHttpSessionIdGenerator.cookieValueFromSession(session);
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("path {}, cookie value {}", request.getPath(), cookieValue);
             }
-            if (base64Decode) {
-                id = Base64.getEncoder().encodeToString(id.getBytes());
-            }
-            cookie = Cookie.of(configuration.getCookieName(), id);
-            if (configuration.isRememberMe()) {
+            cookie = Cookie.of(getConfiguration().getCookieName(), cookieValue);
+            if (getConfiguration().isRememberMe()) {
                 cookie.maxAge(Integer.MAX_VALUE);
             } else {
-                configuration.getCookieMaxAge().ifPresent(cookie::maxAge);
+                getConfiguration().getCookieMaxAge().ifPresent(cookie::maxAge);
             }
         }
 
         cookie.httpOnly(true).secure(request.isSecure());
 
-        configuration.getCookiePath().ifPresent(cookie::path);
-        configuration.getDomainName().ifPresent(cookie::domain);
+        getConfiguration().getCookiePath().ifPresent(cookie::path);
+        getConfiguration().getDomainName().ifPresent(cookie::domain);
 
         response.cookie(cookie);
+    }
+
+    /**
+     *
+     * @return The HTTP session configuration
+     */
+    public HttpSessionConfiguration getConfiguration() {
+        return configuration;
     }
 }
