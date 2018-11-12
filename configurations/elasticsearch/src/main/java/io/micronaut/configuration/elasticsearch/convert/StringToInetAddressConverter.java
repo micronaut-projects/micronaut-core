21528c41abf823d4e8eee2308d5b8b67f1ef7afb
Puneet Behl <behl4485@gmail.com>
2018-11-12 12:40:14 +0530

    #802 Implemented converters for Elasticsearch

---
+++ configurations/elasticsearch/src/main/java/io/micronaut/configuration/elasticsearch/convert/StringToInetAddressConverter.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.micronaut.configuration.elasticsearch.convert;
+
+import io.micronaut.core.convert.ConversionContext;
+import io.micronaut.core.convert.TypeConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Singleton;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+/**
+ * @author Puneet Behl
+ * @since 1.1.0
+ */
+@Singleton
+public class StringToInetAddressConverter implements TypeConverter<CharSequence, InetAddress> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StringToInetAddressConverter.class);
+
+    @Override
+    public Optional<InetAddress> convert(CharSequence object, Class<InetAddress> targetType, ConversionContext context) {
+        String address = object.toString();
+        try {
+            return Optional.of(InetAddress.getByName(address));
+        } catch (UnknownHostException e) {
+            LOG.error(e.getMessage(), e);
+            return Optional.empty();
+        }
+    }
+}
---
+++ configurations/elasticsearch/src/main/java/io/micronaut/configuration/elasticsearch/convert/StringToNodeSelectorConverter.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.micronaut.configuration.elasticsearch.convert;
+
+import io.micronaut.core.convert.ConversionContext;
+import io.micronaut.core.convert.TypeConverter;
+import org.elasticsearch.client.NodeSelector;
+
+import javax.inject.Singleton;
+import java.util.Locale;
+import java.util.Optional;
+
+/**
+ * @author Puneet Behl
+ * @since 1.1.0
+ */
+@Singleton
+public class StringToNodeSelectorConverter implements TypeConverter<CharSequence, NodeSelector> {
+
+    @Override
+    public Optional<NodeSelector> convert(CharSequence object, Class<NodeSelector> targetType, ConversionContext context) {
+        String nodeSelector = object.toString().toUpperCase(Locale.ENGLISH);
+        switch (nodeSelector) {
+            case "SKIP_DEDICATED_MASTERS":
+                return Optional.of(NodeSelector.SKIP_DEDICATED_MASTERS);
+            case "ANY":
+                return Optional.of(NodeSelector.ANY);
+            default:
+                return Optional.empty();
+        }
+    }
+}
