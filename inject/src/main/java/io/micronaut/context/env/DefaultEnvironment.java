3faeaabb5ffee8199491ca6e663314e0d06725b6
sdelamo <sergio.delamo@softamo.com>
2018-11-20 18:17:39 +0100

    timeout digital ocean

--- inject/src/main/java/io/micronaut/context/env/DefaultEnvironment.java
+++ inject/src/main/java/io/micronaut/context/env/DefaultEnvironment.java
@@ -73,6 +73,10 @@ public class DefaultEnvironment extends PropertySourcePropertyResolver implement
     private static final String K8S_ENV = "KUBERNETES_SERVICE_HOST";
     private static final String PCF_ENV = "VCAP_SERVICES";
     private static final String HEROKU_DYNO = "DYNO";
+    private static final int DEFAULT_READ_TIMEOUT = 500;
+    private static final int DEFAULT_CONNECT_TIMEOUT = 500;
+    public static final String DIGITAL_OCEAN_URL = "http://169.254.169.254/metadata/v1.json";
+    public static final String GOOGLE_COMPUTE_METADATA = "http://metadata.google.internal";
 
     protected final ClassPathResourceLoader resourceLoader;
 
@@ -732,10 +736,7 @@ public class DefaultEnvironment extends PropertySourcePropertyResolver implement
     @SuppressWarnings("MagicNumber")
     private static boolean isGoogleCompute() {
         try {
-            URL url = new URL("http://metadata.google.internal");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setReadTimeout(500);
-            con.setConnectTimeout(500);
+            final HttpURLConnection con = instantiateHttpURLConnectionForEnvironmentCheck(GOOGLE_COMPUTE_METADATA);
             con.setRequestMethod("GET");
             con.setDoOutput(true);
             int responseCode = con.getResponseCode();
@@ -805,10 +806,17 @@ public class DefaultEnvironment extends PropertySourcePropertyResolver implement
         return false;
     }
 
+    private static HttpURLConnection instantiateHttpURLConnectionForEnvironmentCheck(String spec) throws IOException {
+        final URL url = new URL(spec);
+        final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setReadTimeout(DEFAULT_READ_TIMEOUT);
+        con.setConnectTimeout(DEFAULT_CONNECT_TIMEOUT);
+        return con;
+    }
+
     private static boolean isDigitalOcean() {
         try {
-            final URL url = new URL("http://169.254.169.254/metadata/v1.json");
-            final HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            final HttpURLConnection con = instantiateHttpURLConnectionForEnvironmentCheck(DIGITAL_OCEAN_URL);
             con.setRequestMethod("HEAD");
             int responseCode = con.getResponseCode();
             return responseCode == 200;
