789e712fbfb9158c3d234f61a6ee4905f72fad80
jameskleeh <james.kleeh@gmail.com>
2018-12-21 16:45:19 -0500

    Normalize view paths to no longer use file separator

--- views/src/main/java/io/micronaut/views/ViewsRenderer.java
+++ views/src/main/java/io/micronaut/views/ViewsRenderer.java
@@ -18,6 +18,7 @@ package io.micronaut.views;
 
 import io.micronaut.core.io.Writable;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.File;
 
@@ -31,8 +32,12 @@ public interface ViewsRenderer {
 
     /**
      * The file separator to use.
+     *
+     * @deprecated Use {@link File#separator} directly
      */
+    @Deprecated
     String FILE_SEPARATOR = File.separator;
+
     /**
      * The extension separator.
      */
@@ -50,4 +55,51 @@ public interface ViewsRenderer {
      * @return true if a template can be found for the supplied view name.
      */
     boolean exists(String viewName);
+
+    /**
+     * Returns a path with unix style folder
+     * separators that starts and ends with a "\".
+     *
+     * @param path The path to normalizeFile
+     * @return The normalized path
+     */
+    @Nonnull
+    default String normalizeFolder(@Nullable String path) {
+        if (path == null) {
+            path = "";
+        } else {
+            path = normalizeFile(path, null);
+        }
+        if (!path.endsWith("/")) {
+            path = path + "/";
+        }
+        return path;
+    }
+
+    /**
+     * Returns a path that is converted to unix style file separators
+     * and never starts with a "\". If an extension is provided and the
+     * path ends with the extension, the extension will be stripped.
+     * The extension parameter supports extensions that do and do not
+     * begin with a ".".
+     *
+     * @param path The path to normalizeFile
+     * @param extension The file extension
+     * @return The normalized path
+     */
+    @Nonnull
+    default String normalizeFile(@Nonnull String path, String extension) {
+        path = path.replace("\\", "/");
+        if (path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        if (extension != null && !extension.startsWith(".")) {
+            extension = "." + extension;
+            if (path.endsWith(extension)) {
+                int idx = path.indexOf(extension);
+                path = path.substring(0, idx);
+            }
+        }
+        return path;
+    }
 }
