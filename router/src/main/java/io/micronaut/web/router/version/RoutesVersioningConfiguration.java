6c879acf6738fa185b943fd0ea6ce43075d914bf
BogdanOros <orossbogdan@gmail.com>
2018-12-24 23:20:00 +0200

    Configuration for routes versioning

---
+++ router/src/main/java/io/micronaut/web/router/version/RoutesVersioningConfiguration.java
@@ -0,0 +1,165 @@
+package io.micronaut.web.router.version;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.core.util.Toggleable;
+
+import static io.micronaut.web.router.version.RoutesVersioningConfiguration.PREFIX;
+
+/**
+ * Routes versioning configuration.
+ *
+ * @author BogdanOros
+ */
+@ConfigurationProperties(PREFIX)
+@Requires(property = PREFIX + ".enabled", value = StringUtils.TRUE)
+public class RoutesVersioningConfiguration implements Toggleable {
+
+    /**
+     * The configuration property.
+     */
+    public static final String PREFIX = "micronaut.router.versioning";
+
+    /**
+     * The default enable value.
+     */
+    private static final boolean DEFAULT_ENABLED = false;
+
+    /**
+     * The enable value.
+     */
+    private boolean enabled = DEFAULT_ENABLED;
+
+    /**
+     * Request header based versioning configuration.
+     */
+    private HeaderBasedVersioningConfiguration header = new HeaderBasedVersioningConfiguration();
+
+    /**
+     * Request parameter based versioning configuration.
+     */
+    private ParameterBasedVersioningConfiguration parameter = new ParameterBasedVersioningConfiguration();
+
+    /**
+     * @return The header based versioning configuration.
+     */
+    public HeaderBasedVersioningConfiguration getHeader() {
+        return header;
+    }
+
+    /**
+     * @param header The header based versioning configuration.
+     */
+    public void setHeader(HeaderBasedVersioningConfiguration header) {
+        this.header = header;
+    }
+
+    /**
+     * @return The request parameter based versioning configuration.
+     */
+    public ParameterBasedVersioningConfiguration getParameter() {
+        return parameter;
+    }
+
+    /**
+     * @param parameter The request parameter based versioning configuration.
+     */
+    public void setParameter(ParameterBasedVersioningConfiguration parameter) {
+        this.parameter = parameter;
+    }
+
+    /**
+     * @param enabled Enables the version based route matches filtering.
+     */
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /**
+     * @return {@code true} if version based matches filtering is enabled.
+     */
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * Abstract specific configuration for different versioning configurations.
+     */
+    private abstract static class AbstractVersioningConfiguration implements Toggleable {
+
+        private boolean enabled;
+        private String name;
+
+        AbstractVersioningConfiguration(String name) {
+            this.name = name;
+        }
+
+        /**
+         * @return the property name to extract specified version from.
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * @param name The property name to extract specified version from.
+         */
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        /**
+         * @return {@code true} if specific version based matches filtering is enabled.
+         */
+        @Override
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        /**
+         * @param enabled Enables the specific version based route matches filtering.
+         */
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+    }
+
+    /**
+     * The request header based versioning configuration.
+     */
+    @ConfigurationProperties("header")
+    public static class HeaderBasedVersioningConfiguration extends AbstractVersioningConfiguration {
+
+        public static final String PREFIX = RoutesVersioningConfiguration.PREFIX + ".header";
+
+        /**
+         * Constructor for header based versioning configuration.
+         * Specifies the default name of the request header.
+         */
+        public HeaderBasedVersioningConfiguration() {
+            super("X-API-VERSION");
+        }
+
+    }
+
+    /**
+     * The request parameter based versioning configuration.
+     */
+    @ConfigurationProperties("parameter")
+    public static class ParameterBasedVersioningConfiguration extends AbstractVersioningConfiguration {
+
+        public static final String PREFIX = RoutesVersioningConfiguration.PREFIX + ".parameter";
+
+        /**
+         * Constructor for request parameter based versioning configuration.
+         * Specifies the default name of the request parameter.
+         */
+        public ParameterBasedVersioningConfiguration() {
+            super("api-version");
+        }
+
+    }
+
+}
