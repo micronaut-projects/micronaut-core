1e6fe4a41f769e1c132066c2b10ae2e485400609
Graeme Rocher <graeme.rocher@gmail.com>
2019-01-03 10:02:36 +0100

    Ensure EC2 instance metadata only contains strings. Fixes #1073

--- runtime/src/main/java/io/micronaut/discovery/cloud/aws/AmazonComputeInstanceMetadataResolver.java
+++ runtime/src/main/java/io/micronaut/discovery/cloud/aws/AmazonComputeInstanceMetadataResolver.java
@@ -37,6 +37,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.regex.Pattern;
@@ -135,7 +136,18 @@ public class AmazonComputeInstanceMetadataResolver implements ComputeInstanceMet
                 LOG.error("error getting public host name from:" + ec2InstanceMetadataURL + EC2MetadataKeys.publicHostname.getName(), e);
             }
 
-            ec2InstanceMetadata.metadata = objectMapper.convertValue(ec2InstanceMetadata, Map.class);
+            Map<?, ?> metadata = objectMapper.convertValue(ec2InstanceMetadata, Map.class);
+            if (metadata != null) {
+                Map<String, String> finalMetadata = new HashMap<>(metadata.size());
+                for (Map.Entry<?, ?> entry : metadata.entrySet()) {
+                    Object key = entry.getKey();
+                    Object value = entry.getValue();
+                    if (value instanceof String) {
+                        finalMetadata.put(key.toString(), value.toString());
+                    }
+                }
+                ec2InstanceMetadata.metadata = finalMetadata;
+            }
             if (LOG.isDebugEnabled()) {
                 LOG.debug("EC2 Metadata found:" + ec2InstanceMetadata.metadata.toString());
             }
