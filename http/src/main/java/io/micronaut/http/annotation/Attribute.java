78943f8a886d5a6222e8e57b9daf8c2a9d58e364
Ahmed.Lafta <Aalafta@gmail.com>
2019-01-07 00:07:34 -0600

    implement client attribute annotation

---
+++ http/src/main/java/io/micronaut/http/annotation/Attribute.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.micronaut.http.annotation;
+
+import io.micronaut.context.annotation.AliasFor;
+import io.micronaut.core.bind.annotation.Bindable;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * <p>An annotation that can be applied to method arguments to indicate that the method argument is bound to an HTTP request attribute
+ *   This can also be used in conjuction with &#064;Attributes to list attributes on a client class tht will always be applied
+ *   <i>Note: Request attributes are intended for internal data sharing only, and are not attached to the outbound request.</i></p>
+ * <p></p>
+ * <p>The following example demonstrates usage at the type level to declare default values to pass in the request when using the {@code Client} annotation;</p>
+ * <p></p>
+ *
+ * <pre class="code">
+ * &#064;Attribute(name="X-Username",value='Freddy'),
+ * &#064;Attribute(name="X-MyParam",value='${foo.bar}')
+ * &#064;Client('/users')
+ * interface UserClient {
+ *
+ * }
+ * </pre>
+ *
+ * <p>When declared as a binding annotation the <code>&#064;Attribute</code> annotation is declared on each parameter to be bound:</p>
+ *
+ * <pre class="code">
+ * &#064;Get('/user')
+ * User get(&#064;Attribute('X-Username') String username, &#064;Attribute('X-MyParam') String myparam) {
+ *    return new User(username, myparam);
+ * }
+ * </pre>
+ *
+ * @author Ahmed Lafta
+ * @since 1.0
+ */
+@Documented
+@Retention(RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.TYPE, ElementType.METHOD}) // this can be either type or param
+@Repeatable(value = Attributes.class)
+@Bindable
+public @interface Attribute {
+
+    /**
+     * If used as a bound parameter, this is the attribute name. If used on a class level this is value and not the attribute name.
+     * @return The name of the attribute, otherwise it is inferred from the parameter name
+     */
+    @AliasFor(annotation = Bindable.class, member = "value")
+    String value() default "";
+
+    /**
+     * If used on a class level with @Attributes this is the attribute name and value is the value.
+     * @return name of header when using with @Attributes
+     */
+    @AliasFor(annotation = Bindable.class, member = "value")
+    String name() default "";
+
+    /**
+     * @see Bindable#defaultValue()
+     * @return The default value
+     */
+    @AliasFor(annotation = Bindable.class, member = "defaultValue")
+    String defaultValue() default "";
+
+}
---
+++ http/src/main/java/io/micronaut/http/annotation/Attributes.java
@@ -0,0 +1,39 @@
+package io.micronaut.http.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This lets you declare several attributes for a client class and have them always included.
+ * Example usage:
+ * <pre><code>
+ * {@literal @}Attributes({
+ *     {@literal @}Attribute(name="api-key",value="my-key-value"),
+ *     {@literal @}Attribute(name="api-name",value="my-api-name")
+ * })
+ * </pre></code>
+ *
+ * @author Ahmed Lafta
+ * @since 1.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Attributes {
+
+    /**
+     * This allows you to have multiple @Attribute's set at the class level with Java and Groovy.
+     * Example usage:
+     * <pre><code>
+     * {@literal @}Attributes({
+     *     {@literal @}Attribute(name="api-key",value="my-key-value"),
+     *     {@literal @}Attribute(name="api-name",value="my-api-name")
+     * })
+     * </pre></code>
+     * @return The attributes
+     */
+    Attribute[] value() default {};
+}
