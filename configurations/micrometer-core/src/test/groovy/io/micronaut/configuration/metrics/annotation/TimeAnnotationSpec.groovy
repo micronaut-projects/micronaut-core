69c6d36e4a50636558fb673659128d9076160c3e
graemerocher <graeme.rocher@gmail.com>
2019-01-07 14:42:04 +0100

    Micrometer: Support for @Timed. Fixes #1088

--- configurations/micrometer-core/src/test/groovy/io/micronaut/configuration/metrics/annotation/TimeAnnotationSpec.groovy
+++ configurations/micrometer-core/src/test/groovy/io/micronaut/configuration/metrics/annotation/TimeAnnotationSpec.groovy
@@ -3,11 +3,15 @@ package io.micronaut.configuration.metrics.annotation
 import io.micrometer.core.annotation.Timed
 import io.micrometer.core.instrument.MeterRegistry
 import io.micronaut.context.ApplicationContext
+import io.reactivex.Flowable
 import io.reactivex.Single
+import io.reactivex.functions.Consumer
 import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
 
 import javax.inject.Singleton
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
 
 class TimeAnnotationSpec extends Specification {
 
@@ -19,10 +23,51 @@ class TimeAnnotationSpec extends Specification {
 
         when:
         def result = tt.max(4, 10)
+        def timer = registry.get("timed.test.max.blocking").timer()
 
         then:
         result == 10
-        registry.get("max.blocking")
+        timer.count() == 1
+        timer.totalTime(TimeUnit.MILLISECONDS) > 0
+
+        when:
+        result = tt.maxFuture(4, 10).get()
+        PollingConditions conditions = new PollingConditions()
+
+
+        then:
+        conditions.eventually {
+            def t = registry.get("timed.test.max.future").timer()
+            result == 10
+            t.count() == 1
+            t.totalTime(TimeUnit.MILLISECONDS) > 0
+
+        }
+        timer.count() == 1
+
+        when:
+        tt.maxSingle(4, 10).subscribe( { o -> result = o} as Consumer)
+
+        then:
+        conditions.eventually {
+            def rxTimer = registry.get("timed.test.max.single").timer()
+
+            result == 10
+            rxTimer.count() == 1
+            rxTimer.totalTime(TimeUnit.MILLISECONDS) > 0
+        }
+
+        when:
+        tt.maxFlow(4, 10).toList().subscribe( { o -> result = o[0]} as Consumer)
+
+        then:
+        conditions.eventually {
+            def rxTimer = registry.get("timed.test.max.flowable").timer()
+
+            result == 10
+            rxTimer.count() == 1
+            rxTimer.totalTime(TimeUnit.MILLISECONDS) > 0
+        }
 
 
         cleanup:
@@ -33,21 +78,34 @@ class TimeAnnotationSpec extends Specification {
     @Singleton
     static class TimedTarget {
 
-        @Timed("max.blocking")
+        @Timed("timed.test.max.blocking")
         Integer max(int a, int b) {
             return Math.max(a, b)
         }
 
-        @Timed(value = "max.future", description = "some desc", extraTags = ['one', 'two'])
+        @Timed("timed.test.max.blocking")
+        Integer error(int a, int b) {
+            throw new NumberFormatException("cannot")
+        }
+
+
+        @Timed(value = "timed.test.max.future", description = "some desc", extraTags = ['one', 'two'])
         CompletableFuture<Integer> maxFuture(int a, int b) {
             CompletableFuture.completedFuture(
                 Math.max(a, b)
             )
         }
 
-        @Timed(value = "max.rx", description = "some desc", extraTags = ['one', 'two'])
-        Single<Integer> maxRx(int a, int b) {
+        @Timed(value = "timed.test.max.single", description = "some desc", extraTags = ['one', 'two'])
+        Single<Integer> maxSingle(int a, int b) {
             Single.just(
+                    Math.max(a, b)
+            )
+        }
+
+        @Timed(value = "timed.test.max.flowable", description = "some desc", extraTags = ['one', 'two'])
+        Flowable<Integer> maxFlow(int a, int b) {
+            Flowable.just(
                     Math.max(a, b)
             )
         }
