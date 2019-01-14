a514554a9bc2ecae22337b1f6e3e2962d8ecabbc
Ahmed.Lafta <Aalafta@gmail.com>
2019-01-13 23:28:56 -0600

    fix tests

--- http-client/src/test/groovy/io/micronaut/http/client/aop/RequestAttributeSpec.groovy
+++ http-client/src/test/groovy/io/micronaut/http/client/aop/RequestAttributeSpec.groovy
@@ -17,11 +17,17 @@
 package io.micronaut.http.client.aop
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Filter
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.RequestAttribute
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.filter.HttpServerFilter
+import io.micronaut.http.filter.ServerFilterChain
 import io.micronaut.runtime.server.EmbeddedServer
+import org.reactivestreams.Publisher
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
@@ -39,36 +45,40 @@ class RequestAttributeSpec extends Specification {
     void "test send and receive request attribute"() {
         given:
         StoryClient storyClient = context.getBean(StoryClient)
-        Story story = storyClient.get("ABC123")
+        Story story = storyClient.get()
 
         expect:
         story.storyId == "ABC123"
         story.title == "The Hungry Caterpillar"
-
     }
 
     @Client('/request_attribute')
-    static interface StoryClient extends StoryApi {
-
+    static interface StoryClient {
+        @Get('/story')
+        Story get()
     }
 
     @Controller('/request_attribute')
-    static class StoryController implements StoryApi {
+    static class StoryController {
 
-        @Override
-        Story get(@RequestAttribute("story-id") Object storyId) {
+        @Get("/story")
+        Story get(@RequestAttribute("x-story-id") Object storyId) {
             return new Story(storyId: storyId, title: 'The Hungry Caterpillar')
         }
-    }
-
-    static interface StoryApi {
-
-        @Get('/story')
-        Story get(@RequestAttribute("story-id") Object storyId)
     }
 
     static class Story {
         String storyId
         String title
+    }
+
+    @Filter("/**")
+    static class ServerFilter implements HttpServerFilter {
+
+        @Override
+        Publisher<MutableHttpResponse<?>> doFilter(HttpRequest<?> request, ServerFilterChain chain) {
+            request.setAttribute("x-story-id", "ABC123")
+            return chain.proceed(request)
+        }
     }
 }
