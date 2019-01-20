a35f6af7a519818464d6dbc5eabe50caf91a5b73
Mark Mindenhall <mmindenhall@vendavo.com>
2019-01-20 13:18:45 -0700

    Checkstyle fixes (#1098)

--- http-server-netty/src/main/java/io/micronaut/http/server/netty/jackson/JsonViewMediaTypeCodecFactory.java
+++ http-server-netty/src/main/java/io/micronaut/http/server/netty/jackson/JsonViewMediaTypeCodecFactory.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.micronaut.http.server.netty.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.codec.CodecConfiguration;
-import io.micronaut.http.codec.MediaTypeCodec;
 import io.micronaut.jackson.JacksonConfiguration;
 import io.micronaut.jackson.codec.JsonMediaTypeCodec;
 import io.micronaut.runtime.ApplicationConfiguration;
@@ -15,7 +30,8 @@ import javax.inject.Singleton;
 import static io.micronaut.jackson.codec.JsonMediaTypeCodec.CONFIGURATION_QUALIFIER;
 
 /**
- * A factory to produce {@link MediaTypeCodec} for JSON and Jackson using a specified JsonView class.
+ * A factory to produce {@link io.micronaut.http.codec.MediaTypeCodec} for JSON and Jackson using a specified
+ * JsonView class.
  */
 @Requires(beans = JacksonConfiguration.class)
 @Requires(property = "jackson.json-view-enabled")
@@ -42,7 +58,7 @@ public class JsonViewMediaTypeCodecFactory {
     /**
      * Creates a {@link JsonMediaTypeCodec} for the view class (specified as the JsonView annotation value).
      * @param viewClass The view class
-     * @return
+     * @return The codec
      */
     public JsonMediaTypeCodec createJsonViewCodec(Class<?> viewClass) {
         ObjectMapper viewMapper = objectMapper.copy();
