9e24cc485ca67f94fbd7501727f6ee8641982d70
Mark Mindenhall <mmindenhall@vendavo.com>
2019-01-20 13:18:45 -0700

    Checkstyle fixes (#1098)

--- http-server-netty/src/main/java/io/micronaut/http/server/netty/jackson/JsonViewServerFilter.java
+++ http-server-netty/src/main/java/io/micronaut/http/server/netty/jackson/JsonViewServerFilter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2018 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.micronaut.http.server.netty.jackson;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -17,6 +33,9 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
+/**
+ * Jackson @JsonView filter.
+ */
 @Requires(beans = JacksonConfiguration.class)
 @Requires(property = "jackson.json-view-enabled")
 @Filter("/**")
@@ -25,6 +44,9 @@ public class JsonViewServerFilter implements HttpServerFilter {
 
     private JsonViewMediaTypeCodecFactory codecFactory;
 
+    /**
+     * @param codecFactory The factory to produce @JsonView enabled codecs
+     */
     public JsonViewServerFilter(JsonViewMediaTypeCodecFactory codecFactory) {
         this.codecFactory = codecFactory;
     }
