23c24bba0efc37aff00d8b2382a0a5af7f71e833
jameskleeh <james.kleeh@gmail.com>
2019-01-25 12:15:14 -0500

    Move RabbitMQ to https://github.com/micronaut-projects/micronaut-rabbitmq

--- configurations/rabbitmq/src/main/java/io/micronaut/configuration/rabbitmq/bind/RabbitArgumentBinder.java
+++
@@ -1,6 +0,0 @@
-package io.micronaut.configuration.rabbitmq.bind;
-
-import io.micronaut.core.bind.ArgumentBinder;
-
-public interface RabbitArgumentBinder<T> extends ArgumentBinder<T, RabbitMessageState> {
-}
