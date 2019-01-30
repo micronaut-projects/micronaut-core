a93e44a2ebccc60d3087cb216a680d8077bf6d53
graemerocher <graeme.rocher@gmail.com>
2019-01-30 09:59:31 +0100

    Stop using apply plugin

--- cli/src/main/groovy/io/micronaut/cli/io/support/GradleBuildTokens.groovy
+++ cli/src/main/groovy/io/micronaut/cli/io/support/GradleBuildTokens.groovy
@@ -74,7 +74,7 @@ class GradleBuildTokens extends BuildTokens {
             if (nameAndVersion.length == 2) {
                 "    id \"${nameAndVersion[0]}\" version \"${nameAndVersion[1]}\""
             } else {
-                "apply plugin:\"$name\""
+                "    id \"${name}\""
             }
         }
 
@@ -91,7 +91,7 @@ class GradleBuildTokens extends BuildTokens {
                 if (nameAndVersion.length == 2) {
                     "    id \"${nameAndVersion[0]}\" version \"${nameAndVersion[1]}\""
                 } else {
-                    "apply plugin:\"$name\""
+                    "    id \"${name}\""
                 }
             }
         }
