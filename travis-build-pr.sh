b877f9ff875afdfbec65a58d8c935b461480f788
Zachary Klein <zak@silver-chalice.com>
2019-02-19 14:56:19 -0600

    Set micronaut.cloud.platform in Gradle build

--- travis-build-pr.sh
+++ travis-build-pr.sh
@@ -7,7 +7,7 @@ EXIT_STATUS=0
 ./gradlew check -x test-suite:test --no-daemon -Dmicronaut.cloud.platform=OTHER || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -eq 0 ]]; then
-    ./gradlew test-suite:test --no-daemon -Dmicronaut.cloud.platform=OTHER || EXIT_STATUS=$?
+    ./gradlew test-suite:test --no-daemon || EXIT_STATUS=$?
 fi
 
 exit $EXIT_STATUS
