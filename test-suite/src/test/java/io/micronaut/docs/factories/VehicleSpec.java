fb53e8b23a6c1773a1ee706528e3fd2d6f2d498f
Nathan Zender <github@nathanzender.com>
2019-03-08 23:38:16 -0500

    Convert factories.adoc over to multi language

---
+++ test-suite/src/test/java/io/micronaut/docs/factories/VehicleSpec.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.docs.factories;
+
+import io.micronaut.context.DefaultBeanContext;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class VehicleSpec {
+
+    @Test
+    public void testStartVehicle() {
+        // tag::start[]
+        Vehicle vehicle = new DefaultBeanContext()
+                .start()
+                .getBean(Vehicle.class);
+        System.out.println( vehicle.start() );
+        // end::start[]
+
+        assertEquals("Starting V8", vehicle.start());
+    }
+}
