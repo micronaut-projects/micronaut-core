f35321323a362495024398b378b4fc0f74491b4c
Silvio Wangler <silvio.wangler@gmail.com>
2019-03-12 14:41:23 +0100

    BeanDefinitionInjectProcessor can cooperate with other third party annotation processor (such as Mapstruct) (#1339)

--- inject-java/src/main/java/io/micronaut/annotation/processing/BeanDefinitionInjectProcessor.java
+++ inject-java/src/main/java/io/micronaut/annotation/processing/BeanDefinitionInjectProcessor.java
@@ -108,7 +108,6 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
     private JavaConfigurationMetadataBuilder metadataBuilder;
     private Map<String, AnnBeanElementVisitor> beanDefinitionWriters;
     private Set<String> processed = new HashSet<>();
-    private boolean executed = false;
 
     @Override
     public final synchronized void init(ProcessingEnvironment processingEnv) {
@@ -119,10 +118,6 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
 
     @Override
     public final boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        if (executed) {
-            return false;
-        }
-        executed = true;
 
         annotations = annotations
                 .stream()
@@ -191,13 +186,6 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
                     });
                 });
 
-                try {
-                    classWriterOutputVisitor.finish();
-                } catch (Exception e) {
-                    String message = e.getMessage();
-                    error("Error occurred writing META-INF files: %s", message != null ? message : e);
-                }
-
                 if (metadataBuilder.hasMetadata()) {
                     ServiceLoader<ConfigurationMetadataWriter> writers = ServiceLoader.load(ConfigurationMetadataWriter.class, getClass().getClassLoader());
 
@@ -213,12 +201,32 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
                         warning("Unable to load ConfigurationMetadataWriter due to : %s", e.getMessage());
                     }
                 }
-
                 AnnotationUtils.invalidateCache();
             }
         }
         AnnotationUtils.invalidateCache();
+
+        /*
+        Since the underlying Filer expects us to write only once into a file we need to make sure it happens in the last
+        processing round.
+        */
+        if (roundEnv.processingOver()) {
+            writeBeanDefinitionsToMetaInf();
+        }
+
         return false;
+    }
+
+    /**
+     * Writes {@link io.micronaut.inject.BeanDefinitionReference} into /META-INF/services/io.micronaut.inject.BeanDefinitionReference
+     */
+    private void writeBeanDefinitionsToMetaInf() {
+        try {
+            classWriterOutputVisitor.finish();
+        } catch (Exception e) {
+            String message = e.getMessage();
+            error("Error occurred writing META-INF files: %s", message != null ? message : e);
+        }
     }
 
     private void processBeanDefinitions(TypeElement beanClassElement, BeanDefinitionVisitor beanDefinitionWriter) {
