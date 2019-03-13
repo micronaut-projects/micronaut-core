7c8d9d8823f0055038d03c1d9b6183a63c0b3108
graemerocher <graeme.rocher@gmail.com>
2019-03-13 08:57:27 +0100

    Fix compilation error

--- test-suite-kotlin/src/test/kotlin/io/micronaut/inject/repeatable/RepeatableSpec.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/inject/repeatable/RepeatableSpec.kt
@@ -27,13 +27,13 @@ class RepeatableSpec: TestCase() {
     }
 
     fun testBeanIsNotAvailable2() {
-        val context = ApplicationContext.run(hashMapOf("foo" to "true") as Map<String, Any>?)
+        val context = ApplicationContext.run(hashMapOf("foo" to "true") as Map<String, Any>)
         TestCase.assertFalse(context.containsBean(MultipleRequires::class.java))
         context.close()
     }
 
     fun testBeanIsAvailable() {
-        val context = ApplicationContext.run(hashMapOf("foo" to "true", "bar" to "y") as Map<String, Any>?)
+        val context = ApplicationContext.run(hashMapOf("foo" to "true", "bar" to "y") as Map<String, Any>)
         TestCase.assertTrue(context.containsBean(MultipleRequires::class.java))
         context.close()
     }
