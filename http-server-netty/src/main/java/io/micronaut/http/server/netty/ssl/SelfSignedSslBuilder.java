d8d03e428146d890ab4a39c65cb4c65b829879c9
Nathan Zender <github@nathanzender.com>
2019-03-16 16:57:54 -0400

    Making SSL builder pluggable. (#1366)
    * Making SSL builder pluggable.
    
    Thought here is that we will be able to build a new let's encrypt/acme
    project that will define its own builder using the @Requires we will be
    able to enable its use as long as you have the new project dependency
    included in your project.
    
    * Clean up "no-op" ssl class to use Optional instead
    
    * License and javadoc on new class

---
+++ http-server-netty/src/main/java/io/micronaut/http/server/netty/ssl/SelfSignedSslBuilder.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.server.netty.ssl;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.annotation.Internal;
+import io.micronaut.core.io.ResourceResolver;
+import io.micronaut.http.ssl.ServerSslConfiguration;
+import io.micronaut.http.ssl.SslBuilder;
+import io.micronaut.http.ssl.SslConfiguration;
+import io.micronaut.http.ssl.SslConfigurationException;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+import javax.inject.Singleton;
+import javax.net.ssl.SSLException;
+import java.security.cert.CertificateException;
+import java.util.Optional;
+
+import static io.micronaut.core.util.StringUtils.FALSE;
+import static io.micronaut.core.util.StringUtils.TRUE;
+
+/**
+ * The Netty implementation of {@link SslBuilder} that generates an {@link SslContext} to create a server handler
+ * with to SSL support via a generated self signed certificate.
+ */
+@Requires(property = SslConfiguration.PREFIX + ".enabled", value = TRUE, defaultValue = FALSE)
+@Requires(property = SslConfiguration.PREFIX + ".build-self-signed", value = TRUE, defaultValue = FALSE)
+@Singleton
+@Internal
+public class SelfSignedSslBuilder extends SslBuilder<SslContext> implements ServerSslBuilder {
+
+    /**
+     * @param ssl              The SSL configuration
+     * @param resourceResolver The resource resolver
+     */
+    public SelfSignedSslBuilder(ServerSslConfiguration ssl, ResourceResolver resourceResolver) {
+        super(ssl, resourceResolver);
+    }
+
+    /**
+     * @return The SSL configuration
+     */
+    public ServerSslConfiguration getSslConfiguration() {
+        return (ServerSslConfiguration) ssl;
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Override
+    public Optional<SslContext> build() {
+        try {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            return Optional.of(SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build());
+        } catch (CertificateException | SSLException e) {
+            throw new SslConfigurationException("Encountered an error while building a self signed certificate", e);
+        }
+    }
+}
