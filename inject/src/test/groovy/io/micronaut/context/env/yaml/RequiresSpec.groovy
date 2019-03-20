44751dbdbeaf8040f249b647b2a9010b84fbd27b
Álvaro Sánchez-Mariscal <alvarosanchez@users.noreply.github.com>
2019-03-20 12:58:55 +0100

    Requires condition based on the presence of files on the file system (#1379)
    * Requires condition based on the presence of files on the file system
    
    * Using ResourceResolver to load both files and classpath resources
    
    * Ensure resource loader uses right classloaders

---
+++ inject/src/test/groovy/io/micronaut/context/env/yaml/RequiresSpec.groovy
@@ -0,0 +1,46 @@
+package io.micronaut.context.env.yaml
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Requires
+import spock.lang.Specification
+
+import javax.inject.Singleton
+
+class RequiresSpec extends Specification {
+
+    void "requirements for existing files are checked"() {
+        given:
+        File tmpFile = new File("/tmp/foo.txt")
+        tmpFile.text = "bar"
+        tmpFile.deleteOnExit()
+
+        when:
+        ApplicationContext ctx = ApplicationContext.run()
+
+        then:
+        ctx.containsBean(ShouldLoadBean)
+        ctx.containsBean(ShouldLoadAsWell)
+        ctx.containsBean(ShouldAlsoLoad)
+        !ctx.containsBean(ShouldNotLoad)
+        !ctx.containsBean(ShouldNotLoadEither)
+    }
+
+    @Requires(resources = "file:/tmp/foo.txt")
+    @Singleton
+    static class ShouldLoadBean {}
+
+    @Requires(resources = "classpath:logback.xml")
+    @Singleton
+    static class ShouldLoadAsWell{}
+
+    @Singleton
+    static class ShouldAlsoLoad {}
+
+    @Requires(resources = "file:/this/does/not/exist.txt")
+    @Singleton
+    static class ShouldNotLoad {}
+
+    @Requires(resources = "classpath:/this/does/not/exist.txt")
+    @Singleton
+    static class ShouldNotLoadEither {}
+}
