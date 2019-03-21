50afce4b21dce07bf5b802bb28207728f6452c13
graemerocher <graeme.rocher@gmail.com>
2019-03-21 12:43:05 +0100

    Progress implementing javax.validation.constraints

---
+++ validation/src/main/java/io/micronaut/validation/validator/constraints/PositiveNumberValidator.java
@@ -0,0 +1,23 @@
+package io.micronaut.validation.validator.constraints;
+
+import io.micronaut.core.annotation.AnnotationMetadata;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.inject.Singleton;
+import javax.validation.constraints.Positive;
+
+/**
+ * Validates a number of a positive value.
+ *
+ * @author graemerocher
+ * @since 1.2
+ */
+@Singleton
+public class PositiveNumberValidator implements ConstraintValidator<Positive, Number> {
+    @Override
+    public boolean isValid(@Nullable Number value, @Nonnull AnnotationMetadata annotationMetadata, @Nonnull ConstraintValidatorContext context) {
+        // null is allowed according to spec
+        return value == null || value.intValue() > 0;
+    }
+}
