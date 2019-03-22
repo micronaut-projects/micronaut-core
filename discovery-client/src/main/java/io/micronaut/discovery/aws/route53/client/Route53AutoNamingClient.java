d8107ba5b98e17048829af406febe4dc48cf5e48
jameskleeh <james.kleeh@gmail.com>
2019-03-22 16:03:40 -0400

    Fix import

--- discovery-client/src/main/java/io/micronaut/discovery/aws/route53/client/Route53AutoNamingClient.java
+++ discovery-client/src/main/java/io/micronaut/discovery/aws/route53/client/Route53AutoNamingClient.java
@@ -28,7 +28,6 @@ import io.micronaut.discovery.aws.route53.AWSServiceDiscoveryResolver;
 import io.micronaut.discovery.aws.route53.Route53ClientDiscoveryConfiguration;
 import io.micronaut.discovery.aws.route53.Route53DiscoveryConfiguration;
 import io.micronaut.discovery.aws.route53.registration.EC2ServiceInstance;
-import io.micronaut.http.client.annotation.Client;
 import io.reactivex.Flowable;
 import org.reactivestreams.Publisher;
 import javax.inject.Singleton;
