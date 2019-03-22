cb3fbd6a7f23a7fdba694757e1d1224e13885194
rvanderwerf <2273ad227ca21d893c8eab73ce4d0b38ce62572e>
2019-03-22 17:04:51 -0500

    discovery client health indicator enable/disable via config - add tests and default value notice in javadoc

---
+++ discovery-client/src/test/groovy/io/micronaut/discovery/management/DiscoveryClientHealthIndicatorConfigurationSpec.groovy
@@ -0,0 +1,51 @@
+package io.micronaut.discovery.management
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.management.health.indicator.discovery.DiscoveryClientHealthIndicator
+import io.micronaut.management.health.indicator.discovery.DiscoveryClientHealthIndicatorConfiguration
+import spock.lang.Specification
+
+/**
+ * Tests that the configuration for turning off DiscoveryClientHealthIndicatorConfiguration is working or not
+ */
+class DiscoveryClientHealthIndicatorConfigurationSpec extends Specification {
+
+    void 'test that the health indicator configuration is not available when disabled via config'() {
+        given:
+        ApplicationContext context = ApplicationContext.run(['discovery-client.indicator.enabled': false])
+
+        expect:
+        !context.containsBean(DiscoveryClientHealthIndicatorConfiguration)
+        !context.containsBean(DiscoveryClientHealthIndicator)
+
+        cleanup:
+        context.close()
+    }
+
+    void 'test that the health indicator configuration is available when enabled via config'() {
+        given:
+        ApplicationContext context = ApplicationContext.run(['endpoints.loggers.enabled': true])
+
+        expect:
+        context.containsBean(DiscoveryClientHealthIndicatorConfiguration)
+        context.containsBean(DiscoveryClientHealthIndicator)
+
+        cleanup:
+        context.close()
+    }
+
+
+    void 'test that the health indicator configuration is available when no entry is in config'() {
+        given:
+        ApplicationContext context = ApplicationContext.run()
+
+        expect:
+        context.containsBean(DiscoveryClientHealthIndicatorConfiguration)
+        context.containsBean(DiscoveryClientHealthIndicator)
+
+        cleanup:
+        context.close()
+    }
+
+
+}
