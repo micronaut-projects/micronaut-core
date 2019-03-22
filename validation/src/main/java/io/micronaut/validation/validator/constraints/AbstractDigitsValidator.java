7c1965e19ada91f6ccc5ef9d412ceefe28d8d6ce
graemerocher <graeme.rocher@gmail.com>
2019-03-22 09:26:24 +0100

    Add digits and pattern

---
+++ validation/src/main/java/io/micronaut/validation/validator/constraints/AbstractDigitsValidator.java
@@ -0,0 +1,60 @@
+package io.micronaut.validation.validator.constraints;
+
+import io.micronaut.core.annotation.AnnotationValue;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.validation.constraints.Digits;
+import java.math.BigDecimal;
+
+/**
+ * Abstract {@link Digits} validator implementation.
+ * @param <T> The target type
+ *
+ * @author graemerocher
+ * @since 1.2
+ */
+public abstract class AbstractDigitsValidator<T> implements ConstraintValidator<Digits, T> {
+    @Nonnull
+    @Override
+    public final Class<Digits> getAnnotationType() {
+        return Digits.class;
+    }
+
+    @Override
+    public final boolean isValid(@Nullable T value, @Nonnull AnnotationValue<Digits> annotationMetadata, @Nonnull ConstraintValidatorContext context) {
+        if (value == null) {
+            // null valid according to spec
+            return true;
+        }
+        final int intMax = annotationMetadata.get("integer", int.class).orElse(0);
+        final int fracMax = annotationMetadata.get("fraction", int.class).orElse(0);
+
+        if (intMax < 0) {
+            throw new IllegalArgumentException("The length of the integer part cannot be negative.");
+        }
+
+        if (fracMax < 0) {
+            throw new IllegalArgumentException("The length of the fraction part cannot be negative.");
+        }
+
+        BigDecimal bigDecimal;
+        try {
+            bigDecimal = getBigDecimal(value);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        int intLen = bigDecimal.precision() - bigDecimal.scale();
+        int fracLen = bigDecimal.scale() < 0 ? 0 : bigDecimal.scale();
+
+        return intMax >= intLen && fracMax >= fracLen;
+    }
+
+    /**
+     * Resolve a big decimal for the given value.
+     * @param value The value
+     * @return The big decimal
+     */
+    protected abstract BigDecimal getBigDecimal(@Nonnull T value);
+}
