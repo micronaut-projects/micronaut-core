f36339df3dfd029b197061d8fa4a9923c0f39ceb
Sergio del Amo <sergio.delamo@softamo.com>
2019-03-25 12:19:31 +0100

    document JWKS support
    Close: https://github.com/micronaut-projects/micronaut-core/issues/1425

---
+++ security-jwt/src/test/groovy/io/micronaut/docs/jwks/JwksSpec.groovy
@@ -0,0 +1,7 @@
+package io.micronaut.docs.jwks;
+
+import io.micronaut.testutils.YamlAsciidocTagCleaner;
+import spock.lang.Specification;
+
+public class JwksSpec extends Specification implements YamlAsciidocTagCleaner {
+}
