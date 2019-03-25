d44ca877741b199ec740774e87c57459515b332f
graemerocher <graeme.rocher@gmail.com>
2019-03-25 09:12:31 +0100

    Cascade to array values support

--- validation/src/main/java/io/micronaut/validation/validator/constraints/DefaultConstraintValidatorRegistry.java
+++ validation/src/main/java/io/micronaut/validation/validator/constraints/DefaultConstraintValidatorRegistry.java
@@ -6,11 +6,14 @@ import io.micronaut.context.annotation.Primary;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.reflect.ReflectionUtils;
 import io.micronaut.core.util.ArgumentUtils;
+import io.micronaut.core.util.clhm.ConcurrentLinkedHashMap;
 import io.micronaut.inject.qualifiers.Qualifiers;
 
 import javax.annotation.Nonnull;
 import javax.inject.Singleton;
 import java.lang.annotation.Annotation;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -24,6 +27,7 @@ import java.util.Optional;
 @Primary
 public class DefaultConstraintValidatorRegistry implements ConstraintValidatorRegistry {
 
+    private final Map<Key, ConstraintValidator> validatorMap = new ConcurrentLinkedHashMap.Builder<Key, ConstraintValidator>().initialCapacity(10).maximumWeightedCapacity(40).build();
     private final BeanContext beanContext;
 
     /**
@@ -41,13 +45,60 @@ public class DefaultConstraintValidatorRegistry implements ConstraintValidatorRe
     public <A extends Annotation, T> Optional<ConstraintValidator<A, T>> findConstraintValidator(@Nonnull Class<A> constraintType, @Nonnull Class<T> targetType) {
         ArgumentUtils.requireNonNull("constraintType", constraintType);
         ArgumentUtils.requireNonNull("targetType", targetType);
-        final Qualifier<ConstraintValidator> qualifier = Qualifiers.byTypeArguments(
-                constraintType,
-                ReflectionUtils.getWrapperType(targetType)
-        );
-        final Optional bean = beanContext
-                .findBean(ConstraintValidator.class, qualifier);
-        return (Optional<ConstraintValidator<A, T>>) bean;
+        final Key key = new Key(constraintType, targetType);
+        final ConstraintValidator constraintValidator = validatorMap.get(key);
+        if (constraintValidator != null) {
+            if (constraintValidator != ConstraintValidator.VALID) {
+                return Optional.of(constraintValidator);
+            }
+            return Optional.empty();
+        } else {
+
+            final Qualifier<ConstraintValidator> qualifier = Qualifiers.byTypeArguments(
+                    constraintType,
+                    ReflectionUtils.getWrapperType(targetType)
+            );
+            final ConstraintValidator cv = beanContext
+                    .findBean(ConstraintValidator.class, qualifier).orElse(ConstraintValidator.VALID);
+            validatorMap.put(key, cv);
+            if (cv != ConstraintValidator.VALID) {
+                return Optional.of(cv);
+            }
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Key for caching validators.
+     * @param <A> The annotation type
+     * @param <T> The target type.
+     */
+    private final class Key<A extends Annotation, T> {
+        final Class<A> constraintType;
+        final Class<T> targetType;
+
+        Key(@Nonnull Class<A> constraintType, @Nonnull Class<T> targetType) {
+            this.constraintType = constraintType;
+            this.targetType = targetType;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Key<?, ?> key = (Key<?, ?>) o;
+            return constraintType.equals(key.constraintType) &&
+                    targetType.equals(key.targetType);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(constraintType, targetType);
+        }
     }
 
 }
