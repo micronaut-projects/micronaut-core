96758a93889c4ab6023c97cc96ee93afd28529c1
graemerocher <graeme.rocher@gmail.com>
2019-03-27 17:02:19 +0100

    Progress fixes for cascading validation

--- validation/src/main/java/io/micronaut/validation/validator/constraints/ConstraintValidatorContext.java
+++ validation/src/main/java/io/micronaut/validation/validator/constraints/ConstraintValidatorContext.java
@@ -17,7 +17,6 @@ package io.micronaut.validation.validator.constraints;
 
 import javax.annotation.Nonnull;
 import javax.validation.ClockProvider;
-import java.time.Clock;
 
 /**
  * Subset of the {@link javax.validation.ConstraintValidatorContext} interface without the unnecessary parts.
@@ -28,13 +27,13 @@ import java.time.Clock;
 public interface ConstraintValidatorContext {
 
     /**
-     * Returns the provider for obtaining the current time in the form of a {@link Clock},
+     * Returns the provider for obtaining the current time in the form of a {@link java.time.Clock},
      * e.g. when validating the {@code Future} and {@code Past} constraints.
      *
      * @return the provider for obtaining the current time, never {@code null}. If no
      * specific provider has been configured during bootstrap, a default implementation using
      * the current system time and the current default time zone as returned by
-     * {@link Clock#systemDefaultZone()} will be returned.
+     * {@link java.time.Clock#systemDefaultZone()} will be returned.
      *
      * @since 2.0
      */
