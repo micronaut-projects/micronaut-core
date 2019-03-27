7eb15cfa8b919dca229db5fd250a3f0ddbfa6d78
graemerocher <graeme.rocher@gmail.com>
2019-03-27 10:27:36 +0100

    Make run public, otherwise kotlin can't use it

--- function/src/main/java/io/micronaut/function/executor/FunctionInitializer.java
+++ function/src/main/java/io/micronaut/function/executor/FunctionInitializer.java
@@ -86,7 +86,7 @@ public class FunctionInitializer extends AbstractExecutor implements Closeable, 
      * @param supplier The function that executes this function
      * @throws IOException If an error occurs
      */
-    protected void run(String[] args, Function<ParseContext, ?> supplier) throws IOException {
+    public void run(String[] args, Function<ParseContext, ?> supplier) throws IOException {
         ApplicationContext applicationContext = this.applicationContext;
         this.functionExitHandler = applicationContext.findBean(FunctionExitHandler.class).orElse(this.functionExitHandler);
         ParseContext context = new ParseContext(args);
