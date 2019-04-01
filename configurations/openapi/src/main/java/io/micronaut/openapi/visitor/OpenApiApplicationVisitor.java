7e4441eb95494071d7cf551bd0ed29c0f5f775c8
Puneet Behl <behl4485@gmail.com>
2019-04-01 14:38:57 +0530

    #639 Ability to specify and merge additional Swagger files
    * System property to read additional Swagger YAML files from disk.
    * Updated OpenApiApplicationVisitor to parse and merge additional swagger files into the application `OpenAPI` object.

--- configurations/openapi/src/main/java/io/micronaut/openapi/visitor/OpenApiApplicationVisitor.java
+++ configurations/openapi/src/main/java/io/micronaut/openapi/visitor/OpenApiApplicationVisitor.java
@@ -32,12 +32,19 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Writer;
 import java.lang.annotation.Annotation;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -52,6 +59,10 @@ public class OpenApiApplicationVisitor extends AbstractOpenApiVisitor implements
      * System property that enables setting the target file to write to.
      */
     public static final String MICRONAUT_OPENAPI_TARGET_FILE = "micronaut.openapi.target.file";
+    /**
+     * System property that specifies the location of additional swagger YAML files to read from.
+     */
+    public static final String MICRONAUT_OPENAPI_ADDITIONAL_FILES = "micronaut.openapi.additional.files";
 
     private ClassElement classElement;
 
@@ -73,6 +84,7 @@ public class OpenApiApplicationVisitor extends AbstractOpenApiVisitor implements
             context.put(ATTR_OPENAPI, openAPI);
         }
 
+        mergeAdditionalSwaggerFiles(element, context, openAPI);
         // handle type level tags
         List<io.swagger.v3.oas.models.tags.Tag> tagList = processOpenApiAnnotation(
                 element,
@@ -111,6 +123,59 @@ public class OpenApiApplicationVisitor extends AbstractOpenApiVisitor implements
         }
 
         this.classElement = element;
+    }
+
+    /**
+     * Merge the OpenAPI YAML files into one single file.
+     *
+     * @param element The element
+     * @param context The visitor context
+     * @param openAPI The {@link OpenAPI} object for the application
+     */
+    private void mergeAdditionalSwaggerFiles(ClassElement element, VisitorContext context, OpenAPI openAPI) {
+        String additionalSwaggerFiles = System.getProperty(MICRONAUT_OPENAPI_ADDITIONAL_FILES);
+        if (StringUtils.isNotEmpty(additionalSwaggerFiles)) {
+            Path directory = Paths.get(additionalSwaggerFiles);
+            if (Files.isDirectory(directory)) {
+                context.info("Merging Swagger OpenAPI YAML files from location :" + additionalSwaggerFiles);
+                try {
+                    Files.newDirectoryStream(directory,
+                            path -> path.toString().endsWith(".yml"))
+                            .forEach(path -> {
+                                context.info("Reading Swagger OpenAPI YAML file " + path.getFileName());
+                                OpenAPI parsedOpenApi = null;
+                                try {
+                                    parsedOpenApi = yamlMapper.readValue(path.toFile(), OpenAPI.class);
+                                } catch (IOException e) {
+                                    context.warn("Unable to read file " + path.getFileName() + ": " + e.getMessage() , classElement);
+                                }
+                                if (parsedOpenApi != null) {
+                                    if (!parsedOpenApi.getOpenapi().equals(openAPI.getOpenapi())) {
+                                        context.warn("The OpenAPI version " + parsedOpenApi.getOpenapi() + " in the file doesn't match the OpenAPI version " + openAPI.getOpenapi() + " of application", element);
+                                        return;
+                                    }
+                                    Optional.ofNullable(parsedOpenApi.getServers()).ifPresent(servers -> servers.forEach(openAPI::addServersItem));
+                                    Optional.ofNullable(parsedOpenApi.getPaths()).ifPresent(paths -> paths.forEach(openAPI::path));
+                                    Optional.ofNullable(parsedOpenApi.getComponents()).ifPresent(components -> {
+                                        Map<String, Schema> schemas = components.getSchemas();
+                                        if (schemas != null && !schemas.isEmpty()) {
+                                            schemas.forEach(openAPI::schema);
+                                        }
+                                        Map<String, SecurityScheme> securitySchemes = components.getSecuritySchemes();
+                                        if (securitySchemes != null && !securitySchemes.isEmpty()) {
+                                            securitySchemes.forEach(openAPI::schemaRequirement);
+                                        }
+                                    });
+                                    Optional.ofNullable(parsedOpenApi.getSecurity()).ifPresent(securityRequirements -> securityRequirements.forEach(openAPI::addSecurityItem));
+                                    Optional.ofNullable(parsedOpenApi.getTags()).ifPresent(tags -> tags.forEach(openAPI::addTagsItem));
+                                    Optional.ofNullable(parsedOpenApi.getExtensions()).ifPresent(extensions -> extensions.forEach(openAPI::addExtension));
+                                }
+                            });
+                } catch (IOException e) {
+                    context.warn("Unable to read  file from "+ directory.getFileName() + ": " + e.getMessage() , classElement);
+                }
+            }
+        }
     }
 
     private <T, A extends Annotation> List<T> processOpenApiAnnotation(ClassElement element, VisitorContext context, Class<A> annotationType, Class<T> modelType, List<T> tagList) {
