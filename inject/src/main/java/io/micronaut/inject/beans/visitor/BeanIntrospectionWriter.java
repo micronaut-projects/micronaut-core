8ea6b724cf96697760fe2356cb5a67cca5f3b7a3
graemerocher <graeme.rocher@gmail.com>
2019-04-08 16:29:22 +0200

    Make most compiled metadata package private

--- inject/src/main/java/io/micronaut/inject/beans/visitor/BeanIntrospectionWriter.java
+++ inject/src/main/java/io/micronaut/inject/beans/visitor/BeanIntrospectionWriter.java
@@ -330,7 +330,7 @@ class BeanIntrospectionWriter extends AbstractAnnotationMetadataWriter {
         classWriterOutputVisitor.visitServiceDescriptor(BeanIntrospectionReference.class, referenceName);
 
         try (OutputStream referenceStream = classWriterOutputVisitor.visitClass(referenceName)) {
-            startFinalClass(referenceWriter, targetClassType.getInternalName(), superType);
+            startPublicFinalClass(referenceWriter, targetClassType.getInternalName(), superType);
             final ClassWriter classWriter = generateClassBytes(referenceWriter);
             referenceStream.write(classWriter.toByteArray());
         }
