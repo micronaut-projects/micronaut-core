f5a2963dabece69ad646b7f41117f394577de27b
graemerocher <graeme.rocher@gmail.com>
2019-04-15 17:47:26 +0200

    Use lateinit in kotlin example

--- test-suite-kotlin/src/test/kotlin/io/micronaut/docs/ioc/validation/PersonServiceSpec.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/ioc/validation/PersonServiceSpec.kt
@@ -13,12 +13,12 @@ import javax.validation.ConstraintViolationException
 class PersonServiceSpec {
 
     @Inject
-    var personService: PersonService? = null
+    lateinit var personService: PersonService
 
     @Test
     fun testThatNameIsValidated() {
         val exception = assertThrows(ConstraintViolationException::class.java
-        ) { personService!!.sayHello("") } // <1>
+        ) { personService.sayHello("") } // <1>
 
         assertEquals("sayHello.name: must not be blank", exception.message) // <2>
     }
