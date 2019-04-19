e835852b47753ee992747e235195adb7594cc39b
Will Buck <william.parker.buck@gmail.com>
2019-04-19 11:56:16 -0500

    fixes whitespace, removes Requires from imports tag

--- test-suite-groovy/src/test/groovy/io/micronaut/docs/config/builder/EngineConfig.groovy
+++ test-suite-groovy/src/test/groovy/io/micronaut/docs/config/builder/EngineConfig.groovy
@@ -18,6 +18,7 @@ package io.micronaut.docs.config.builder
 // tag::imports[]
 import io.micronaut.context.annotation.ConfigurationBuilder
 import io.micronaut.context.annotation.ConfigurationProperties
+
 // end::imports[]
 
 /**
@@ -28,17 +29,17 @@ import io.micronaut.context.annotation.ConfigurationProperties
 @ConfigurationProperties('my.engine') // <1>
 class EngineConfig {
 
-	@ConfigurationBuilder(prefixes = "with") // <2>
-	EngineImpl.Builder builder = EngineImpl.builder()
+    @ConfigurationBuilder(prefixes = "with") // <2>
+    EngineImpl.Builder builder = EngineImpl.builder()
 
-	@ConfigurationBuilder(prefixes = "with", configurationPrefix = "crank-shaft") // <3>
-	CrankShaft.Builder crankShaft = CrankShaft.builder()
+    @ConfigurationBuilder(prefixes = "with", configurationPrefix = "crank-shaft") // <3>
+    CrankShaft.Builder crankShaft = CrankShaft.builder()
 
-	SparkPlug.Builder sparkPlug = SparkPlug.builder()
+    SparkPlug.Builder sparkPlug = SparkPlug.builder()
 
-	@ConfigurationBuilder(prefixes = "with", configurationPrefix = "spark-plug") // <4>
-	void setSparkPlug(SparkPlug.Builder sparkPlug) {
-		this.sparkPlug = sparkPlug
-	}
+    @ConfigurationBuilder(prefixes = "with", configurationPrefix = "spark-plug") // <4>
+    void setSparkPlug(SparkPlug.Builder sparkPlug) {
+        this.sparkPlug = sparkPlug
+    }
 }
 // end::class[]
