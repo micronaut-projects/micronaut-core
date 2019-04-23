1276c0ddaae536e66c4fc3f2bea061a6c2dcc165
Sergio del Amo <sergio.delamo@softamo.com>
2019-04-23 08:20:49 +0200

    Add {COMPLETION-CANDIDATES} (#1571)

--- cli/src/main/groovy/io/micronaut/cli/profile/commands/AbstractCreateCommand.groovy
+++ cli/src/main/groovy/io/micronaut/cli/profile/commands/AbstractCreateCommand.groovy
@@ -62,10 +62,10 @@ abstract class AbstractCreateCommand extends ArgumentCompletingCommand implement
     @Option(names = ['-i', '--inplace'], description = 'Create a service using the current directory')
     boolean inplace
 
-    @Option(names = ['-p', '--profile'], paramLabel = 'PROFILE', description = 'The profile to use', completionCandidates = ProfileCompletionCandidates)
+    @Option(names = ['-p', '--profile'], paramLabel = 'PROFILE', description = 'The profile to use. Possible values: ${COMPLETION-CANDIDATES}.', completionCandidates = ProfileCompletionCandidates)
     String profile
 
-    @Option(names = ['-f', '--features'], paramLabel = 'FEATURE', split = ",", description = 'The features to use', completionCandidates = FeatureCompletionCandidates)
+    @Option(names = ['-f', '--features'], paramLabel = 'FEATURE', split = ",", description = 'The features to use. Possible values: ${COMPLETION-CANDIDATES}', completionCandidates = FeatureCompletionCandidates)
     List<String> features = []
 
     @Mixin
