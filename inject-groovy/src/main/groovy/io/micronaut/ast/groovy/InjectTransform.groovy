c0358295cdfb01dab2859623ffb525a6a6d55c2f
graemerocher <graeme.rocher@gmail.com>
2019-05-03 11:43:26 +0200

    Fix incorrect alignment of generic types

--- inject-groovy/src/main/groovy/io/micronaut/ast/groovy/InjectTransform.groovy
+++ inject-groovy/src/main/groovy/io/micronaut/ast/groovy/InjectTransform.groovy
@@ -1096,15 +1096,15 @@ class InjectTransform implements ASTTransformation, CompilationUnitAware {
             }
         }
 
-        Map<String, Object> resolveGenericTypes(Parameter parameter) {
+        Map<String, Object> resolveGenericTypes(Parameter parameter, Map<String, ClassNode> boundTypes = Collections.emptyMap()) {
             ClassNode parameterType = parameter.type
             GenericsType[] genericsTypes = parameterType.genericsTypes
             if (genericsTypes != null && genericsTypes.length > 0) {
-                AstGenericUtils.extractPlaceholders(parameterType)
+                return AstGenericUtils.buildGenericTypeInfo(parameterType, boundTypes)
             } else if (parameterType.isArray()) {
                 Map<String, Object> genericTypeList = [:]
-                genericTypeList.put('E', AstGenericUtils.resolveTypeReference(parameterType.componentType))
-                genericTypeList
+                genericTypeList.put('E', AstGenericUtils.resolveTypeReference(parameterType.componentType, boundTypes))
+                return genericTypeList
             }
         }
 
@@ -1609,7 +1609,7 @@ class InjectTransform implements ASTTransformation, CompilationUnitAware {
 
                 anntationMetadata.put(parameterName, AstAnnotationUtils.getAnnotationMetadata(sourceUnit, new ExtendedParameter(methodNode, param)))
 
-                genericTypeMap.put(parameterName, resolveGenericTypes(param))
+                genericTypeMap.put(parameterName, resolveGenericTypes(param, boundTypes))
             }
         }
 
