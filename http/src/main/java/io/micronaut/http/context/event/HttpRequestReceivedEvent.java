ebd2b920c7ece9ac8026a27aba8fd6adc8308f78
Graeme Rocher <graeme.rocher@gmail.com>
2019-05-16 18:48:16 +0200

    Replacement PR: Added custom @Request scope (PR for #1578) (#1676)
    * Initial commit @Request scope beans
    
    * Added test for @Request bean
    
    * Refactoring @Request scoped bean
    
    * Updated test to check if requestScope map is cleared
    
    * Updated @since tag to 1.2.0
    
    * Renamed @Request to @RequestScope
    
    * Renamed @Request to @RequestScope
    
    * Use request attributes for RequestScope
    
    * Fix failing test
    
    * Using polling conditions
    
    * Fix versions / make getRequestScopedBeans synchronized

---
+++ http/src/main/java/io/micronaut/http/context/event/HttpRequestReceivedEvent.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.micronaut.http.context.event;
+
+import io.micronaut.context.event.ApplicationEvent;
+import io.micronaut.http.HttpRequest;
+
+import javax.annotation.Nonnull;
+
+/**
+ * An event fired when an {@link HttpRequest} is received by the server. Not that the event is fired in a
+ * non-blocking manner and access to the request body is not provided. Consumers can use this event to
+ * trace the URI, headers and so on but should not perform I/O.
+ *
+ * @author graemerocher
+ * @since 1.2.0
+ */
+public class HttpRequestReceivedEvent extends ApplicationEvent {
+
+    /**
+     * @param request The request. Never null.
+     */
+    public HttpRequestReceivedEvent(@Nonnull HttpRequest<?> request) {
+        super(request);
+    }
+
+    @Override
+    @Nonnull
+    public HttpRequest<?> getSource() {
+        return (HttpRequest<?>) super.getSource();
+    }
+}
---
+++ http/src/main/java/io/micronaut/http/context/event/HttpRequestTerminatedEvent.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.context.event;
+
+import io.micronaut.context.event.ApplicationEvent;
+import io.micronaut.http.HttpRequest;
+
+import javax.annotation.Nonnull;
+
+/**
+ * An event fired when an {@link HttpRequest} is finalized by the server. Note that the event is fired asynchronously and
+ * consumers of the event should generally not perform I/O, instead this designed for tracing of headers, URI etc.
+ *
+ * @author graemerocher
+ * @since 1.2.0
+ */
+public class HttpRequestTerminatedEvent extends ApplicationEvent {
+
+    /**
+     * @param request The request. Never null.
+     */
+    public HttpRequestTerminatedEvent(@Nonnull HttpRequest<?> request) {
+        super(request);
+    }
+
+    @Override
+    @Nonnull
+    public HttpRequest<?> getSource() {
+        return (HttpRequest<?>) super.getSource();
+    }
+}
