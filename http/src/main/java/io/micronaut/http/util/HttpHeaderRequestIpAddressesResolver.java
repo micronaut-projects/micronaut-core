a1854117ffaf575755d82b2e58b733195e345a8d
Sergio del Amo <sergio.delamo@softamo.com>
2019-05-16 16:59:16 +0200

    Originating IP Address resolution (#1672)
    * Originating IP Address resolution
    
    Close: https://github.com/micronaut-projects/micronaut-core/issues/1671
    
    * Set version to 1.2.0
    
    * Use default value in javadoc
    
    * Document originating IP Address
    
    * Document originating IP Address resolver
    
    * Rename to RequestIpAddressesResolver
    
    Also rename aggregator to CompositeRequestIpAddressResolver and annotate it with @Primary
    
    * Move to util

---
+++ http/src/main/java/io/micronaut/http/util/HttpHeaderRequestIpAddressesResolver.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.util;
+
+import io.micronaut.http.HttpRequest;
+
+import javax.annotation.Nonnull;
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Resolves the originating IP Addresses using the value of an HTTP Header.
+ * @see <a href="https://en.wikipedia.org/wiki/X-Forwarded-For">X-Forwarded-For</a>
+ * @author Sergio del Amo
+ * @since 1.2.0
+ */
+@Singleton
+public class HttpHeaderRequestIpAddressesResolver implements RequestIpAddressesResolver {
+
+    private final HttpHeaderRequestIpAddressesResolverConfiguration configuration;
+
+    /**
+     *
+     * @param configuration Configuration
+     */
+    public HttpHeaderRequestIpAddressesResolver(HttpHeaderRequestIpAddressesResolverConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    @Nonnull
+    public List<String> requestIpAddresses(HttpRequest<?> request) {
+        String value = request.getHeaders().get(configuration.getHeaderName());
+        if (value == null) {
+            return new ArrayList<>();
+        }
+
+        String[] arr = value.split(configuration.getDelimiter());
+
+        return Stream.of(arr)
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+}
