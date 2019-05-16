9a9ae931ec08a9ad5736d852e9b2fda40cd2d3f6
Sergio del Amo <sergio.delamo@softamo.com>
2019-05-16 16:59:16 +0200

    Originating IP Address resolution (#1672)
    * Originating IP Address resolution
    
    Close: https://github.com/micronaut-projects/micronaut-core/issues/1671
    
    * Set version to 1.2.0
    
    * Use default value in javadoc
    
    * Document originating IP Address
    
    * Document originating IP Address resolver
    
    * Rename to RequestIpAddressesResolver
    
    Also rename aggregator to CompositeRequestIpAddressResolver and annotate it with @Primary
    
    * Move to util

---
+++ http/src/test/groovy/io/micronaut/http/util/RemoteAddressRequestIpAddressesResolverSpec.groovy
@@ -0,0 +1,37 @@
+package io.micronaut.http.util
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.util.RemoteAddressRequestIpAddressesResolver
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class RemoteAddressRequestIpAddressesResolverSpec extends Specification {
+    @Shared
+    @AutoCleanup
+    ApplicationContext applicationContext = ApplicationContext.run()
+
+    void "Bean RemoteAddressIpAddressesResolver exists"() {
+        expect:
+        applicationContext.containsBean(RemoteAddressRequestIpAddressesResolver)
+    }
+
+    void "can resolve a list of IPS from remoteAddress"() {
+        when:
+        RemoteAddressRequestIpAddressesResolver resolver = applicationContext.getBean(RemoteAddressRequestIpAddressesResolver)
+
+        then:
+        noExceptionThrown()
+
+        when:
+        InetAddress inetAddress = InetAddress.getByName("localhost")
+        def request = Stub(HttpRequest) {
+            getRemoteAddress() >> new InetSocketAddress(inetAddress, 8080)
+        }
+        List<String> result = resolver.requestIpAddresses(request)
+
+        then:
+        result == ['127.0.0.1']
+    }
+}
