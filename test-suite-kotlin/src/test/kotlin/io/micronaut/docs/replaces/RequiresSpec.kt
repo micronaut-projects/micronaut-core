095f9c423432b09895f41df6f13caf20b5304238
Will Buck <william.parker.buck@gmail.com>
2019-05-17 15:35:37 -0500

    #1265 adds kt examples for requires and replaces pkgs

---
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/replaces/RequiresSpec.kt
@@ -0,0 +1,17 @@
+package io.micronaut.docs.replaces
+
+import io.kotlintest.matchers.types.shouldBeInstanceOf
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.micronaut.context.ApplicationContext
+import io.micronaut.docs.requires.Book
+
+class RequiresSpec : StringSpec({
+
+    "test bean replaces" {
+        val applicationContext = ApplicationContext.run()
+        applicationContext.getBean(BookService::class.java).shouldBeInstanceOf<MockBookService>()
+        applicationContext.getBean(Book::class.java).title.shouldBe("An OK Novel")
+        applicationContext.getBean(TextBook::class.java).title.shouldBe("Learning 305")
+    }
+})
