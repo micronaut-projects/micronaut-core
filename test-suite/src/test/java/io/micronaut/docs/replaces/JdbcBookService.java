f4fd3b40a37f2bee729d6a6726fd567301ecbf80
Will Buck <william.parker.buck@gmail.com>
2019-05-17 13:47:24 -0500

    #1265 cleans up java examples

--- test-suite/src/test/java/io/micronaut/docs/replaces/JdbcBookService.java
+++ test-suite/src/test/java/io/micronaut/docs/replaces/JdbcBookService.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2017-2019 original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.micronaut.docs.replaces;
 
 import io.micronaut.context.annotation.Requires;
@@ -25,10 +10,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- * @author Graeme Rocher
- * @since 1.0
- */
 // tag::replaces[]
 @Singleton
 @Requires(beans = DataSource.class)
