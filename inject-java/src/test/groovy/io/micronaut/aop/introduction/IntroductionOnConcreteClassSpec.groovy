b1f03b87e3b69d941b636f922bc7896be719b673
graemerocher <graeme.rocher@gmail.com>
2019-06-04 14:01:35 +0200

    Add a test for #1700

---
+++ inject-java/src/test/groovy/io/micronaut/aop/introduction/IntroductionOnConcreteClassSpec.groovy
@@ -0,0 +1,29 @@
+package io.micronaut.aop.introduction
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.event.ApplicationEventListener
+import io.micronaut.context.event.StartupEvent
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class IntroductionOnConcreteClassSpec extends Specification {
+
+    @Shared @AutoCleanup ApplicationContext applicationContext = ApplicationContext.run()
+
+    void "test introduction of new interface on concrete class"() {
+        when:
+        ConcreteClass cc = applicationContext.getBean(ConcreteClass)
+
+        then:
+        cc instanceof ApplicationEventListener
+
+
+        when:
+        def event = new StartupEvent(applicationContext)
+        cc.onApplicationEvent(event)
+
+        then:
+        applicationContext.getBean(ListenerAdviceInterceptor).recievedMessages.contains(event)
+    }
+}
