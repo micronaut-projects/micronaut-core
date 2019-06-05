4d980f35d4b8ae74f64a966daba60d4d6f51da18
jameskleeh <james.kleeh@gmail.com>
2019-06-04 23:40:59 -0400

    Basic XML parsing is functional

--- http-server-netty/src/test/groovy/io/micronaut/http/server/netty/xml/XmlContentProcessorSpec.groovy
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/xml/XmlContentProcessorSpec.groovy
@@ -3,9 +3,11 @@ package io.micronaut.http.server.netty.xml
 import io.micronaut.context.ApplicationContext
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.RxStreamingHttpClient
 import io.micronaut.runtime.server.EmbeddedServer
 import io.reactivex.Flowable
 import spock.lang.AutoCleanup
@@ -29,17 +31,47 @@ class XmlContentProcessorSpec extends Specification {
         books[0].title == "First Book"
     }
 
+    void "test sending a list of books"() {
+        RxStreamingHttpClient client = embeddedServer.applicationContext.createBean(RxStreamingHttpClient, embeddedServer.getURL())
+
+        when:
+        List<Book> books = client.jsonStream(
+                HttpRequest.POST("/xml/stream/list", '<books><book><title>First Book</title></book><book><title>Second Book</title></book></books>')
+                        .contentType(MediaType.TEXT_XML_TYPE), Book.class).toList().blockingGet()
+
+        then:
+        books.size() == 2
+        books[0].title == "First Book"
+        books[1].title == "Second Book"
+    }
+
+    void "test streaming books"() {
+        RxStreamingHttpClient client = embeddedServer.applicationContext.createBean(RxStreamingHttpClient, embeddedServer.getURL())
+
+        when:
+        List<Book> books = client.jsonStream(
+                HttpRequest.POST("/xml/stream", '<books><book><title>First Book</title></book><book><title>Second Book</title></book></books>')
+                        .contentType(MediaType.TEXT_XML_TYPE), Book.class).toList().blockingGet()
+
+        then:
+        books.size() == 2
+        books[0].title == "First Book"
+        books[1].title == "Second Book"
+    }
+
     @Controller("/xml/stream")
     static class StreamController {
 
         @Post(consumes = MediaType.TEXT_XML)
-        Flowable<Book> stream(Flowable<Book> books) {
+        Flowable<Book> stream(@Body Flowable<Book> books) {
             return books
         }
 
         @Post(uri = "/list", consumes = MediaType.TEXT_XML)
-        Flowable<Book> streamList(Flowable<List<Book>> books) {
-            return books.flatMap({ bookList -> Flowable.fromIterable(bookList) })
+        Flowable<Book> streamList(@Body Flowable<List<Book>> books) {
+            return books.flatMap({ bookList ->
+                return Flowable.fromIterable(bookList)
+            })
         }
 
     }
