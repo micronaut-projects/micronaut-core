37b3b08858a0f1685ba52c65e2f14b57d6fe5762
Jeff Scott Brown <brownj@objectcomputing.com>
2019-06-12 15:35:13 -0500

    Fix typo in test class name

--- test-suite-kotlin/src/test/kotlin/io/micronaut/core/beans/RecursiveGenericsSpec.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/core/beans/RecursiveGenericsSpec.kt
@@ -3,7 +3,7 @@ package io.micronaut.core.beans
 import io.kotlintest.matchers.types.shouldNotBeNull
 import io.kotlintest.specs.StringSpec
 
-class RecusiveGenericsSpec : StringSpec({
+class RecursiveGenericsSpec : StringSpec({
     // issue https://github.com/micronaut-projects/micronaut-core/issues/1607
     "test recursive generics on bean introspection".config(enabled = false) {
         val introspection = BeanIntrospection.getIntrospection(Item::class.java)
