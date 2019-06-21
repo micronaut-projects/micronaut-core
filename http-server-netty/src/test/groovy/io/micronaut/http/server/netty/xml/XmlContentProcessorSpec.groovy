f1b7ea0cd226daeb94fdf5c34f6f65c0bf2eb3da
jameskleeh <james.kleeh@gmail.com>
2019-06-21 10:29:10 -0400

    Progress

--- http-server-netty/src/test/groovy/io/micronaut/http/server/netty/xml/XmlContentProcessorSpec.groovy
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/xml/XmlContentProcessorSpec.groovy
@@ -59,25 +59,64 @@ class XmlContentProcessorSpec extends Specification {
         books[1].title == "Second Book"
     }
 
-    @Controller("/xml/stream")
+    void "test sending a blocking author"() {
+        RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+
+        when:
+        Author author = client.toBlocking().retrieve(
+                HttpRequest.POST("/xml/stream/author", '<author name="Joe"><books><book><title>First Book</title></book></books></author>')
+                        .contentType(MediaType.TEXT_XML_TYPE), Author.class)
+
+        then:
+        author.books.size() == 1
+        author.books[0].title == "First Book"
+        author.name == "Joe"
+    }
+
+    void "test sending a blocking author with 2 books"() {
+        RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+
+        when:
+        Author author = client.toBlocking().retrieve(
+                HttpRequest.POST("/xml/stream/author", '<author name="Joe"><books><book><title>First Book</title></book><book><title>Second Book</title></book></books></author>')
+                        .contentType(MediaType.TEXT_XML_TYPE), Author.class)
+
+        then:
+        author.books.size() == 2
+        author.books[0].title == "First Book"
+        author.books[1].title == "Second Book"
+        author.name == "Joe"
+    }
+
+    @Controller(value = "/xml/stream", consumes = MediaType.TEXT_XML)
     static class StreamController {
 
-        @Post(consumes = MediaType.TEXT_XML)
+        @Post
         Flowable<Book> stream(@Body Flowable<Book> books) {
             return books
         }
 
-        @Post(uri = "/list", consumes = MediaType.TEXT_XML)
+        @Post("/list")
         Flowable<Book> streamList(@Body Flowable<List<Book>> books) {
             return books.flatMap({ bookList ->
                 return Flowable.fromIterable(bookList)
             })
         }
 
+        @Post("/author")
+        Author author(@Body Author author) {
+            return author
+        }
+
     }
 
     static class Book {
         String title
+    }
+
+    static class Author {
+        String name
+        List<Book> books
     }
 
 
