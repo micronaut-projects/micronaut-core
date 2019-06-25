211aa49d158e009bd275c2c98289feace2948507
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2019-06-25 14:19:23 +0200

    Implementing `/env/${propertySourceName}`

--- management/src/main/java/io/micronaut/management/endpoint/env/EnvEndpoint.java
+++ management/src/main/java/io/micronaut/management/endpoint/env/EnvEndpoint.java
@@ -4,6 +4,7 @@ import io.micronaut.context.env.Environment;
 import io.micronaut.context.env.PropertySource;
 import io.micronaut.management.endpoint.annotation.Endpoint;
 import io.micronaut.management.endpoint.annotation.Read;
+import io.micronaut.management.endpoint.annotation.Selector;
 
 import java.util.*;
 
@@ -18,7 +19,7 @@ public class EnvEndpoint {
 
     public final static String NAME = "env";
 
-    private Environment environment;
+    private final Environment environment;
 
     public EnvEndpoint(Environment environment) {
         this.environment = environment;
@@ -26,25 +27,40 @@ public class EnvEndpoint {
 
     @Read
     public Map<String, Object> getEnvironmentInfo() {
-        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> result = new LinkedHashMap<>();
         result.put("activeEnvironments", environment.getActiveNames());
         result.put("packages", environment.getPackages());
         Collection<Map<String, Object>> propertySources = new ArrayList<>();
         environment.getPropertySources()
                 .stream()
                 .sorted(Comparator.comparing(PropertySource::getOrder))
-                .forEach(ps -> {
-                    Map<String, Object> propertySource = new HashMap<>();
-                    propertySource.put("name", ps.getName());
-                    propertySource.put("order", ps.getOrder());
-                    Map<String, Object> properties = new HashMap<>();
-                    ps.forEach(k -> properties.put(k, ps.get(k)));
-                    propertySource.put("properties", properties);
-                    propertySources.add(propertySource);
-                });
+                .forEach(ps -> propertySources.add(buildPropertySourceInfo(ps)));
         result.put("propertySources", propertySources);
         return result;
     }
 
+    @Read
+    public Map<String, Object> getProperties(@Selector String propertySourceName) {
+        return environment.getPropertySources()
+                .stream()
+                .filter(ps -> ps.getName().equals(propertySourceName))
+                .findFirst()
+                .map(this::buildPropertySourceInfo)
+                .orElse(null);
+    }
 
+    private Map<String, Object> getAllProperties(PropertySource propertySource) {
+        Map<String, Object> properties = new LinkedHashMap<>();
+        propertySource.forEach(k -> properties.put(k, propertySource.get(k)));
+        return properties;
+    }
+
+    private Map<String, Object> buildPropertySourceInfo(PropertySource propertySource) {
+        Map<String, Object> propertySourceInfo = new LinkedHashMap<>();
+        propertySourceInfo.put("name", propertySource.getName());
+        propertySourceInfo.put("order", propertySource.getOrder());
+        propertySourceInfo.put("convention", propertySource.getConvention().name());
+        propertySourceInfo.put("properties", getAllProperties(propertySource));
+        return propertySourceInfo;
+    }
 }
--- management/src/test/groovy/io/micronaut/management/endpoint/env/EnvEndpointSpec.groovy
+++ management/src/test/groovy/io/micronaut/management/endpoint/env/EnvEndpointSpec.groovy
@@ -1,31 +1,90 @@
 package io.micronaut.management.endpoint.env
 
-import groovy.json.JsonOutput
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
 import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.Shared
 import spock.lang.Specification
 
 class EnvEndpointSpec extends Specification {
 
-    void "it return property sources"() {
+    @Shared
+    private EmbeddedServer embeddedServer
+
+    void "the env endpoint is sensitive by default"() {
         given:
-        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, ['endpoints.env.sensitive': false], Environment.TEST)
+        this.embeddedServer = ApplicationContext.run(EmbeddedServer, Environment.TEST)
         RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
 
         when:
+        client.exchange("/${EnvEndpoint.NAME}").blockingFirst()
+
+        then:
+        HttpClientResponseException responseException = thrown()
+        responseException.status.code == 401
+
+        cleanup:
+        doCleanup(client)
+    }
+
+    void "it returns all the environment information"() {
+        given:
+        RxHttpClient client = buildClient()
+
+        when:
         Map result = client.exchange("/${EnvEndpoint.NAME}", Map).blockingFirst().body()
-        println JsonOutput.prettyPrint(JsonOutput.toJson(result))
 
         then:
         result.activeEnvironments == ["test"]
         result.packages.contains("io.micronaut.management.endpoint.env")
         result.propertySources.size() == 3
-        result.propertySources.find {it.name == 'context'}.properties['endpoints.env.sensitive'] == false
+        result.propertySources.find {it.name == 'context'}.properties['foo.bar'] == 'baz'
 
         cleanup:
+        doCleanup(client)
+    }
+
+    void "it returns all the properties of a property source"() {
+        given:
+        RxHttpClient client = buildClient()
+
+        when:
+        Map result = client.exchange("/${EnvEndpoint.NAME}/context", Map).blockingFirst().body()
+
+        then:
+        result.order == 0
+        result.properties['foo.bar'] == 'baz'
+
+        cleanup:
+        doCleanup(client)
+    }
+
+    void "it returns not found if the property source doesn't exist"() {
+        given:
+        RxHttpClient client = buildClient()
+
+        when:
+        client.exchange("/${EnvEndpoint.NAME}/blah").blockingFirst()
+
+        then:
+        HttpClientResponseException responseException = thrown()
+        responseException.status.code == 404
+
+        cleanup:
+        doCleanup(client)
+    }
+
+    private RxHttpClient buildClient() {
+        this.embeddedServer = ApplicationContext.run(EmbeddedServer, ['endpoints.env.sensitive': false, 'foo.bar':'baz'], Environment.TEST)
+        return embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+    }
+
+    private void doCleanup(RxHttpClient client) {
         client.close()
         embeddedServer.close()
+        embeddedServer = null
     }
+
 }
