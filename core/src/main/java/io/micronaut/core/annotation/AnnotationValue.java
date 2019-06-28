0e63ad45825e0f9efc598c96a33f45b10981e724
graemerocher <graeme.rocher@gmail.com>
2019-06-28 11:16:56 +0200

    Allow a custom key

--- core/src/main/java/io/micronaut/core/annotation/AnnotationValue.java
+++ core/src/main/java/io/micronaut/core/annotation/AnnotationValue.java
@@ -147,12 +147,24 @@ public class AnnotationValue<A extends Annotation> implements AnnotationValueRes
      * @return The properties as a immutable map.
      */
     public @Nonnull Map<String, String> getProperties(@Nonnull String member) {
+        return getProperties(member, "name");
+    }
+
+    /**
+     * Resolve properties with a custom key member.
+     * @param member The member to resolve the properties from
+     * @param keyMember The member of the sub annotation that represents the key.
+     * @return The properties.
+     * @see #getProperties(String)
+     */
+    public Map<String, String> getProperties(@Nonnull String member, String keyMember) {
+        ArgumentUtils.requireNonNull("keyMember", keyMember);
         if (StringUtils.isNotEmpty(member)) {
             List<AnnotationValue<Annotation>> values = getAnnotations(member);
             if (CollectionUtils.isNotEmpty(values)) {
                 Map<String, String> props = new LinkedHashMap<>(values.size());
                 for (AnnotationValue<Annotation> av : values) {
-                    String name = av.contains("name") ? av.stringValue("name").orElse(null) : av.stringValue("key").orElse(null);
+                    String name = av.stringValue(keyMember).orElse(null);
                     if (StringUtils.isNotEmpty(name)) {
                         av.stringValue(AnnotationMetadata.VALUE_MEMBER, valueMapper).ifPresent(v -> props.put(name, v));
                     }
