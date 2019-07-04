4f0302af8b333cb80d58f697076a4aa4bf9d5ab4
Graeme Rocher <graeme.rocher@gmail.com>
2019-07-04 17:36:45 +0200

    Revert "Temporarily remove other build steps"
    This reverts commit b9f2a4e1f0d663ff44025cf575c756a3a0959977.

--- travis-build.sh
+++ travis-build.sh
@@ -33,6 +33,61 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
           ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
       fi
 
+      ./gradlew --console=plain --no-daemon docs  || EXIT_STATUS=$?
+
+      git clone https://${GH_TOKEN}@github.com/micronaut-projects/micronaut-docs.git -b gh-pages gh-pages --single-branch > /dev/null
+
+      cd gh-pages
+
+      # If this is the master branch then update the snapshot
+      if [[ $TRAVIS_BRANCH =~ ^master$ ]]; then
+         mkdir -p snapshot
+         cp -r ../build/docs/. ./snapshot/
+          git add snapshot/*
+      fi
+
+      # If there is a tag present then this becomes the latest
+      if [[ -n $TRAVIS_TAG ]]; then
+         mkdir -p latest
+         cp -r ../build/docs/. ./latest/
+         git add latest/*
+
+          version="$TRAVIS_TAG"
+          version=${version:1}
+          majorVersion=${version:0:4}
+          majorVersion="${majorVersion}x"
+
+          mkdir -p "$version"
+          cp -r ../build/docs/. "./$version/"
+          git add "$version/*"
+
+          mkdir -p "$majorVersion"
+          cp -r ../build/docs/. "./$majorVersion/"
+          git add "$majorVersion/*"
+
+      fi
+
+      git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" && {
+        git push origin HEAD || true
+      }
+      cd ..
+
+      rm -rf gh-pages
+
+      if [[ -n $TRAVIS_TAG ]]; then
+        echo "set released version in static website"
+        git clone https://${GH_TOKEN}@github.com/micronaut-projects/static-website.git -b master static-website-master --single-branch > /dev/null
+        cd static-website-master
+        version="$TRAVIS_TAG"
+        version=${version:1}
+        ./release.sh $version
+        git commit -a -m "Updating micronaut version at static website for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" && {
+          git push origin HEAD || true
+        }
+        cd ..
+        rm -rf static-website-master
+      fi
+
     fi
 fi
 
