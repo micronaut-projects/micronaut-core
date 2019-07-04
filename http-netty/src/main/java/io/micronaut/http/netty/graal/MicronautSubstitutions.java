815b610528d80cafde46f312058e77b3bbd31965
croudet <croudet@gmail.com>
2019-07-04 02:41:08 -0400

    update dependencies (#1802)
    * update dependencies
    
    * Upgrade to Netty 4.1.37 and fix Graal support
    
    * Disable checkstyle for FlowControlHandler (forked from Netty)

--- http-netty/src/main/java/io/micronaut/http/netty/graal/MicronautSubstitutions.java
+++ http-netty/src/main/java/io/micronaut/http/netty/graal/MicronautSubstitutions.java
@@ -18,10 +18,8 @@ package io.micronaut.http.netty.graal;
 
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
-import com.oracle.svm.core.annotate.RecomputeFieldValue.Kind;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
-
 import io.micronaut.core.annotation.Experimental;
 import io.micronaut.core.annotation.Internal;
 import io.netty.util.internal.logging.InternalLoggerFactory;
@@ -51,42 +49,11 @@ final class Target_io_netty_util_internal_logging_InternalLoggerFactory {
 }
 
 /**
- * Substitutions for Netty UnsafeRefArrayAccess.
+ * Substitutions for Caffeine UnsafeRefArrayAccess.
  */
 @TargetClass(className = "io.micronaut.caffeine.cache.UnsafeRefArrayAccess")
 final class Target_io_micronaut_caffeine_cache_UnsafeRefArrayAccess {
     @Alias @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.ArrayIndexShift, declClass = Object[].class)
-    public static int REF_ELEMENT_SHIFT;
-}
-
-/**
- * Substitutions for Netty PlatformDependent0.
- */
-@TargetClass(className = "io.netty.util.internal.PlatformDependent0")
-final class Target_io_netty_util_internal_PlatformDependent0 {
-    @Alias @RecomputeFieldValue(kind = Kind.FieldOffset, //
-            declClassName = "java.nio.Buffer", //
-            name = "address") //
-    private static long ADDRESS_FIELD_OFFSET;
-}
-
-/**
- * Substitutions for Netty CleanerJava6.
- */
-@TargetClass(className = "io.netty.util.internal.CleanerJava6")
-final class Target_io_netty_util_internal_CleanerJava6 {
-    @Alias @RecomputeFieldValue(kind = Kind.FieldOffset, //
-            declClassName = "java.nio.DirectByteBuffer", //
-            name = "cleaner") //
-    private static long CLEANER_FIELD_OFFSET;
-}
-
-/**
- * Substitutions for Netty UnsafeRefArrayAccess.
- */
-@TargetClass(className = "io.netty.util.internal.shaded.org.jctools.util.UnsafeRefArrayAccess")
-final class Target_io_netty_util_internal_shaded_org_jctools_util_UnsafeRefArrayAccess {
-    @Alias @RecomputeFieldValue(kind = Kind.ArrayIndexShift, declClass = Object[].class) //
     public static int REF_ELEMENT_SHIFT;
 }
 //CHECKSTYLE:ON
