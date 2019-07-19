e540819fa5c1bb69fff059d59eacd5a132e0d95a
Will Buck <william.parker.buck@gmail.com>
2019-07-19 13:21:56 -0500

    #1265 updates section 4.1 (environments) and 4.2 (propertySource) of docs to multi-lang

---
+++ test-suite/src/test/java/io/micronaut/docs/context/env/EnvironmentSpec.java
@@ -0,0 +1,47 @@
+package io.micronaut.docs.context.env;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.Environment;
+import io.micronaut.context.env.PropertySource;
+import io.micronaut.core.util.CollectionUtils;
+import org.junit.Test;
+
+public class EnvironmentSpec {
+
+    @Test
+    public void testRunEnvironment() {
+        // tag::env[]
+        ApplicationContext applicationContext = ApplicationContext.run("test", "android");
+        Environment environment = applicationContext.getEnvironment();
+
+        assertTrue(environment.getActiveNames().contains("test"));
+        assertTrue(environment.getActiveNames().contains("android"));
+        // end::env[]
+
+    }
+
+    @Test
+    public void testRunEnvironmentWithProperties() {
+        // tag::envProps[]
+        ApplicationContext applicationContext = ApplicationContext.run(
+                PropertySource.of(
+                        "test",
+                        CollectionUtils.mapOf(
+                            "micronaut.server.host", "foo",
+                            "micronaut.server.port", 8080
+                        )
+                ),
+                "test", "android");
+        Environment environment = applicationContext.getEnvironment();
+
+        assertEquals(
+                "foo",
+                environment.getProperty("micronaut.server.host", String.class).orElse("localhost")
+        );
+        // end::envProps[]
+
+    }
+}
