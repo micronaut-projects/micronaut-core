1b4b33ea1c4db17aa18ee7dffd06376ebdbdf207
Graeme Rocher <graeme.rocher@gmail.com>
2019-07-23 11:40:23 +0200

    Fix support for ordering visitors in inject-groovy implementation

--- inject-groovy/src/test/groovy/io/micronaut/AbstractBeanDefinitionSpec.groovy
+++ inject-groovy/src/test/groovy/io/micronaut/AbstractBeanDefinitionSpec.groovy
@@ -19,6 +19,7 @@ import groovy.transform.CompileStatic
 import io.micronaut.ast.groovy.utils.ExtendedParameter
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.DefaultApplicationContext
+import io.micronaut.core.beans.BeanIntrospection
 import io.micronaut.core.io.scan.ClassPathResourceLoader
 import io.micronaut.inject.BeanDefinitionReference
 import org.codehaus.groovy.ast.ASTNode
@@ -49,6 +50,21 @@ abstract class AbstractBeanDefinitionSpec extends Specification {
         def classLoader = new InMemoryByteCodeGroovyClassLoader()
         classLoader.parseClass(classStr)
         return (BeanDefinition)classLoader.loadClass(beanFullName).newInstance()
+    }
+
+    /**
+     * Build and return a {@link io.micronaut.core.beans.BeanIntrospection} for the given class name and class data.
+     *
+     * @return the introspection if it is correct
+     **/
+    protected BeanIntrospection buildBeanIntrospection(String className, String cls) {
+        def beanDefName= '$' + NameUtils.getSimpleName(className) + '$Introspection'
+        def packageName = NameUtils.getPackageName(className)
+        String beanFullName = "${packageName}.${beanDefName}"
+
+        def classLoader = new InMemoryByteCodeGroovyClassLoader()
+        classLoader.parseClass(cls)
+        return (BeanIntrospection)classLoader.loadClass(beanFullName).newInstance()
     }
 
     InMemoryByteCodeGroovyClassLoader buildClassLoader(String classStr) {
