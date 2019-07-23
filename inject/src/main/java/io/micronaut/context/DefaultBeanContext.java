235aa088b5e34a347dbb80ca436c3bbce667d7b6
Vladimir Orany <vladimir@orany.cz>
2019-07-23 15:08:57 +0200

    fixed ClassCastException when retrieving bean
    fixes #1851 (again)

--- inject/src/main/java/io/micronaut/context/DefaultBeanContext.java
+++ inject/src/main/java/io/micronaut/context/DefaultBeanContext.java
@@ -2205,12 +2205,12 @@ public class DefaultBeanContext implements BeanContext {
         synchronized (singletonObjects) {
             if (definition instanceof NoInjectionBeanDefinition) {
                 NoInjectionBeanDefinition<T> manuallyRegistered = (NoInjectionBeanDefinition) definition;
-                T bean = (T) singletonObjects.get(new BeanKey(beanType, manuallyRegistered.getQualifier()));
-                if (bean == null) {
+                BeanRegistration<T> reg = (BeanRegistration<T>) singletonObjects.get(new BeanKey(beanType, manuallyRegistered.getQualifier()));
+                if (reg == null) {
                     throw new IllegalStateException("Manually registered singleton no longer present in bean context");
                 }
-                registerSingletonBean(definition, beanType, bean, qualifier, true);
-                return bean;
+                registerSingletonBean(definition, beanType, reg.getBean(), qualifier, true);
+                return reg.getBean();
             } else {
                 T createdBean = doCreateBean(resolutionContext, definition, qualifier, true, null);
                 registerSingletonBean(definition, beanType, createdBean, qualifier, true);
