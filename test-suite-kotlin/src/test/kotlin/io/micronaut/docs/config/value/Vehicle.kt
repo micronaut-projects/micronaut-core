e886302d3a81d65fb23e04ab727e19b759412cec
Will Buck <william.parker.buck@gmail.com>
2019-07-26 13:13:21 -0500

    #1265 converts 4.3 (config value annotation) to multi-lang samples

---
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/config/value/Vehicle.kt
@@ -0,0 +1,12 @@
+package io.micronaut.docs.config.value
+
+import javax.inject.Singleton
+
+@Singleton
+class Vehicle(val engine: Engine)// <6>
+{
+
+    fun start(): String {
+        return engine.start()
+    }
+}
