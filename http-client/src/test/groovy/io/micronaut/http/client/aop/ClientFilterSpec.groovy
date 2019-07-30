6f9742347b4ea6d69242cab975ee5c20ebcdf1a3
jameskleeh <james.kleeh@gmail.com>
2019-07-30 17:18:10 -0400

    Add tests for root filter matching. Start the uri template with / for declarative clients. Fixes #1954

--- http-client/src/test/groovy/io/micronaut/http/client/aop/ClientFilterSpec.groovy
+++ http-client/src/test/groovy/io/micronaut/http/client/aop/ClientFilterSpec.groovy
@@ -16,6 +16,7 @@
 package io.micronaut.http.client.aop
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.MutableHttpRequest
@@ -41,7 +42,9 @@ class ClientFilterSpec extends Specification{
 
     @Shared
     @AutoCleanup
-    ApplicationContext context = ApplicationContext.run()
+    ApplicationContext context = ApplicationContext.run([
+            'spec.name': 'ClientFilterSpec',
+    ])
 
     @Shared
     EmbeddedServer embeddedServer = context.getBean(EmbeddedServer).start()
@@ -68,6 +71,30 @@ class ClientFilterSpec extends Specification{
         client.close()
     }
 
+    void "test a client filter matching to the root"() {
+        given:
+        RootApi rootApi = context.getBean(RootApi)
+
+        expect:
+        rootApi.name() == 'processed'
+    }
+
+    void "test a root url matching a manually service client"() {
+        given:
+        ApplicationContext ctx = ApplicationContext.build([
+                'spec.name': 'ClientFilterSpec',
+                'micronaut.http.services.my-service.url': embeddedServer.getURL().toString()
+        ]).start()
+        ServiceApi serviceApi = ctx.getBean(ServiceApi)
+
+        expect:
+        serviceApi.name() == 'processed'
+
+        cleanup:
+        ctx.close()
+    }
+
+    @Requires(property = 'spec.name', value = "ClientFilterSpec")
     @Controller('/filters')
     static class TestController {
 
@@ -77,14 +104,41 @@ class ClientFilterSpec extends Specification{
         }
     }
 
+    @Requires(property = 'spec.name', value = "ClientFilterSpec")
+    @Controller
+    static class RootController {
+
+        @Get
+        String name(@Header('X-Root-Filter') String value) {
+            return value
+        }
+    }
+
+    @Requires(property = 'spec.name', value = "ClientFilterSpec")
     @Client('/filters')
     static interface MyApi {
         @Get('/name')
         String name()
     }
 
+    @Requires(property = 'spec.name', value = "ClientFilterSpec")
+    @Client('/')
+    static interface RootApi {
+
+        @Get
+        String name()
+    }
+
+    @Requires(property = 'spec.name', value = "ClientFilterSpec")
+    @Client('my-service')
+    static interface ServiceApi {
+
+        @Get
+        String name()
+    }
 
     // this filter should match
+    @Requires(property = 'spec.name', value = "ClientFilterSpec")
     @Filter('/filters/**')
     static class MyFilter implements HttpClientFilter {
 
@@ -96,6 +150,7 @@ class ClientFilterSpec extends Specification{
     }
 
     // this filter should not match the test
+    @Requires(property = 'spec.name', value = "ClientFilterSpec")
     @Filter('/surnames/**')
     static class SurnameFilter implements HttpClientFilter {
 
@@ -107,6 +162,7 @@ class ClientFilterSpec extends Specification{
     }
 
     // this filter should not match the test
+    @Requires(property = 'spec.name', value = "ClientFilterSpec")
     @Filter(patterns = '/filters/**', serviceId = 'otherClient')
     static class AnotherFilter implements HttpClientFilter {
 
@@ -124,6 +180,17 @@ class ClientFilterSpec extends Specification{
         @Override
         Publisher<? extends HttpResponse<?>> doFilter(MutableHttpRequest<?> request, ClientFilterChain chain) {
             request.header("X-Auth-Lastname", "Flintstone")
+            return chain.proceed(request)
+        }
+    }
+
+    @Requires(property = 'spec.name', value = "ClientFilterSpec")
+    @Filter("/**")
+    static class RootFilter implements HttpClientFilter {
+
+        @Override
+        Publisher<? extends HttpResponse<?>> doFilter(MutableHttpRequest<?> request, ClientFilterChain chain) {
+            request.header("X-Root-Filter", "processed")
             return chain.proceed(request)
         }
     }
