42a0d36afaf0ca48b6e0405ccdb90a83b8e8f030
Will Buck <william.parker.buck@gmail.com>
2019-08-02 10:23:18 -0500

    #1265 adds java missing tags to props/EngineImpl, cleans up string building

--- test-suite-kotlin/src/test/kotlin/io/micronaut/docs/config/properties/EngineImpl.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/config/properties/EngineImpl.kt
@@ -2,14 +2,15 @@ package io.micronaut.docs.config.properties
 
 import javax.inject.Singleton
 
+// tag::class[]
 @Singleton
-class EngineImpl(val config: EngineConfig)// <1>
-    : Engine {
+class EngineImpl(val config: EngineConfig) : Engine {// <1>
 
     override val cylinders: Int
         get() = config.cylinders
 
     override fun start(): String {// <2>
-        return config.manufacturer + " Engine Starting V" + config.cylinders.toString() + " [rodLength=" + config.crankShaft.rodLength.orElse(6.0).toString() + "]"
+        return "${config.manufacturer} Engine Starting V${config.cylinders} [rodLength=${config.crankShaft.rodLength ?: 6.0}]"
     }
 }
+// end::class[]
--- test-suite/src/test/java/io/micronaut/docs/config/properties/EngineImpl.java
+++ test-suite/src/test/java/io/micronaut/docs/config/properties/EngineImpl.java
@@ -2,8 +2,11 @@ package io.micronaut.docs.config.properties;
 
 import javax.inject.Singleton;
 
+// tag::class[]
 @Singleton
 public class EngineImpl implements Engine {
+    private final EngineConfig config;
+
     public EngineImpl(EngineConfig config) {// <1>
         this.config = config;
     }
@@ -14,12 +17,12 @@ public class EngineImpl implements Engine {
     }
 
     public String start() {// <2>
-        return getConfig().getManufacturer() + " Engine Starting V" + String.valueOf(getConfig().getCylinders()) + " [rodLength=" + String.valueOf(getConfig().getCrankShaft().getRodLength().orElse(6.0d)) + "]";
+        return getConfig().getManufacturer() + " Engine Starting V" + getConfig().getCylinders() +
+                " [rodLength=" + getConfig().getCrankShaft().getRodLength().orElse(6.0d) + "]";
     }
 
     public final EngineConfig getConfig() {
         return config;
     }
-
-    private final EngineConfig config;
 }
+// end::class[]
