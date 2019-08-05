18181a134f3ccfa6a0dccf9945c9452cdb264314
jameskleeh <james.kleeh@gmail.com>
2019-08-05 10:08:56 -0400

    Consolidate compeleted file classes. Allow the user to request n files

--- http-server-netty/src/main/java/io/micronaut/http/server/netty/multipart/MultipartBodyArgumentBinder.java
+++ http-server-netty/src/main/java/io/micronaut/http/server/netty/multipart/MultipartBodyArgumentBinder.java
@@ -17,7 +17,6 @@ import io.netty.buffer.EmptyByteBuf;
 import io.netty.handler.codec.http.multipart.Attribute;
 import io.netty.handler.codec.http.multipart.FileUpload;
 import io.netty.handler.codec.http.multipart.HttpData;
-import io.netty.util.ReferenceCountUtil;
 import io.netty.util.ReferenceCounted;
 import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
@@ -27,6 +26,12 @@ import javax.inject.Singleton;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * A {@link io.micronaut.http.annotation.Body} argument binder for a {@link MultipartBody} argument.
+ *
+ * @author James Kleeh
+ * @since 1.3.0
+ */
 @Singleton
 public class MultipartBodyArgumentBinder implements NonBlockingBodyArgumentBinder<MultipartBody> {
 
@@ -39,7 +44,6 @@ public class MultipartBodyArgumentBinder implements NonBlockingBodyArgumentBinde
         this.beanLocator = beanLocator;
         this.httpServerConfiguration = httpServerConfiguration;
     }
-
 
     @Override
     public Argument<MultipartBody> argumentType() {
@@ -71,7 +75,7 @@ public class MultipartBodyArgumentBinder implements NonBlockingBodyArgumentBinde
                             @Override
                             public void request(long n) {
                                 if (partsRequested.getAndUpdate((prev) -> prev + n) == 0) {
-                                    s.request(1);
+                                    s.request(n);
                                 }
                             }
 
@@ -98,9 +102,9 @@ public class MultipartBodyArgumentBinder implements NonBlockingBodyArgumentBinde
                             if (data.isCompleted()) {
                                 partsRequested.decrementAndGet();
                                 if (data instanceof FileUpload) {
-                                    subscriber.onNext(new NettyMultipartFile(((FileUpload) data)));
+                                    subscriber.onNext(new NettyCompletedFileUpload((FileUpload) data, false));
                                 } else if (data instanceof Attribute) {
-                                    subscriber.onNext(new NettyMultipartAttribute((Attribute) data));
+                                    subscriber.onNext(new NettyCompletedAttribute((Attribute) data));
                                 }
                             }
                         }
