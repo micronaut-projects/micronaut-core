300d3937057ffdcf81d73ecf16a7751faf97a92d
James Kleeh <james.kleeh@gmail.com>
2019-08-12 13:47:09 -0400

    Update HttpHeaderValues.java

--- http/src/main/java/io/micronaut/http/HttpHeaderValues.java
+++ http/src/main/java/io/micronaut/http/HttpHeaderValues.java
@@ -24,14 +24,14 @@ package io.micronaut.http;
 public interface HttpHeaderValues {
 
     /**
-     * {@code "Bearer "}.
+     * {@code "Bearer"}.
      */
-    String AUTHORIZATION_PREFIX_BEARER = "Bearer ";
+    String AUTHORIZATION_PREFIX_BEARER = "Bearer";
 
     /**
-     * {@code "Basic "}.
+     * {@code "Basic"}.
      */
-    String AUTHORIZATION_PREFIX_BASIC = "Basic ";
+    String AUTHORIZATION_PREFIX_BASIC = "Basic";
 
     /**
      * @see <a href="https://tools.ietf.org/html/rfc7234#section-5.2.1.1">Rfc 7234 section-5.2.1.1</a>, <a href="https://tools.ietf.org/html/rfc7234#section-5.2.2.8">Rfc 7234 section-5.2.2.8</a>.
