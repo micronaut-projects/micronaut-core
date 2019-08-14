d09b38c3a235d587b6ca3e6f9c03fca4aa021f5f
jameskleeh <james.kleeh@gmail.com>
2019-08-14 15:30:29 -0400

    Resolve issue with stringValues not correctly handling placeholders. Fixes #2012

---
+++ inject/src/test/groovy/io/micronaut/context/env/AnnotationPlaceholderSpec.groovy
@@ -0,0 +1,33 @@
+package io.micronaut.context.env
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.inject.BeanDefinition
+import spock.lang.Specification
+import javax.inject.Singleton
+
+class AnnotationPlaceholderSpec extends Specification {
+
+    void "test placeholder binding to a string array"() {
+        given:
+        ApplicationContext ctx = ApplicationContext.run(['from.config': ['a', 'b', 'c'], 'more.values': ['d', 'e']])
+        BeanDefinition beanDefinition = ctx.getBeanDefinition(Test)
+
+        when:
+        String[] values = beanDefinition.stringValues(StringArrayValue)
+
+        then:
+        values == ['a', 'b', 'c', 'd', 'e'] as String[]
+
+        when:
+        values = beanDefinition.getValue(StringArrayValue, String[].class).get()
+
+        then:
+        values == ['a', 'b', 'c', 'd', 'e'] as String[]
+    }
+
+    @Singleton
+    @StringArrayValue(['${from.config}', '${more.values}'])
+    static class Test {
+
+    }
+}
