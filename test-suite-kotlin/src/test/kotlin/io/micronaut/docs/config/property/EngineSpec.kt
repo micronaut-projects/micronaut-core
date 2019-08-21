8a29da34f54328ff8f873762b2dbe55e7bef69e8
jameskleeh <james.kleeh@gmail.com>
2019-08-21 11:56:03 -0400

    Improve documentation for @Property

---
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/config/property/EngineSpec.kt
@@ -0,0 +1,22 @@
+package io.micronaut.docs.config.property
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.micronaut.context.ApplicationContext
+import org.junit.Test
+
+import java.util.LinkedHashMap
+
+import org.junit.Assert.assertEquals
+
+class EngineSpec : StringSpec({
+
+    "test start vehicle with configuration" {
+        val ctx = ApplicationContext.run(mapOf("my.engine.cylinders" to "8", "my.engine.manufacturer" to "Honda"))
+
+        val engine = ctx.getBean(Engine::class.java)
+
+        engine.manufacturer shouldBe "Honda"
+        engine.cylinders() shouldBe 8
+    }
+})
