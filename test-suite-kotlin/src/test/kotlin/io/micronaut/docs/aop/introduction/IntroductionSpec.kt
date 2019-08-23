c5bddb1d008937102f638e01000857717606b3a8
Will Buck <william.parker.buck@gmail.com>
2019-08-23 14:44:16 -0500

    #1265 adds kotlin examples for section 5.2

---
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/aop/introduction/IntroductionSpec.kt
@@ -0,0 +1,22 @@
+package io.micronaut.docs.aop.introduction
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.AnnotationSpec
+import io.micronaut.context.ApplicationContext
+
+class IntroductionSpec: AnnotationSpec() {
+
+    @Test
+    fun testStubIntroduction() {
+        val applicationContext = ApplicationContext.run()
+
+        // tag::test[]
+        val stubExample = applicationContext.getBean(StubExample::class.java)
+
+        stubExample.number.toLong().shouldBe(10)
+        stubExample.date.shouldBe(null)
+        // end::test[]
+
+        applicationContext.stop()
+    }
+}
