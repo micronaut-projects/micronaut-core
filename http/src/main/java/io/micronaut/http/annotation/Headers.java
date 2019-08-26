f8ce250023ae739faea788a0ab4334afee9fa8dc
jameskleeh <james.kleeh@gmail.com>
2019-08-26 12:05:59 -0400

    Support multiple headers on methods in delcarative clients. Closes #2033

--- http/src/main/java/io/micronaut/http/annotation/Headers.java
+++ http/src/main/java/io/micronaut/http/annotation/Headers.java
@@ -36,7 +36,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE})
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Headers {
 
     /***
