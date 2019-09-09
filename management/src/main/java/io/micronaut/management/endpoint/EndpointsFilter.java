25f4c53f069ab14255f426aedc8ae844dce167fd
jameskleeh <james.kleeh@gmail.com>
2019-09-09 16:57:02 -0400

    Resolve issue with endpoints in combination with server context path. Fixes #2090

--- management/src/main/java/io/micronaut/management/endpoint/EndpointsFilter.java
+++ management/src/main/java/io/micronaut/management/endpoint/EndpointsFilter.java
@@ -43,7 +43,7 @@ import static io.micronaut.management.endpoint.EndpointDefaultConfiguration.PATH
  * @since 1.0
  */
 @Requires(property = "micronaut.security.enabled", notEquals = StringUtils.TRUE)
-@Filter("${" + PATH + ":/}**")
+@Filter("/**")
 public class EndpointsFilter extends OncePerRequestHttpServerFilter {
 
     private final Map<ExecutableMethod, Boolean> endpointMethods;
@@ -68,10 +68,8 @@ public class EndpointsFilter extends OncePerRequestHttpServerFilter {
         Optional<RouteMatch> routeMatch = RouteMatchUtils.findRouteMatchAtRequest(request);
         if (routeMatch.isPresent() && routeMatch.get() instanceof MethodBasedRouteMatch) {
             ExecutableMethod method = ((MethodBasedRouteMatch) routeMatch.get()).getExecutableMethod();
-            if (endpointMethods.containsKey(method)) {
-                if (endpointMethods.get(method)) {
-                    return Publishers.just(HttpResponse.status(HttpStatus.UNAUTHORIZED));
-                }
+            if (endpointMethods.getOrDefault(method, false)) {
+                return Publishers.just(HttpResponse.status(HttpStatus.UNAUTHORIZED));
             }
         }
         return chain.proceed(request);
