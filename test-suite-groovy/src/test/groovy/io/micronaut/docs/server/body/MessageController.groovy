183a18fcf365f9c3724f500cedd271bc0c249536
niravassar <assarn@objectcomputing.com>
2019-09-24 11:06:52 -0500

    add body tests

--- test-suite-groovy/src/test/groovy/io/micronaut/docs/server/body/MessageController.groovy
+++ test-suite-groovy/src/test/groovy/io/micronaut/docs/server/body/MessageController.groovy
@@ -1,17 +1,17 @@
-package io.micronaut.docs.server.body;
-
-// tag::imports[]
+package io.micronaut.docs.server.body
 
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.MediaType;
-import io.micronaut.http.MutableHttpResponse;
-import io.micronaut.http.annotation.Body;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Post;
-import io.reactivex.Flowable;
-import io.reactivex.Single;
 
-import javax.validation.constraints.Size;
+// tag::imports[]
+import io.micronaut.http.MediaType
+import io.micronaut.http.MutableHttpResponse
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import io.reactivex.Flowable
+import io.reactivex.Single
+
+import javax.validation.constraints.Size
 // end::imports[]
 
 /**
@@ -33,7 +33,7 @@ class MessageController {
     // tag::echoReactive[]
     @Post(value = "/echo-flow", consumes = MediaType.TEXT_PLAIN) // <1>
     Single<MutableHttpResponse<String>> echoFlow(@Body Flowable<String> text) { //<2>
-        return text.collect(StringBuffer.&new, StringBuffer.&append) // <3>
+        return text.collect({ x -> new StringBuffer()}, { StringBuffer sb, String s -> sb.append(s)}) // <3>
                    .map({ buffer ->
                        HttpResponse.ok(buffer.toString())
                    });
