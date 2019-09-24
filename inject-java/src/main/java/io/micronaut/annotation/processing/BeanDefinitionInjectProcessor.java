micronaut-projects/micronaut-core

commit 8f539ac4fa2f7c6db500e213b57b9a92dffab018

Fix bug with post construct not called for classes with @Inject on constructor. Fixes #2025

inject-java/src/main/java/io/micronaut/annotation/processing/BeanDefinitionInjectProcessor.java
@@ -320,8 +320,10 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
             this.isConfigurationPropertiesType = isConfigurationProperties(concreteClass);
             this.isAopProxyType = concreteClassMetadata.hasStereotype(AROUND_TYPE) && !modelUtils.isAbstract(concreteClass);
             this.aopSettings = isAopProxyType ? concreteClassMetadata.getValues(AROUND_TYPE, Boolean.class) : OptionalValues.empty();
+            ExecutableElement constructor = modelUtils.concreteConstructorFor(concreteClass, annotationUtils);
+            this.constructorParameterInfo = populateParameterData(null, constructor, Collections.emptyMap());
             this.isExecutableType = isAopProxyType || concreteClassMetadata.hasStereotype(Executable.class);
-            this.isDeclaredBean = isExecutableType || isConfigurationPropertiesType || isFactoryType || concreteClassMetadata.hasStereotype(Scope.class) || concreteClassMetadata.hasStereotype(DefaultScope.class);
+            this.isDeclaredBean = isExecutableType || isConfigurationPropertiesType || isFactoryType || concreteClassMetadata.hasStereotype(Scope.class) || concreteClassMetadata.hasStereotype(DefaultScope.class) || constructorParameterInfo.getAnnotationMetadata().hasStereotype(Inject.class);
         }
 
         /**
@@ -398,8 +400,6 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
                         qualifiedName.equals(classElementQualifiedName)) {
 
                     if (qualifiedName.equals(classElementQualifiedName)) {
-                        ExecutableElement constructor = modelUtils.concreteConstructorFor(classElement, annotationUtils);
-                        this.constructorParameterInfo = populateParameterData(null, constructor, Collections.emptyMap());
                         final boolean isBean = isAopProxyType ||
                                 isConfigurationPropertiesType ||
                                 typeAnnotationMetadata.hasStereotype(ANNOTATION_STEREOTYPES) ||

inject-groovy/src/main/groovy/io/micronaut/ast/groovy/InjectTransform.groovy
@@ -329,7 +329,10 @@ class InjectTransform implements ASTTransformation, CompilationUnitAware {
             } else if (isFactoryClass || isConfigurationProperties || annotationMetadata.hasStereotype(Bean, Scope)) {
                 defineBeanDefinition(concreteClass)
             }
-            this.isDeclaredBean = isExecutableType || isConfigurationProperties || isFactoryClass || annotationMetadata.hasStereotype(Scope.class) || annotationMetadata.hasStereotype(DefaultScope.class)
+            this.isDeclaredBean = isExecutableType || isConfigurationProperties || isFactoryClass || annotationMetadata.hasStereotype(Scope.class) || annotationMetadata.hasStereotype(DefaultScope.class) || concreteClass.declaredConstructors.any {
+                AnnotationMetadata constructorMetadata = AstAnnotationUtils.getAnnotationMetadata(sourceUnit, it)
+                constructorMetadata.hasStereotype(Inject)
+            }
         }
 
         BeanDefinitionVisitor getBeanWriter() {

inject-groovy/src/test/groovy/io/micronaut/inject/lifecyle/PostConstructCompileSpec.groovy
@@ -7,7 +7,7 @@ class PostConstructCompileSpec extends AbstractBeanDefinitionSpec {
 
     void "test that a @PostConstruct method on a type not defined as a bean doesn't create a bean"() {
         when:
-        buildBeanDefinition('test.Test', '''
+        BeanDefinition definition = buildBeanDefinition('test.Test', '''
 package test;
 
 import javax.annotation.PostConstruct;
@@ -24,8 +24,6 @@ class Test {
 
         then:
         thrown(ClassNotFoundException)
-
-
     }
 
     void "test visit constructor @Inject"() {
@@ -48,7 +46,30 @@ class Test {
 
         then:
         definition != null
+        definition.postConstructMethods.empty
 
+    }
 
+    void "test visit constructor @Inject and @PostConstruct"() {
+        when:
+        def definition = buildBeanDefinition('test.Test', '''
+package test;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+class Test {
+
+    @Inject
+    Test() {  }
+    
+    @PostConstruct
+    void init() {   }
+}
+''')
+
+        then:
+        definition != null
+        definition.postConstructMethods.size() == 1
     }
 }

inject-java/src/test/groovy/io/micronaut/inject/lifecycle/PostConstructCompileSpec.groovy
@@ -25,4 +25,51 @@ class Test {
         then:
         definition == null
     }
+
+    void "test visit constructor @Inject"() {
+        when:
+        def definition = buildBeanDefinition('test.Test', '''
+package test;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+class Test {
+
+    @Inject
+    Test() {
+    
+
+    }
+}
+''')
+
+        then:
+        definition != null
+        definition.postConstructMethods.empty
+
+    }
+
+    void "test visit constructor @Inject and @PostConstruct"() {
+        when:
+        def definition = buildBeanDefinition('test.Test', '''
+package test;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+class Test {
+
+    @Inject
+    Test() {  }
+    
+    @PostConstruct
+    void init() {   }
+}
+''')
+
+        then:
+        definition != null
+        definition.postConstructMethods.size() == 1
+    }
 }

commit 9b13dd1c0a6ab2781f5499216e2d9394d9c5160f

Update BeanDefinitionInjectProcessor.java
fix issue introduced in 8f539ac4fa2f7c6db500e213b57b9a92dffab018

inject-java/src/main/java/io/micronaut/annotation/processing/BeanDefinitionInjectProcessor.java
@@ -184,8 +184,8 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
             beanDefinitions.forEach(className -> {
                 if (processed.add(className)) {
                     final TypeElement refreshedClassElement = elementUtils.getTypeElement(className);
-                    final AnnBeanElementVisitor visitor = new AnnBeanElementVisitor(refreshedClassElement);
                     try {
+                        final AnnBeanElementVisitor visitor = new AnnBeanElementVisitor(refreshedClassElement);
                         refreshedClassElement.accept(visitor, className);
                         visitor.getBeanDefinitionWriters().forEach((name, writer) -> {
                             String beanDefinitionName = writer.getBeanDefinitionName();