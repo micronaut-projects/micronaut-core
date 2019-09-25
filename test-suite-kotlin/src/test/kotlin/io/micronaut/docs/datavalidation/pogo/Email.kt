2ae1d73ea326d5c7305d7786341dacf486139437
niravassar <assarn@objectcomputing.com>
2019-09-25 10:53:53 -0500

    pojo email validated 6.13 complete

---
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/datavalidation/pogo/Email.kt
@@ -0,0 +1,17 @@
+package io.micronaut.docs.datavalidation.pogo
+
+import io.micronaut.core.annotation.Introspected
+
+import javax.validation.constraints.NotBlank
+
+@Introspected
+class Email {
+
+    @NotBlank // <1>
+    lateinit
+    var subject: String
+
+    @NotBlank
+    lateinit // <1>
+    var recipient: String
+}
