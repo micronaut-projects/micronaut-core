684f7dd8cbce8a037a9cf490bd13e87742993d10
jameskleeh <james.kleeh@gmail.com>
2019-09-25 12:28:01 -0400

    Use a DNS address resolver for connection pool clients. Fixes #2152

--- http-client/src/main/java/io/micronaut/http/client/DefaultHttpClient.java
+++ http-client/src/main/java/io/micronaut/http/client/DefaultHttpClient.java
@@ -81,6 +81,7 @@ import io.netty.buffer.*;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.pool.*;
+import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.TooLongFrameException;
@@ -98,6 +99,8 @@ import io.netty.handler.stream.ChunkedWriteHandler;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.resolver.dns.DefaultDnsServerAddressStreamProvider;
+import io.netty.resolver.dns.RoundRobinDnsAddressResolverGroup;
 import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 import io.netty.util.concurrent.DefaultThreadFactory;
@@ -258,8 +261,9 @@ public class DefaultHttpClient implements RxWebSocketClient, RxHttpClient, RxStr
                     @Override
                     protected ChannelPool newPool(RequestKey key) {
                         Bootstrap newBootstrap = bootstrap.clone(group);
-                        newBootstrap.remoteAddress(key.getRemoteAddress());
-
+                        newBootstrap
+                                .resolver(new RoundRobinDnsAddressResolverGroup(NioDatagramChannel.class, DefaultDnsServerAddressStreamProvider.INSTANCE))
+                                .remoteAddress(key.getRemoteAddress());
 
                         AbstractChannelPoolHandler channelPoolHandler = newPoolHandler(key);
                         final Long acquireTimeoutMillis = connectionPoolConfiguration.getAcquireTimeout().map(Duration::toMillis).orElse(-1L);
@@ -280,7 +284,10 @@ public class DefaultHttpClient implements RxWebSocketClient, RxHttpClient, RxStr
                     @Override
                     protected ChannelPool newPool(RequestKey key) {
                         Bootstrap newBootstrap = bootstrap.clone(group);
-                        newBootstrap.remoteAddress(key.getRemoteAddress());
+                        newBootstrap
+                                .resolver(new RoundRobinDnsAddressResolverGroup(NioDatagramChannel.class, DefaultDnsServerAddressStreamProvider.INSTANCE))
+                                .remoteAddress(key.getRemoteAddress());
+
                         AbstractChannelPoolHandler channelPoolHandler = newPoolHandler(key);
                         return new SimpleChannelPool(
                                 newBootstrap,
@@ -2303,7 +2310,7 @@ public class DefaultHttpClient implements RxWebSocketClient, RxHttpClient, RxStr
         }
 
         public InetSocketAddress getRemoteAddress() {
-            return new InetSocketAddress(host, port);
+            return InetSocketAddress.createUnresolved(host, port);
         }
 
         public boolean isSecure() {
