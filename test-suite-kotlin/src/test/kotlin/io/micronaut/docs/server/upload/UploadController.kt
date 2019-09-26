af9283dff47591d61e6820166d30176dee6bbe8d
niravassar <assarn@objectcomputing.com>
2019-09-26 15:28:10 -0500

    6.19 kotlin not odne but java/groovy done

--- test-suite-kotlin/src/test/kotlin/io/micronaut/docs/server/upload/UploadController.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/server/upload/UploadController.kt
@@ -46,54 +46,54 @@ import java.nio.file.Paths
 class UploadController {
     // end::class[]
 
-    // tag::upload[]
-    @Post(value = "/", consumes = [MediaType.MULTIPART_FORM_DATA]) // <1>
-    @Throws(IOException::class)
-    fun upload(file: StreamingFileUpload): Single<HttpResponse<String>> { // <2>
-        val tempFile = File.createTempFile(file.filename, "temp")
-        val uploadPublisher = file.transferTo(tempFile) // <3>
-        return Single.fromPublisher(uploadPublisher)  // <4>
-                .map { success ->
-                    if (success) {
-                        return@Single.fromPublisher(uploadPublisher)  // <4>
-                                .map HttpResponse . ok < String >("Uploaded")
-                    } else {
-                        return@Single.fromPublisher(uploadPublisher)  // <4>
-                                .map HttpResponse . status < String >(HttpStatus.CONFLICT)
-                                .body("Upload Failed")
-                    }
-                }
-    }
-    // end::upload[]
-
-    // tag::completedUpload[]
-    @Post(value = "/completed", consumes = [MediaType.MULTIPART_FORM_DATA]) // <1>
-    fun uploadCompleted(file: CompletedFileUpload): HttpResponse<String> { // <2>
-        try {
-            val tempFile = File.createTempFile(file.filename, "temp") //<3>
-            val path = Paths.get(tempFile.absolutePath)
-            Files.write(path, file.bytes) //<3>
-            return HttpResponse.ok("Uploaded")
-        } catch (exception: IOException) {
-            return HttpResponse.badRequest("Upload Failed")
-        }
-
-    }
-    // end::completedUpload[]
-
-    // tag::bytesUpload[]
-    @Post(value = "/bytes", consumes = [MediaType.MULTIPART_FORM_DATA]) // <1>
-    fun uploadBytes(file: ByteArray, fileName: String): HttpResponse<String> { // <2>
-        try {
-            val tempFile = File.createTempFile(fileName, "temp")
-            val path = Paths.get(tempFile.absolutePath)
-            Files.write(path, file) // <3>
-            return HttpResponse.ok("Uploaded")
-        } catch (exception: IOException) {
-            return HttpResponse.badRequest("Upload Failed")
-        }
-
-    }
-    // end::bytesUpload[]
+//    // tag::upload[]
+//    @Post(value = "/", consumes = [MediaType.MULTIPART_FORM_DATA]) // <1>
+//    @Throws(IOException::class)
+////    fun upload(file: StreamingFileUpload): Single<HttpResponse<String>> { // <2>
+////        val tempFile = File.createTempFile(file.filename, "temp")
+////        val uploadPublisher = file.transferTo(tempFile) // <3>
+////        return Single.fromPublisher(uploadPublisher)  // <4>
+////                .map { success ->
+////                    if (success) {
+////                        return@Single.fromPublisher(uploadPublisher)  // <4>
+////                                .map HttpResponse . ok < String >("Uploaded")
+////                    } else {
+////                        return@Single.fromPublisher(uploadPublisher)  // <4>
+////                                .map HttpResponse . status < String >(HttpStatus.CONFLICT)
+////                                .body("Upload Failed")
+////                    }
+////                }
+////    }
+//    // end::upload[]
+//
+//    // tag::completedUpload[]
+//    @Post(value = "/completed", consumes = [MediaType.MULTIPART_FORM_DATA]) // <1>
+//    fun uploadCompleted(file: CompletedFileUpload): HttpResponse<String> { // <2>
+//        try {
+//            val tempFile = File.createTempFile(file.filename, "temp") //<3>
+//            val path = Paths.get(tempFile.absolutePath)
+//            Files.write(path, file.bytes) //<3>
+//            return HttpResponse.ok("Uploaded")
+//        } catch (exception: IOException) {
+//            return HttpResponse.badRequest("Upload Failed")
+//        }
+//
+//    }
+//    // end::completedUpload[]
+//
+//    // tag::bytesUpload[]
+//    @Post(value = "/bytes", consumes = [MediaType.MULTIPART_FORM_DATA]) // <1>
+//    fun uploadBytes(file: ByteArray, fileName: String): HttpResponse<String> { // <2>
+//        try {
+//            val tempFile = File.createTempFile(fileName, "temp")
+//            val path = Paths.get(tempFile.absolutePath)
+//            Files.write(path, file) // <3>
+//            return HttpResponse.ok("Uploaded")
+//        } catch (exception: IOException) {
+//            return HttpResponse.badRequest("Upload Failed")
+//        }
+//
+//    }
+//    // end::bytesUpload[]
 
 }
