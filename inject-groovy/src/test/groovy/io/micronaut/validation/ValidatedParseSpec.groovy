b847b8ec067764a4bf8dd7b659792905b866e052
jameskleeh <james.kleeh@gmail.com>
2019-10-09 09:25:17 -0400

    In progress client validation

--- inject-groovy/src/test/groovy/io/micronaut/validation/ValidatedParseSpec.groovy
+++ inject-groovy/src/test/groovy/io/micronaut/validation/ValidatedParseSpec.groovy
@@ -6,6 +6,8 @@ import io.micronaut.core.beans.BeanIntrospection
 import io.micronaut.inject.ProxyBeanDefinition
 import io.micronaut.inject.writer.BeanDefinitionVisitor
 
+import java.time.LocalDate
+
 class ValidatedParseSpec extends AbstractBeanDefinitionSpec {
     void "test constraints on beans make them @Validated"() {
         given:
@@ -64,5 +66,28 @@ class Test {
         expect:
         beanIntrospection.getProperty("webs").isPresent()
         beanIntrospection.getRequiredProperty("webs", List).annotationMetadata.getDefaultValue("test.ValidURLs", "message", String).get() == "invalid url"
+    }
+
+    void "test constraints on a declarative client makes it @Validated"() {
+        given:
+        def definition = buildBeanDefinition('test.ExchangeRates' + BeanDefinitionVisitor.PROXY_SUFFIX,'''
+package test
+
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.client.annotation.Client
+
+import javax.validation.constraints.PastOrPresent
+import java.time.LocalDate
+
+@Client("https://exchangeratesapi.io")
+interface ExchangeRates {
+
+    @Get("{date}")
+    String rate(@PastOrPresent LocalDate date)
+}
+''')
+
+        expect:
+        definition.findMethod("rate", LocalDate).get().hasStereotype(Validated)
     }
 }
