ae80bd80680e610f7f8fe58b7edc094ba05dfe38
jameskleeh <james.kleeh@gmail.com>
2019-10-09 09:25:17 -0400

    In progress client validation

--- inject-java/src/main/java/io/micronaut/annotation/processing/BeanDefinitionInjectProcessor.java
+++ inject-java/src/main/java/io/micronaut/annotation/processing/BeanDefinitionInjectProcessor.java
@@ -572,6 +572,18 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
                                 typeAnnotationMetadata
                         );
                     }
+
+                    if (!annotationMetadata.hasStereotype("io.micronaut.validation.Validated") &&
+                            isDeclaredBean &&
+                            params.getParameterMetadata().values().stream().anyMatch(IS_CONSTRAINT)) {
+                        annotationMetadata = javaVisitorContext.getAnnotationUtils().newAnnotationBuilder().annotate(
+                                annotationMetadata,
+                                io.micronaut.core.annotation.AnnotationValue.builder("io.micronaut.validation.Validated").build());
+                        if (!aopProxyWriter.isValidated()) {
+                            aopProxyWriter.setValidated(true);
+                        }
+                    }
+
                     if (annotationUtils.hasStereotype(method, AROUND_TYPE)) {
                         Object[] interceptorTypes = annotationMetadata
                                 .getAnnotationNamesByStereotype(AROUND_TYPE)
