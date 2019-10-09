9589b0771b3948eabb6a8cd83a4e6ae93d18bef7
jameskleeh <james.kleeh@gmail.com>
2019-10-09 14:07:04 -0400

    Revert "Fix compilation issue"
    This reverts commit 707e57eaf1da56f1ab9fd7512c2abb50c8092269.

--- test-suite/src/test/java/io/micronaut/docs/server/routing/BackwardCompatibleController.java
+++ test-suite/src/test/java/io/micronaut/docs/server/routing/BackwardCompatibleController.java
@@ -11,7 +11,7 @@ import io.micronaut.http.annotation.Get;
 @Controller("/hello")
 public class BackwardCompatibleController {
 
-    @Get(value = {"/{name}", "/person/{name}"}) // <1>
+    @Get("/{name}", "/person/{name}") // <1>
     public String hello(String name) { // <2>
         return "Hello, " + name;
     }
