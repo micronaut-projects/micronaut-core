a4d380d02b11208dbe7a4b29215a41171395db18
benrhine <rhine.ben@gmail.com>
2019-10-10 14:15:02 -0400

    more updated code samples with tests

--- test-suite-kotlin/src/test/kotlin/io/micronaut/docs/session/ShoppingController.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/session/ShoppingController.kt
@@ -8,46 +8,48 @@ import io.micronaut.http.annotation.Post
 import io.micronaut.session.Session
 import io.micronaut.session.annotation.SessionValue
 
-import javax.annotation.Nullable
-import javax.validation.constraints.NotBlank
 // end::imports[]
 
 // tag::class[]
 @Controller("/shopping")
 class ShoppingController {
-    static final String ATTR_CART = "cart" // <1>
-// end::class[]
+    // end::class[]
+
+    companion object {
+        private const val ATTR_CART = "cart" // <1>
+    }
 
     // tag::view[]
     @Get("/cart")
-    @SessionValue("cart") // <1>
-    Cart viewCart(@SessionValue @Nullable Cart cart) { // <2>
+    @SessionValue(ATTR_CART) // <1>
+    internal fun viewCart(@SessionValue cart: Cart?): Cart { // <2>
+        var cart = cart
         if (cart == null) {
-            cart = new Cart()
+            cart = Cart()
         }
-        cart
+        return cart
     }
     // end::view[]
 
     // tag::add[]
     @Post("/cart/{name}")
-    Cart addItem(Session session, @NotBlank String name) { // <2>
-        Cart cart = session.get(ATTR_CART, Cart.class).orElseGet({ ->  // <3>
-            Cart newCart = new Cart()
+    internal fun addItem(session: Session, name: String): Cart { // <2>
+        require(name.isNotBlank()) { "Name cannot be blank" }
+        val cart = session.get(ATTR_CART, Cart::class.java).orElseGet({
+            // <3>
+            val newCart = Cart()
             session.put(ATTR_CART, newCart) // <4>
             newCart
         })
-        cart.getItems().add(name)
-        cart
+        cart.items.add(name)
+        return cart
     }
     // end::add[]
 
     // tag::clear[]
     @Post("/cart/clear")
-    void clearCart(@Nullable Session session) {
-        if (session != null) {
-            session.remove(ATTR_CART)
-        }
+    internal fun clearCart(session: Session?) {
+        session?.remove(ATTR_CART)
     }
     // end::clear[]
 
