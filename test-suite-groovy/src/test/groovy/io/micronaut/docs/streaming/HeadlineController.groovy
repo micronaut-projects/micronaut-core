4f1968f4fab215de7c3b9cc9fc4a2159f9d2d6c5
benrhine <rhine.ben@gmail.com>
2019-10-14 14:32:20 -0400

    completed kotlin for section 6.23, section 7.1.2, updating location of file for 7.1.3, section 7.1.4

---
+++ test-suite-groovy/src/test/groovy/io/micronaut/docs/streaming/HeadlineController.groovy
@@ -0,0 +1,28 @@
+package io.micronaut.docs.streaming
+
+// tag::imports[]
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.reactivex.Flowable
+
+import java.time.ZonedDateTime
+import java.util.concurrent.TimeUnit
+// end::imports[]
+
+@Controller("/streaming")
+class HeadlineController {
+
+    // tag::streaming[]
+    @Get(value = "/headlines", produces = MediaType.APPLICATION_JSON_STREAM) // <1>
+    Flowable<Headline> streamHeadlines() {
+        Flowable.fromCallable({ // <2>
+            Headline headline = new Headline()
+            headline.setText("Latest Headline at " + ZonedDateTime.now())
+            return headline
+        }).repeat(100) // <3>
+          .delay(1, TimeUnit.SECONDS) // <4>
+    }
+    // end::streaming[]
+}
