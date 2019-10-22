8adc02b3e7290ee16279bb44fdabd0aac22a4f34
benrhine <rhine.ben@gmail.com>
2019-10-22 15:05:34 -0400

    fixing missed section 3.11, fixing missed test case 6.13

---
+++ test-suite/src/test/java/io/micronaut/docs/lifecycle/PreDestroyBeanSpec.java
@@ -0,0 +1,22 @@
+package io.micronaut.docs.lifecycle;
+
+import io.micronaut.context.BeanContext;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PreDestroyBeanSpec {
+
+    @Test
+    public void testBeanClosingOnContextClose() {
+        // tag::start[]
+        BeanContext ctx = BeanContext.build().start();
+        PreDestroyBean preDestroyBean = ctx.getBean(PreDestroyBean.class);
+        Connection connection = ctx.getBean(Connection.class);
+        ctx.stop();
+        // end::start[]
+
+        assertTrue(preDestroyBean.stopped.get());
+        assertTrue(connection.stopped.get());
+    }
+}
