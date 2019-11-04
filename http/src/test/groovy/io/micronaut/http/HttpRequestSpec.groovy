875ca7c9d2b58c91c1a03298e1b5c87f90fd234a
coestre <acetrike@yahoo.com>
2019-11-04 10:48:37 -0600

    Cleanup from jameskleeh review

--- http/src/test/groovy/io/micronaut/http/HttpRequestSpec.groovy
+++ http/src/test/groovy/io/micronaut/http/HttpRequestSpec.groovy
@@ -21,17 +21,15 @@ class HttpRequestSpec extends Specification {
 
     def "Converting to url and getting host and authority work as expected"() {
         expect:
-        verifyAll {
-            assert new URI("https://foo_bar").toString() == "https://foo_bar"
-            assert HttpRequest.GET("https://localhost.company.com").getUri().toString() == "https://localhost.company.com"
-            assert HttpRequest.GET("https://localhost").getUri().toString() == "https://localhost"
-            assert HttpRequest.GET("https://foo_bar").getUri().toString() == "https://foo_bar"
-            assert HttpRequest.GET("https://slave1-6x8-build-agent-2.0.1-5h7sl").getUri().toString() == "https://slave1-6x8-build-agent-2.0.1-5h7sl"
-            assert HttpRequest.GET("https://slave1-6x8-build-agent-2.0.1-5h7sl:8080").getUri().toString() == "https://slave1-6x8-build-agent-2.0.1-5h7sl:8080"
-            assert HttpRequest.GET(new URI("https://slave1-6x8-build-agent-2.0.1-5h7sl:8080")).getUri().toString() == "https://slave1-6x8-build-agent-2.0.1-5h7sl:8080"
-            assert HttpRequest.GET(new URI("https://slave1-6x8-build-agent-2.0.1-5h7sl:8080")).getUri().getHost() == null
-            assert HttpRequest.GET(new URI("https://slave1-6x8-build-agent-2.0.1-5h7sl:8080")).getUri().getAuthority() == "slave1-6x8-build-agent-2.0.1-5h7sl:8080"
-            assert HttpRequest.GET(new URI("https://slave1-6x8-build-agent-2.0.1-5h7sl")).getUri().getAuthority() == "slave1-6x8-build-agent-2.0.1-5h7sl"
-        }
+        new URI("https://foo_bar").toString() == "https://foo_bar"
+        HttpRequest.GET("https://localhost.company.com").getUri().toString() == "https://localhost.company.com"
+        HttpRequest.GET("https://localhost").getUri().toString() == "https://localhost"
+        HttpRequest.GET("https://foo_bar").getUri().toString() == "https://foo_bar"
+        HttpRequest.GET("https://slave1-6x8-build-agent-2.0.1-5h7sl").getUri().toString() == "https://slave1-6x8-build-agent-2.0.1-5h7sl"
+        HttpRequest.GET("https://slave1-6x8-build-agent-2.0.1-5h7sl:8080").getUri().toString() == "https://slave1-6x8-build-agent-2.0.1-5h7sl:8080"
+        HttpRequest.GET(new URI("https://slave1-6x8-build-agent-2.0.1-5h7sl:8080")).getUri().toString() == "https://slave1-6x8-build-agent-2.0.1-5h7sl:8080"
+        HttpRequest.GET(new URI("https://slave1-6x8-build-agent-2.0.1-5h7sl:8080")).getUri().getHost() == null
+        HttpRequest.GET(new URI("https://slave1-6x8-build-agent-2.0.1-5h7sl:8080")).getUri().getAuthority() == "slave1-6x8-build-agent-2.0.1-5h7sl:8080"
+        HttpRequest.GET(new URI("https://slave1-6x8-build-agent-2.0.1-5h7sl")).getUri().getAuthority() == "slave1-6x8-build-agent-2.0.1-5h7sl"
     }
 }
