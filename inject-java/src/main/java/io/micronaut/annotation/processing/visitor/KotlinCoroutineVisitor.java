567fb1ab3771ffd1b8e2a0303ff6cffc67e1f3b2
graemerocher <graeme.rocher@gmail.com>
2019-11-14 14:18:11 +0100

    Checkstyle

--- inject-java/src/main/java/io/micronaut/annotation/processing/visitor/KotlinCoroutineVisitor.java
+++ inject-java/src/main/java/io/micronaut/annotation/processing/visitor/KotlinCoroutineVisitor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.annotation.processing.visitor;
 
 import io.micronaut.context.annotation.Executable;
@@ -10,7 +25,10 @@ import io.micronaut.inject.visitor.TypeElementVisitor;
 import io.micronaut.inject.visitor.VisitorContext;
 
 /**
- * A {@link TypeElementVisitor} that will add the {@code io.micronaut.kotlin.coroutines.intercept.KotlinCoroutineAroundInterceptor}
+ * A {@link TypeElementVisitor} that will add the {@code io.micronaut.kotlin.coroutines.intercept.KotlinCoroutineAroundInterceptor}.
+ *
+ * @author graemerocher
+ * @since 1.3.0
  */
 @Internal
 public class KotlinCoroutineVisitor implements TypeElementVisitor<Object, Executable> {
