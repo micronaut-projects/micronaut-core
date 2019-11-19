3fc491a1c3b304457303199c7537acbf8ab42bbb
Fabien Renaud <fabienrenaud.fr@gmail.com>
2019-11-19 12:32:42 -0800

    Specialized UnsatisfiedRouteException
    Each Bindable annotation defined under io.micronaut.http.annotation now
    has a dedicated UnsatisfiedRouteException.
    
    Resolves issue #2249

---
+++ router/src/main/java/io/micronaut/web/router/exceptions/UnsatisfiedRequestAttributeRouteException.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.web.router.exceptions;
+
+import io.micronaut.core.type.Argument;
+
+/**
+ * An exception thrown when the an {@link io.micronaut.http.annotation.RequestAttribute} to a {@link io.micronaut.web.router.Route} cannot be satisfied.
+ *
+ * @author Fabien Renaud
+ * @since 1.3.0
+ */
+public final class UnsatisfiedRequestAttributeRouteException extends UnsatisfiedRouteException {
+
+    private final String name;
+
+    /**
+     * @param name     The name of the request attribute
+     * @param argument The {@link Argument}
+     */
+    public UnsatisfiedRequestAttributeRouteException(String name, Argument<?> argument) {
+        super("Required RequestAttribute [" + name + "] not specified", argument);
+        this.name = name;
+    }
+
+    /**
+     * @return The name of the request attribute
+     */
+    public String getRequestAttributeName() {
+        return name;
+    }
+}
