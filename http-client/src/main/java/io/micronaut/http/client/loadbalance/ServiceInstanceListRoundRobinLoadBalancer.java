b69286f2b846c69da34e2fac9c119882702cbf2a
jameskleeh <james.kleeh@gmail.com>
2020-01-21 11:10:13 -0500

    Ensure NoServiceAvailable exceptions are emitted and not thrown

--- http-client/src/main/java/io/micronaut/http/client/loadbalance/ServiceInstanceListRoundRobinLoadBalancer.java
+++ http-client/src/main/java/io/micronaut/http/client/loadbalance/ServiceInstanceListRoundRobinLoadBalancer.java
@@ -18,6 +18,7 @@ package io.micronaut.http.client.loadbalance;
 import io.micronaut.core.async.publisher.Publishers;
 import io.micronaut.discovery.ServiceInstance;
 import io.micronaut.discovery.ServiceInstanceList;
+import io.reactivex.Flowable;
 import org.reactivestreams.Publisher;
 
 import javax.annotation.Nullable;
@@ -38,7 +39,7 @@ public class ServiceInstanceListRoundRobinLoadBalancer extends AbstractRoundRobi
 
     @Override
     public Publisher<ServiceInstance> select(@Nullable Object discriminator) {
-        return Publishers.just(getNextAvailable(serviceInstanceList.getInstances()));
+        return Flowable.fromCallable(() -> getNextAvailable(serviceInstanceList.getInstances()));
     }
 
     @Override
