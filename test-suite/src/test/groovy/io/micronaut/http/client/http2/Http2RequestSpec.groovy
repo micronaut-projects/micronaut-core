ff5df1729a0fd3cf73b9a19977e15f40b1c95eb7
graemerocher <graeme.rocher@gmail.com>
2020-03-11 13:03:54 +0100

    Add json stream test

--- test-suite/src/test/groovy/io/micronaut/http/client/http2/Http2RequestSpec.groovy
+++ test-suite/src/test/groovy/io/micronaut/http/client/http2/Http2RequestSpec.groovy
@@ -1,12 +1,16 @@
 package io.micronaut.http.client.http2
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.core.type.Argument
+import io.micronaut.docs.server.json.Person
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.RxStreamingHttpClient
 import io.micronaut.runtime.server.EmbeddedServer
+import io.reactivex.Flowable
 import spock.lang.Ignore
 import spock.lang.PendingFeature
 import spock.lang.Specification
@@ -27,7 +31,7 @@ class Http2RequestSpec extends Specification {
                 "micronaut.http.client.log-level" : "TRACE",
                 "micronaut.server.netty.log-level" : "TRACE"
         ])
-        RxHttpClient client = server.getApplicationContext().getBean(RxHttpClient)
+        RxStreamingHttpClient client = server.getApplicationContext().getBean(RxStreamingHttpClient)
 
         when:
         def result = client.retrieve("${server.URL}/http2").blockingFirst()
@@ -40,6 +44,20 @@ class Http2RequestSpec extends Specification {
 
         then:
         result == 'Version: HTTP_2_0'
+
+        when:"A non stream request is executed"
+        def people = client.retrieve(HttpRequest.GET("${server.URL}/http2/personStream"), Argument.listOf(Person)).blockingFirst()
+
+        then:
+        people == Http2Controller.people
+
+//      TODO: Not working yet, client JSON stream handling broken with HTTP/2
+//
+//        when:"A stream request is executed"
+//        def people = client.jsonStream(HttpRequest.GET("${server.URL}/http2/personStream"), Person).toList().blockingGet()
+//
+//        then:
+//        people == Http2Controller.people
 
         cleanup:
         server.close()
@@ -66,25 +84,6 @@ class Http2RequestSpec extends Specification {
         result = client.retrieve("${server.URL}/http2").blockingFirst()
 
         then:
-        result == 'Version: HTTP_2_0'
-
-        cleanup:
-        server.close()
-    }
-
-    @Ignore
-    void "test make HTTP/2 request - upgrade over HTTP 2"() {
-        given:
-        EmbeddedServer server = ApplicationContext.run(EmbeddedServer, [
-                "micronaut.http.client.http-version" : "2.0",
-                "micronaut.http.client.read-timeout": -1,
-                "micronaut.http.client.log-level" : "TRACE",
-                "micronaut.server.netty.log-level" : "TRACE"
-        ])
-        RxHttpClient client = server.getApplicationContext().getBean(RxHttpClient)
-        def result = client.retrieve("http://localhost:8080").blockingFirst()
-
-        expect:
         result == 'Version: HTTP_2_0'
 
         cleanup:
@@ -136,9 +135,27 @@ class Http2RequestSpec extends Specification {
     @Controller("/http2")
     static class Http2Controller {
 
+
+        public static final List<Person> people = [new Person("Fred", "Flinstone"), new Person("Barney", "Rubble")]
+
         @Get(produces = MediaType.TEXT_HTML)
         String index(HttpRequest<?> request) {
             return "Version: ${request.httpVersion}"
+        }
+
+        @Get(value = '/stream', produces = MediaType.TEXT_PLAIN)
+        Flowable<String> flowable(HttpRequest<?> request) {
+            return Flowable.fromIterable(
+                    ["Version: ",
+                    request.httpVersion.toString()]
+            )
+        }
+
+        @Get(value = '/personStream', produces = MediaType.APPLICATION_JSON)
+        Flowable<Person> personStream(HttpRequest<?> request) {
+            return Flowable.fromIterable(
+                    people
+            )
         }
     }
 }
