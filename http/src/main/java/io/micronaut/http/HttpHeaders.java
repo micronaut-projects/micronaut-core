a177964de6922d988ddabf88a1c1c216de7bed9c
Sam Gammon <samuel.gammon@gmail.com>
2020-03-19 20:43:00 -0700

    Add `FeaturePolicy` header to `HttpHeaders`
    This small change adds the [`Feature-Policy`](https://developer.mozilla.org/en-US/docs/Web/HTTP/Feature_Policy) header to the `HttpHeaders` class in Micronaut's HTTP core. *Feature Policy* allows the server to restrict various browser features when serving an HTML document.

--- http/src/main/java/io/micronaut/http/HttpHeaders.java
+++ http/src/main/java/io/micronaut/http/HttpHeaders.java
@@ -215,6 +215,11 @@ public interface HttpHeaders extends Headers {
     String EXPIRES = "Expires";
 
     /**
+     * {@code "Feature-Policy"}.
+     */
+    String FEATURE_POLICY = "Feature-Policy";
+
+    /**
      * {@code "Forwarded"}.
      */
     String FORWARDED = "Forwarded";
