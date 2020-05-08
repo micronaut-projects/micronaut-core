e4472f4d926cda4be008e3e46ccb697eefc4cbab
graemerocher <graeme.rocher@gmail.com>
2020-05-08 13:08:31 +0200

    Reconstruct type with Jackson mapping. Fixes #2919

--- runtime/src/main/java/io/micronaut/jackson/bind/MapToObjectConverter.java
+++ runtime/src/main/java/io/micronaut/jackson/bind/MapToObjectConverter.java
@@ -15,19 +15,20 @@
  */
 package io.micronaut.jackson.bind;
 
+import io.micronaut.core.bind.ArgumentBinder;
 import io.micronaut.core.bind.BeanPropertyBinder;
+import io.micronaut.core.convert.ArgumentConversionContext;
 import io.micronaut.core.convert.ConversionContext;
+import io.micronaut.core.convert.ConversionError;
 import io.micronaut.core.convert.TypeConverter;
-import io.micronaut.core.naming.NameUtils;
-import io.micronaut.core.reflect.InstantiationUtils;
+import io.micronaut.core.convert.exceptions.ConversionErrorException;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.BiFunction;
 
 /**
  * A class that uses the {@link BeanPropertyBinder} to bind maps to {@link Object} instances.
@@ -59,26 +60,20 @@ public class MapToObjectConverter implements TypeConverter<Map, Object> {
 
     @Override
     public Optional<Object> convert(Map map, Class<Object> targetType, ConversionContext context) {
-        final BiFunction<Object, Map<?, ?>, Object> propertiesBinderFunction = (object, properties) -> {
-            Map bindMap = new LinkedHashMap(properties.size());
-            for (Map.Entry entry : properties.entrySet()) {
-                Object key = entry.getKey();
-                bindMap.put(NameUtils.decapitalize(NameUtils.dehyphenate(key.toString())), entry.getValue());
-            }
-            return beanPropertyBinder.get().bind(object, bindMap);
-        };
-
-        Optional<Object> instance = InstantiationUtils.tryInstantiate(targetType, map, context)
-                    .map(object -> propertiesBinderFunction.apply(object, map));
-
-        if (instance.isPresent()) {
-            return instance;
-        } else if (targetType.isInstance(map)) {
-            return Optional.of(map);
+        ArgumentConversionContext<Object> conversionContext;
+        if (context instanceof ArgumentConversionContext) {
+            conversionContext = (ArgumentConversionContext<Object>) context;
         } else {
-            return InstantiationUtils
-                    .tryInstantiate(targetType)
-                    .map(object -> propertiesBinderFunction.apply(object, map));
+            conversionContext = ConversionContext.of(targetType);
+        }
+        ArgumentBinder binder = this.beanPropertyBinder.get();
+        ArgumentBinder.BindingResult result = binder.bind(conversionContext, map);
+        List<ConversionError> conversionErrors = result.getConversionErrors();
+        if (!conversionErrors.isEmpty()) {
+            context.reject(map, new ConversionErrorException(conversionContext.getArgument(), conversionErrors.iterator().next()));
+            return Optional.empty();
+        } else {
+            return result.getValue();
         }
     }
 }
