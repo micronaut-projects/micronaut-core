53be8506b1ec8cc73b525d3c51fd4c8e39e652d0
graemerocher <graeme.rocher@gmail.com>
2020-06-10 15:31:01 +0200

    Fix test

--- tracing/src/test/groovy/io/micronaut/tracing/instrument/ExecutorServiceInstrumentSpec.groovy
+++ tracing/src/test/groovy/io/micronaut/tracing/instrument/ExecutorServiceInstrumentSpec.groovy
@@ -8,7 +8,9 @@ import spock.lang.Specification
 class ExecutorServiceInstrumentSpec extends Specification {
 
     @Shared @AutoCleanup
-    ApplicationContext applicationContext = ApplicationContext.run()
+    ApplicationContext applicationContext = ApplicationContext.run(
+            (IExecutorServiceImpl.PROP): true
+    )
 
     void "test that sub interfaces for executor service are not instrumented"() {
         expect:
--- tracing/src/test/groovy/io/micronaut/tracing/instrument/IExecutorServiceImpl.groovy
+++ tracing/src/test/groovy/io/micronaut/tracing/instrument/IExecutorServiceImpl.groovy
@@ -1,16 +1,23 @@
 package io.micronaut.tracing.instrument
 
 import groovy.transform.InheritConstructors
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.annotation.Secondary
+import io.micronaut.core.util.StringUtils
 
 import javax.inject.Named
 import javax.inject.Singleton
 import java.util.concurrent.AbstractExecutorService
-import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
 
 @Singleton
 @Named("custom")
+@Requires(property = PROP, value = StringUtils.TRUE)
+@Secondary
 class IExecutorServiceImpl extends AbstractExecutorService implements IExecutorService{
+
+    public static final String PROP = 'iexecutor.enabled'
+
     @Override
     void newMethod() {
 
