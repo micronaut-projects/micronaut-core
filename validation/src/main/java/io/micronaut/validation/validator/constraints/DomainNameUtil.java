40995d1736ce8729efff68d3df5fb334a162d316
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-06-30 08:32:17 +0200

    Updating license headers

--- validation/src/main/java/io/micronaut/validation/validator/constraints/DomainNameUtil.java
+++ validation/src/main/java/io/micronaut/validation/validator/constraints/DomainNameUtil.java
@@ -5,19 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-/*
- * Hibernate Validator, declare and validate application constraints
- *
- * License: Apache License, Version 2.0
- * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
  */
 package io.micronaut.validation.validator.constraints;
 
