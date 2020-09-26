54fae50af1f23502112617b32f1556f9f8d62893
Denis Stepanov <denis.stepanov@gmail.com>
2020-09-26 12:04:28 +0300

    Correct ExecutableMethod cast and allow to change the target in ExecutableMethod generated by `@Around` (#4186)
    * Correct ExecutableMethod cast
    
    * ExecutableMethod: Only invoke proxy on the parent of target is the same

--- inject-java/src/test/groovy/io/micronaut/aop/introduction/with_around/ProxyAdviceInterceptor.java
+++ inject-java/src/test/groovy/io/micronaut/aop/introduction/with_around/ProxyAdviceInterceptor.java
@@ -17,19 +17,43 @@ package io.micronaut.aop.introduction.with_around;
 
 import io.micronaut.aop.MethodInterceptor;
 import io.micronaut.aop.MethodInvocationContext;
+import io.micronaut.context.BeanContext;
+import io.micronaut.inject.ExecutableMethod;
 
 import javax.inject.Singleton;
 
 @Singleton
 public class ProxyAdviceInterceptor implements MethodInterceptor<Object, Object> {
 
+    private final BeanContext beanContext;
+
+    public ProxyAdviceInterceptor(BeanContext beanContext) {
+        this.beanContext = beanContext;
+    }
+
     @Override
     public Object intercept(MethodInvocationContext<Object, Object> context) {
+        if (context.getMethodName().equalsIgnoreCase("getId")) {
+            // Test invocation delegation
+            if (context.getTarget() instanceof MyBean5) {
+                MyBean5 delegate = new MyBean5();
+                delegate.setId(1L);
+                return context.getExecutableMethod().invoke(delegate, context.getParameterValues());
+            } else if (context.getTarget() instanceof MyBean6) {
+                try {
+                    ExecutableMethod<MyBean6, Object> proxyTargetMethod = beanContext.getProxyTargetMethod(MyBean6.class, context.getMethodName(), context.getArgumentTypes());
+                    MyBean6 delegate = new MyBean6();
+                    delegate.setId(1L);
+                    return proxyTargetMethod.invoke(delegate, context.getParameterValues());
+                } catch (NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                return 1L;
+            }
+        }
         if (context.getMethodName().equalsIgnoreCase("isProxy")) {
             return true;
-        }
-        if (context.getMethodName().equalsIgnoreCase("getId")) {
-            return 1L;
         }
         return context.proceed();
     }
