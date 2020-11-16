80374a24bd856ae970773c400c31961e8d3cf637
James Kleeh <james.kleeh@gmail.com>
2020-11-16 11:54:46 -0500

    Fix hang of suspend declarative clients. Fixes #4494 (#4497)

--- test-suite-kotlin/src/test/kotlin/io/micronaut/http/client/SuspendClient.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/http/client/SuspendClient.kt
@@ -1,5 +1,7 @@
 package io.micronaut.http.client
 
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Put
 import io.micronaut.http.client.annotation.Client
 
@@ -8,4 +10,7 @@ interface SuspendClient {
 
     @Put
     suspend fun call(newState: String): String
+
+    @Get
+    suspend fun notFound(): HttpResponse<String?>
 }
--- test-suite-kotlin/src/test/kotlin/io/micronaut/http/client/SuspendClientSpec.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/http/client/SuspendClientSpec.kt
@@ -1,6 +1,7 @@
 package io.micronaut.http.client
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpStatus
 import io.micronaut.runtime.server.EmbeddedServer
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
@@ -17,5 +18,16 @@ class SuspendClientSpec {
         }
 
         Assertions.assertEquals(response, "{\"newState\":\"test\"}")
+    }
+
+    @Test
+    fun testNotFound() {
+        val server = ApplicationContext.run(EmbeddedServer::class.java, mapOf("spec.name" to "SuspendClientSpec"))
+        val ctx = server.applicationContext
+        val response = runBlocking {
+            ctx.getBean(SuspendClient::class.java).notFound()
+        }
+
+        Assertions.assertEquals(response.status, HttpStatus.NOT_FOUND)
     }
 }
