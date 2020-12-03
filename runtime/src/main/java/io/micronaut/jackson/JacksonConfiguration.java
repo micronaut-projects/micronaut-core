1ef08864371091e4b65c2b4ffbaf357e61e1932a
Rafael Renan Pacheco <rafael.renan.pacheco@gmail.com>
2020-12-02 21:30:31 -0300

    optimize imports

--- runtime/src/main/java/io/micronaut/jackson/JacksonConfiguration.java
+++ runtime/src/main/java/io/micronaut/jackson/JacksonConfiguration.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import io.micronaut.context.annotation.ConfigurationProperties;
 import io.micronaut.core.annotation.Experimental;
 import io.micronaut.core.annotation.TypeHint;
@@ -27,8 +28,6 @@ import io.micronaut.core.type.Argument;
 import io.micronaut.core.util.ArgumentUtils;
 import io.micronaut.core.util.ArrayUtils;
 import io.micronaut.core.util.CollectionUtils;
-
-import edu.umd.cs.findbugs.annotations.NonNull;
 
 import java.util.*;
 
