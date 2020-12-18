71609081093512576831125676ccf1c5fd1cfa90
James Kleeh <james.kleeh@gmail.com>
2020-12-18 14:02:41 -0500

    Locale resolver (#4717)
    * Initial cut of locale resolution
    
    * Checkstyle. Make interface generic. Docs
    
    * refactor: changes to locale resolver (#4727)
    
    * refactor: changes to locale resolver
    
    * :%s/DefaultLocaleResolver/AbstractLocaleResolver
    
    * add a padding to fixed locale resolver
    
    * don't sort locale resolvers they are already sorted
    
    * remove @Indexed
    
    * don't override getOrder in Composite
    
    * make test more robust for ordering
    
    * don't extend FixedLocaleResolver from AbstractLocaleResolver
    
    * Split default and fix configuration
    
    * Rename package. Remove unnecessary interfaces
    
    * Fix tests
    
    * Add javadoc
    
    * Add more javadoc
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

---
+++ http-server/src/main/java/io/micronaut/http/server/util/locale/CompositeHttpLocaleResolver.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.server.util.locale;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import io.micronaut.context.annotation.Primary;
+import io.micronaut.http.HttpRequest;
+
+import javax.inject.Singleton;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Optional;
+
+/**
+ * {@link Primary} {@link HttpLocaleResolver} which evaluates every {@link HttpLocaleResolver} by order to resolve a {@link java.util.Locale}.
+ *
+ * @author Sergio del Amo
+ * @author James Kleeh
+ * @since 2.3.0
+ */
+@Singleton
+@Primary
+public class CompositeHttpLocaleResolver extends HttpAbstractLocaleResolver {
+
+    private final HttpLocaleResolver[] localeResolvers;
+
+    /**
+     * @param localeResolvers HTTP Locale Resolvers
+     * @param httpLocaleResolutionConfiguration Locale Resolution configuration for HTTP Requests
+     */
+    public CompositeHttpLocaleResolver(HttpLocaleResolver[] localeResolvers,
+                                       HttpLocaleResolutionConfiguration httpLocaleResolutionConfiguration) {
+        super(httpLocaleResolutionConfiguration);
+        this.localeResolvers = localeResolvers;
+    }
+
+    @Override
+    @NonNull
+    public Optional<Locale> resolve(@NonNull HttpRequest<?> request) {
+        return Arrays.stream(localeResolvers)
+                .map(resolver -> resolver.resolve(request))
+                .filter(Optional::isPresent)
+                .findFirst()
+                .orElse(Optional.empty());
+    }
+}
---
+++ http-server/src/main/java/io/micronaut/http/server/util/locale/CookieLocaleResolver.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.server.util.locale;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.server.HttpServerConfiguration;
+
+import javax.inject.Singleton;
+import java.util.Locale;
+import java.util.Optional;
+
+/**
+ * Resolves the Locale from a Cookie within a HTTP Request.
+ *
+ * @author Sergio del Amo
+ * @author James Kleeh
+ * @since 2.3.0
+ */
+@Singleton
+@Requires(property = HttpServerConfiguration.HttpLocaleResolutionConfigurationProperties.PREFIX + ".cookie-name")
+public class CookieLocaleResolver extends HttpAbstractLocaleResolver {
+
+    private final String cookieName;
+
+    /**
+     * @param httpLocaleResolutionConfiguration Locale resolution configuration
+     */
+    public CookieLocaleResolver(HttpLocaleResolutionConfiguration httpLocaleResolutionConfiguration) {
+        super(httpLocaleResolutionConfiguration);
+        this.cookieName = httpLocaleResolutionConfiguration.getCookieName()
+                .orElseThrow(() -> new IllegalArgumentException("The locale cookie name must be set"));
+    }
+
+    @Override
+    @NonNull
+    public Optional<Locale> resolve(@NonNull HttpRequest<?> request) {
+        return request.getCookies().get(cookieName, Locale.class);
+    }
+}
