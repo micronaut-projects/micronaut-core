3c9d0016204030d84a11293a86f0c46f8abd96b0
graemerocher <graeme.rocher@gmail.com>
2021-02-16 15:08:53 +0100

    checkstyle

--- inject/src/main/java/io/micronaut/inject/annotation/internal/CoreNullableTransformer.java
+++ inject/src/main/java/io/micronaut/inject/annotation/internal/CoreNullableTransformer.java
@@ -18,9 +18,6 @@ package io.micronaut.inject.annotation.internal;
 import io.micronaut.core.annotation.AnnotationUtil;
 import io.micronaut.core.annotation.AnnotationValue;
 import io.micronaut.core.annotation.Internal;
-import io.micronaut.core.annotation.NonNull;
-import io.micronaut.core.naming.NameUtils;
-import io.micronaut.inject.annotation.AnnotationRemapper;
 import io.micronaut.inject.annotation.NamedAnnotationTransformer;
 import io.micronaut.inject.visitor.VisitorContext;
 
@@ -49,3 +46,4 @@ public class CoreNullableTransformer implements NamedAnnotationTransformer {
         );
     }
 }
+
