99bbefa124b5d7a5b370b40c476aef3bb7b4771b
Graeme Rocher <graeme.rocher@oracle.com>
2021-02-27 16:05:00 +0100

    Don't create AOP advice for outer class of inner bean. Fixes #4987

--- inject-java/src/main/java/io/micronaut/annotation/processing/BeanDefinitionInjectProcessor.java
+++ inject-java/src/main/java/io/micronaut/annotation/processing/BeanDefinitionInjectProcessor.java
@@ -403,8 +403,11 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
                     this.currentClassMetadata = typeAnnotationMetadata;
                 }
             }
+            // don't process inner class unless this is the visitor for it
+            final Name qualifiedName = concreteClass.getQualifiedName();
+            boolean isTypeConcreteClass = qualifiedName.equals(classElementQualifiedName);
 
-            if (typeAnnotationMetadata.hasStereotype(INTRODUCTION_TYPE)) {
+            if (typeAnnotationMetadata.hasStereotype(INTRODUCTION_TYPE) && isTypeConcreteClass) {
                 AopProxyWriter aopProxyWriter = createIntroductionAdviceWriter(concreteClassElement);
 
                 if (constructorElement != null) {
@@ -431,12 +434,8 @@ public class BeanDefinitionInjectProcessor extends AbstractInjectAnnotationProce
 
             } else {
                 Element enclosingElement = classElement.getEnclosingElement();
-                // don't process inner class unless this is the visitor for it
-                final Name qualifiedName = concreteClass.getQualifiedName();
-                if (!JavaModelUtils.isClass(enclosingElement) ||
-                        qualifiedName.equals(classElementQualifiedName)) {
-
-                    if (qualifiedName.equals(classElementQualifiedName)) {
+                if (!JavaModelUtils.isClass(enclosingElement) || isTypeConcreteClass) {
+                    if (isTypeConcreteClass) {
                         if (isDeclaredBean) {
                             // we know this class has supported annotations so we need a beandef writer for it
                             PackageElement packageElement = elementUtils.getPackageOf(classElement);
