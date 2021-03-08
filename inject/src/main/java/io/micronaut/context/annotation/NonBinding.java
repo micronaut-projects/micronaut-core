a9afeb789b7b177099c218932ac1edd2d4299703
graemerocher <graeme.rocher@gmail.com>
2021-03-08 18:44:32 +0100

    Add author and since tags

--- inject/src/main/java/io/micronaut/context/annotation/NonBinding.java
+++ inject/src/main/java/io/micronaut/context/annotation/NonBinding.java
@@ -23,6 +23,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Excludes a member of an annotation type (such as a qualifier type or interceptor binding type) from consideration when the container compares two annotation instances.
+ *
+ * @author graemerocher
+ * @since 3.0
  */
 @Retention(RUNTIME)
 @Target(ElementType.METHOD)
