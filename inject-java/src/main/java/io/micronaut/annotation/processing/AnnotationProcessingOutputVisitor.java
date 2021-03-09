b0b326962c34c79acc442e6d2aad7aa5949fa997
Denis Stepanov <denis.s.stepanov@oracle.com>
2021-03-09 08:50:01 -0600

    Improve performance of writing generated classes (#5085)

--- inject-java/src/main/java/io/micronaut/annotation/processing/AnnotationProcessingOutputVisitor.java
+++ inject-java/src/main/java/io/micronaut/annotation/processing/AnnotationProcessingOutputVisitor.java
@@ -16,6 +16,7 @@
 package io.micronaut.annotation.processing;
 
 import edu.umd.cs.findbugs.annotations.Nullable;
+import io.micronaut.core.reflect.ReflectionUtils;
 import io.micronaut.core.util.ArrayUtils;
 import io.micronaut.inject.writer.AbstractClassWriterOutputVisitor;
 import io.micronaut.inject.writer.ClassGenerationException;
@@ -27,11 +28,13 @@ import javax.tools.FileObject;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardLocation;
 import java.io.FileNotFoundException;
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.util.*;
 
@@ -39,7 +42,7 @@ import java.util.*;
  * An implementation of {@link io.micronaut.inject.writer.ClassWriterOutputVisitor} for annotation processing.
  *
  * @author Graeme Rocher
- * @since 1.
+ * @since 1.0
  */
 public class AnnotationProcessingOutputVisitor extends AbstractClassWriterOutputVisitor {
 
@@ -48,6 +51,17 @@ public class AnnotationProcessingOutputVisitor extends AbstractClassWriterOutput
     private final Map<String, FileObject> openedFiles = new LinkedHashMap<>();
     private final Map<String, Optional<GeneratedFile>> generatedFiles = new LinkedHashMap<>();
     private final boolean isGradleFiler;
+
+    private static final Field FILTER_OUTPUT_STREAM_OUT = ReflectionUtils.findField(FilterOutputStream.class, "out")
+            .map(field -> {
+                try {
+                    field.setAccessible(true);
+                    return field;
+                } catch (Exception e) {
+                    return null;
+                }
+            })
+            .orElse(null);
 
     /**
      * @param filer The {@link Filer} for creating new files
@@ -96,7 +110,52 @@ public class AnnotationProcessingOutputVisitor extends AbstractClassWriterOutput
             nativeOriginatingElements = new Element[0];
         }
         javaFileObject = filer.createClassFile(classname, nativeOriginatingElements);
-        return javaFileObject.openOutputStream();
+        OutputStream os = javaFileObject.openOutputStream();
+        return unwrapFilterOutputStream(os);
+    }
+
+    private OutputStream unwrapFilterOutputStream(OutputStream os) {
+        // https://bugs.openjdk.java.net/browse/JDK-8255729
+        // FilterOutputStream and JavacFiler$FilerOutputStream is always using write(int) and killing performance, unwrap if possible
+        if (FILTER_OUTPUT_STREAM_OUT != null && os instanceof FilterOutputStream) {
+            try {
+                OutputStream osToWrite = (OutputStream) FILTER_OUTPUT_STREAM_OUT.get(os);
+                if (osToWrite == null) {
+                    return os;
+                }
+                return new OutputStream() {
+                    @Override
+                    public void write(int b) throws IOException {
+                        osToWrite.write(b);
+                    }
+
+                    @Override
+                    public void write(byte[] b) throws IOException {
+                        osToWrite.write(b);
+                    }
+
+                    @Override
+                    public void write(byte[] b, int off, int len) throws IOException {
+                        osToWrite.write(b, off, len);
+                    }
+
+                    @Override
+                    public void flush() throws IOException {
+                        osToWrite.flush();
+                    }
+
+                    @Override
+                    public void close() throws IOException {
+                        // Close original output stream
+                        os.close();
+                    }
+                };
+            } catch (Exception e) {
+                // Use original output stream if we cannot unwrap it
+                return os;
+            }
+        }
+        return os;
     }
 
     @Override
