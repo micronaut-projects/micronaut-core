d7d725d0a10cdb7f5e718ebec208ce997c95a131
Denis Stepanov <denis.s.stepanov@oracle.com>
2021-05-05 08:06:52 -0600

    Improve bean context startup performance (#5352)

--- inject-java-test/src/main/groovy/io/micronaut/annotation/processing/test/AbstractTypeElementSpec.groovy
+++ inject-java-test/src/main/groovy/io/micronaut/annotation/processing/test/AbstractTypeElementSpec.groovy
@@ -49,6 +49,8 @@ import javax.lang.model.element.TypeElement
 import javax.lang.model.element.VariableElement
 import javax.tools.JavaFileObject
 import java.lang.annotation.Annotation
+import java.util.function.Predicate
+
 /**
  * Base class to extend from to allow compilation of Java sources
  * at runtime to allow testing of compile time behavior.
@@ -217,14 +219,14 @@ class Test {
 
         return new DefaultApplicationContext(ClassPathResourceLoader.defaultLoader(classLoader), "test") {
             @Override
-            protected List<BeanDefinitionReference> resolveBeanDefinitionReferences() {
+            protected List<BeanDefinitionReference> resolveBeanDefinitionReferences(Predicate<BeanDefinitionReference> predicate) {
                 files.findAll { JavaFileObject jfo ->
                     jfo.kind == JavaFileObject.Kind.CLASS && jfo.name.endsWith("DefinitionClass.class")
                 }.collect { JavaFileObject jfo ->
                     def name = jfo.toUri().toString().substring("mem:///CLASS_OUTPUT/".length())
                     name = name.replace('/', '.') - '.class'
                     return classLoader.loadClass(name).newInstance()
-                } as List<BeanDefinitionReference>
+                }.findAll { predicate == null || predicate.test(it) } as List<BeanDefinitionReference>
             }
         }.start()
     }
