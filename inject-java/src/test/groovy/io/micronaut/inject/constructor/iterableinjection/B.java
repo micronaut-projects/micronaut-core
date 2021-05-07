23ad94c4e12fc8a7f2b3c571a07f75b285d4e95d
Matthew Moss <mossm@objectcomputing.com>
2021-05-07 18:30:29 -0500

    Update copyright year.

--- inject-java/src/test/groovy/io/micronaut/inject/constructor/iterableinjection/B.java
+++ inject-java/src/test/groovy/io/micronaut/inject/constructor/iterableinjection/B.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 original authors
+ * Copyright 2017-2021 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 package io.micronaut.inject.constructor.iterableinjection;
 
 import javax.inject.Inject;
-import java.util.Collection;
 
 public class B {
     private final Iterable<A> all;
