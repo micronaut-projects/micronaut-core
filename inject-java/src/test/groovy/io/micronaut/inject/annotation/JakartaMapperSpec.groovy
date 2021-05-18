86c5107b84cfdcef8c901c9b570e89fddcb8dca4
graemerocher <graeme.rocher@gmail.com>
2021-05-18 12:43:26 +0200

    Fix jakarta annotations appearing in stereotypes

--- inject-java/src/test/groovy/io/micronaut/inject/annotation/JakartaMapperSpec.groovy
+++ inject-java/src/test/groovy/io/micronaut/inject/annotation/JakartaMapperSpec.groovy
@@ -2,6 +2,7 @@ package io.micronaut.inject.annotation
 
 import io.micronaut.annotation.processing.test.AbstractTypeElementSpec
 
+import javax.inject.Named
 import javax.inject.Qualifier
 import javax.inject.Scope
 import javax.inject.Singleton
@@ -24,6 +25,39 @@ class Test {
         metadata.hasDeclaredAnnotation(Singleton)
         metadata.hasDeclaredStereotype(Scope)
         metadata.getAnnotationNamesByStereotype(Scope.class).size() == 1
+        metadata.getAnnotationNamesByStereotype(Scope.class).contains(Singleton.name)
         metadata.getAnnotationNamesByStereotype(Qualifier.class).size() == 1
+        metadata.getAnnotationNamesByStereotype(Qualifier.class).contains(Named.name)
+    }
+
+    void "test annotation mapper map stereotypes correctly with meta annotations"() {
+        def metadata = buildTypeAnnotationMetadata('''
+package test;
+
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.*;
+
+@Meta
+class Test {
+
+}
+
+@jakarta.inject.Singleton
+@jakarta.inject.Named("test")
+@Retention(RUNTIME)
+@interface Meta {
+
+}
+''')
+
+        expect:
+        metadata != null
+        metadata.hasDeclaredStereotype(Singleton)
+        !metadata.hasStereotype(jakarta.inject.Singleton)
+        metadata.hasDeclaredStereotype(Scope)
+        metadata.getAnnotationNamesByStereotype(Scope.class).size() == 1
+        metadata.getAnnotationNamesByStereotype(Scope.class).contains(Singleton.name)
+        metadata.getAnnotationNamesByStereotype(Qualifier.class).size() == 1
+        metadata.getAnnotationNamesByStereotype(Qualifier.class).contains(Named.name)
     }
 }
