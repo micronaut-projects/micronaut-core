741141ee4386370b4f007aefdb7b411f6938b969
jameskleeh <james.kleeh@gmail.com>
2021-05-26 09:50:09 -0400

    Add failing test for scope issue

--- inject-java/src/test/groovy/io/micronaut/inject/scope/SingletonAnn.java
+++ inject-java/src/test/groovy/io/micronaut/inject/scope/SingletonAnn.java
@@ -1,2 +1,17 @@
-package io.micronaut.inject.scope;public @interface SingletonAnn {
+package io.micronaut.inject.scope;
+
+import jakarta.inject.Singleton;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Singleton
+public @interface SingletonAnn {
 }
--- inject-java/src/test/groovy/io/micronaut/inject/scope/StereotypeSpec.groovy
+++ inject-java/src/test/groovy/io/micronaut/inject/scope/StereotypeSpec.groovy
@@ -1,4 +1,25 @@
 package io.micronaut.inject.scope
 
-class StereotypeSpec {
+import io.micronaut.annotation.processing.test.AbstractTypeElementSpec
+import io.micronaut.core.annotation.AnnotationUtil
+import io.micronaut.inject.BeanDefinition
+
+class StereotypeSpec extends AbstractTypeElementSpec {
+
+    void "test singleton is a declared stereotype"() {
+        when:
+        BeanDefinition beanDefinition = buildBeanDefinition("test.Test", """
+package test;
+
+import io.micronaut.inject.scope.SingletonAnn;
+
+@SingletonAnn
+class Test {
+
+}
+""")
+
+        then:
+        beanDefinition.getDeclaredAnnotationNameByStereotype(AnnotationUtil.SCOPE).get() == "io.micronaut.inject.scope.SingletonAnn"
+    }
 }
