95f8d7c2fb60c04dd6a3522f5afac668533bcf9a
Subhadeep Sen <subhadeep0977@gmail.com>
2021-06-02 22:07:59 +0530

    setting JAEGER_SERVICE_NAME system prop if not present in system prop or env

--- tracing/src/main/java/io/micronaut/tracing/jaeger/JaegerConfiguration.java
+++ tracing/src/main/java/io/micronaut/tracing/jaeger/JaegerConfiguration.java
@@ -63,7 +63,8 @@ public class JaegerConfiguration implements Toggleable  {
      */
     public JaegerConfiguration(
             ApplicationConfiguration applicationConfiguration) {
-        if (StringUtils.isEmpty(System.getProperty(JAEGER_SERVICE_NAME))) {
+		if (StringUtils.isEmpty(System.getProperty(JAEGER_SERVICE_NAME))
+				&& StringUtils.isEmpty(System.getenv(JAEGER_SERVICE_NAME))) {
             System.setProperty(JAEGER_SERVICE_NAME, applicationConfiguration.getName().orElse(Environment.DEFAULT_NAME));
         }
         configuration = Configuration.fromEnv();
