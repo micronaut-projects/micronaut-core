0f6cec56d635326911077fb221df40f0c4d9fbe6
James Kleeh <james.kleeh@gmail.com>
2021-06-10 09:57:26 -0400

    Delay emission of response headers until at least 1 item is emitted in a streaming response (#5558)
    * Delay emission of response headers until at least 1 item is emitted in a streaming response. Fixes #5488
    
    * Fix test. Emit message if publisher was empty
    
    * Make class final

--- http-server-netty/src/main/java/io/micronaut/http/server/netty/RoutingInBoundHandler.java
+++ http-server-netty/src/main/java/io/micronaut/http/server/netty/RoutingInBoundHandler.java
@@ -74,6 +74,7 @@ import io.micronaut.http.netty.AbstractNettyHttpRequest;
 import io.micronaut.http.netty.NettyHttpResponseBuilder;
 import io.micronaut.http.netty.NettyMutableHttpResponse;
 import io.micronaut.http.netty.content.HttpContentUtil;
+import io.micronaut.http.netty.stream.ArrayBracketSubscriber;
 import io.micronaut.http.netty.stream.StreamedHttpRequest;
 import io.micronaut.http.server.binding.RequestArgumentSatisfier;
 import io.micronaut.http.server.exceptions.ExceptionHandler;
@@ -1359,32 +1360,8 @@ class RoutingInBoundHandler extends SimpleChannelInboundHandler<io.micronaut.htt
                                 if (isJson) {
                                     // if the Publisher is returning JSON then in order for it to be valid JSON for each emitted element
                                     // we must wrap the JSON in array and delimit the emitted items
-                                    httpContentPublisher = Flowable.concat(
-                                            Flowable.fromCallable(HttpContentUtil::openBracket),
-                                            httpContentPublisher,
-                                            Flowable.fromCallable(HttpContentUtil::closeBracket)
-                                    );
-                                }
-
-                                if (mediaType.equals(MediaType.TEXT_EVENT_STREAM_TYPE)) {
-                                    httpContentPublisher = Publishers.onComplete(httpContentPublisher, () -> {
-                                        CompletableFuture<Void> future = new CompletableFuture<>();
-                                        if (!request.getHeaders().isKeepAlive()) {
-                                            if (context.channel().isOpen()) {
-                                                context.pipeline()
-                                                        .writeAndFlush(new DefaultLastHttpContent())
-                                                        .addListener(f -> {
-                                                                    if (f.isSuccess()) {
-                                                                        future.complete(null);
-                                                                    } else {
-                                                                        future.completeExceptionally(f.cause());
-                                                                    }
-                                                                }
-                                                        );
-                                            }
-                                        }
-                                        return future;
-                                    });
+                                    httpContentPublisher = Flowable.fromPublisher(httpContentPublisher)
+                                            .lift((FlowableOperator<HttpContent, HttpContent>) ArrayBracketSubscriber::new);
                                 }
 
                                 httpContentPublisher = Publishers.then(httpContentPublisher, httpContent ->
