7abe0801ce2ee3e8e9a812cd1d1c502897a5aa5a
phil.hardwick <philipchardwick@gmail.com>
2021-06-15 12:38:15 +0100

    Fix multiple factory replacements

---
+++ inject-java/src/test/groovy/io/micronaut/inject/qualifiers/replaces/DSecondFactoryMethodReplacement.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.inject.qualifiers.replaces;
+
+import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.context.annotation.Requirements;
+import io.micronaut.context.annotation.Requires;
+
+import javax.inject.Singleton;
+
+@Factory
+@Requires(env = "factory-replacement-chain")
+public class DSecondFactoryMethodReplacement {
+
+    @Singleton
+    @Replaces(value = D.class, factory = DFactoryMethodReplacement.class)
+    D getD() {
+        return new D3();
+    }
+}
