1821a703975cb925a5353f84f38040394c701f07
Sergio del Amo <sergio.delamo@softamo.com>
2021-06-15 18:49:10 +0200

    Use HttpClient instead of ReactorHttpClient

--- function-client/src/main/java/io/micronaut/function/client/http/HttpFunctionExecutor.java
+++ function-client/src/main/java/io/micronaut/function/client/http/HttpFunctionExecutor.java
@@ -27,6 +27,7 @@ import io.micronaut.function.client.exceptions.FunctionNotFoundException;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.MutableHttpRequest;
+import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.ReactorHttpClient;
 import jakarta.inject.Singleton;
 import org.reactivestreams.Publisher;
@@ -48,13 +49,13 @@ import java.util.Optional;
 @Singleton
 public class HttpFunctionExecutor<I, O> implements FunctionInvoker<I, O>, Closeable, FunctionInvokerChooser {
 
-    private final ReactorHttpClient httpClient;
+    private final HttpClient httpClient;
 
     /**
      * Constructor.
      * @param httpClient  The HTTP client
      */
-    public HttpFunctionExecutor(ReactorHttpClient httpClient) {
+    public HttpFunctionExecutor(HttpClient httpClient) {
         super();
         this.httpClient = httpClient;
     }
