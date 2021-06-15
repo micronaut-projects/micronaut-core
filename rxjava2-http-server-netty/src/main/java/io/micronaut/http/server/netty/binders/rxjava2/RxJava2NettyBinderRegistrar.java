dbdbdbe45120af9d628263abfe9cfdf250b3ba37
Sergio del Amo <sergio.delamo@softamo.com>
2021-06-15 18:08:58 +0200

    ObservableBodyBinder to rxjava2-http-server-netty

--- rxjava2-http-server-netty/src/main/java/io/micronaut/http/server/netty/binders/rxjava2/RxJava2NettyBinderRegistrar.java
+++ rxjava2-http-server-netty/src/main/java/io/micronaut/http/server/netty/binders/rxjava2/RxJava2NettyBinderRegistrar.java
@@ -38,6 +38,10 @@ class RxJava2NettyBinderRegistrar implements BeanCreatedEventListener<RequestBin
                 conversionService,
                 httpContentProcessorResolver
         ));
+        registry.addRequestArgumentBinder(new ObservableBodyBinder(
+                conversionService,
+                httpContentProcessorResolver
+        ));
         registry.addRequestArgumentBinder(new SingleBodyBinder(
                 conversionService,
                 httpContentProcessorResolver
