da45cea0d58f77aad87b82052b058cf2b2c908af
Sergio del Amo <sergio.delamo@softamo.com>
2021-06-22 17:55:36 +0200

    avoid bean pollution

--- http-client/src/test/groovy/io/micronaut/http/client/sse/ServerSentEventMultilineSpec.groovy
+++ http-client/src/test/groovy/io/micronaut/http/client/sse/ServerSentEventMultilineSpec.groovy
@@ -1,10 +1,18 @@
 package io.micronaut.http.client.sse
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
 import io.micronaut.context.ApplicationContext
-import io.micronaut.http.client.sse.ServerSentEventStreamingSpec.Product
-import io.micronaut.http.client.sse.ServerSentEventStreamingSpec.ProductClient
+import io.micronaut.context.annotation.Requires
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.sse.Event
 import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
+import reactor.core.publisher.Flux
 import spock.lang.AutoCleanup
 import spock.lang.Issue
 import spock.lang.Shared
@@ -13,7 +21,7 @@ import spock.lang.Specification
 @Issue("https://github.com/micronaut-projects/micronaut-core/issues/2586")
 class ServerSentEventMultilineSpec extends Specification {
 
-    @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, ['jackson.serialization.indentOutput': true])
+    @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, ['jackson.serialization.indentOutput': true, 'spec.name': 'ServerSentEventMultilineSpec'])
     @Shared @AutoCleanup ReactorSseClient sseClient = embeddedServer.applicationContext.createBean(ReactorSseClient, embeddedServer.getURL())
     @Shared ProductClient productClient = embeddedServer.applicationContext.getBean(ProductClient)
 
@@ -40,5 +48,51 @@ class ServerSentEventMultilineSpec extends Specification {
         results[1].quantity == 2
         results[2].name == "Banana"
         results[3].name == "Kiwi"
+    }
+
+    @Requires(property = 'spec.name', value = 'ServerSentEventMultilineSpec')
+    @Client("/stream/sse")
+    static interface ProductClient {
+
+        @Get(value = '/pojo/objects', processes = MediaType.TEXT_EVENT_STREAM)
+        Flux<Product> pojoStream()
+
+    }
+
+    @Requires(property = 'spec.name', value = 'ServerSentEventMultilineSpec')
+    @Controller("/stream/sse")
+    @ExecuteOn(TaskExecutors.IO)
+    static class SseController {
+
+        @Get(value = '/pojo/objects', produces = MediaType.TEXT_EVENT_STREAM)
+        Flux<Product> pojoStream() {
+            return Flux.fromIterable(dataSet().collect { it.data })
+        }
+
+        @Get(value = '/pojo/events', produces = MediaType.TEXT_EVENT_STREAM)
+        Flux<Event<Product>> pojoEventStream() {
+            return Flux.fromIterable(dataSet())
+        }
+    }
+
+    @EqualsAndHashCode
+    @ToString(includePackage = false)
+    static class Product {
+        String name
+        int quantity = 1
+    }
+
+    static List<Event<Product>> dataSet() {
+        [
+                Event.of(new Product(name: "Apple")),
+                Event.of(new Product(name: "Orange", quantity: 2))
+                        .id('o1')
+                        .comment("From Valencia"),
+                Event.of(new Product(name: "Banana", quantity: 5)),
+                Event.of(new Product(name: "Kiwi", quantity: 15))
+                        .comment("Green")
+                        .id('k1')
+
+        ]
     }
 }
