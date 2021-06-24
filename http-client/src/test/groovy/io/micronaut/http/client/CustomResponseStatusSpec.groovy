13ab2be8b1e66df48ff21e13d3bd86d0e61efed1
Hernán Cervera <hjcerveram@hotmail.com>
2021-06-24 17:12:15 -0500

    Refactor test cases to reduce repeated code

--- http-client/src/test/groovy/io/micronaut/http/client/CustomResponseStatusSpec.groovy
+++ http-client/src/test/groovy/io/micronaut/http/client/CustomResponseStatusSpec.groovy
@@ -19,13 +19,13 @@ class CustomResponseStatusSpec extends Specification {
     @Inject
     EmbeddedServer embeddedServer
 
-    void "test response status custom code and generic reason, from HttpResponse"() {
+    void "test response status custom code and generic reason"() {
         given:
         HttpClient client = HttpClient.create(embeddedServer.getURL())
         HttpResponse response = null
 
-        URI uri = UriBuilder.of("/status-in-HttpResponse")
-                .queryParam("code", requiredStatusCode)
+        URI uri = UriBuilder.of("/status-in-${targetObject}")
+                .queryParam("code", requiredCode)
                 .build()
 
         try {
@@ -35,108 +35,56 @@ class CustomResponseStatusSpec extends Specification {
             HttpStatus status = ex.getResponse().getStatus()
             if (status.getCode() < 400) {
                 throw new RuntimeException(
-                        ex.getClass().getName() + "should not be thrown on status code ${status.getCode()}")
+                        "${ex.getClass().getName()} should not be thrown on status code ${status.getCode()}")
             }
             response = ex.getResponse()
         }
 
         expect:
-        response.getStatus().getCode() == responseStatusCode
+        response.getStatus().getCode() == responseCode
         response.getStatus().getReason() == responseReason
 
         cleanup:
         client.close()
 
         where:
-        requiredStatusCode | responseStatusCode | responseReason
-        180                | 180                | 'Informational (180)'
-        280                | 280                | 'Success (280)'
-        380                | 380                | 'Redirection (380)'
-        480                | 480                | 'Client Error (480)'
-        580                | 580                | 'Server Error (580)'
-        680                | 680                | 'Unknown Status (680)'
+        targetObject          | requiredCode | responseCode | responseReason
+        'HttpResponse'        | 180          | 180          | 'Informational (180)'
+        'HttpResponse'        | 280          | 280          | 'Success (280)'
+        'HttpResponse'        | 380          | 380          | 'Redirection (380)'
+        'HttpResponse'        | 480          | 480          | 'Client Error (480)'
+        'HttpResponse'        | 580          | 580          | 'Server Error (580)'
+        'HttpResponse'        | 680          | 680          | 'Unknown Status (680)'
+        'MutableHttpResponse' | 180          | 180          | 'Informational (180)'
+        'MutableHttpResponse' | 280          | 280          | 'Success (280)'
+        'MutableHttpResponse' | 380          | 380          | 'Redirection (380)'
+        'MutableHttpResponse' | 480          | 480          | 'Client Error (480)'
+        'MutableHttpResponse' | 580          | 580          | 'Server Error (580)'
+        'MutableHttpResponse' | 680          | 680          | 'Unknown Status (680)'
     }
 
-    void "test response status custom code and generic reason, from MutableHttpResponse"() {
+    void "test response status custom code and reason"() {
         given:
         HttpClient client = HttpClient.create(embeddedServer.getURL())
-        HttpResponse response = null
 
-        URI uri = UriBuilder.of("/status-in-MutableHttpResponse")
-                .queryParam("code", requiredStatusCode)
+        URI uri = UriBuilder.of("/status-in-${targetObject}")
+                .queryParam("code", requiredCode)
+                .queryParam("reason", requiredReason)
                 .build()
-
-        try {
-            response = client.toBlocking().exchange(
-                    HttpRequest.GET(uri))
-        } catch(HttpClientResponseException ex) {
-            HttpStatus status = ex.getResponse().getStatus()
-            if (status.getCode() < 400) {
-                throw new RuntimeException(
-                        ex.getClass().getName() + "should not be thrown on status code ${status.getCode()}")
-            }
-            response = ex.getResponse()
-        }
+        HttpResponse response = client.toBlocking()
+                .exchange(HttpRequest.GET(uri))
 
         expect:
-        response.getStatus().getCode() == responseStatusCode
+        response.getStatus().getCode() == responseCode
         response.getStatus().getReason() == responseReason
 
         cleanup:
         client.close()
 
         where:
-        requiredStatusCode | responseStatusCode | responseReason
-        180                | 180                | 'Informational (180)'
-        280                | 280                | 'Success (280)'
-        380                | 380                | 'Redirection (380)'
-        480                | 480                | 'Client Error (480)'
-        580                | 580                | 'Server Error (580)'
-        680                | 680                | 'Unknown Status (680)'
-    }
-
-    void "test response status custom code and reason, from HttpResponse"() {
-        given:
-        int code = 380
-        String reason = "My Custom Reason"
-        HttpClient client = HttpClient.create(embeddedServer.getURL())
-
-        when:
-        URI uri = UriBuilder.of("/status-in-HttpResponse")
-                .queryParam("code", code)
-                .queryParam("reason", reason)
-                .build()
-        HttpResponse response = client.toBlocking()
-                .exchange(HttpRequest.GET(uri))
-
-        then:
-        response.getStatus().getCode() == code
-        response.getStatus().getReason() == reason
-
-        cleanup:
-        client.close()
-    }
-
-    void "test response status custom code and reason, from MutableHttpResponse"() {
-        given:
-        int code = 380
-        String reason = "My Custom Reason"
-        HttpClient client = HttpClient.create(embeddedServer.getURL())
-
-        when:
-        URI uri = UriBuilder.of("/status-in-MutableHttpResponse")
-                .queryParam("code", code)
-                .queryParam("reason", reason)
-                .build()
-        HttpResponse response = client.toBlocking()
-                .exchange(HttpRequest.GET(uri))
-
-        then:
-        response.getStatus().getCode() == code
-        response.getStatus().getReason() == reason
-
-        cleanup:
-        client.close()
+        targetObject          | requiredCode | requiredReason | responseCode | responseReason
+        'HttpResponse'        | 380          | 'My Reason'    | 380          | 'My Reason'
+        'MutableHttpResponse' | 380          | 'My Reason'    | 380          | 'My Reason'
     }
 
     @Controller
