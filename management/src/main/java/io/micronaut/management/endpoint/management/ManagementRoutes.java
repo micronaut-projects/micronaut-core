9d16bc8563c3bced9e6f249781e74a74e482334d
Hernán Cervera <hjcerveram@hotmail.com>
2021-07-06 17:57:17 -0500

    Return management endpoints in hateoas form

---
+++ management/src/main/java/io/micronaut/management/endpoint/management/ManagementRoutes.java
@@ -0,0 +1,6 @@
+package io.micronaut.management.endpoint.management;
+
+import io.micronaut.http.hateoas.AbstractResource;
+
+public class ManagementRoutes extends AbstractResource<ManagementRoutes> {
+}
