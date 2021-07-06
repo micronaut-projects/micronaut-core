731c7827cec755d2376ac1d946176906822065af
Graeme Rocher <graeme.rocher@oracle.com>
2021-07-06 11:19:52 +0200

    Use the owning type name for child factory beans. Fixes #5704 (#5707)

---
+++ inject-java/src/test/groovy/io/micronaut/inject/factory/inheritance/FactoryAbstractInheritanceSpec.groovy
@@ -0,0 +1,59 @@
+package io.micronaut.inject.factory.inheritance
+
+import io.micronaut.annotation.processing.test.AbstractTypeElementSpec
+import io.micronaut.aop.InterceptedProxy
+import io.micronaut.inject.BeanDefinition
+
+class FactoryAbstractInheritanceSpec extends AbstractTypeElementSpec {
+
+    void "test that beans are generated for child factories but not parent"() {
+        given:
+        def context = buildContext('''
+package factinhertest;
+
+import jakarta.inject.Singleton;
+import io.micronaut.runtime.context.scope.Refreshable;
+import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Replaces;
+
+
+@Factory
+class MyMockPublisherFactory extends MockPublisherFactory {
+    
+}
+
+abstract class MockPublisherFactory {
+    @Refreshable
+    @Singleton
+    @Replaces(PublisherFactory.class)
+    Publisher createPublisher() {
+        return new MockPublisher();
+    }
+}
+
+class MockPublisher implements Publisher {}
+
+interface Publisher {}
+interface PublisherFactory {
+    Publisher createPublisher();
+}   
+class ConcretePublisher implements Publisher {}
+
+@Singleton
+class DefaultPublisherFactory implements PublisherFactory {
+    public Publisher createPublisher() {
+        return new ConcretePublisher();
+    }
+}
+''')
+        def bean = getBean(context, 'factinhertest.Publisher')
+        BeanDefinition definition = context.getBeanDefinition(
+                context.classLoader.loadClass('factinhertest.Publisher')
+        )
+
+        expect:
+        bean instanceof InterceptedProxy
+        bean.interceptedTarget().getClass().simpleName == 'MockPublisher'
+        definition.getClass().name.contains('MyMockPublisherFactory')
+    }
+}
