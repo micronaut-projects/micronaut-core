ab66e81fb1b760dff25fbd03a40deeec0da17c4f
Sergio del Amo <sergio.delamo@softamo.com>
2021-07-07 08:53:41 +0200

    test: remove @Ignore

--- http-client/src/test/groovy/io/micronaut/http/client/OctetStreamSpec.groovy
+++ http-client/src/test/groovy/io/micronaut/http/client/OctetStreamSpec.groovy
@@ -13,41 +13,44 @@ import io.micronaut.runtime.server.EmbeddedServer
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
 import spock.lang.AutoCleanup
-import spock.lang.Ignore
+import spock.lang.IgnoreIf
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.util.environment.Jvm
+
 import java.nio.charset.StandardCharsets
 
 class OctetStreamSpec extends Specification {
 
     @Shared
     @AutoCleanup
-    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer,['micronaut.server.max-request-size': '10KB',
-                                                                                                'spec.name': 'OctetStreamSpec',])
-    @Shared UploadClient client = embeddedServer.applicationContext.getBean(UploadClient)
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, ['micronaut.server.max-request-size': '10KB',
+                                                                            'spec.name': 'OctetStreamSpec',])
+
+    @Shared
+    UploadClient client = embeddedServer.applicationContext.getBean(UploadClient)
 
     void "test exchange byte[] blocking"() {
-
         given:
         def data = new String("xyz" * 500).bytes
+
         expect:
         new String(client.byteArray(data), StandardCharsets.UTF_8) == new String(data, StandardCharsets.UTF_8)
     }
 
-    @Ignore
     void "test exchange byte[] blocking - too big"() {
-
         given:
         def data = new String("xyz" * 50000).bytes
+
         when:
         new String(client.byteArray(data), StandardCharsets.UTF_8) == new String(data, StandardCharsets.UTF_8)
+
         then:
-        def e = thrown(HttpClientResponseException)
+        HttpClientResponseException e = thrown()
         e.message == 'The content length [150000] exceeds the maximum allowed content length [10240]'
     }
 
     void "test exchange byte[] non blocking"() {
-
         given:
         def data = new String("xyz" * 500).bytes
 
@@ -55,11 +58,12 @@ class OctetStreamSpec extends Specification {
         new String(Flux.from(client.byteArrayFlowable(Flux.just(data))).reduce({ byte[] a, byte[] b -> ArrayUtils.concat(a, b)}).block(), StandardCharsets.UTF_8) == new String(data, StandardCharsets.UTF_8)
     }
 
-    @Ignore
+    // TODO: Investigate why this fails on JDK 11
+    @IgnoreIf({ Jvm.current.isJava9Compatible() })
     void "test exchange byte[] non blocking - too big"() {
-
         given:
         def data = new String("xyz" * 100000).bytes
+
         when:
         new String(Flux.from(client.byteArrayFlowable(Flux.just(data))).reduce({ byte[] a, byte[] b -> ArrayUtils.concat(a, b) }).block(), StandardCharsets.UTF_8)
 
