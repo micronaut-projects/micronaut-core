c4dd3508d70bb289bba3b5df1ffffe4e614e1f26
Hernán Cervera <hjcerveram@hotmail.com>
2021-07-08 17:04:13 -0500

    Fix checkstyle violations

--- management/src/main/java/io/micronaut/management/endpoint/management/ManagementController.java
+++ management/src/main/java/io/micronaut/management/endpoint/management/ManagementController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2021 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.management.endpoint.management;
 
 import io.micronaut.context.annotation.Requires;
@@ -12,14 +27,14 @@ import io.reactivex.Single;
 import java.util.stream.Stream;
 
 /**
- * <p>Exposes the available management endpoints of the application</p>
+ * <p>Exposes the available management endpoints of the application.</p>
  *
  * @author Hernán Cervera
  * @since 3.0.0
  */
 @Controller
 @Requires(beans = EmbeddedServer.class)
-public class ManagementController {
+public final class ManagementController {
 
     private final HttpHostResolver httpHostResolver;
 
