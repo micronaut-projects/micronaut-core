f1316ead8bdb620314891e9dc347c50ff2a59d06
jameskleeh <james.kleeh@gmail.com>
2021-07-14 12:54:47 -0400

    Fix import

--- core/src/main/java/io/micronaut/core/io/scan/DefaultClassPathResourceLoader.java
+++ core/src/main/java/io/micronaut/core/io/scan/DefaultClassPathResourceLoader.java
@@ -21,6 +21,7 @@ import io.micronaut.core.util.clhm.ConcurrentLinkedHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
