9781f1bb68c498507befee504809e9049ddbc95a
Denis Stepanov <denis.s.stepanov@oracle.com>
2021-07-15 11:32:16 -0500

    Groovy default problem

---
+++ inject-groovy/src/test/groovy/io/micronaut/inject/defaults/MyRepository.groovy
@@ -0,0 +1,10 @@
+package io.micronaut.inject.defaults
+
+import io.micronaut.aop.Introduction;
+import io.micronaut.context.annotation.Type;
+
+@Introduction
+@Type(DataIntroductionAdvice.class)
+@SomeRepositoryConfiguration
+interface MyRepository {
+}
