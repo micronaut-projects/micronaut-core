1c1030dec0c43388ac75c1de04149704cfc92fcb
Vladislav Chernogorov <chernogorovvm@gmail.com>
2021-09-06 22:48:41 +0300

    Create a spec for JsonFactory bean

---
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/jackson/JsonFactorySetupSpec.groovy
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.server.netty.jackson
+
+import com.fasterxml.jackson.core.JsonFactory
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.DefaultApplicationContext
+import io.micronaut.context.env.MapPropertySource
+import io.micronaut.jackson.JacksonConfiguration
+import spock.lang.Specification
+
+class JsonFactorySetupSpec extends Specification {
+
+    void "verify default jackson setup with JsonFactory bean"() {
+
+        given:
+        ApplicationContext applicationContext = new DefaultApplicationContext("test").start()
+
+        expect:
+        applicationContext.containsBean(JsonFactory)
+
+        cleanup:
+        applicationContext?.close()
+    }
+}
