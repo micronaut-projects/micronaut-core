fb4c4640a9f67912ad11a6f0ce35c02b04417b78
graemerocher <graeme.rocher@gmail.com>
2021-09-14 12:36:14 +0200

    fix kotlin test

--- test-suite-kotlin/src/test/kotlin/io/micronaut/docs/http/server/secondary/SecondaryServerTest.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/http/server/secondary/SecondaryServerTest.kt
@@ -7,23 +7,28 @@ import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
 import jakarta.inject.Named
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 @MicronautTest
-// tag::inject[]
-class SecondaryServerTest(
-    @param:Client(path = "/", id = SecondaryNettyServer.SERVER_ID)
-    var httpClient : HttpClient, // <1>
-    @param:Named(SecondaryNettyServer.SERVER_ID)
-    var embeddedServer : EmbeddedServer // <2>
-) {
-// end::inject[]
+
+class SecondaryServerTest {
+    // tag::inject[]
+    @Inject
+    @field:Client(path = "/", id = SecondaryNettyServer.SERVER_ID)
+    lateinit var httpClient : HttpClient // <1>
+
+    @Inject
+    @field:Named(SecondaryNettyServer.SERVER_ID)
+    lateinit var embeddedServer : EmbeddedServer // <2>
+    // end::inject[]
+
     @Test
     fun testCallSecondaryServer() {
-        val result = httpClient!!.toBlocking().retrieve("/test/secondary/server")
-        Assertions.assertTrue(result.endsWith(embeddedServer!!.port.toString()))
+        val result = httpClient.toBlocking().retrieve("/test/secondary/server")
+        Assertions.assertTrue(result.endsWith(embeddedServer.port.toString()))
     }
 }
 
