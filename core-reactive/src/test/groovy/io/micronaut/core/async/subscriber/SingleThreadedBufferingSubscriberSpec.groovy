065ef01fd61d255afcb8b378d2e9f45d32dbff38
yawkat <jonas.konrad@oracle.com>
2021-09-16 11:27:53 +0200

    Do not forward onComplete before all elements have been processed

--- core-reactive/src/test/groovy/io/micronaut/core/async/subscriber/SingleThreadedBufferingSubscriberSpec.groovy
+++ core-reactive/src/test/groovy/io/micronaut/core/async/subscriber/SingleThreadedBufferingSubscriberSpec.groovy
@@ -50,4 +50,59 @@ class SingleThreadedBufferingSubscriberSpec extends Specification {
         then:
         seen == ['foo']
     }
+
+    def 'completion does not drop remaining buffered data'() {
+        given:
+        def seen = new ArrayList<String>()
+        def completed = false
+        def subscriber = new SingleThreadedBufferingSubscriber<String>() {
+            @Override
+            protected void doOnSubscribe(Subscription subscription) {
+            }
+
+            @Override
+            protected void doOnNext(String message) {
+                seen.add(message)
+            }
+
+            @Override
+            protected void doOnError(Throwable t) {
+            }
+
+            @Override
+            protected void doOnComplete() {
+                completed = true
+            }
+        }
+        def downstreamSubscription = subscriber.newDownstreamSubscription()
+        subscriber.onSubscribe(new Subscription() {
+            @Override
+            void request(long n) {
+            }
+
+            @Override
+            void cancel() {
+            }
+        })
+
+        when:
+        subscriber.onNext('foo')
+        downstreamSubscription.request(1)
+        then:
+        seen == ['foo']
+        !completed
+
+        when:
+        subscriber.onNext('bar')
+        subscriber.onComplete()
+        then:
+        seen == ['foo']
+        !completed
+
+        when:
+        downstreamSubscription.request(1)
+        then:
+        seen == ['foo', 'bar']
+        completed
+    }
 }
