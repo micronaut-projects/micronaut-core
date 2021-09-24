bd062384e22de524186ab0aa03c6bac15a2b1482
Artur Siembab <37847530+ajtuss@users.noreply.github.com>
2021-09-24 11:30:08 +0200

    Add ElementType.ANNOTATION_TYPE to all tracing annotations (#6213)

--- tracing/src/main/java/io/micronaut/tracing/annotation/NewSpan.java
+++ tracing/src/main/java/io/micronaut/tracing/annotation/NewSpan.java
@@ -35,7 +35,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
-@Target(value = { ElementType.METHOD })
+@Target(value = { ElementType.METHOD, ElementType.ANNOTATION_TYPE })
 @Around
 @Type(TraceInterceptor.class)
 public @interface NewSpan {
