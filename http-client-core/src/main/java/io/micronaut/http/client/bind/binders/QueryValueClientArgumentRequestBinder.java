3cf23b6229390495e16c097fd45f3b5b778a795d
Jonas Konrad <jonas.konrad@oracle.com>
2021-09-24 09:47:05 +0200

    Remove unused imports to fix checkstyle

--- http-client-core/src/main/java/io/micronaut/http/client/bind/binders/QueryValueClientArgumentRequestBinder.java
+++ http-client-core/src/main/java/io/micronaut/http/client/bind/binders/QueryValueClientArgumentRequestBinder.java
@@ -16,7 +16,6 @@
 package io.micronaut.http.client.bind.binders;
 
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.core.bind.annotation.Bindable;
 import io.micronaut.core.convert.ArgumentConversionContext;
 import io.micronaut.core.convert.ConversionContext;
 import io.micronaut.core.convert.ConversionService;
