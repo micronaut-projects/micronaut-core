dc34ae1821d432cfb9ec4ce85630c9150f88b665
Artur Siembab <37847530+ajtuss@users.noreply.github.com>
2021-09-30 12:59:59 +0200

    Fix HttpClientFilterRequestInReactorContextSpec (#6240)

--- http-server-netty/src/test/groovy/io/micronaut/http/server/netty/context/HttpClientFilterRequestInReactorContextSpec.groovy
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/context/HttpClientFilterRequestInReactorContextSpec.groovy
@@ -30,7 +30,7 @@ import spock.lang.Unroll
 class HttpClientFilterRequestInReactorContextSpec extends Specification {
 
     @Unroll
-    void "HTTP Client filers can access original request via Reactor Context"(String path, String expected) {
+    void "HTTP Client filters can access original request via Reactor Context"(String path, String expected) {
         given:
         int mockPort = SocketUtils.findAvailableTcpPort()
         EmbeddedServer mockServer = ApplicationContext.run(EmbeddedServer, [
@@ -116,15 +116,15 @@ class HttpClientFilterRequestInReactorContextSpec extends Specification {
         @Get("/foo")
         @Produces(MediaType.TEXT_PLAIN)
         @SingleResult
-        Publisher<String> foo() {
-            fooClient.hi()
+        String foo() {
+            fooClient.hello()
         }
 
         @Get("/bar")
         @Produces(MediaType.TEXT_PLAIN)
         @SingleResult
-        Publisher<String> bar() {
-            fooClient.hi()
+        String bar() {
+            fooClient.hello()
         }
     }
 
