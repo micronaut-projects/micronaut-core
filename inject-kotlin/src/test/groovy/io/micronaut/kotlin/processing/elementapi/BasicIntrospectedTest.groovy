5d1a57699f85b29e42b86860b1f37371529d5c00
jameskleeh <james.kleeh@gmail.com>
2021-10-21 14:14:04 -0400

    annotation metadata progress

--- inject-kotlin/src/test/groovy/io/micronaut/kotlin/processing/elementapi/BasicIntrospectedTest.groovy
+++ inject-kotlin/src/test/groovy/io/micronaut/kotlin/processing/elementapi/BasicIntrospectedTest.groovy
@@ -1,16 +1,12 @@
 package io.micronaut.kotlin.processing.elementapi
 
-import com.tschuchort.compiletesting.KotlinCompilation
-import com.tschuchort.compiletesting.KspKt
-import com.tschuchort.compiletesting.SourceFile
-import io.micronaut.kotlin.processing.visitor.TypeElementSymbolProcessorProvider
 import spock.lang.Specification
 
 class BasicIntrospectedTest extends Specification {
 
     void "test basic introspection"() {
         when:
-        Compiler.compile("test.Test","""
+        def introspection = Compiler.buildBeanIntrospection("test.Test", """
 package test
 
 import io.micronaut.core.annotation.Introspected
@@ -23,5 +19,7 @@ class Test {
 
         then:
         noExceptionThrown()
+        introspection != null
+        introspection.instantiate().class.name == "test.Test"
     }
 }
--- inject-kotlin/src/test/groovy/io/micronaut/kotlin/processing/elementapi/Compiler.java
+++ inject-kotlin/src/test/groovy/io/micronaut/kotlin/processing/elementapi/Compiler.java
@@ -3,14 +3,17 @@ package io.micronaut.kotlin.processing.elementapi;
 import com.tschuchort.compiletesting.KotlinCompilation;
 import com.tschuchort.compiletesting.KspKt;
 import com.tschuchort.compiletesting.SourceFile;
+import io.micronaut.core.beans.BeanIntrospection;
+import io.micronaut.core.beans.BeanIntrospector;
 import io.micronaut.kotlin.processing.visitor.TypeElementSymbolProcessorProvider;
 import org.intellij.lang.annotations.Language;
 
+import java.net.URLClassLoader;
 import java.util.Collections;
 
 public class Compiler {
 
-    static void compile(String name, @Language("kotlin") String clazz) {
+    static BeanIntrospection<?> buildBeanIntrospection(String name, @Language("kotlin") String clazz) {
         KotlinCompilation compilation = new KotlinCompilation();
         compilation.setSources(Collections.singletonList(SourceFile.Companion.kotlin(name + ".kt", clazz, true)));
         KspKt.setSymbolProcessorProviders(compilation, Collections.singletonList(new TypeElementSymbolProcessorProvider()));
@@ -18,6 +21,12 @@ public class Compiler {
         final KotlinCompilation.Result result = compilation.compile();
         if (result.getExitCode() != KotlinCompilation.ExitCode.OK) {
             throw new RuntimeException(result.getMessages());
+        }
+        final URLClassLoader classLoader = result.getClassLoader();
+        try {
+            return BeanIntrospector.forClassLoader(classLoader).findIntrospection(classLoader.loadClass(name)).orElse(null);
+        } catch (ClassNotFoundException e) {
+            return null;
         }
     }
 }
