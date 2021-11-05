micronaut-projects/micronaut-core

commit 23adb4bdaa55dac0d5ac74cf937e3f26f181ec4b

Add missing properties when deserializing type with partial reflective access (#6451)
* Add missing properties when deserializing type with partial reflective access
BeanIntrospectionModule correctly handled the case where a bean had no reflection-visible properties at all, but did not handle the case where some but not all properties are visible. The invisible properties would then be dropped. This can happen if the supertype of a bean is marked as `@ReflectiveAccess` but the subtype is not.

Fixes #6421

* Use findSerializerFromAnnotation

jackson-databind/src/main/java/io/micronaut/jackson/modules/BeanIntrospectionModule.java
@@ -355,7 +355,7 @@ public class BeanIntrospectionModule extends SimpleModule {
                 return builder;
             } else {
                 final Iterator<SettableBeanProperty> properties = builder.getProperties();
-                if ((!properties.hasNext() || ignoreReflectiveProperties) && introspection.getPropertyNames().length > 0) {
+                if ((ignoreReflectiveProperties || !properties.hasNext()) && introspection.getPropertyNames().length > 0) {
                     // mismatch, probably GraalVM reflection not enabled for bean. Try recreate
                     for (BeanProperty<Object, Object> beanProperty : introspection.getBeanProperties()) {
                         builder.addOrReplaceProperty(new VirtualSetter(
@@ -366,24 +366,35 @@ public class BeanIntrospectionModule extends SimpleModule {
                                 true);
                     }
                 } else {
+                    Map<String, BeanProperty<Object, Object>> remainingProperties = new LinkedHashMap<>();
+                    for (BeanProperty<Object, Object> beanProperty : introspection.getBeanProperties()) {
+                        remainingProperties.put(beanProperty.getName(), beanProperty);
+                    }
                     while (properties.hasNext()) {
                         final SettableBeanProperty settableBeanProperty = properties.next();
                         if (settableBeanProperty instanceof MethodProperty) {
                             MethodProperty methodProperty = (MethodProperty) settableBeanProperty;
-                            final Optional<BeanProperty<Object, Object>> beanProperty =
-                                    introspection.getProperty(settableBeanProperty.getName());
+                            final BeanProperty<Object, Object> beanProperty =
+                                    remainingProperties.remove(settableBeanProperty.getName());
 
-                            if (beanProperty.isPresent()) {
-                                BeanProperty<Object, Object> bp = beanProperty.get();
-                                if (!bp.isReadOnly()) {
-                                    SettableBeanProperty newProperty = new BeanIntrospectionSetter(
-                                            methodProperty,
-                                            bp
-                                    );
-                                    builder.addOrReplaceProperty(newProperty, true);
-                                }
+                            if (beanProperty != null && !beanProperty.isReadOnly()) {
+                                SettableBeanProperty newProperty = new BeanIntrospectionSetter(
+                                        methodProperty,
+                                        beanProperty
+                                );
+                                builder.addOrReplaceProperty(newProperty, true);
                             }
                         }
+                    }
+                    // add any remaining properties. This can happen if the supertype has reflection-visible properties
+                    // so `properties` isn't empty, but the subtype doesn't have reflection enabled.
+                    for (Map.Entry<String, BeanProperty<Object, Object>> entry : remainingProperties.entrySet()) {
+                        builder.addOrReplaceProperty(new VirtualSetter(
+                                        beanDesc.getClassInfo(),
+                                        config.getTypeFactory(),
+                                        entry.getValue(),
+                                        findSerializerFromAnnotation(entry.getValue(), JsonDeserialize.class)),
+                                true);
                     }
                 }
 

commit 8b0a3e94390be115d4021e30f39642569cd695af

Do not replace creator properties from introspection 23adb4bdaa55dac0d5ac74cf937e3f26f181ec4b introduced a regression that would replace any creator properties that do not have a corresponding getter with a new VirtualSetter. This patch ignores these properties.

jackson-databind/src/main/java/io/micronaut/jackson/modules/BeanIntrospectionModule.java
@@ -389,12 +389,15 @@ public class BeanIntrospectionModule extends SimpleModule {
                     // add any remaining properties. This can happen if the supertype has reflection-visible properties
                     // so `properties` isn't empty, but the subtype doesn't have reflection enabled.
                     for (Map.Entry<String, BeanProperty<Object, Object>> entry : remainingProperties.entrySet()) {
-                        builder.addOrReplaceProperty(new VirtualSetter(
-                                        beanDesc.getClassInfo(),
-                                        config.getTypeFactory(),
-                                        entry.getValue(),
-                                        findSerializerFromAnnotation(entry.getValue(), JsonDeserialize.class)),
-                                true);
+                        SettableBeanProperty existing = builder.findProperty(PropertyName.construct(entry.getKey()));
+                        if (existing == null) {
+                            builder.addOrReplaceProperty(new VirtualSetter(
+                                            beanDesc.getClassInfo(),
+                                            config.getTypeFactory(),
+                                            entry.getValue(),
+                                            findSerializerFromAnnotation(entry.getValue(), JsonDeserialize.class)),
+                                    true);
+                        }
                     }
                 }
 

jackson-databind/src/test/groovy/io/micronaut/jackson/modules/BeanIntrospectionModuleSpec.groovy
@@ -487,6 +487,25 @@ class BeanIntrospectionModuleSpec extends Specification {
         ignoreReflectiveProperties << [true, false]
     }
 
+    void "creator property that doesn't have a getter"() {
+        given:
+        ApplicationContext ctx = ApplicationContext.run()
+        ctx.getBean(BeanIntrospectionModule).ignoreReflectiveProperties = ignoreReflectiveProperties
+        ObjectMapper objectMapper = ctx.getBean(ObjectMapper)
+
+        expect:
+        objectMapper.writeValueAsString(new DifferentCreator('baz')) == '{"fooBar":"baz"}'
+        objectMapper.readValue('{"foo_bar":"baz"}', DifferentCreator).fooBar == 'baz'
+        // this is currently broken with ignoreReflectiveProperties
+        ignoreReflectiveProperties || objectMapper.readValue('{"fooBar":"baz"}', DifferentCreator).fooBar == 'baz'
+
+        cleanup:
+        ctx.close()
+
+        where:
+        ignoreReflectiveProperties << [true, false]
+    }
+
     @Introspected
     static class Book {
         @JsonProperty("book_title")
@@ -731,6 +750,20 @@ class BeanIntrospectionModuleSpec extends Specification {
             String deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
                 return p.valueAsString.toLowerCase(Locale.ENGLISH)
             }
+        }
+    }
+
+    @Introspected
+    static class DifferentCreator {
+        private final String fooBar;
+
+        @JsonCreator
+        public DifferentCreator(@JsonProperty('foo_bar') String fooBar) {
+            this.fooBar = fooBar
+        }
+
+        public String getFooBar() {
+            return fooBar;
         }
     }
 }