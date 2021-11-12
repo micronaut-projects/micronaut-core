22dd345bd82cd9f5540c1d6dc4f5eb42b5062365
yawkat <jonas.konrad@oracle.com>
2021-11-12 16:13:49 +0100

    Replace usages of identityHashCode to prevent collisions System.identityHashCode is, by default, set to a random int by the JVM. Using it as a map key could lead to collisions. This patch replaces those usages with a new wrapper class that, on top of the identityHashCode for hashing, uses == for the equals check. This prevents any collisions in the map keys.
    There is unfortunately no way to test for these collisions without a JVM option (-XX:hashCode), so there is no unit test in this patch.

--- http-server-netty/src/main/java/io/micronaut/http/server/netty/NettyHttpRequest.java
+++ http-server-netty/src/main/java/io/micronaut/http/server/netty/NettyHttpRequest.java
@@ -94,7 +94,7 @@ public class NettyHttpRequest<T> extends AbstractNettyHttpRequest<T> implements 
     private MutableConvertibleValues<Object> attributes;
     private NettyCookies nettyCookies;
     private List<ByteBufHolder> receivedContent = new ArrayList<>();
-    private Map<Integer, AbstractHttpData> receivedData = new LinkedHashMap<>();
+    private Map<IdentityWrapper, AbstractHttpData> receivedData = new LinkedHashMap<>();
 
     private Supplier<Optional<T>> body;
     private RouteMatch<?> matchedRoute;
@@ -345,7 +345,7 @@ public class NettyHttpRequest<T> extends AbstractNettyHttpRequest<T> implements 
     @Internal
     public void addContent(ByteBufHolder httpContent) {
         if (httpContent instanceof AbstractHttpData) {
-            receivedData.computeIfAbsent(System.identityHashCode(httpContent), key -> {
+            receivedData.computeIfAbsent(new IdentityWrapper(httpContent), key -> {
                 httpContent.retain();
                 return (AbstractHttpData) httpContent;
             });
