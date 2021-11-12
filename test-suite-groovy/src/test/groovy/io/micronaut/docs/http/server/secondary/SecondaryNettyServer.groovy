80a63ede76dd0922d5ee9133e4f53b593225f64e
Graeme Rocher <graeme.rocher@oracle.com>
2021-11-12 15:13:34 +0100

    Fix support for secondary servers with custom SSL config (#6506)

--- test-suite-groovy/src/test/groovy/io/micronaut/docs/http/server/secondary/SecondaryNettyServer.groovy
+++ test-suite-groovy/src/test/groovy/io/micronaut/docs/http/server/secondary/SecondaryNettyServer.groovy
@@ -6,15 +6,17 @@ import io.micronaut.context.annotation.Context
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Requires
 import io.micronaut.context.env.Environment
+import io.micronaut.core.util.StringUtils
 import io.micronaut.discovery.ServiceInstanceList
 import io.micronaut.discovery.StaticServiceInstanceList
 import io.micronaut.http.server.netty.NettyEmbeddedServer
 import io.micronaut.http.server.netty.NettyEmbeddedServerFactory
 import io.micronaut.http.server.netty.configuration.NettyHttpServerConfiguration
+import io.micronaut.http.ssl.ServerSslConfiguration
 import jakarta.inject.Named
 // end::imports[]
 
-
+@Requires(property = "secondary.enabled", value = StringUtils.TRUE)
 // tag::class[]
 @Factory
 class SecondaryNettyServer {
@@ -27,8 +29,12 @@ class SecondaryNettyServer {
     NettyEmbeddedServer nettyEmbeddedServer(NettyEmbeddedServerFactory serverFactory) { // <3>
         def configuration =
                 new NettyHttpServerConfiguration() // <4>
+        def sslConfiguration = new ServerSslConfiguration() // <5>
+        sslConfiguration.setBuildSelfSigned(true)
+        sslConfiguration.enabled = true
+        sslConfiguration.port = -1 // random port
         // configure server programmatically
-        final NettyEmbeddedServer embeddedServer = serverFactory.build(configuration) // <5>
+        final NettyEmbeddedServer embeddedServer = serverFactory.build(configuration, sslConfiguration) // <5>
         embeddedServer.start() // <6>
         return embeddedServer // <7>
     }
--- test-suite-kotlin/src/test/kotlin/io/micronaut/docs/http/server/secondary/SecondaryNettyServer.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/http/server/secondary/SecondaryNettyServer.kt
@@ -6,14 +6,17 @@ import io.micronaut.context.annotation.Context
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Requires
 import io.micronaut.context.env.Environment
+import io.micronaut.core.util.StringUtils
 import io.micronaut.discovery.ServiceInstanceList
 import io.micronaut.discovery.StaticServiceInstanceList
 import io.micronaut.http.server.netty.NettyEmbeddedServer
 import io.micronaut.http.server.netty.NettyEmbeddedServerFactory
 import io.micronaut.http.server.netty.configuration.NettyHttpServerConfiguration
+import io.micronaut.http.ssl.ServerSslConfiguration
 import jakarta.inject.Named
 // end::imports[]
 
+@Requires(property = "secondary.enabled", value = StringUtils.TRUE)
 // tag::class[]
 @Factory
 class SecondaryNettyServer {
@@ -29,14 +32,20 @@ class SecondaryNettyServer {
         serverFactory: NettyEmbeddedServerFactory // <3>
     ) : NettyEmbeddedServer {
         val configuration = NettyHttpServerConfiguration() // <4>
+        val sslConfiguration = ServerSslConfiguration() // <5>
+
+        sslConfiguration.setBuildSelfSigned(true)
+        sslConfiguration.isEnabled = true
+        sslConfiguration.port = -1 // random port
+
         // configure server programmatically
-        val embeddedServer = serverFactory.build(configuration) // <5>
-        embeddedServer.start() // <6>
-        return embeddedServer // <7>
+        val embeddedServer = serverFactory.build(configuration, sslConfiguration) // <6>
+        embeddedServer.start() // <7>
+        return embeddedServer // <8>
     }
 
     @Bean
-    fun serviceInstanceList( // <8>
+    fun serviceInstanceList( // <9>
         @Named(SERVER_ID) nettyEmbeddedServer: NettyEmbeddedServer
     ): ServiceInstanceList {
         return StaticServiceInstanceList(
