fc4ad6c0e994db0cd2a90a2c7a4a0dbae10c98af
yawkat <jonas.konrad@oracle.com>
2021-11-18 09:12:25 +0100

    test

---
+++ jackson-databind/src/test/groovy/io/micronaut/jackson/convert/JsonConverterRegistrarSpec.groovy
@@ -0,0 +1,33 @@
+package io.micronaut.jackson.convert
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.core.convert.ConversionService
+import io.micronaut.core.convert.value.ConvertibleValues
+import io.micronaut.core.type.Argument
+import io.micronaut.json.tree.JsonNode
+import spock.lang.Specification
+
+class JsonConverterRegistrarSpec extends Specification {
+    def 'array node to collection'() {
+        given:
+        def ctx = ApplicationContext.run()
+        def converter = ctx.getBean(ConversionService)
+
+        expect:
+        converter.convert(JsonNode.createArrayNode([JsonNode.createStringNode("foo")]), Argument.of(List, String)).get() == ['foo']
+        converter.convert(JsonNode.createArrayNode([JsonNode.createStringNode("foo")]), Argument.of(Set, String)).get() == new HashSet(['foo'])
+        // SortedSet not supported
+        converter.convert(JsonNode.createArrayNode([JsonNode.createStringNode("foo")]), Argument.of(SortedSet, String)).isEmpty()
+    }
+
+    def 'json node to ConvertibleValues'() {
+        given:
+        def ctx = ApplicationContext.run()
+        def converter = ctx.getBean(ConversionService)
+
+        expect:
+        converter.convert(JsonNode.createArrayNode([JsonNode.createStringNode("foo")]), Argument.of(ConvertibleValues, String)).isEmpty()
+        converter.convert(JsonNode.createObjectNode(['bar': JsonNode.createStringNode("foo")]), Argument.of(ConvertibleValues, String)).get().asMap(String, String) == ['bar': 'foo']
+        converter.convert(JsonNode.createStringNode("foo"), Argument.of(ConvertibleValues, String)).isEmpty()
+    }
+}
