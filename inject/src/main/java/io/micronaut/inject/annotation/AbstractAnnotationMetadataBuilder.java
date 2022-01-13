61a95339624b084bb9afd4ed8029b9cff131282d
James Kleeh <james.kleeh@gmail.com>
2022-01-13 12:33:27 -0500

    Fix mutated metadata being cleared before it was written in Groovy classes (#6749)
    * Fix mutated metadata being cleared before it was written in Groovy classes
    
    * Add test for mutated metadata being used in bean definitions
    
    * Read defaults from compile time information for mutable metadata
    
    * Reuse existing test instead
    
    * Fix test

--- inject/src/main/java/io/micronaut/inject/annotation/AbstractAnnotationMetadataBuilder.java
+++ inject/src/main/java/io/micronaut/inject/annotation/AbstractAnnotationMetadataBuilder.java
@@ -2004,8 +2004,15 @@ public abstract class AbstractAnnotationMetadataBuilder<T, A> {
      * Used to clear mutated metadata at the end of a compilation cycle.
      */
     @Internal
-    public static void clearCaches() {
+    public static void clearMutated() {
         MUTATED_ANNOTATION_METADATA.clear();
+    }
+
+    /**
+     * Used to clear caches at the end of a compilation cycle.
+     */
+    @Internal
+    public static void clearCaches() {
         ANNOTATION_DEFAULTS.clear();
     }
 
