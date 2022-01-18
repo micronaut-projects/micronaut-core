ffe26cd7826468a963c9ebfac2045d044a3a54f1
Tim Yates <tim.yates@gmail.com>
2022-01-18 16:03:47 +0000

    Handle void return type from Function clients (#6771)

---
+++ test-suite/src/test/groovy/io/micronaut/function/web/NullReturningConsumerFunctionSpec.groovy
@@ -0,0 +1,47 @@
+package io.micronaut.function.web
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.env.Environment
+import io.micronaut.function.FunctionBean
+import io.micronaut.function.client.FunctionClient
+import io.micronaut.runtime.server.EmbeddedServer
+import jakarta.inject.Named
+import spock.lang.Issue
+import spock.lang.Specification
+
+import java.util.function.Consumer
+
+class NullReturningConsumerFunctionSpec extends Specification {
+
+    @Issue("https://github.com/micronaut-projects/micronaut-core/issues/2024")
+    def "test a client with a void return type"() {
+        given:
+        def embeddedServer = ApplicationContext.run(EmbeddedServer)
+        def client = embeddedServer.applicationContext.getBean(MicronautFunctionProblemsClient)
+
+        when:
+        client.sendVoid("woo")
+
+        then:
+        PojoConsumer.LAST_VALUE == "woo"
+    }
+
+    @FunctionClient
+    static interface MicronautFunctionProblemsClient {
+
+        @Named("consumer/void")
+        void sendVoid(String message)
+    }
+
+    @FunctionBean("consumer/void")
+    static class PojoConsumer implements Consumer<String> {
+
+        static String LAST_VALUE
+
+        @Override
+        void accept(String book) {
+            LAST_VALUE = book
+        }
+    }
+}
