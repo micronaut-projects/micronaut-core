e601ff110f7cadf6ecd905b142a6a43a08fb9b16
Jonas Konrad <jonas.konrad@oracle.com>
2022-01-18 21:03:05 +0100

    Fix record serialization for native image (#6770)
    * Fix record serialization for native image
    Without `@ReflectiveAccess` on native-image, jackson fails serialization and deserialization of record types with an exception message `Failed to access RecordComponents of type ...` (see issue). This happens before the `BeanIntrospectionModule` comes into play, so mapping is not possible even though the `BeanIntrospectionModule` could have replaced the missing reflection information.
    
    This patch hooks the jackson `AccessorNamingStrategy` to bypass the relevant code for serialization. This seems reasonable enough. It also hooks `BeanDeserializerFactory` for deserialization, but both the approach in patching `_constructDefaultValueInstantiator` and the way the factory is inserted (can't be done in the `BeanIntrospectionModule`) are very hacky.
    
    I hope to fix this upstream in jackson at some point, once I get CLA approval, but we're still on 2.12 so we couldn't take advantage of that for a while still.
    
    Fixes #6277
    
    * style

--- jackson-databind/src/main/java/io/micronaut/jackson/ObjectMapperFactory.java
+++ jackson-databind/src/main/java/io/micronaut/jackson/ObjectMapperFactory.java
@@ -26,7 +26,10 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.KeyDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.cfg.DeserializerFactoryConfig;
+import com.fasterxml.jackson.databind.deser.BeanDeserializerFactory;
 import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
+import com.fasterxml.jackson.databind.deser.DefaultDeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
@@ -111,7 +114,7 @@ public class ObjectMapperFactory {
     public ObjectMapper objectMapper(@Nullable JacksonConfiguration jacksonConfiguration,
                                      @Nullable JsonFactory jsonFactory) {
 
-        ObjectMapper objectMapper = jsonFactory != null ? new ObjectMapper(jsonFactory) : new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper(jsonFactory, null, new DefaultDeserializationContext.Impl(new ResilientBeanDeserializerFactory(new DeserializerFactoryConfig())));
 
         final boolean hasConfiguration = jacksonConfiguration != null;
         if (!hasConfiguration || jacksonConfiguration.isModuleScan()) {
