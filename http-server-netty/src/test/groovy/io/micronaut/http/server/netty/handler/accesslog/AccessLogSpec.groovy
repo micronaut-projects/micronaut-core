9c6a3deb00f1dda463895c062d9ba87b1deb7b76
Tim Yates <tim.yates@gmail.com>
2022-01-21 18:15:32 +0000

    Test interleaved access logs with exclusions

--- http-server-netty/src/test/groovy/io/micronaut/http/server/netty/handler/accesslog/AccessLogSpec.groovy
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/handler/accesslog/AccessLogSpec.groovy
@@ -124,6 +124,69 @@ class AccessLogSpec extends Specification {
     }
 
     @Issue('https://github.com/micronaut-projects/micronaut-core/issues/6782')
+    def 'http1.1 concurrent pipelined requests with exclusions'() {
+        given:
+        def ctx = ApplicationContext.run([
+                'spec.name': 'AccessLogSpec',
+                'micronaut.server.netty.access-logger.enabled': true,
+                'micronaut.server.netty.access-logger.logger-name': 'http-access-log',
+                'micronaut.server.netty.access-logger.exclusions[0]': '/interleave/open',
+        ])
+        def server = ctx.getBean(EmbeddedServer)
+        server.start()
+
+        def responses = new CopyOnWriteArrayList<FullHttpResponse>()
+        Bootstrap bootstrap = new Bootstrap()
+                .group(new NioEventLoopGroup(1))
+                .channel(NioSocketChannel)
+                .option(ChannelOption.AUTO_READ, true)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(@NotNull Channel ch) throws Exception {
+                        ch.pipeline()
+                                .addLast(new HttpClientCodec())
+                                .addLast(new HttpObjectAggregator(1024))
+                                .addLast(new ChannelInboundHandlerAdapter() {
+                                    @Override
+                                    void channelRead(@NotNull ChannelHandlerContext ctx_, @NotNull Object msg) throws Exception {
+                                        responses.add(msg)
+                                    }
+                                })
+                    }
+                })
+                .remoteAddress(server.host, server.port)
+
+        def request1 = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, '/interleave/open')
+        request1.headers().add(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE)
+        def request2 = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, '/interleave/finish')
+        request2.headers().add(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE)
+
+        def listAppender = new ListAppender<ILoggingEvent>()
+        listAppender.start()
+        ((Logger) LoggerFactory.getLogger('http-access-log')).addAppender(listAppender)
+
+        when:
+        def channel = bootstrap.connect().sync().channel()
+        channel.write(request1)
+        channel.writeAndFlush(request2)
+
+        then:
+        new PollingConditions(timeout: 5).eventually {
+            responses.size() == 2
+        }
+        responses[0].content().toString(StandardCharsets.UTF_8) == 'open'
+        responses[1].content().toString(StandardCharsets.UTF_8) == 'finish'
+
+        listAppender.list.size() == 1
+        listAppender.list[0].message.contains('/interleave/finish')
+
+        cleanup:
+        server.close()
+        channel.close()
+        bootstrap.config().group().shutdownGracefully()
+    }
+
+    @Issue('https://github.com/micronaut-projects/micronaut-core/issues/6782')
     def 'http1.1 continue status'() {
         given:
         def ctx = ApplicationContext.run([
@@ -387,6 +450,99 @@ class AccessLogSpec extends Specification {
         listAppender.list[1].message.contains('/interleave/simple')
         listAppender.list[2].message.contains('/interleave/open')
         listAppender.list[3].message.contains('/interleave/finish')
+
+        cleanup:
+        server.close()
+        channel.close()
+        bootstrap.config().group().shutdownGracefully()
+    }
+
+    @Issue('https://github.com/micronaut-projects/micronaut-core/issues/6782')
+    def 'h2c truly concurrent requests with http1 upgrade with exclusions'() {
+        given:
+        def ctx = ApplicationContext.run([
+                'spec.name': 'AccessLogSpec',
+                'micronaut.server.http-version': '2.0',
+                'micronaut.ssl.enabled': false,
+                'micronaut.server.netty.access-logger.enabled': true,
+                'micronaut.server.netty.access-logger.logger-name': 'http-access-log',
+                'micronaut.server.netty.access-logger.exclusions[0]': '/interleave/.i.*', // exclude simple and finish
+        ])
+        def server = ctx.getBean(EmbeddedServer)
+        server.start()
+
+        def request1 = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, '/interleave/simple')
+        request1.headers().add(HttpConversionUtil.ExtensionHeaderNames.SCHEME.text(), ':https')
+        def request2 = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, '/interleave/open')
+        request2.headers().add(HttpConversionUtil.ExtensionHeaderNames.SCHEME.text(), ':https')
+        request2.headers().add(HttpConversionUtil.ExtensionHeaderNames.STREAM_ID.text(), 3)
+        def request3 = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, '/interleave/simple')
+        request3.headers().add(HttpConversionUtil.ExtensionHeaderNames.STREAM_ID.text(), 5)
+        request3.headers().add(HttpConversionUtil.ExtensionHeaderNames.SCHEME.text(), ':https')
+        def request4 = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, '/interleave/finish')
+        request4.headers().add(HttpConversionUtil.ExtensionHeaderNames.STREAM_ID.text(), 7)
+        request4.headers().add(HttpConversionUtil.ExtensionHeaderNames.SCHEME.text(), ':https')
+
+        def responses = new CopyOnWriteArrayList<FullHttpResponse>()
+        Bootstrap bootstrap = new Bootstrap()
+                .group(new NioEventLoopGroup(1))
+                .channel(NioSocketChannel)
+                .option(ChannelOption.AUTO_READ, true)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(@NotNull Channel ch) throws Exception {
+                        def connection = new DefaultHttp2Connection(false)
+                        def connectionHandler = new HttpToHttp2ConnectionHandlerBuilder()
+                                .initialSettings(Http2Settings.defaultSettings())
+                                .frameListener(new DelegatingDecompressorFrameListener(
+                                        connection,
+                                        new InboundHttp2ToHttpAdapterBuilder(connection)
+                                                .maxContentLength(Integer.MAX_VALUE)
+                                                .propagateSettings(false)
+                                                .build()
+                                ))
+                                .connection(connection)
+                                .build()
+                        def clientCodec = new HttpClientCodec()
+                        def upgradeCodec = new Http2ClientUpgradeCodec(ChannelPipelineCustomizer.HANDLER_HTTP2_CONNECTION, connectionHandler)
+                        def upgradeHandler = new HttpClientUpgradeHandler(clientCodec, upgradeCodec, 1000000)
+                        ch.pipeline()
+                                .addLast(ChannelPipelineCustomizer.HANDLER_HTTP_CLIENT_CODEC, clientCodec)
+                                .addLast(upgradeHandler)
+                                .addLast(new ChannelInboundHandlerAdapter() {
+                                    @Override
+                                    void channelRead(@NotNull ChannelHandlerContext ctx_, @NotNull Object msg) throws Exception {
+                                        if (responses.isEmpty()) {
+                                            ctx_.channel().write(request2)
+                                            ctx_.channel().write(request3)
+                                            ctx_.channel().writeAndFlush(request4)
+                                        }
+                                        responses.add(msg)
+                                    }
+                                })
+                    }
+                })
+                .remoteAddress(server.host, server.port)
+
+        def listAppender = new ListAppender<ILoggingEvent>()
+        listAppender.start()
+        ((Logger) LoggerFactory.getLogger('http-access-log')).addAppender(listAppender)
+
+        when:
+        def channel = bootstrap.connect().sync().channel()
+        channel.writeAndFlush(request1)
+
+        then:
+        new PollingConditions(timeout: 5).eventually {
+            responses.size() == 4
+        }
+        responses[0].content().toString(StandardCharsets.UTF_8) == 'simple'
+        responses[1].content().toString(StandardCharsets.UTF_8) == 'simple'
+        responses[2].content().toString(StandardCharsets.UTF_8) == 'open'
+        responses[3].content().toString(StandardCharsets.UTF_8) == 'finish'
+
+        listAppender.list.size() == 1
+        listAppender.list[0].message.contains('/interleave/open')
 
         cleanup:
         server.close()
