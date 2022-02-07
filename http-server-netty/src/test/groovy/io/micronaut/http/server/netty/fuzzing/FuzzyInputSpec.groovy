6988ca14c72e31400a809bf26ba82647f5ee2f2d
Jonas Konrad <jonas.konrad@oracle.com>
2022-02-07 16:05:23 +0100

    Release streaming HTTP data when request is invalid (#6879)
    Buffer leak discovered by fuzzing. This patch also adds infrastructure to http-server-netty that can be used to test for buffer leaks.
    
    The IllegalArgumentException in this particular test case is caused by an invalid URI passed to the server.

---
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/fuzzing/FuzzyInputSpec.groovy
@@ -0,0 +1,63 @@
+package io.micronaut.http.server.netty.fuzzing
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.netty.channel.EventLoopGroupConfiguration
+import io.micronaut.http.netty.channel.EventLoopGroupRegistry
+import io.micronaut.runtime.server.EmbeddedServer
+import io.netty.bootstrap.Bootstrap
+import io.netty.buffer.Unpooled
+import io.netty.channel.Channel
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.ChannelOption
+import io.netty.channel.socket.nio.NioSocketChannel
+import io.netty.handler.timeout.ReadTimeoutHandler
+import io.netty.resolver.NoopAddressResolverGroup
+import spock.lang.Specification
+
+/**
+ * HTTP inputs generated from fuzzing.
+ */
+class FuzzyInputSpec extends Specification {
+    def 'http1 cleartext buffer leaks'() {
+        given:
+        BufferLeakDetection.startTracking()
+
+        ApplicationContext ctx = ApplicationContext.run([
+                "micronaut.server.port": "-1",
+                "micronaut.netty.event-loops.default.num-threads": "1"
+        ])
+        def embeddedServer = ctx.getBean(EmbeddedServer)
+        embeddedServer.start()
+
+        def clientBootstrap = new Bootstrap()
+                .remoteAddress(new InetSocketAddress(embeddedServer.getHost(),
+                        embeddedServer.getPort()))
+                .resolver(NoopAddressResolverGroup.INSTANCE)
+                .group(ctx.getBean(EventLoopGroupRegistry.class).getEventLoopGroup(EventLoopGroupConfiguration.DEFAULT).get())
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.AUTO_READ, true)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) throws Exception {
+                        ch.pipeline()
+                                .addLast(new ReadTimeoutHandler(1))
+                    }
+                })
+
+        when:
+        def channel = clientBootstrap.connect().sync().channel()
+        channel.writeAndFlush(Unpooled.wrappedBuffer(input))
+        channel.closeFuture().sync()
+
+        then:
+        BufferLeakDetection.stopTrackingAndReportLeaks()
+
+        cleanup:
+        embeddedServer.stop()
+
+        where:
+        input << [
+                Base64.decoder.decode("T1BUSU9OUyAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKiBIVFRQLzEuMQpUcmFuc2Zlci1FbmNvZGluZzpjaHVua2VkCgo0CgoNSU9OUyAqIEhUVFAvMS4xClRyYW5zZmVyLUVuY29kaW5nOmNodW5rZWSKCg")
+        ]
+    }
+}
