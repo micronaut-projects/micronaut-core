c19e21c835527caf965ff4d7624c6daab61c6e9d
Matthew Moss <mossm@objectcomputing.com>
2022-02-08 10:05:31 -0600

    Write out status code and message from HttpStatusException

--- http-netty/src/main/java/io/micronaut/http/netty/stream/HttpStreamsHandler.java
+++ http-netty/src/main/java/io/micronaut/http/netty/stream/HttpStreamsHandler.java
@@ -16,6 +16,8 @@
 package io.micronaut.http.netty.stream;
 
 import io.micronaut.core.annotation.Internal;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.exceptions.HttpStatusException;
 import io.micronaut.http.netty.AbstractNettyHttpRequest;
 import io.micronaut.http.netty.reactive.HandlerPublisher;
 import io.micronaut.http.netty.reactive.HandlerSubscriber;
@@ -410,8 +412,15 @@ abstract class HttpStreamsHandler<In extends HttpMessage, Out extends HttpMessag
                         if (LOG.isErrorEnabled()) {
                             LOG.error("Error occurred writing stream response: " + error.getMessage(), error);
                         }
-                        ctx.writeAndFlush(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.INTERNAL_SERVER_ERROR))
-                           .addListener(ChannelFutureListener.CLOSE);
+                        if (error instanceof HttpStatusException) {
+                            final HttpStatus status = ((HttpStatusException) error).getStatus();
+                            final String message = error.getMessage();
+                            ctx.writeAndFlush(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.valueOf(status.getCode(), message)))
+                                            .addListener(ChannelFutureListener.CLOSE);
+                        } else {
+                            ctx.writeAndFlush(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.INTERNAL_SERVER_ERROR))
+                                    .addListener(ChannelFutureListener.CLOSE);
+                        }
                     } finally {
                         ctx.read();
                     }
