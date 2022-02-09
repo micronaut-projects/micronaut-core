c66dc6db8a9c29f63ba3efcce9b63448cc973712
jameskleeh <james.kleeh@gmail.com>
2022-02-09 11:07:27 -0500

    Add netty buffer leak detection for all tests in http-server-netty

---
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/BufferLeakDetectionExtension.java
@@ -0,0 +1,20 @@
+package io.micronaut.http.server.netty;
+
+import io.micronaut.http.server.netty.fuzzing.BufferLeakDetection;
+import org.spockframework.runtime.extension.IGlobalExtension;
+import org.spockframework.runtime.model.SpecInfo;
+
+public class BufferLeakDetectionExtension implements IGlobalExtension {
+
+    @Override
+    public void visitSpec(SpecInfo spec) {
+        spec.addSetupInterceptor(invocation -> {
+            BufferLeakDetection.startTracking();
+            invocation.proceed();
+        });
+        spec.addCleanupInterceptor(invocation -> {
+            invocation.proceed();
+            BufferLeakDetection.stopTrackingAndReportLeaks();
+        });
+    }
+}
