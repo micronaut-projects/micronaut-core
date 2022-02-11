3137b9429486be9dcb1a0405278bc26ef18c7f99
yawkat <jonas.konrad@oracle.com>
2022-02-11 10:26:03 +0100

    release in test

--- http-server-netty/src/test/groovy/io/micronaut/http/server/netty/MaxRequestSizeSpec.groovy
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/MaxRequestSizeSpec.groovy
@@ -320,6 +320,7 @@ class MaxRequestSizeSpec extends Specification {
         }
 
         cleanup:
+        responses.forEach(r -> r.release())
         channel.close()
         embeddedServer.close()
     }
