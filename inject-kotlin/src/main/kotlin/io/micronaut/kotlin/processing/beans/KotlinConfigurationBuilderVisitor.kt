9f1efeaaf546fbf1717a85caf15158c511851f13
jameskleeh <james.kleeh@gmail.com>
2022-02-16 16:52:38 -0500

    More configuration builder fixes and tests

--- inject-kotlin/src/main/kotlin/io/micronaut/kotlin/processing/beans/KotlinConfigurationBuilderVisitor.kt
+++ inject-kotlin/src/main/kotlin/io/micronaut/kotlin/processing/beans/KotlinConfigurationBuilderVisitor.kt
@@ -2,6 +2,7 @@ package io.micronaut.kotlin.processing.beans
 
 import io.micronaut.context.annotation.ConfigurationBuilder
 import io.micronaut.core.annotation.AccessorsStyle
+import io.micronaut.core.annotation.AnnotationMetadata
 import io.micronaut.core.naming.NameUtils
 import io.micronaut.inject.ast.ElementQuery
 import io.micronaut.inject.ast.MethodElement
@@ -24,74 +25,94 @@ class KotlinConfigurationBuilderVisitor(
     private val prefixes = propertyElement.getValue(AccessorsStyle::class.java, "writePrefixes", Array<String>::class.java)
         .orElse(arrayOf("set"))
 
+    companion object {
+        val notDeprecated: (AnnotationMetadata) -> Boolean = { annotationMetadata ->
+            !annotationMetadata.hasStereotype(Deprecated::class.java) &&
+                    !annotationMetadata.hasStereotype("java.lang.Deprecated")
+        }
+    }
+
     fun visit() {
         val classElement = propertyElement.genericType
-        classElement.getEnclosedElements(ElementQuery.of(MethodElement::class.java).onlyAccessible())
+        classElement.getEnclosedElements(ElementQuery.of(MethodElement::class.java)
+            .onlyAccessible()
+            .annotated(notDeprecated))
             .forEach(this::visitMethod)
+        classElement.getEnclosedElements(ElementQuery.of(PropertyElement::class.java)
+            .onlyAccessible()
+            .annotated(notDeprecated))
+            .forEach(this::visitProperty)
+    }
+
+    private fun visitProperty(propertyElement: PropertyElement) {
+        propertyElement.writeMethod.ifPresent { writeMethod ->
+            visit(writeMethod, propertyElement.name, propertyElement)
+        }
     }
 
     private fun visitMethod(methodElement: MethodElement) {
-        if (methodElement.hasStereotype(Deprecated::class.java) || methodElement.hasStereotype("java.lang.Deprecated")) {
-            return
-        }
         if (NameUtils.isWriterName(methodElement.name, prefixes)) {
             val propertyName = NameUtils.getPropertyNameForSetter(methodElement.name, prefixes)
-            if (shouldExclude(includes, excludes, propertyName)) {
+            visit(methodElement, propertyName, methodElement)
+        }
+    }
+
+    private fun visit(methodElement: MethodElement, propertyName: String, annotationMetadata: AnnotationMetadata) {
+        if (shouldExclude(includes, excludes, propertyName)) {
+            return
+        }
+
+        val params = methodElement.parameters
+        val paramCount = params.size
+        if (paramCount < 2) {
+            val parameter = if (params.size == 1) {
+                params[0]
+            } else {
+                null
+            }
+
+            if (parameter == null && !allowZeroArgs) {
                 return
             }
 
-            val params = methodElement.parameters
-            val paramCount = params.size
-            if (paramCount < 2) {
-                val parameter = if (params.size == 1) {
-                    params[0]
-                } else {
-                    null
-                }
+            val metadata = configurationMetadataBuilder.visitProperty(
+                propertyElement.declaringType,
+                methodElement.declaringType,
+                parameter?.type?.name,
+                configurationPrefix + propertyName,
+                null,
+                null
+            )
 
-                if (parameter == null && !allowZeroArgs) {
-                    return
-                }
+            beanDefinitionWriter.visitConfigBuilderMethod(
+                getMethodPrefix(methodElement.name),
+                methodElement.returnType,
+                methodElement.name,
+                parameter?.type,
+                parameter?.type?.typeArguments,
+                metadata.path
+            )
+        } else if (paramCount == 2) {
+            // check the params are a long and a TimeUnit
+            val first = params[0]
+            val second = params[1]
 
+            if (second.type.name == "java.util.concurrent.TimeUnit" && first.type.name == "long") {
                 val metadata = configurationMetadataBuilder.visitProperty(
                     propertyElement.declaringType,
                     methodElement.declaringType,
-                    parameter?.type?.name,
+                    "java.time.Duration",
                     configurationPrefix + propertyName,
                     null,
                     null
                 )
 
-                beanDefinitionWriter.visitConfigBuilderMethod(
+                beanDefinitionWriter.visitConfigBuilderDurationMethod(
                     getMethodPrefix(methodElement.name),
                     methodElement.returnType,
                     methodElement.name,
-                    parameter?.type,
-                    parameter?.type?.typeArguments,
                     metadata.path
                 )
-            } else if (paramCount == 2) {
-                // check the params are a long and a TimeUnit
-                val first = params[0]
-                val second = params[1]
-
-                if (second.type.name == "java.util.concurrent.TimeUnit" && first.type.name == "long") {
-                    val metadata = configurationMetadataBuilder.visitProperty(
-                        propertyElement.declaringType,
-                        methodElement.declaringType,
-                        "java.time.Duration",
-                        configurationPrefix + propertyName,
-                        null,
-                        null
-                    )
-
-                    beanDefinitionWriter.visitConfigBuilderDurationMethod(
-                        getMethodPrefix(methodElement.name),
-                        methodElement.returnType,
-                        methodElement.name,
-                        metadata.path
-                    )
-                }
             }
         }
     }
---
+++ inject-kotlin/src/test/groovy/io/micronaut/kotlin/processing/beans/configproperties/ConfigurationPropertiesBuilderSpec.groovy
@@ -0,0 +1,394 @@
+package io.micronaut.kotlin.processing.beans.configproperties
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.inject.BeanDefinition
+import io.micronaut.inject.BeanFactory
+import org.neo4j.driver.v1.Config
+import spock.lang.PendingFeature
+import spock.lang.Specification
+
+import static io.micronaut.kotlin.processing.KotlinCompiler.*
+
+class ConfigurationPropertiesBuilderSpec extends Specification {
+
+    void "test configuration builder on method"() {
+        given:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.MyProperties', '''
+package test
+
+import io.micronaut.context.annotation.*
+
+@ConfigurationProperties("test")
+class MyProperties {
+    
+    @ConfigurationBuilder(factoryMethod="build")
+    var test: Test? = null
+}
+
+class Test private constructor() {
+        
+    var foo: String? = null
+        
+    companion object {
+        @JvmStatic fun build(): Test {
+            return Test()
+        }
+    }
+}
+''')
+
+        when:"The bean was built and a warning was logged"
+        BeanFactory factory = beanDefinition
+        ApplicationContext applicationContext = ApplicationContext.run(
+                'test.foo':'good'
+        )
+        def bean = factory.build(applicationContext, beanDefinition)
+
+        then:
+        bean.test.foo == 'good'
+    }
+
+    void "test configuration builder with includes"() {
+        given:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.MyProperties', '''
+package test
+
+import io.micronaut.context.annotation.*
+
+@ConfigurationProperties("test")
+class MyProperties {
+
+    @ConfigurationBuilder(factoryMethod="build", includes=["foo"])
+    var test: Test? = null
+}
+
+class Test private constructor() {
+        
+    var foo: String? = null
+    var bar: String? = null
+        
+    companion object {
+        @JvmStatic fun build(): Test {
+            return Test()
+        }
+    }
+}
+''')
+
+        when:"The bean was built and a warning was logged"
+        BeanFactory factory = beanDefinition
+        ApplicationContext applicationContext = ApplicationContext.run(
+                'test.foo':'good',
+                'test.bar':'bad'
+        )
+        def bean = factory.build(applicationContext, beanDefinition)
+
+        then:
+        bean.test.foo == 'good'
+        bean.test.bar == null
+    }
+
+    void "test with setter methods that return this"() {
+        given:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.MyProperties', '''
+package test
+
+import io.micronaut.context.annotation.*
+
+@ConfigurationProperties("test")
+class MyProperties {
+
+    @ConfigurationBuilder(factoryMethod="build")
+    var test: Test? = null
+}
+
+class Test private constructor() {
+        
+    private var foo: String? = null
+    
+    fun getFoo() = foo
+    
+    fun setFoo(foo: String): Test {
+        this.foo = foo
+        return this
+    }
+    
+    private var bar: Int = 0
+    
+    fun getBar() = bar
+    
+    fun setBar(bar: Int): Test {
+        this.bar = bar
+        return this
+    }
+    
+    private var baz: Long? = null
+    
+    fun getBaz() = baz
+    
+    @Deprecated("do not use")
+    fun setBaz(baz: Long): Test {
+        this.baz = baz
+        return this
+    }
+        
+    companion object {
+        @JvmStatic fun build(): Test {
+            return Test()
+        }
+    }
+}
+''')
+
+        when:
+        BeanFactory factory = beanDefinition
+        ApplicationContext applicationContext = ApplicationContext.run(
+                'test.foo':'good',
+                'test.bar': '10',
+                'test.baz':'20'
+        )
+        def bean = factory.build(applicationContext, beanDefinition)
+
+        then:
+        bean != null
+        bean.test != null
+
+        when:
+        def test = bean.test
+
+        then:
+        test.foo == 'good'
+        test.bar == 10
+        test.baz == null //deprecated properties not settable
+    }
+
+    void "test different inject types for config properties"() {
+        when:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.Neo4jProperties', '''
+package test
+
+import io.micronaut.context.annotation.*
+import org.neo4j.driver.v1.*
+
+@ConfigurationProperties("neo4j.test")
+class Neo4jProperties {
+    internal var uri: java.net.URI? = null
+
+    @ConfigurationBuilder(
+        prefixes=["with"],
+        allowZeroArgs=true
+    )
+    val options: Config.ConfigBuilder = Config.build()
+}
+''')
+        then:
+        beanDefinition.injectedMethods.size() == 1
+        beanDefinition.injectedMethods.first().name == 'setUri$main'
+
+        when:
+        BeanFactory factory = beanDefinition
+        ApplicationContext applicationContext = ApplicationContext.run(
+                'neo4j.test.encryptionLevel':'none',
+                'neo4j.test.leakedSessionsLogging':true,
+                'neo4j.test.maxIdleSessions':2
+        )
+        def bean = factory.build(applicationContext, beanDefinition)
+
+        then:
+        bean != null
+        bean.options != null
+
+        when:
+        Config config = bean.options.toConfig()
+
+        then:
+        config.maxIdleConnectionPoolSize() == 2
+        config.encrypted() == true // deprecated properties are ignored
+        config.logLeakedSessions()
+    }
+
+    @PendingFeature(reason = "annotation defaults")
+    void "test specifying a configuration prefix"() {
+        when:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.Neo4jProperties', '''
+package test
+
+import io.micronaut.context.annotation.*
+import org.neo4j.driver.v1.*
+
+@ConfigurationProperties("neo4j.test")
+class Neo4jProperties {
+    internal var uri: java.net.URI? = null
+
+    @ConfigurationBuilder(
+        prefixes=["with"],
+        allowZeroArgs=true,
+        configurationPrefix="options"
+    )
+    val options: Config.ConfigBuilder = Config.build()
+}
+''')
+        then:
+        beanDefinition.injectedMethods.size() == 1
+        beanDefinition.injectedMethods.first().name == 'setUri$main'
+
+        when:
+        BeanFactory factory = beanDefinition
+        ApplicationContext applicationContext = ApplicationContext.run(
+                'neo4j.test.options.encryptionLevel':'none',
+                'neo4j.test.options.leakedSessionsLogging':true,
+                'neo4j.test.options.maxIdleSessions':2
+        )
+        def bean = factory.build(applicationContext, beanDefinition)
+
+        then:
+        bean != null
+        bean.options != null
+
+        when:
+        Config config = bean.options.toConfig()
+
+        then:
+        config.maxIdleConnectionPoolSize() == 2
+        config.encrypted() == true // deprecated properties are ignored
+        config.logLeakedSessions()
+    }
+
+    void "test specifying a configuration prefix with value"() {
+        when:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.Neo4jProperties', '''
+package test;
+
+import io.micronaut.context.annotation.*;
+import org.neo4j.driver.v1.*;
+
+@ConfigurationProperties("neo4j.test")
+class Neo4jProperties {
+    internal var uri: java.net.URI? = null
+
+    @ConfigurationBuilder(
+        prefixes=["with"],
+        allowZeroArgs=true,
+        value="options"
+    )
+    val options: Config.ConfigBuilder = Config.build()
+
+
+}
+''')
+        then:
+        beanDefinition.injectedMethods.size() == 1
+        beanDefinition.injectedMethods.first().name == 'setUri$main'
+
+        when:
+        BeanFactory factory = beanDefinition
+        ApplicationContext applicationContext = ApplicationContext.run(
+                'neo4j.test.options.encryptionLevel':'none',
+                'neo4j.test.options.leakedSessionsLogging':true,
+                'neo4j.test.options.maxIdleSessions':2
+        )
+        def bean = factory.build(applicationContext, beanDefinition)
+
+        then:
+        bean != null
+        bean.options != null
+
+        when:
+        Config config = bean.options.toConfig()
+
+        then:
+        config.maxIdleConnectionPoolSize() == 2
+        config.encrypted() == true // deprecated properties are ignored
+        config.logLeakedSessions()
+    }
+
+    void "test specifying a configuration prefix with value using @AccessorsStyle"() {
+        when:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.Neo4jProperties', '''
+package test
+
+import io.micronaut.context.annotation.*
+import io.micronaut.core.annotation.AccessorsStyle
+import org.neo4j.driver.v1.*
+
+@ConfigurationProperties("neo4j.test")
+class Neo4jProperties {
+    internal var uri: java.net.URI? = null
+
+    @ConfigurationBuilder(
+        allowZeroArgs = true,
+        value = "options"
+    )
+    @AccessorsStyle(writePrefixes = ["with"])
+    val options: Config.ConfigBuilder = Config.build()
+}
+''')
+        then:
+        beanDefinition.injectedMethods.size() == 1
+        beanDefinition.injectedMethods.first().name == 'setUri$main'
+
+        when:
+        BeanFactory factory = beanDefinition
+        ApplicationContext applicationContext = ApplicationContext.run(
+            'neo4j.test.options.encryptionLevel':'none',
+            'neo4j.test.options.leakedSessionsLogging':true,
+            'neo4j.test.options.maxIdleSessions':2
+        )
+        def bean = factory.build(applicationContext, beanDefinition)
+
+        then:
+        bean != null
+        bean.options != null
+
+        when:
+        Config config = bean.options.toConfig()
+
+        then:
+        config.maxIdleConnectionPoolSize() == 2
+        config.encrypted() == true // deprecated properties are ignored
+        config.logLeakedSessions()
+    }
+
+    void "test builder method long and TimeUnit arguments"() {
+        when:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.Neo4jProperties', '''
+package test
+
+import io.micronaut.context.annotation.*
+import org.neo4j.driver.v1.*
+
+@ConfigurationProperties("neo4j.test")
+class Neo4jProperties {
+    internal var uri: java.net.URI? = null
+
+    @ConfigurationBuilder(
+        prefixes=["with"],
+        allowZeroArgs=true
+    )
+    val options: Config.ConfigBuilder = Config.build()
+
+}
+''')
+        then:
+        beanDefinition.injectedMethods.size() == 1
+        beanDefinition.injectedMethods.first().name == 'setUri$main'
+
+        when:
+        BeanFactory factory = beanDefinition
+        ApplicationContext applicationContext = ApplicationContext.run(
+                'neo4j.test.connectionLivenessCheckTimeout': '6s'
+        )
+        def bean = factory.build(applicationContext, beanDefinition)
+
+        then:
+        bean != null
+        bean.options != null
+
+        when:
+        Config config = bean.options.toConfig()
+
+        then:
+        config.idleTimeBeforeConnectionTest() == 6000
+    }
+
+}
