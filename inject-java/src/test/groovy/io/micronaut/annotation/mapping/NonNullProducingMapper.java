b9aa85e0cc3a8f55552272f0c3bdd69f39ce97fd
yawkat <jonas.konrad@oracle.com>
2022-03-03 16:14:22 +0100

    use string for test case

--- inject-java/src/test/groovy/io/micronaut/annotation/mapping/NonNullProducingMapper.java
+++ inject-java/src/test/groovy/io/micronaut/annotation/mapping/NonNullProducingMapper.java
@@ -4,14 +4,13 @@ import io.micronaut.core.annotation.AnnotationValue;
 import io.micronaut.inject.annotation.TypedAnnotationMapper;
 import io.micronaut.inject.visitor.VisitorContext;
 
-import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
 
 public class NonNullProducingMapper implements TypedAnnotationMapper<NonNullStereotyped> {
     @Override
     public List<AnnotationValue<?>> map(AnnotationValue<NonNullStereotyped> annotation, VisitorContext visitorContext) {
-        return Collections.singletonList(AnnotationValue.builder(Nullable.class).build());
+        return Collections.singletonList(AnnotationValue.builder("javax.annotation.Nullable").build());
     }
 
     @Override
