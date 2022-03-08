09ec86b24ede76afff0914821f7e35b4d7763390
jameskleeh <james.kleeh@gmail.com>
2022-03-08 16:49:06 -0500

    Fix aop proxy target on methods only. Add tests

---
+++ inject-kotlin/src/test/groovy/io/micronaut/kotlin/processing/aop/compile/LifeCycleWithProxySpec.groovy
@@ -0,0 +1,216 @@
+package io.micronaut.kotlin.processing.aop.compile
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.inject.BeanDefinition
+import io.micronaut.inject.BeanFactory
+import io.micronaut.inject.writer.BeanDefinitionWriter
+import spock.lang.Specification
+
+import static io.micronaut.kotlin.processing.KotlinCompiler.*
+
+class LifeCycleWithProxySpec extends Specification {
+
+    void "test that a proxy target AOP definition lifecycle hooks are invoked - annotation at class level"() {
+        when:
+        ApplicationContext context = buildContext('''
+package test
+
+import io.micronaut.kotlin.processing.aop.proxytarget.Mutating
+import io.micronaut.core.convert.ConversionService
+
+@Mutating("someVal")
+@jakarta.inject.Singleton
+open class MyBean {
+
+    @jakarta.inject.Inject 
+    lateinit var conversionService: ConversionService<*>
+    
+    var count = 0
+    
+    open fun someMethod(): String {
+        return "good"
+    }
+    
+    @jakarta.annotation.PostConstruct
+    fun created() {
+        count++
+    }
+    
+    @javax.annotation.PreDestroy
+    fun destroyed() {
+        count--
+    }
+
+}
+''')
+
+        def instance = getBean(context, "test.MyBean")
+
+
+        then:"proxy post construct methods are not invoked"
+        instance.conversionService // injection works
+        instance.someMethod() == 'good'
+        instance.count == 0
+
+        and:"proxy target post construct methods are invoked"
+        instance.interceptedTarget().count == 1
+
+        cleanup:
+        context.close()
+    }
+/*
+    void "test that a simple AOP definition lifecycle hooks are invoked - annotation at class level"() {
+        when:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.$MyBean' + BeanDefinitionWriter.CLASS_SUFFIX + BeanDefinitionWriter.PROXY_SUFFIX, '''
+package test;
+
+import io.micronaut.aop.simple.*;
+import io.micronaut.context.annotation.*;
+import io.micronaut.core.convert.*;
+
+@Mutating("someVal")
+@jakarta.inject.Singleton
+class MyBean {
+
+    @jakarta.inject.Inject public ConversionService conversionService;
+    public int count = 0;
+
+    public String someMethod() {
+        return "good";
+    }
+
+    @jakarta.annotation.PostConstruct
+    void created() {
+        count++;
+    }
+
+    @javax.annotation.PreDestroy
+    void destroyed() {
+        count--;
+    }
+
+}
+''')
+        then:
+        !beanDefinition.isAbstract()
+        beanDefinition != null
+        beanDefinition.postConstructMethods.size() == 1
+        beanDefinition.preDestroyMethods.size() == 1
+
+        when:
+        def context = ApplicationContext.run()
+
+        def instance = ((BeanFactory) beanDefinition).build(context, beanDefinition)
+
+
+        then:
+        instance.conversionService // injection works
+        instance.someMethod() == 'good'
+        instance.count == 1
+
+        cleanup:
+        context.close()
+    }
+
+    void "test that a simple AOP definition lifecycle hooks are invoked - annotation at method level with hooks last"() {
+        when:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.$MyBean' + BeanDefinitionWriter.CLASS_SUFFIX + BeanDefinitionWriter.PROXY_SUFFIX, '''
+package test;
+
+import io.micronaut.aop.simple.*;
+import io.micronaut.core.convert.*;
+
+@jakarta.inject.Singleton
+class MyBean {
+
+    @jakarta.inject.Inject public ConversionService conversionService;
+
+    public int count = 0;
+
+    @Mutating("someVal")
+    public String someMethod() {
+        return "good";
+    }
+
+    @jakarta.annotation.PostConstruct
+    void created() {
+        count++;
+    }
+
+    @javax.annotation.PreDestroy
+    void destroyed() {
+        count--;
+    }
+}
+''')
+        then:
+        !beanDefinition.isAbstract()
+        beanDefinition != null
+        beanDefinition.postConstructMethods.size() == 1
+        beanDefinition.preDestroyMethods.size() == 1
+
+        when:
+        def context = ApplicationContext.run()
+        def instance = ((BeanFactory) beanDefinition).build(context, beanDefinition)
+
+
+        then:
+        instance.conversionService != null
+        instance.someMethod() == 'good'
+        instance.count == 1
+
+        cleanup:
+        context.close()
+    }
+
+    void "test that a simple AOP definition lifecycle hooks are invoked - annotation at method level"() {
+        when:
+        BeanDefinition beanDefinition = buildBeanDefinition('test.$MyBean' + BeanDefinitionWriter.CLASS_SUFFIX + BeanDefinitionWriter.PROXY_SUFFIX, '''
+package test;
+
+import io.micronaut.aop.simple.*;
+import io.micronaut.context.annotation.*;
+import io.micronaut.core.convert.*;
+
+@jakarta.inject.Singleton
+class MyBean {
+
+    @jakarta.inject.Inject public ConversionService conversionService;
+    public int count = 0;
+
+    @jakarta.annotation.PostConstruct
+    void created() {
+        count++;
+    }
+
+    @javax.annotation.PreDestroy
+    void destroyed() {
+        count--;
+    }
+
+    @Mutating("someVal")
+    public String someMethod() {
+        return "good";
+    }
+
+
+}
+''')
+        then:
+        !beanDefinition.isAbstract()
+        beanDefinition != null
+
+        when:
+        def context = ApplicationContext.run()
+        def instance = ((BeanFactory) beanDefinition).build(context, beanDefinition)
+
+
+        then:
+        instance.conversionService != null
+        instance.someMethod() == 'good'
+        instance.count == 1
+
+        cleanup:
+        context.close()
+    }*/
+}
