d7e792666615a3c35ddb34b84f4db72a0005a6e9
Sergio del Amo <sergio.delamo@softamo.com>
2022-04-19 12:29:58 +0200

    test: close bean context

--- test-suite-jakarta-inject-bean-import/src/test/java/io/micronaut/jakartainject/tck/beanimport/BeanImportTck.java
+++ test-suite-jakarta-inject-bean-import/src/test/java/io/micronaut/jakartainject/tck/beanimport/BeanImportTck.java
@@ -2,18 +2,26 @@ package io.micronaut.jakartainject.tck.beanimport;
 
 import io.micronaut.context.BeanContext;
 import io.micronaut.context.annotation.Import;
-import junit.framework.Test;
+import junit.framework.TestResult;
 import org.atinject.tck.Tck;
 import org.atinject.tck.auto.Car;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Import(
         packages = {"org.atinject.tck.auto", "org.atinject.tck.auto.accessories"},
         annotated = "*")
-public class BeanImportTck {
+class BeanImportTck {
 
-    public static Test suite() {
+    @Test
+    void suite() {
         BeanContext beanContext = BeanContext.run();
-        // Tests are running after this method, keep the bean context open
-        return Tck.testsFor(beanContext.getBean(Car.class), false, true);
+        junit.framework.Test test = Tck.testsFor(beanContext.getBean(Car.class), false, true);
+        TestResult result = new TestResult();
+        test.run(result);
+        assertTrue(result.runCount() > 0);
+        assertTrue(result.wasSuccessful());
+        beanContext.close();
     }
 }
