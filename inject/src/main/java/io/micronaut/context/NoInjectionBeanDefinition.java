25d324921aa52db04a279eddca186c863fab4899
Attila Korompai <attila.korompai@gmail.com>
2022-05-13 07:02:40 +0200

    DefaultBeanContext#getBeansOfType should return programmatically registered singletons (#7308)

--- inject/src/main/java/io/micronaut/context/NoInjectionBeanDefinition.java
+++ inject/src/main/java/io/micronaut/context/NoInjectionBeanDefinition.java
@@ -35,6 +35,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -206,7 +207,7 @@ final class NoInjectionBeanDefinition<T> implements BeanDefinition<T>, BeanDefin
         }
 
         NoInjectionBeanDefinition<?> that = (NoInjectionBeanDefinition) o;
-        return singletonClass.equals(that.singletonClass);
+        return singletonClass.equals(that.singletonClass) && Objects.equals(qualifier, that.qualifier);
     }
 
     @Override
