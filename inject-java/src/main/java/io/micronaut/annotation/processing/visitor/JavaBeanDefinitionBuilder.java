96f88e92d3d90b1fff533025a61fb44f252fd2d0
Sergio del Amo <sergio.delamo@softamo.com>
2022-06-09 09:57:54 +0200

    sonar: Inmmeditately return this expression instead of assigning it to temporary variabable

--- inject-java/src/main/java/io/micronaut/annotation/processing/visitor/JavaBeanDefinitionBuilder.java
+++ inject-java/src/main/java/io/micronaut/annotation/processing/visitor/JavaBeanDefinitionBuilder.java
@@ -116,14 +116,13 @@ class JavaBeanDefinitionBuilder extends AbstractBeanDefinitionBuilder {
     protected BeanDefinitionVisitor createAopWriter(BeanDefinitionWriter beanDefinitionWriter, AnnotationMetadata annotationMetadata) {
         AnnotationValue<?>[] interceptorTypes =
                 InterceptedMethodUtil.resolveInterceptorBinding(annotationMetadata, InterceptorKind.AROUND);
-        BeanDefinitionVisitor aopProxyWriter = new AopProxyWriter(
+        return new AopProxyWriter(
                 beanDefinitionWriter,
                 annotationMetadata.getValues(Around.class, Boolean.class),
                 ConfigurationMetadataBuilder.getConfigurationMetadataBuilder().orElse(null),
                 visitorContext,
                 interceptorTypes
         );
-        return aopProxyWriter;
     }
 
     @Override
