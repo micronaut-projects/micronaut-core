f2d7a39fa89b5e390b979664687398e0446614f4
Denis Stepanov <denis.s.stepanov@oracle.com>
2022-09-29 15:06:43 +0200

    Fix entity becoming a bean

---
+++ inject-java/src/test/groovy/io/micronaut/inject/validation/Account3.java
@@ -0,0 +1,41 @@
+package io.micronaut.inject.validation;
+
+import javax.annotation.Nullable;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Nullable
+public class Account3 {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NotBlank
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
