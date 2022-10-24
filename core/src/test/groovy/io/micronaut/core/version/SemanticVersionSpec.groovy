124a6565e95b5e361c105faed1dceb7a3cca1c20
Alexey Zhokhov <alexey@zhokhov.com>
2022-10-24 17:33:11 +0800

    Let SemanticVersion.isAtLeastMajorMinor be able to compare two different major versions. (#8208)

--- core/src/test/groovy/io/micronaut/core/version/SemanticVersionSpec.groovy
+++ core/src/test/groovy/io/micronaut/core/version/SemanticVersionSpec.groovy
@@ -19,4 +19,15 @@ class SemanticVersionSpec extends Specification {
         semver << ["1.0.0", "1.0.0.M1", "1.0.0.RC1", "1.0.0.BUILD-SNAPSHOT", "1.0.0-M1", "1.0.0-RC1", "1.0.0-BUILD-SNAPSHOT", "1.0.0-SNAPSHOT"]
     }
 
+    void "it compare two different major versions: #semver"(String semver) {
+        expect:
+        SemanticVersion.isAtLeastMajorMinor(semver, 3, 3)
+        SemanticVersion.isAtLeastMajorMinor(semver, 3, 0)
+        !SemanticVersion.isAtLeastMajorMinor(semver, 5, 3)
+        !SemanticVersion.isAtLeastMajorMinor(semver, 5, 0)
+
+        where:
+        semver << ["4.0.0", "4.0.0.M1", "4.0.0.RC1", "4.0.0.BUILD-SNAPSHOT", "4.0.0-M1", "4.0.0-RC1", "4.0.0-BUILD-SNAPSHOT", "4.0.0-SNAPSHOT"]
+    }
+
 }
