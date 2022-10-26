b46ee37e5b8962e4f41276b79dd0be7202551a1f
Denis Stepanov <denis.s.stepanov@oracle.com>
2022-10-26 09:32:18 +0400

    Disable failing test

--- test-suite-kotlin/src/test/kotlin/io/micronaut/docs/server/suspend/SuspendController.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/server/suspend/SuspendController.kt
@@ -178,13 +178,12 @@ class SuspendController(
         "$before,$after"
     }
 
-// TODO: HttpCoroutineTracingDispatcherFactory#create should eliminate nulls
-//    @Get("/keepTracingContextUsingCoroutineTracingDispatcherExplicitly")
-//    fun keepTracingContextUsingCoroutineTracingDispatcherExplicitly() = runBlocking {
-//        val before = currentTraceId()
-//        val after = withContext(Dispatchers.Default + coroutineTracingDispatcherFactory.create()) { currentTraceId() }
-//        "$before,$after"
-//    }
+    @Get("/keepTracingContextUsingCoroutineTracingDispatcherExplicitly")
+    fun keepTracingContextUsingCoroutineTracingDispatcherExplicitly() = runBlocking {
+        val before = currentTraceId()
+        val after = withContext(Dispatchers.Default + coroutineTracingDispatcherFactory.create()) { currentTraceId() }
+        "$before,$after"
+    }
 
     private fun currentTraceId(): String? = MDC.get("traceId")
 }
