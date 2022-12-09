dc42add597516c21f7548d6f60f35f84cc93375c
Sergio del Amo <sergio.delamo@softamo.com>
2022-12-09 13:18:22 +0100

    test: verify DefaultHttpClient::traceRequest

--- http-client/src/test/groovy/io/micronaut/http/client/netty/DefaultClientHeaderMaskTest.groovy
+++ http-client/src/test/groovy/io/micronaut/http/client/netty/DefaultClientHeaderMaskTest.groovy
@@ -1,10 +1,11 @@
 package io.micronaut.http.client.netty
 
 import ch.qos.logback.classic.Level
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.client.HttpClient
 import org.slf4j.Logger
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.AppenderBase
-import io.micronaut.http.util.HttpHeadersUtil
 import io.netty.handler.codec.http.DefaultHttpHeaders
 import org.slf4j.LoggerFactory
 import spock.lang.Specification
@@ -16,10 +17,17 @@ class DefaultClientHeaderMaskTest extends Specification {
 
     def "check mask detects common security headers"() {
         given:
+        ApplicationContext ctx = ApplicationContext.run()
+        HttpClient client = ctx.createBean(HttpClient, "http://localhost:8080")
+
+        expect:
+        client instanceof DefaultHttpClient
+
+        when:
         MemoryAppender appender = new MemoryAppender()
         Logger log = LoggerFactory.getLogger(DefaultHttpClient.class)
 
-        expect:
+        then:
         log instanceof ch.qos.logback.classic.Logger
 
         when:
@@ -28,22 +36,26 @@ class DefaultClientHeaderMaskTest extends Specification {
         logger.setLevel(Level.TRACE)
         appender.start()
 
-        DefaultHttpHeaders headers = new DefaultHttpHeaders()
-        headers.add("Authorization", "Bearer foo")
-        headers.add("Proxy-Authorization", "AWS4-HMAC-SHA256 bar")
-        headers.add("Cookie", "baz")
-        headers.add("Set-Cookie", "qux")
-        headers.add("X-Forwarded-For", "quux")
-        headers.add("X-Forwarded-Host", "quuz")
-        headers.add("X-Real-IP", "waldo")
-        headers.add("X-Forwarded-For", "fred")
-        headers.add("Credential", "foo")
-        headers.add("Signature", "bar probably secret")
+        DefaultHttpHeaders httpHeaders = new DefaultHttpHeaders()
+        httpHeaders.add("Authorization", "Bearer foo")
+        httpHeaders.add("Proxy-Authorization", "AWS4-HMAC-SHA256 bar")
+        httpHeaders.add("Cookie", "baz")
+        httpHeaders.add("Set-Cookie", "qux")
+        httpHeaders.add("X-Forwarded-For", "quux")
+        httpHeaders.add("X-Forwarded-Host", "quuz")
+        httpHeaders.add("X-Real-IP", "waldo")
+        httpHeaders.add("X-Forwarded-For", "fred")
+        httpHeaders.add("Credential", "foo")
+        httpHeaders.add("Signature", "bar probably secret")
 
-        HttpHeadersUtil.trace(log, headers.names(), headers::getAll)
+        def request = Mock(io.micronaut.http.HttpRequest)
+        def nettyRequest = Stub(io.netty.handler.codec.http.HttpRequest) {
+            headers() >> httpHeaders
+        }
+        ((io.micronaut.http.client.netty.DefaultHttpClient) client).traceRequest(request, nettyRequest)
 
         then:
-        appender.events.size() == headers.size()
+        appender.events.size() == httpHeaders.size()
         appender.events.join("\n") == """Authorization: *MASKED*
             |Proxy-Authorization: *MASKED*
             |Cookie: baz
@@ -57,6 +69,7 @@ class DefaultClientHeaderMaskTest extends Specification {
 
         cleanup:
         appender.stop()
+        ctx.close()
     }
 
     static class MemoryAppender extends AppenderBase<ILoggingEvent> {
