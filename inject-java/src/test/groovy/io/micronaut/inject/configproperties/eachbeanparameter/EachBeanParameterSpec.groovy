b5cf6e537119a554217146bc228b17f11fea2326
Sergio del Amo <sergio.delamo@softamo.com>
2022-12-21 11:00:33 +0100

    more tests

--- inject-java/src/test/groovy/io/micronaut/inject/configproperties/eachbeanparameter/EachBeanParameterSpec.groovy
+++ inject-java/src/test/groovy/io/micronaut/inject/configproperties/eachbeanparameter/EachBeanParameterSpec.groovy
@@ -3,7 +3,9 @@ package io.micronaut.inject.configproperties.eachbeanparameter
 import io.micronaut.annotation.processing.test.AbstractTypeElementSpec
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.Qualifier
+import io.micronaut.context.annotation.Primary
 import io.micronaut.context.exceptions.NonUniqueBeanException
+import io.micronaut.inject.qualifiers.PrimaryQualifier
 import io.micronaut.inject.qualifiers.Qualifiers
 
 class EachBeanParameterSpec extends AbstractTypeElementSpec {
@@ -20,6 +22,7 @@ class EachBeanParameterSpec extends AbstractTypeElementSpec {
         when:
             def service = ctx.getBean(MyService)
             Qualifier defaultNameQualifier = Qualifiers.byName("default")
+
         then:
             service.getDefaultBean().name == "default"
             service.getBarBean().name == "bar"
@@ -31,6 +34,9 @@ class EachBeanParameterSpec extends AbstractTypeElementSpec {
             ctx.getBeansOfType(MyBean).size() == datasourcesConfiguration.size() + 1
             ctx.getBeansOfType(AbstractDataSource).stream().filter(it -> it instanceof MyDataSource).count() == 3
             ctx.getBeansOfType(AbstractDataSource).stream().filter(it -> it instanceof DefaultDataSource).count() == 1
+            ctx.getBeansOfType(AbstractDataSource, PrimaryQualifier.INSTANCE).size() == 2
+            ctx.getBeansOfType(MyHelper, PrimaryQualifier.INSTANCE).size() == 2
+            ctx.getBeansOfType(MyBean, PrimaryQualifier.INSTANCE).size() == 2
 
             ctx.getBeansOfType(AbstractDataSource, defaultNameQualifier).size() == 2
             ctx.getBeansOfType(MyHelper, defaultNameQualifier).size() == 2
@@ -40,6 +46,16 @@ class EachBeanParameterSpec extends AbstractTypeElementSpec {
             ctx.getBean(AbstractDataSource, defaultNameQualifier)
         then:
             thrown(NonUniqueBeanException) // DefaultDataSource is annotated with `@Primary`. MyDataSource also defines primary via the annotation member `@EachProperty(value = "mydatasources", primary = "default")`
+
+        when:
+            ctx.getBean(MyHelper, defaultNameQualifier)
+        then:
+            noExceptionThrown() // should not throw thrown(NonUniqueBeanException)?
+
+        when:
+            ctx.getBean(MyBean, defaultNameQualifier)
+        then:
+            noExceptionThrown() // should not throw thrown(NonUniqueBeanException)?
 
         cleanup:
             ctx.close()
