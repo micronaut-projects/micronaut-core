2454443c4e2a7313660277022a8df5ea82962799
Sergio del Amo <sergio.delamo@softamo.com>
2023-01-09 13:01:18 +0100

    add test

---
+++ http-server-tck/src/main/java/io/micronaut/http/server/tck/tests/cors/CrossOriginTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017-2022 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.server.tck.tests.cors;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.server.cors.CrossOrigin;
+import io.micronaut.http.server.tck.AssertionUtils;
+import io.micronaut.http.server.tck.HttpResponseAssertion;
+import io.micronaut.http.uri.UriBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static io.micronaut.http.server.tck.TestScenario.asserts;
+
+
+@SuppressWarnings({
+    "java:S2259", // The tests will show if it's null
+    "java:S5960", // We're allowed assertions, as these are used in tests only
+    "checkstyle:MissingJavadocType",
+})
+public class CrossOriginTest {
+
+    private static final String SPECNAME = "CrossOriginTest";
+
+    @Test
+    void corsSimpleRequestNotAllowedForLocalhostAndAny() throws IOException {
+        asserts(SPECNAME,
+            HttpRequest.GET(UriBuilder.of("/foo").path("bar").build()).header(HttpHeaders.ORIGIN, "https://foo.com"),
+            (server, request) -> AssertionUtils.assertDoesNotThrow(server, request, HttpResponseAssertion.builder()
+                .status(HttpStatus.OK)
+                .body("bar")
+                .build()));
+    }
+
+    @Requires(property = "spec.name", value = SPECNAME)
+    @Controller("/foo")
+    static class Foo {
+
+        @CrossOrigin("https://foo.com")
+        @Produces(MediaType.TEXT_PLAIN)
+        @Get("/bar")
+        String index() {
+            return "bar";
+        }
+    }
+}
