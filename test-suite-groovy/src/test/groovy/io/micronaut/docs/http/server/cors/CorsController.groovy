f601bbb11f08e587ba5934543cbfdc2eda51a6f6
Dean Wette <wetted@objectcomputing.com>
2023-01-16 12:48:27 -0600

    Improve documentation with a java/groovy/kotlin snippet

---
+++ test-suite-groovy/src/test/groovy/io/micronaut/docs/http/server/cors/CorsController.groovy
@@ -0,0 +1,26 @@
+package io.micronaut.docs.http.server.cors
+
+// tag::imports[]
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+import io.micronaut.http.server.cors.CrossOrigin
+// end::imports[]
+
+// tag::controller[]
+@Controller("/hello")
+class CorsController {
+    @CrossOrigin("https://myui.com") // <1>
+    @Get(produces = MediaType.TEXT_PLAIN) // <2>
+    String cors() {
+        return "Welcome to the worlds of CORS"
+    }
+
+    @Produces(MediaType.TEXT_PLAIN)
+    @Get("/nocors") // <3>
+    String nocorstoday() {
+        return "No more CORS for you"
+    }
+}
+// end::controller[]
