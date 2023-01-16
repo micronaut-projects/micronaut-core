e114f99222af49c4c20b851e970c4dd57a2c1b47
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-01-16 11:35:33 +0100

    Fix failing test

---
+++ inject-java/src/test/groovy/io/micronaut/inject/lifecycle/proxytargetbeanwithpredestroy/package-info.java
@@ -0,0 +1,5 @@
+
+@Requires(property = "spec", value = "ProxyTargetBeanWithPreDestroySpec")
+package io.micronaut.inject.lifecycle.proxytargetbeanwithpredestroy;
+
+import io.micronaut.context.annotation.Requires;
