40e9b6774e3fdefcc3417138df67871fe95523fb
yawkat <jonas.konrad@oracle.com>
2023-01-17 12:08:14 +0100

    checkstyle

--- http-server-tck/src/main/java/io/micronaut/http/server/tck/tests/filter/RequestFilterTest.java
+++ http-server-tck/src/main/java/io/micronaut/http/server/tck/tests/filter/RequestFilterTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.http.server.tck.tests.filter;
 
 import io.micronaut.context.annotation.Requires;
@@ -25,6 +40,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings({
+    "java:S5960", // We're allowed assertions, as these are used in tests only
+    "checkstyle:MissingJavadocType",
+    "checkstyle:DesignForExtension"
+})
 public class RequestFilterTest {
     public static final String SPEC_NAME = "RequestFilterTest";
 
