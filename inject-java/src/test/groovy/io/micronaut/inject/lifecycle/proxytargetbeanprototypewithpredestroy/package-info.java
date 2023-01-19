3cac50ae10d6ab84a64e6840fa53ea128bd833c8
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-01-19 14:36:33 +0100

    Fix NPE in isCandidateBean (#8631)
    Fixes #8627

--- inject-java/src/test/groovy/io/micronaut/inject/lifecycle/proxytargetbeanprototypewithpredestroy/package-info.java
+++ inject-java/src/test/groovy/io/micronaut/inject/lifecycle/proxytargetbeanprototypewithpredestroy/package-info.java
@@ -1,5 +1,7 @@
 
+@Configuration
 @Requires(property = "spec", value = "ProxyLazyCachedTargetPrototypeBeanWithPreDestroySpec")
 package io.micronaut.inject.lifecycle.proxytargetbeanprototypewithpredestroy;
 
+import io.micronaut.context.annotation.Configuration;
 import io.micronaut.context.annotation.Requires;
