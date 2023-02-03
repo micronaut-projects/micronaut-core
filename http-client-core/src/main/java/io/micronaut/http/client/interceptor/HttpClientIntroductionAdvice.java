ac0722e744b16b1f0312621b4fcee449ca3d2ec2
Adam Wilder <awilder@arrivelogistics.com>
2023-02-03 12:08:39 -0600

    add test for the filter behavior and attempt a fix

--- http-client-core/src/main/java/io/micronaut/http/client/interceptor/HttpClientIntroductionAdvice.java
+++ http-client-core/src/main/java/io/micronaut/http/client/interceptor/HttpClientIntroductionAdvice.java
@@ -347,9 +347,11 @@ public class HttpClientIntroductionAdvice implements MethodInterceptor<Object, O
                         CompletableFuture<Object> future = new CompletableFuture<>();
                         csPublisher.subscribe(new CompletionAwareSubscriber<Object>() {
                             AtomicReference<Object> reference = new AtomicReference<>();
+                            Subscription subscription;
 
                             @Override
                             protected void doOnSubscribe(Subscription subscription) {
+                                this.subscription = subscription;
                                 subscription.request(1);
                             }
 
@@ -358,6 +360,9 @@ public class HttpClientIntroductionAdvice implements MethodInterceptor<Object, O
                                 if (Void.class != reactiveValueType) {
                                     reference.set(message);
                                 }
+                                // we only want the first item
+                                subscription.cancel();
+                                doOnComplete();
                             }
 
                             @Override
