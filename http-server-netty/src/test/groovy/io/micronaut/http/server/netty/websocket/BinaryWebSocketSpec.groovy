8fb8d04f43da0c8046debf3afe00f136cd8e8bfc
Tim Yates <tim.yates@gmail.com>
2023-02-06 15:15:18 +0000

    Deflake the BinaryWebSocketSpec
    Step 1. Remove the Retry annotation as Flakey test detection checks this and it makes it hard to see the failure

--- http-server-netty/src/test/groovy/io/micronaut/http/server/netty/websocket/BinaryWebSocketSpec.groovy
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/websocket/BinaryWebSocketSpec.groovy
@@ -32,17 +32,14 @@ import jakarta.inject.Singleton
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import spock.lang.Issue
-import spock.lang.Retry
 import spock.lang.Specification
 import spock.util.concurrent.PollingConditions
 
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
 
-@Retry
 class BinaryWebSocketSpec extends Specification {
 
-    @Retry
     void "test binary websocket exchange"() {
         given:
         EmbeddedServer embeddedServer = ApplicationContext.builder('micronaut.server.netty.log-level':'TRACE').run(EmbeddedServer)
@@ -117,7 +114,6 @@ class BinaryWebSocketSpec extends Specification {
         }
 
         cleanup:
-        wsClient.close()
         embeddedServer.close()
     }
 
