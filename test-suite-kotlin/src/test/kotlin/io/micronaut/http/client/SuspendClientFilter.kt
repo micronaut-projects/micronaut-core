8f97ec95cd6a9c19c9a0bd08912a2562df45de6b
Adam Wilder <awilder@arrivelogistics.com>
2023-02-10 11:40:45 -0600

    fix the test suspend client filter to actually do the flow intermediary step on the right test

--- test-suite-kotlin/src/test/kotlin/io/micronaut/http/client/SuspendClientFilter.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/http/client/SuspendClientFilter.kt
@@ -2,28 +2,33 @@ package io.micronaut.http.client
 
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MutableHttpRequest
+import io.micronaut.http.annotation.Filter
 import io.micronaut.http.filter.ClientFilterChain
 import io.micronaut.http.filter.HttpClientFilter
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.asPublisher
+import kotlinx.coroutines.withContext
 import org.reactivestreams.Publisher
 
+@Filter
 class SuspendClientFilter : HttpClientFilter {
     override fun doFilter(request: MutableHttpRequest<*>, chain: ClientFilterChain): Publisher<out HttpResponse<*>> {
         // if request contains filterCheck, then do flow step, else proceed
-        if ((request.body.get() as String).contains(SuspendClientFilter.filterCheck)) {
-            return flow { emit("testString") }.flatMapMerge {
+        return if ((request.body.orElse(null) as? Map<*, *>)?.containsValue(filterCheck) == true) {
+            flow { emit(getValue()) }.flatMapMerge {
                 chain.proceed(request.header("myTestHeader", it)).asFlow()
             }.asPublisher()
         } else {
-            return chain.proceed(request)
+            chain.proceed(request)
         }
     }
 
     companion object {
         val filterCheck = java.util.UUID.randomUUID().toString()
+        suspend fun getValue() = withContext(Dispatchers.Default) { "testString" }
     }
 }
 
