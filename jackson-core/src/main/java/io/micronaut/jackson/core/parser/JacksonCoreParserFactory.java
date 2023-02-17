cc0e32ed7a39a6dfd1c2badb3230bb2d7b1c3e6c
yawkat <jonas.konrad@oracle.com>
2023-02-17 17:49:42 +0100

    spotless

--- jackson-core/src/main/java/io/micronaut/jackson/core/parser/JacksonCoreParserFactory.java
+++ jackson-core/src/main/java/io/micronaut/jackson/core/parser/JacksonCoreParserFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.jackson.core.parser;
 
 import com.fasterxml.jackson.core.JsonFactory;
@@ -20,7 +35,8 @@ import java.io.InputStream;
 public class JacksonCoreParserFactory {
     private static final boolean HAS_NETTY_BUFFER;
 
-    private JacksonCoreParserFactory() {}
+    private JacksonCoreParserFactory() {
+    }
 
     static {
         boolean hasNettyBuffer;
