48aa4651241ca1f8b9736b321b2d6fe265a6d6f0
Graeme Rocher <graeme.rocher@oracle.com>
2023-03-01 18:24:00 +0100

    Run the HTTP TCK against Netty natively

--- test-suite-http-server-tck-netty/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("io.micronaut.build.internal.convention-test-library")
-}
-dependencies {
-    testImplementation(projects.httpServerNetty)
-    testImplementation(projects.httpClient)
-    testImplementation(projects.httpServerTck)
-    testImplementation(libs.junit.platform.engine)
-}
