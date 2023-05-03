d124f67eb43a1d8f44bb4c9519a4f0f986d53793
Filip Filipovic <filipf36999@gmail.com>
2023-05-03 12:47:57 +0200

    Added tests

---
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/context/ContextURISpec.groovy
@@ -0,0 +1,35 @@
+package io.micronaut.http.server.netty.context
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.Specification
+
+class ContextURISpec extends Specification {
+
+    void "test getContextURI returns the URI with the context path when context path is set"() {
+        when:
+        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, [
+                'micronaut.server.context-path': '/context',
+                'micronaut.server.port': 60006
+        ])
+
+        then:
+        embeddedServer.getContextURI().toString() == 'http://localhost:60006/context'
+
+        cleanup:
+        embeddedServer.close()
+    }
+
+    void "test getContextURI returns the base URI when context path is not set"() {
+        when:
+        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, [
+                'micronaut.server.port': 60006
+        ])
+
+        then:
+        embeddedServer.getContextURI().toString() == 'http://localhost:60006'
+
+        cleanup:
+        embeddedServer.close()
+    }
+}
