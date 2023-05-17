e50078e67c2d42d8088074cee034097b61b54ae3
altro3 <alexuyt@gmail.com>
2023-05-17 22:59:33 +0700

    Add nullable annotations transformers (#9247)

---
+++ core-processor/src/main/java/io/micronaut/inject/annotation/internal/ReactivexNullableTransformer.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.inject.annotation.internal;
+
+import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.List;
+
+import io.micronaut.core.annotation.AnnotationUtil;
+import io.micronaut.core.annotation.AnnotationValue;
+import io.micronaut.core.annotation.Internal;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.inject.annotation.NamedAnnotationTransformer;
+import io.micronaut.inject.visitor.VisitorContext;
+
+/**
+ * A transformer that remaps io.reactivex.annotations.Nullable to {@code javax.annotation.Nullable}.
+ *
+ * @since 4.0.0
+ */
+@Internal
+public class ReactivexNullableTransformer implements NamedAnnotationTransformer {
+
+    @Override
+    public @NonNull String getName() {
+        return "io.reactivex.annotations.Nullable";
+    }
+
+    @Override
+    public List<AnnotationValue<?>> transform(AnnotationValue<Annotation> annotation, VisitorContext visitorContext) {
+        return Collections.singletonList(
+            AnnotationValue.builder(AnnotationUtil.NULLABLE).build()
+        );
+    }
+}
