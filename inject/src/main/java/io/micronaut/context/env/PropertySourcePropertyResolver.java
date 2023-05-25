micronaut-projects/micronaut-core

commit ac216c5daafcaff1f2cd8a1d31a8606aa1324fdf

Only convert config placeholders when necessary (#9310)
* Only convert config placeholders when necessary

* don't comment out the test

inject/src/main/java/io/micronaut/context/env/PropertySourcePropertyResolver.java
@@ -32,7 +32,6 @@ import io.micronaut.core.util.EnvironmentProperties;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.core.value.MapPropertyResolver;
 import io.micronaut.core.value.PropertyResolver;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.helpers.NOPLogger;
@@ -782,7 +781,7 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
 
     private Object resolvePlaceHoldersIfNecessary(Object value) {
         if (value instanceof CharSequence) {
-            return propertyPlaceholderResolver.resolveRequiredPlaceholders(value.toString());
+            return propertyPlaceholderResolver.resolveRequiredPlaceholdersObject(value.toString());
         } else if (value instanceof List) {
             List<?> list = (List) value;
             List<?> newList = new ArrayList<>(list);

inject/src/main/java/io/micronaut/context/env/DefaultPropertyPlaceholderResolver.java
@@ -101,6 +101,19 @@ public class DefaultPropertyPlaceholderResolver implements PropertyPlaceholderRe
     @Override
     public String resolveRequiredPlaceholders(String str) throws ConfigurationException {
         List<Segment> segments = buildSegments(str);
+        return resolveRequiredPlaceholdersString(segments);
+    }
+
+    @Override
+    public Object resolveRequiredPlaceholdersObject(String str) throws ConfigurationException {
+        List<Segment> segments = buildSegments(str);
+        if (segments.size() == 1) {
+            return segments.get(0).getValue(Object.class);
+        }
+        return resolveRequiredPlaceholdersString(segments);
+    }
+
+    private static String resolveRequiredPlaceholdersString(List<Segment> segments) {
         StringBuilder value = new StringBuilder();
         for (Segment segment: segments) {
             value.append(segment.getValue(String.class));

inject/src/main/java/io/micronaut/context/env/PropertyPlaceholderResolver.java
@@ -16,8 +16,8 @@
 package io.micronaut.context.env;
 
 import io.micronaut.context.exceptions.ConfigurationException;
-
 import io.micronaut.core.annotation.NonNull;
+
 import java.util.Optional;
 
 /**
@@ -44,14 +44,27 @@ public interface PropertyPlaceholderResolver {
     }
 
     /**
-     * Resolve the placeholders and return an Optional String if it was possible to resolve them.
+     * Resolve the placeholders and return a string if it was possible to resolve them.
      *
      * @param str The placeholder to resolve
-     * @return The optional string or {@link Optional#empty()} if resolution was not possible
+     * @return The resolved string
      * @throws ConfigurationException If the placeholders could not be resolved
      */
     default @NonNull String resolveRequiredPlaceholders(String str) throws ConfigurationException {
         return resolvePlaceholders(str).orElseThrow(() -> new ConfigurationException("Unable to resolve placeholders for property: " + str));
+    }
+
+    /**
+     * Resolve the placeholders in the given string. This behaves like
+     * {@link #resolveRequiredPlaceholders(String)}, except that when the whole input is a
+     * placeholder, the value is not converted to String but returned as-is.
+     *
+     * @param str The placeholder to resolve
+     * @return The resolved object or string
+     * @throws ConfigurationException If the placeholders could not be resolved
+     */
+    default @NonNull Object resolveRequiredPlaceholdersObject(String str) throws ConfigurationException {
+        return resolveRequiredPlaceholders(str);
     }
 
     /**

inject/src/test/groovy/io/micronaut/context/env/PropertySourcePropertyResolverSpec.groovy
@@ -155,7 +155,8 @@ class PropertySourcePropertyResolverSpec extends Specification {
         def values = [
                 'foo.bar': '10',
                 'foo.baz': 20,
-                'bar'    : 30
+                'bar'    : 30,
+                'arr'    : new byte[] {1, 2, 3, 0x80},
         ]
         PropertySourcePropertyResolver resolver = new PropertySourcePropertyResolver(
                 PropertySource.of("test", [(property): value] + values)
@@ -188,6 +189,7 @@ class PropertySourcePropertyResolverSpec extends Specification {
         'my.property' | '${FOO_BAR_1}'                                       | 'my.property' | String  | 'foo bar 1'
         'my.property' | 'bolt://${NEO4J_HOST:localhost}:${NEO4J_PORT:32781}' | 'my.property' | String  | 'bolt://localhost:32781'
         'my.property' | '${bar}'                                             | 'my.property' | Integer | 30
+        'my.property' | '${arr}'                                             | 'my.property' | byte[]  | new byte[] {1, 2, 3, 0x80}
     }
 
 

commit 75b767e393c68d661ce95e2331475b6ff66c4e7d

Fix property caching This fixes the build broken by #9310. Basically:
https://github.com/micronaut-projects/micronaut-core/blob/94f458ac04b0180a989d0cf28984d5e3b5df592f/http-client/src/test/groovy/io/micronaut/http/client/filter/StaticClientFilterSpec.groovy#L24-L28

The resolution of micronaut.server.port used to request the value of test-port as String. With this patch, it requests the value as Object instead (`.getValue(Object.class)`). Object is not cachable however, so it gets a new uncached random port, which causes the test failure.

I don't really understand the distinction between cachable and non-cachable types. Making all types cacheable causes other test failures that request types like Map. This change (allowing caching for Object) seems to only affect placeholder resolution, and does not cause additional test failures.

inject/src/main/java/io/micronaut/context/env/PropertySourcePropertyResolver.java
@@ -318,7 +318,7 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
         } else {
             Objects.requireNonNull(conversionContext, "Conversion context should not be null");
             Class<T> requiredType = conversionContext.getArgument().getType();
-            boolean cacheableType = ClassUtils.isJavaLangType(requiredType);
+            boolean cacheableType = ClassUtils.isJavaLangType(requiredType) || requiredType == Object.class;
             Object cached = cacheableType ? resolvedValueCache.get(cacheKey(name, requiredType)) : null;
             if (cached != null) {
                 return cached == NO_VALUE ? Optional.empty() : Optional.of((T) cached);