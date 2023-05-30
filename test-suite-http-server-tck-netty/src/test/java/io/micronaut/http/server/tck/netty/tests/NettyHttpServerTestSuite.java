cb79e287d9e6351be4f3c98a4b666d23f1abd502
Graeme Rocher <graeme.rocher@oracle.com>
2023-05-30 12:35:33 +0200

    rollback test suite change

--- test-suite-http-server-tck-netty/src/test/java/io/micronaut/http/server/tck/netty/tests/NettyHttpServerTestSuite.java
+++ test-suite-http-server-tck-netty/src/test/java/io/micronaut/http/server/tck/netty/tests/NettyHttpServerTestSuite.java
@@ -1,13 +1,11 @@
 package io.micronaut.http.server.tck.netty.tests;
 
-import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
-@IncludeClassNamePatterns("io.micronaut.http.server.tck.tests.filter.RequestFilterTest")
 @SuiteDisplayName("HTTP Server TCK for Netty")
 public class NettyHttpServerTestSuite {
 }
