523fa3440791114c896998170326bb98141ea9f4
yawkat <jonas.konrad@oracle.com>
2023-06-07 11:52:29 +0200

    controller test

--- http-server-netty/src/test/groovy/io/micronaut/http/server/netty/filters/FilterBodySpec.groovy
+++ http-server-netty/src/test/groovy/io/micronaut/http/server/netty/filters/FilterBodySpec.groovy
@@ -7,6 +7,10 @@ import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Consumes
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Produces
 import io.micronaut.http.annotation.RequestFilter
 import io.micronaut.http.annotation.ServerFilter
 import io.micronaut.http.client.HttpClient
@@ -17,9 +21,9 @@ import reactor.core.publisher.Sinks
 import spock.lang.Specification
 
 class FilterBodySpec extends Specification {
-    def "delayed body"() {
+    def "delayed body"(String uri, String response) {
         given:
-        def ctx = ApplicationContext.run(["spec.name":"FilterBodySpec"])
+        def ctx = ApplicationContext.run(["spec.name": "FilterBodySpec"])
         def server = ctx.getBean(EmbeddedServer)
         server.start()
         def client = ctx.createBean(HttpClient, server.URI)
@@ -27,35 +31,55 @@ class FilterBodySpec extends Specification {
 
         when:
         filter.secondBodyPartSink = Sinks.one()
-        def resp = client.toBlocking().exchange(HttpRequest.POST("/foo", Flux.concat(
+        def resp = client.toBlocking().exchange(HttpRequest.POST(uri, Flux.concat(
                 Flux.just("foo"),
                 filter.secondBodyPartSink.asMono()
         )).contentType(MediaType.TEXT_PLAIN_TYPE), String)
         then:
-        resp.body() == "foobar"
+        resp.body() == response
 
         cleanup:
         client.close()
         server.stop()
         ctx.close()
+
+        where:
+        uri                                    | response
+        "/filter-body/delayed-body"            | "filter: foobar"
+        "/filter-body/delayed-body-controller" | "controller: foobar"
     }
 
     @Requires(property = 'spec.name', value = 'FilterBodySpec')
-    @ServerFilter("/foo")
+    @ServerFilter("/filter-body")
     @Singleton
     static class MyFilter {
         Sinks.One<String> secondBodyPartSink
 
-        @RequestFilter
+        @RequestFilter("/**")
         @Order(-1)
         void signal() {
             secondBodyPartSink.emitValue("bar", Sinks.EmitFailureHandler.FAIL_FAST)
         }
 
-        @RequestFilter
+        @RequestFilter("/delayed-body")
         @Order(0)
         HttpResponse<String> await(@Body String body) {
-            return HttpResponse.ok(body)
+            return HttpResponse.ok("filter: " + body)
+        }
+
+        @RequestFilter("/delayed-body-controller")
+        @Order(0)
+        void awaitController(@Body String body) {
+        }
+    }
+
+    @Controller("/filter-body")
+    static class MyController {
+        @Post("/delayed-body-controller")
+        @Produces(MediaType.TEXT_PLAIN)
+        @Consumes(MediaType.TEXT_PLAIN)
+        String controller(@Body String body) {
+            return "controller: " + body
         }
     }
 }
