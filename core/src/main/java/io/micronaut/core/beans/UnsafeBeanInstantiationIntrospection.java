bf5f0bfcb97450e82f539d0600df9284f02dd0f2
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-06-16 12:07:52 -0500

    Introduce `UnsafeBeanInstantiationIntrospection` (#9459)

---
+++ core/src/main/java/io/micronaut/core/beans/UnsafeBeanInstantiationIntrospection.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.core.beans;
+
+import io.micronaut.core.annotation.Experimental;
+import io.micronaut.core.annotation.NonNull;
+
+/**
+ * A variation of {@link BeanIntrospection} that includes {@link #instantiateUnsafe(Object...)} allowing to skip instantiation validation.
+ *
+ * @param <T> The bean type
+ * @author Denis Stepanov
+ * @since 4.0.
+ */
+@Experimental
+public interface UnsafeBeanInstantiationIntrospection<T> extends BeanIntrospection<T>  {
+
+    /**
+     * Instantiates an instance of the bean.
+     *
+     * @param arguments The arguments required to instantiate bean.
+     * @return An instance
+     */
+    @NonNull T instantiateUnsafe(@NonNull Object... arguments) ;
+
+
+}
