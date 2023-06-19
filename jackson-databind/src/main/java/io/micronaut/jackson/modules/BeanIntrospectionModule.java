bab418396af6cd81396ae988e1c167bbdf67d0a0
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-06-19 11:51:28 -0500

    Remove Jackson nullability check

--- jackson-databind/src/main/java/io/micronaut/jackson/modules/BeanIntrospectionModule.java
+++ jackson-databind/src/main/java/io/micronaut/jackson/modules/BeanIntrospectionModule.java
@@ -173,8 +173,7 @@ public class BeanIntrospectionModule extends SimpleModule {
     }
 
     private PropertyMetadata newPropertyMetadata(Argument<?> argument, AnnotationMetadata annotationMetadata) {
-        final Boolean required = argument.isNonNull() ||
-            annotationMetadata.booleanValue(JsonProperty.class, "required").orElse(false);
+        final Boolean required = annotationMetadata.booleanValue(JsonProperty.class, "required").orElse(false);
 
         int index = annotationMetadata.intValue(JsonProperty.class, "index").orElse(-1);
         return PropertyMetadata.construct(
