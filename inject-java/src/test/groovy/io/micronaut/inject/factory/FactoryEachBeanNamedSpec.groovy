e08df6ac4049e5e3eb9ebac98298677547dc228d
Tim Yates <tim.yates@gmail.com>
2023-07-03 10:47:53 +0100

    Possible bug in Factory and EachBean
    More likely to be me making a mistake, but we have the following pattern in the micronaut-security-keys-jwks
    guide, which is failing (as does this) with io.micronaut.context.exceptions.NonUniqueBeanException
    
    Shouldn't this be handled by the EachBean annotation

---
+++ inject-java/src/test/groovy/io/micronaut/inject/factory/FactoryEachBeanNamedSpec.groovy
@@ -0,0 +1,67 @@
+package io.micronaut.inject.factory
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.BeanContext
+import io.micronaut.context.annotation.EachBean
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Requires
+import io.micronaut.runtime.context.scope.Refreshable
+import jakarta.inject.Named
+import spock.lang.Specification
+
+class FactoryEachBeanNamedSpec extends Specification {
+
+    static final SPEC_NAME = "FactoryEachBeanNamedSpec"
+
+    void "check the factory"() {
+        given:
+        BeanContext beanContext = ApplicationContext.run(["spec.name": SPEC_NAME])
+
+        expect:
+        beanContext.getBeansOfType(IntegerReturnerWrapper).size() == 2
+
+        cleanup:
+        beanContext.close()
+    }
+
+    static interface IntegerReturner {
+        int getInteger()
+    }
+
+    @Refreshable
+    @Requires(property = "spec.name", value = SPEC_NAME)
+    static class One implements IntegerReturner {
+        @Override
+        int getInteger() {
+            1
+        }
+    }
+
+    @Refreshable
+    @Named("two")
+    @Requires(property = "spec.name", value = SPEC_NAME)
+    static class Two implements IntegerReturner {
+        @Override
+        int getInteger() {
+            2
+        }
+    }
+
+    static class IntegerReturnerWrapper {
+        final IntegerReturner inty
+
+        IntegerReturnerWrapper(IntegerReturner inty) {
+            this.inty = inty
+        }
+    }
+
+    @Factory
+    @Requires(property = "spec.name", value = SPEC_NAME)
+    static class IntegerReturnerFactory {
+
+        @EachBean(IntegerReturner)
+        IntegerReturnerWrapper buildWithFactory(IntegerReturner inty) {
+            new IntegerReturnerWrapper(inty)
+        }
+    }
+}
