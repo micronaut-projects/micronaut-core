0e1b355289bb9a3e7834972bd7d980c672f0ee6a
Sergio del Amo <sergio.delamo@softamo.com>
2023-07-24 15:29:40 +0200

    @Mapper example

---
+++ test-suite-groovy/src/test/groovy/io/micronaut/docs/ioc/mappers/ContactForm.groovy
@@ -0,0 +1,11 @@
+package io.micronaut.docs.ioc.mappers;
+
+import io.micronaut.core.annotation.Introspected;
+
+//tag::class[]
+@Introspected
+class ContactForm {
+    String firstName
+    String lastName
+}
+//end::class[]
---
+++ test-suite/src/test/java/io/micronaut/docs/ioc/mappers/ContactMappers.java
@@ -0,0 +1,14 @@
+package io.micronaut.docs.ioc.mappers;
+
+//tag::imports[]
+import io.micronaut.context.annotation.Mapper;
+//end::imports[]
+import io.micronaut.context.annotation.Requires;
+
+@Requires(property = "spec.name", value = "SimpleMapperSpec")
+//tag::class[]
+public interface ContactMappers {
+    @Mapper
+    ContactEntity toEntity(ContactForm contactForm);
+}
+//end::class[]
