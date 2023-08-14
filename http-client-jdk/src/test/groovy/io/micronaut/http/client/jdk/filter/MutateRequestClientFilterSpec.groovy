9132cc6c15aa0829c5059866f0f04f5b0fca0b59
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-14 14:10:56 +0200

    Update MutateRequestClientFilterSpec.groovy

--- http-client-jdk/src/test/groovy/io/micronaut/http/client/jdk/filter/MutateRequestClientFilterSpec.groovy
+++ http-client-jdk/src/test/groovy/io/micronaut/http/client/jdk/filter/MutateRequestClientFilterSpec.groovy
@@ -15,52 +15,60 @@
  */
 package io.micronaut.http.client.jdk.filter
 
-import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Property
 import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MutableHttpRequest
+import io.micronaut.http.annotation.ClientFilter
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Filter
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.annotation.RequestFilter
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.filter.ClientFilterChain
 import io.micronaut.http.filter.HttpClientFilter
 import io.micronaut.http.uri.UriBuilder
-import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.annotation.Nullable
+import jakarta.inject.Inject
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
-import spock.lang.AutoCleanup
 import spock.lang.Issue
-import spock.lang.Shared
 import spock.lang.Specification
 
 @Issue("https://github.com/micronaut-projects/micronaut-core/issues/917")
+@Property(name = 'spec.name', value = 'MutateRequestClientFilterSpec')
+@MicronautTest
 class MutateRequestClientFilterSpec extends Specification {
+    @Inject
+    FooClient myClient
 
-    @Shared
-    @AutoCleanup
-    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, ['spec.name': 'MutateRequestClientFilterSpec'])
-
-    @Shared
-    MyClient myClient = embeddedServer.applicationContext.getBean(MyClient)
+    @Inject
+    BarClient barClient
 
     void "test mutate outgoing URI"() {
         expect:
         myClient.simple() == "xxxxxxxxxxx"
         myClient.withQuery("foo") == "fooxxxxxxxxxxx"
+
+        and:
+        barClient.simple() == "xxxxxxxxxxx"
+        barClient.withQuery("foo") == "fooxxxxxxxxxxx"
     }
 
     void "test mutate stream request URI"() {
         expect:
         myClient.stream().collectList().block().join("") == '["xxxxxxxxxxx"]'
+
+        and:
+        barClient.stream().collectList().block().join("") == '["xxxxxxxxxxx"]'
     }
 
     @Requires(property = 'spec.name', value = 'MutateRequestClientFilterSpec')
-    @Client("/filters/uri/test")
-    static interface MyClient {
-        @Get("/")
+    @Client("/foo/filters/uri/test")
+    static interface FooClient {
+        @Get
         String simple()
 
         @Get("/foo{?q}")
@@ -71,16 +79,31 @@ class MutateRequestClientFilterSpec extends Specification {
     }
 
     @Requires(property = 'spec.name', value = 'MutateRequestClientFilterSpec')
-    @Controller('/filters/uri/test')
-    static class UriController {
+    @Client("/bar/filters/uri/test")
+    static interface BarClient {
+        @Get
+        String simple()
+
+        @Get("/foo{?q}")
+        String withQuery(@Nullable String q)
+
+        @Get("/stream")
+        Flux<String> stream()
+    }
+
+    @Requires(property = 'spec.name', value = 'MutateRequestClientFilterSpec')
+    @Controller('/foo/filters/uri/test')
+    static class FooController {
         @Get
         String result(@QueryValue String signature) {
             signature
         }
+
         @Get('/foo')
         String query(@QueryValue String signature, @QueryValue String q) {
             q + signature
         }
+
         @Get('/stream')
         Flux<String> stream(@QueryValue String signature) {
             Flux.fromArray('"' + signature + '"')
@@ -88,14 +111,43 @@ class MutateRequestClientFilterSpec extends Specification {
     }
 
     @Requires(property = 'spec.name', value = 'MutateRequestClientFilterSpec')
-    @Filter('/filters/uri/**')
-    static class MyFilter implements HttpClientFilter {
+    @Controller('/bar/filters/uri/test')
+    static class BarController {
+        @Get
+        String result(@QueryValue String signature) {
+            signature
+        }
 
+        @Get('/foo')
+        String query(@QueryValue String signature, @QueryValue String q) {
+            q + signature
+        }
+
+        @Get('/stream')
+        Flux<String> stream(@QueryValue String signature) {
+            Flux.fromArray('"' + signature + '"')
+        }
+    }
+
+    @Requires(property = 'spec.name', value = 'MutateRequestClientFilterSpec')
+    @Filter('/foo/filters/uri/**')
+    static class MyHttpClientFilter implements HttpClientFilter {
         @Override
         Publisher<? extends HttpResponse<?>> doFilter(MutableHttpRequest<?> request, ClientFilterChain chain) {
-            UriBuilder builder = UriBuilder.of(request.getUri())
-            builder.queryParam("signature", "xxxxxxxxxxx")
-            return chain.proceed(request.uri(builder.build()))
+            chain.proceed(request.uri(UriBuilder.of(request.getUri())
+                    .queryParam("signature", "xxxxxxxxxxx")
+                    .build()))
+        }
+    }
+
+    @Requires(property = 'spec.name', value = 'MutateRequestClientFilterSpec')
+    @ClientFilter('/bar/filters/uri/**')
+    static class MyFilter {
+        @RequestFilter
+        void filter(MutableHttpRequest<?> request) {
+            request.uri(UriBuilder.of(request.getUri())
+                    .queryParam("signature", "xxxxxxxxxxx")
+                    .build())
         }
     }
 }
