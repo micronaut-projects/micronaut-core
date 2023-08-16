2661523c7292b334f34a76e8bcc4006934657bbe
Tim Yates <tim.yates@gmail.com>
2023-08-16 12:58:13 +0100

    License and exclude for JDK client

--- test-suite-http-server-tck-jdk/src/test/java/io/micronaut/http/server/tck/netty/tests/JdkHttpServerTestSuite.java
+++ test-suite-http-server-tck-jdk/src/test/java/io/micronaut/http/server/tck/netty/tests/JdkHttpServerTestSuite.java
@@ -1,5 +1,6 @@
 package io.micronaut.http.server.tck.netty.tests;
 
+import org.junit.platform.suite.api.ExcludeClassNamePatterns;
 import org.junit.platform.suite.api.ExcludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
@@ -8,6 +9,9 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Javanet client")
+@ExcludeClassNamePatterns({
+    "io.micronaut.http.server.tck.tests.FilterProxyTest", // There's no proxy client for the JDK client
+})
 @ExcludeTags("multipart") // Multipart not supported by HttpClient
 public class JdkHttpServerTestSuite {
 }
