e29a24e1e75a2ff42df77cb105a8596d3c8f331b
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-29 11:34:33 +0200

    test: tck test for request.getUri() contains query values (#9791)
    * test: tck test for request.getUri() contains query values
    
    * fix checkstyle

---
+++ http-server-tck/src/main/java/io/micronaut/http/server/tck/tests/RequestUriContainsQueryValueTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.server.tck.tests;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.tck.AssertionUtils;
+import io.micronaut.http.tck.HttpResponseAssertion;
+import io.micronaut.http.uri.UriBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static io.micronaut.http.tck.TestScenario.asserts;
+
+@SuppressWarnings({
+    "java:S5960", // We're allowed assertions, as these are used in tests only
+    "checkstyle:MissingJavadocType",
+    "checkstyle:DesignForExtension"
+})
+public class RequestUriContainsQueryValueTest {
+
+    public static final String SPEC_NAME = "RequestUriContainsQueryValueTest";
+
+    @Test
+    void testRequestUriContainsQueryValue() throws IOException {
+        asserts(SPEC_NAME,
+            HttpRequest.GET(UriBuilder.of("/requesturi").queryParam("foo", "bar").build()),
+            (server, request) -> AssertionUtils.assertDoesNotThrow(server, request,
+                HttpResponseAssertion.builder()
+                    .status(HttpStatus.OK)
+                    .body("/requesturi?foo=bar")
+                    .build()));
+    }
+
+    @Requires(property = "spec.name", value = SPEC_NAME)
+    @Controller("/requesturi")
+    static class TestController {
+        @Get
+        String index(HttpRequest<?> request) {
+            return request.getUri().toASCIIString();
+        }
+    }
+}
