1ddbbb3d8a021d48068c241d856f1e75139c7d37
Tim Yates <tim.yates@gmail.com>
2023-11-02 14:52:49 +0000

    Remove useIR as it's just in test

--- inject-kotlin-test/src/main/kotlin/io/micronaut/annotation/processing/test/support/KotlinCompilation.kt
+++ inject-kotlin-test/src/main/kotlin/io/micronaut/annotation/processing/test/support/KotlinCompilation.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 original authors
+ * Copyright 2017-2023 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,13 +56,6 @@ class KotlinCompilation : AbstractKotlinCompilation<K2JVMCompilerArguments>() {
 
 	/** Generate metadata for Java 1.8 reflection on method parameters */
 	var javaParameters: Boolean = false
-
-    /**
-     * Use the IR backend
-     *
-     * @deprecated Removed in 1.9.20
-     */
-	var useIR: Boolean = false
 
 	/** Use the old JVM backend */
 	var useOldBackend: Boolean = false
