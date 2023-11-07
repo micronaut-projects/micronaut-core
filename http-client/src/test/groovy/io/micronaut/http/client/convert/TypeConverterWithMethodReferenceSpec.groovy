ef85714bdb051d43a03e665467a36c6bfd3ab256
Tim Yates <tim.yates@gmail.com>
2023-11-07 16:55:36 +0000

    Move FooBarController into the test

--- http-client/src/test/groovy/io/micronaut/http/client/convert/TypeConverterWithMethodReferenceSpec.groovy
+++ http-client/src/test/groovy/io/micronaut/http/client/convert/TypeConverterWithMethodReferenceSpec.groovy
@@ -1,21 +1,10 @@
-/*
- * Copyright 2017-2019 original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.micronaut.http.client.convert
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Requires
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.client.HttpClient
 import io.micronaut.runtime.server.EmbeddedServer
 import spock.lang.AutoCleanup
@@ -24,8 +13,14 @@ import spock.lang.Specification
 
 class TypeConverterWithMethodReferenceSpec extends Specification {
 
-    @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
-    @Shared HttpClient client = embeddedServer.applicationContext.createBean(HttpClient, embeddedServer.getURL())
+    @Shared
+    @AutoCleanup
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, [
+            'spec.name': 'TypeConverterWithMethodReferenceSpec',
+    ])
+
+    @Shared
+    HttpClient client = embeddedServer.applicationContext.createBean(HttpClient, embeddedServer.getURL())
 
     void "test type converters can be used with method references"() {
         when:
@@ -35,5 +30,20 @@ class TypeConverterWithMethodReferenceSpec extends Specification {
         then:
         foo.value == 'val'
         bar.value == 'val'
+    }
+
+    @Controller("/convert")
+    @Requires(property = "spec.name", value = 'TypeConverterWithMethodReferenceSpec')
+    static class FooBarController {
+
+        @Get("/foo/{foo}")
+        Foo foo(@PathVariable(name = "foo") Foo foo) {
+            return foo;
+        }
+
+        @Get("/bar/{bar}")
+        Bar bar(@PathVariable(name = "bar") Bar bar) {
+            return bar;
+        }
     }
 }
