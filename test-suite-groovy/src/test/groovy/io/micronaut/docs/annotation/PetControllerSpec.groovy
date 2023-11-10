903f2c3f6a32a7613c9d347c2263e0663a995204
Tim Yates <tim.yates@gmail.com>
2023-11-10 11:49:59 +0000

    PetControllerSpec is a Spock spec, so make it Spock

--- test-suite-groovy/src/test/groovy/io/micronaut/docs/annotation/PetControllerSpec.groovy
+++ test-suite-groovy/src/test/groovy/io/micronaut/docs/annotation/PetControllerSpec.groovy
@@ -6,34 +6,37 @@ import jakarta.validation.ConstraintViolationException
 import reactor.core.publisher.Mono
 import spock.lang.Specification
 
-import static org.junit.jupiter.api.Assertions.assertEquals
-import static org.junit.jupiter.api.Assertions.assertThrows
-
 class PetControllerSpec extends Specification {
 
     void "test post pet"() {
-        when:
-        // tag::post[]
+        given:
         EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+
+        when:
         PetClient client = embeddedServer.getApplicationContext().getBean(PetClient)
         Pet pet = Mono.from(client.save("Dino", 10)).block()
 
         then:
         "Dino" == pet.getName()
         10 == pet.getAge()
-        // end::post[]
 
+        cleanup:
         embeddedServer.stop()
     }
 
     void "test post pet validation"() {
+        given:
         EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
-        def ex = assertThrows(ConstraintViolationException.class, () -> {
-            PetClient client = embeddedServer.getApplicationContext().getBean(PetClient)
-            Mono.from(client.save("Fred", -1)).block()
-        })
 
-        assertEquals(ex.getMessage(), "save.age: must be greater than or equal to 1")
+        when:
+        PetClient client = embeddedServer.getApplicationContext().getBean(PetClient)
+        Mono.from(client.save("Fred", -1)).block()
+
+        then:
+        ConstraintViolationException ex = thrown()
+        ex.message == "save.age: must be greater than or equal to 1"
+
+        cleanup:
         embeddedServer.stop()
     }
 }
