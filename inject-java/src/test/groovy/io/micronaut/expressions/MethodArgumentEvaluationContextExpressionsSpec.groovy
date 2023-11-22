83b3cbdc1aedb74b235776b34179a308e932cfb1
Tim Yates <tim.yates@gmail.com>
2023-11-22 10:52:11 +0000

    Reproducer

--- inject-java/src/test/groovy/io/micronaut/expressions/MethodArgumentEvaluationContextExpressionsSpec.groovy
+++ inject-java/src/test/groovy/io/micronaut/expressions/MethodArgumentEvaluationContextExpressionsSpec.groovy
@@ -29,4 +29,51 @@ class MethodArgumentEvaluationContextExpressionsSpec extends AbstractEvaluatedEx
         result instanceof String && result == 'arg1abc'
     }
 
+    void "test wrapped value method argument access"() {
+        given:
+        Object result = evaluateSingle("test.Expr", """
+            package test;
+            import io.micronaut.context.annotation.Executable;
+            import io.micronaut.context.annotation.Requires;
+            import jakarta.inject.Singleton;
+
+            @Singleton
+            class Expr {
+
+                @Executable
+                @Requires(value = "#{ #second + 'abc' }")
+                void test(String first, Integer second) {
+                }
+            }
+
+
+        """, ["arg0", 100] as Object[])
+
+        expect:
+        result instanceof String && result == '100abc'
+    }
+
+    void "test primitive value method argument access"() {
+        given:
+        Object result = evaluateSingle("test.Expr", """
+            package test;
+            import io.micronaut.context.annotation.Executable;
+            import io.micronaut.context.annotation.Requires;
+            import jakarta.inject.Singleton;
+
+            @Singleton
+            class Expr {
+
+                @Executable
+                @Requires(value = "#{ #second + 'abc' }")
+                void test(String first, int second) {
+                }
+            }
+
+
+        """, ["arg0", 100] as Object[])
+
+        expect:
+        result instanceof String && result == '100abc'
+    }
 }
