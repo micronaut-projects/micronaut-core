85e62c07e0ce06bcd7885b25e512df81ab2c4c08
Dean Wette <wetted@unityfoundation.io>
2023-12-05 15:45:31 -0600

    change example to use custom context element

--- test-suite/src/test/java/io/micronaut/docs/propagation/reactor/HelloController.java
+++ test-suite/src/test/java/io/micronaut/docs/propagation/reactor/HelloController.java
@@ -3,10 +3,9 @@ package io.micronaut.docs.propagation.reactor;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.async.propagation.ReactorPropagation;
 import io.micronaut.core.propagation.PropagatedContext;
-import io.micronaut.http.HttpRequest;
+import io.micronaut.core.propagation.PropagatedContextElement;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.context.ServerHttpRequestContext;
 import reactor.core.publisher.Mono;
 
 @Requires(property = "spec.name", value = "PropagatedContextSpec")
@@ -15,10 +14,12 @@ import reactor.core.publisher.Mono;
 class HelloController {
 
     @Get("/hello")
-    Mono<String> hello(HttpRequest<?> httpRequest) {
-        PropagatedContext propagatedContext = PropagatedContext.get().plus(new ServerHttpRequestContext(httpRequest)); // <1>
+    Mono<String> hello() {
+        PropagatedContext propagatedContext = PropagatedContext.get().plus(new MyContextElement()); // <1>
         return Mono.just("Hello, World")
             .contextWrite(ctx -> ReactorPropagation.addPropagatedContext(ctx, propagatedContext)); // <2>
     }
 }
+
+class MyContextElement implements PropagatedContextElement { /*...*/ }
 // end::example[]
