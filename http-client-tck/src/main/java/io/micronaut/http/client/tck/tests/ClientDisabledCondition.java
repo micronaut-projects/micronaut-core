46d76dd3f4a02fa8eff1e06ae35c1bd716934de7
Tim Yates <tim.yates@gmail.com>
2023-12-06 17:29:00 +0000

    Checkstyle 😡

--- http-client-tck/src/main/java/io/micronaut/http/client/tck/tests/ClientDisabledCondition.java
+++ http-client-tck/src/main/java/io/micronaut/http/client/tck/tests/ClientDisabledCondition.java
@@ -39,16 +39,14 @@ import static org.junit.platform.commons.util.AnnotationUtils.findAnnotation;
  */
 public class ClientDisabledCondition implements ExecutionCondition {
 
-    private static final ConditionEvaluationResult ENABLED_BY_DEFAULT = enabled("@ClientDisabled is not present");
-
-    private static final ConditionEvaluationResult ENABLED = enabled("Enabled");
-
-    private static final ConditionEvaluationResult DISABLED = disabled("Disabled");
-
     public static final String JDK = "jdk";
     public static final String NETTY = "netty";
     public static final String HTTP_CLIENT_CONFIGURATION = "httpClient";
 
+    private static final ConditionEvaluationResult ENABLED_BY_DEFAULT = enabled("@ClientDisabled is not present");
+    private static final ConditionEvaluationResult ENABLED = enabled("Enabled");
+    private static final ConditionEvaluationResult DISABLED = disabled("Disabled");
+    
     private static boolean jdkMajorVersionMatches(ClientDisabled d) {
         return Integer.toString(Runtime.version().feature()).equals(d.jdk());
     }
