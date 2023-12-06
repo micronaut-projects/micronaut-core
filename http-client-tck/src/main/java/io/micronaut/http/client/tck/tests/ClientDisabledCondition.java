946601abdfe50e9dd9599f128b03233d4c727692
Tim Yates <tim.yates@gmail.com>
2023-12-06 17:10:46 +0000

    Javadoc and license

--- http-client-tck/src/main/java/io/micronaut/http/client/tck/tests/ClientDisabledCondition.java
+++ http-client-tck/src/main/java/io/micronaut/http/client/tck/tests/ClientDisabledCondition.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.http.client.tck.tests;
 
 import org.junit.jupiter.api.extension.ConditionEvaluationResult;
@@ -15,6 +30,13 @@ import static org.junit.jupiter.api.extension.ConditionEvaluationResult.disabled
 import static org.junit.jupiter.api.extension.ConditionEvaluationResult.enabled;
 import static org.junit.platform.commons.util.AnnotationUtils.findAnnotation;
 
+/**
+ * A condition that enables or disables a test based on the presence of the {@link ClientDisabled} annotation.
+ * <p>
+ * The condition is enabled by default, unless the annotation is present and the {@link ClientDisabled#jdk()} or
+ * {@link ClientDisabled#httpClient()} parameters match the current JDK version and the HTTP client configuration
+ * parameter.
+ */
 public class ClientDisabledCondition implements ExecutionCondition {
 
     private static final ConditionEvaluationResult ENABLED_BY_DEFAULT = enabled("@ClientDisabled is not present");
@@ -44,6 +66,9 @@ public class ClientDisabledCondition implements ExecutionCondition {
             .orElse(ENABLED_BY_DEFAULT);
     }
 
+    /**
+     * Annotation that can be used to disable a test based on the JDK version and the HTTP client configuration.
+     */
     @Documented
     @Target({ElementType.TYPE, ElementType.METHOD})
     @Retention(RetentionPolicy.RUNTIME)
