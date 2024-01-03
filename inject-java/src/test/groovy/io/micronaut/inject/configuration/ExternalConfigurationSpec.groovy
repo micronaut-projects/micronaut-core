8dd158b19bdd02ead5dd3f783b4ab18e4b5caa70
Denis Stepanov <denis.s.stepanov@oracle.com>
2024-01-03 12:02:45 +0100

    Remove pending

--- inject-java/src/test/groovy/io/micronaut/inject/configuration/ExternalConfigurationSpec.groovy
+++ inject-java/src/test/groovy/io/micronaut/inject/configuration/ExternalConfigurationSpec.groovy
@@ -3,11 +3,9 @@ package io.micronaut.inject.configuration
 import io.micronaut.annotation.processing.test.AbstractTypeElementSpec
 import io.micronaut.context.ApplicationContext
 import io.micronaut.inject.test.external.ExternalConfiguration
-import spock.lang.PendingFeature
 
 class ExternalConfigurationSpec extends AbstractTypeElementSpec {
 
-    @PendingFeature(reason = "Fixed in 4.3")
     void "test import a bean with primitives"() {
         given:
         ApplicationContext ctx = ApplicationContext.builder().start()
