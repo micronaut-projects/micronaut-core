65fce8553ecf8dacac9c6abdd3eb01f2a61dfb8b
yawkat <jonas.konrad@oracle.com>
2024-03-11 11:56:53 +0100

    randomize tck port

--- http-tck/src/main/java/io/micronaut/http/tck/EmbeddedServerUnderTest.java
+++ http-tck/src/main/java/io/micronaut/http/tck/EmbeddedServerUnderTest.java
@@ -45,6 +45,8 @@ public class EmbeddedServerUnderTest implements ServerUnderTest {
     private BlockingHttpClient client;
 
     public EmbeddedServerUnderTest(@NonNull Map<String, Object> properties) {
+        properties.putIfAbsent("micronaut.server.port", -1);
+        properties.putIfAbsent("micronaut.ssl.port", -1);
         this.embeddedServer = ApplicationContext.run(EmbeddedServer.class, properties);
         this.isBlockingClient = (boolean) properties.getOrDefault(BLOCKING_CLIENT_PROPERTY, true);
     }
