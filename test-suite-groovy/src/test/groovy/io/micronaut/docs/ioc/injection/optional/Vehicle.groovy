a649645f4ae904dad0b454b1308b9d4311270b0d
Graeme Rocher <graeme.rocher@oracle.com>
2024-05-16 15:09:24 +0200

    document injection types and optional injection

---
+++ test-suite-groovy/src/test/groovy/io/micronaut/docs/ioc/injection/optional/Vehicle.groovy
@@ -0,0 +1,21 @@
+package io.micronaut.docs.ioc.injection.optional
+
+import io.micronaut.context.annotation.Autowired
+import jakarta.inject.Singleton
+
+@Singleton
+class Vehicle {
+    @Autowired(required = false) // <1>
+    Engine engine = new Engine()
+
+    void start() {
+        engine.start()
+    }
+}
+
+@Singleton
+class Engine {
+    void start() {
+        println("Vrooom!")
+    }
+}
