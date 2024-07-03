6f27055747d6161ec526f33d785f148fa20ab1a5
Denis Stepanov <denis.s.stepanov@oracle.com>
2024-07-03 12:15:28 +0200

    Properly handle missing classes in metadata + precalculated requirements (#10936)

---
+++ inject/src/main/java/io/micronaut/context/conditions/MatchesAbsenceOfBeansCondition.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.context.conditions;
+
+import io.micronaut.context.DefaultBeanContext;
+import io.micronaut.context.condition.Condition;
+import io.micronaut.context.condition.ConditionContext;
+import io.micronaut.core.annotation.AnnotationClassValue;
+import io.micronaut.core.annotation.AnnotationMetadataProvider;
+import io.micronaut.core.annotation.Internal;
+import io.micronaut.core.annotation.UsedByGeneratedCode;
+import io.micronaut.core.type.Argument;
+import io.micronaut.inject.BeanDefinition;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+
+/**
+ * Missing beans condition.
+ *
+ * @param missingBeans The missing beans
+ * @author Denis Stepanov
+ * @since 4.6
+ */
+@UsedByGeneratedCode
+@Internal
+public record MatchesAbsenceOfBeansCondition(AnnotationClassValue<?>[] missingBeans) implements Condition {
+    @Override
+    public boolean matches(ConditionContext context) {
+        AnnotationMetadataProvider component = context.getComponent();
+        if (component instanceof BeanDefinition<?> bd) {
+            DefaultBeanContext beanContext = (DefaultBeanContext) context.getBeanContext();
+
+            for (AnnotationClassValue<?> bean : missingBeans) {
+                Class<?> type = bean.getType().orElse(null);
+                if (type == null) {
+                    continue;
+                }
+                // remove self by passing definition as filter
+                final Collection<? extends BeanDefinition<?>> beanDefinitions = beanContext.findBeanCandidates(
+                    context.getBeanResolutionContext(),
+                    Argument.of(type),
+                    bd
+                );
+                for (BeanDefinition<?> beanDefinition : beanDefinitions) {
+                    if (!beanDefinition.isAbstract()) {
+                        context.fail("Existing bean [" + beanDefinition.getName() + "] of type [" + type.getName() + "] registered in context");
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MatchesAbsenceOfBeansCondition that = (MatchesAbsenceOfBeansCondition) o;
+        return Objects.deepEquals(missingBeans, that.missingBeans);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(missingBeans);
+    }
+
+    @Override
+    public String toString() {
+        return "MatchesAbsenceOfBeansCondition{" +
+            "missingBeans=" + Arrays.toString(missingBeans) +
+            '}';
+    }
+}
---
+++ inject/src/main/java/io/micronaut/context/conditions/MatchesPresenceOfResourcesCondition.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.context.conditions;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.BeanContext;
+import io.micronaut.context.condition.Condition;
+import io.micronaut.context.condition.ConditionContext;
+import io.micronaut.core.annotation.Internal;
+import io.micronaut.core.annotation.UsedByGeneratedCode;
+import io.micronaut.core.io.ResourceLoader;
+import io.micronaut.core.io.ResourceResolver;
+import io.micronaut.core.io.file.FileSystemResourceLoader;
+import io.micronaut.core.io.scan.ClassPathResourceLoader;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Matches resources of classes condition.
+ *
+ * @param resourcePaths The resources
+ * @author Denis Stepanov
+ * @since 4.6
+ */
+@UsedByGeneratedCode
+@Internal
+public record MatchesPresenceOfResourcesCondition(String[] resourcePaths) implements Condition {
+    @Override
+    public boolean matches(ConditionContext context) {
+        final BeanContext beanContext = context.getBeanContext();
+        final List<ResourceLoader> resourceLoaders;
+        if (beanContext instanceof ApplicationContext applicationContext) {
+            ResourceLoader resourceLoader = applicationContext.getEnvironment();
+            resourceLoaders = Arrays.asList(resourceLoader, FileSystemResourceLoader.defaultLoader());
+        } else {
+            resourceLoaders = Arrays.asList(
+                ClassPathResourceLoader.defaultLoader(beanContext.getClassLoader()),
+                FileSystemResourceLoader.defaultLoader()
+            );
+        }
+        ResourceResolver resolver = new ResourceResolver(resourceLoaders);
+        for (String resourcePath : resourcePaths) {
+            if (resolver.getResource(resourcePath).isEmpty()) {
+                context.fail("Resource [" + resourcePath + "] does not exist");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MatchesPresenceOfResourcesCondition that = (MatchesPresenceOfResourcesCondition) o;
+        return Objects.deepEquals(resourcePaths, that.resourcePaths);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(resourcePaths);
+    }
+
+    @Override
+    public String toString() {
+        return "MatchesPresenceOfResourcesCondition{" +
+            "resourcePaths=" + Arrays.toString(resourcePaths) +
+            '}';
+    }
+}
