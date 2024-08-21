197c08b71b2871f2f5ae0d477491686a9276b086
Denis Stepanov <denis.s.stepanov@oracle.com>
2024-08-21 17:58:59 +0200

    Correct

--- test-suite-http-server-tck-netty/src/test/java/io/micronaut/http/server/tck/netty/tests/NettyHttpServerTestSuite.java
+++ test-suite-http-server-tck-netty/src/test/java/io/micronaut/http/server/tck/netty/tests/NettyHttpServerTestSuite.java
@@ -1,13 +1,11 @@
 package io.micronaut.http.server.tck.netty.tests;
 
-import io.micronaut.http.server.tck.tests.codec.JsonCodecAdditionalType2Test;
-import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
-//@SelectPackages("io.micronaut.http.server.tck.tests")
-@SelectClasses(JsonCodecAdditionalType2Test.class)
+@SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Netty")
 public class NettyHttpServerTestSuite {
 }
