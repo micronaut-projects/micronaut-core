895c005de6d9cb3ba95f0be9bb02338b107dc36b
yawkat <jonas.konrad@oracle.com>
2024-10-09 11:03:34 +0200

    redirect and filter tests

--- http-client-tck/src/main/java/io/micronaut/http/client/tck/tests/RawTest.java
+++ http-client-tck/src/main/java/io/micronaut/http/client/tck/tests/RawTest.java
@@ -19,14 +19,20 @@ import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.io.buffer.ByteArrayBufferFactory;
 import io.micronaut.http.ByteBodyHttpResponse;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.ClientFilter;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.RequestFilter;
+import io.micronaut.http.annotation.ResponseFilter;
 import io.micronaut.http.body.stream.AvailableByteArrayBody;
 import io.micronaut.http.client.RawHttpClient;
 import io.micronaut.http.tck.ServerUnderTest;
 import io.micronaut.http.tck.ServerUnderTestProviderUtils;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
@@ -34,6 +40,8 @@ import reactor.core.publisher.Mono;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ThreadLocalRandom;
 
 @SuppressWarnings({
@@ -71,7 +79,7 @@ class RawTest {
         try (ServerUnderTest server = ServerUnderTestProviderUtils.getServerUnderTestProvider().getServer(SPEC_NAME);
              RawHttpClient client = server.getApplicationContext().createBean(RawHttpClient.class);
              ByteBodyHttpResponse<?> response = Mono.from(client.exchange(
-                     HttpRequest.POST(server.getURL().get() + "/raw/echo", LONG_PAYLOAD),
+                     HttpRequest.POST(server.getURL().get() + "/raw/echo", null),
                      AvailableByteArrayBody.create(ByteArrayBufferFactory.INSTANCE, LONG_PAYLOAD),
                      null
                  ))
@@ -100,6 +108,53 @@ class RawTest {
         }
     }
 
+    @Test
+    public void filterRequestReplaceResponse() throws Exception {
+        try (ServerUnderTest server = ServerUnderTestProviderUtils.getServerUnderTestProvider().getServer(SPEC_NAME);
+             RawHttpClient client = server.getApplicationContext().createBean(RawHttpClient.class)) {
+            HttpResponse<?> response = Mono.from(client.exchange(
+                    HttpRequest.POST(server.getURL().get() + "/raw/filter-request-replace-response", null),
+                    AvailableByteArrayBody.create(ByteArrayBufferFactory.INSTANCE, "foo".getBytes(StandardCharsets.UTF_8)),
+                    null
+                ))
+                .block();
+
+            Assertions.assertFalse(response instanceof ByteBodyHttpResponse<?>);
+            Assertions.assertEquals("Replaced response. Request body: foo", response.getBody(String.class).get());
+        }
+    }
+
+    @Test
+    public void filterReplaceResponse() throws Exception {
+        try (ServerUnderTest server = ServerUnderTestProviderUtils.getServerUnderTestProvider().getServer(SPEC_NAME);
+             RawHttpClient client = server.getApplicationContext().createBean(RawHttpClient.class)) {
+            HttpResponse<?> response = Mono.from(client.exchange(
+                    HttpRequest.GET(server.getURL().get() + "/raw/filter-replace-response"),
+                    null,
+                    null
+                ))
+                .block();
+
+            Assertions.assertFalse(response instanceof ByteBodyHttpResponse<?>);
+            Assertions.assertEquals("Replaced response. Response body: bar", response.getBody(String.class).get());
+        }
+    }
+
+    @Test
+    public void redirect() throws Exception {
+        try (ServerUnderTest server = ServerUnderTestProviderUtils.getServerUnderTestProvider().getServer(SPEC_NAME);
+             RawHttpClient client = server.getApplicationContext().createBean(RawHttpClient.class);
+             ByteBodyHttpResponse<?> response = Mono.from(client.exchange(HttpRequest.GET(server.getURL().get() + "/raw/redirect-from"), null, null))
+                 .cast(ByteBodyHttpResponse.class)
+                 .block()) {
+
+            Assertions.assertEquals(
+                "redirect successful",
+                response.byteBody().buffer().get().toString(StandardCharsets.UTF_8)
+            );
+        }
+    }
+
     @Controller("/raw")
     @Requires(property = "spec.name", value = SPEC_NAME)
     static class RawController {
@@ -111,6 +166,39 @@ class RawTest {
         @Post("/echo")
         public InputStream echo(@Body InputStream body) throws IOException {
             return body;
+        }
+
+        @Get("/filter-replace-response")
+        public String filterReplaceResponse() {
+            return "bar";
+        }
+
+        @Get("/redirect-from")
+        public HttpResponse<?> redirectFrom() {
+            return HttpResponse.redirect(URI.create("/raw/redirect-to"));
+        }
+
+        @Get("/redirect-to")
+        public String redirectTo() {
+            return "redirect successful";
+        }
+    }
+
+    @ClientFilter("/raw")
+    @Requires(property = "spec.name", value = SPEC_NAME)
+    static class RawFilter {
+        @RequestFilter("/filter-request-replace-response")
+        public HttpResponse<?> filterRequestReplaceResponse(HttpRequest<?> request, @Body String body) throws Exception {
+            // @Body happens to work, but this should very much be considered experimental (and will only work for the raw client)
+            return HttpResponse.ok("Replaced response. Request body: " + body);
+        }
+
+        @ResponseFilter("/filter-replace-response")
+        @ExecuteOn(TaskExecutors.BLOCKING)
+        public HttpResponse<?> filterReplaceResponse(HttpResponse<?> response) throws Exception {
+            try (ByteBodyHttpResponse<?> r = (ByteBodyHttpResponse<?>) response) {
+                return HttpResponse.ok("Replaced response. Response body: " + r.byteBody().buffer().get().toString(StandardCharsets.UTF_8));
+            }
         }
     }
 }
