fe23af213a2b926965d3771aa4c4933909eb7d3e
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-23 12:33:00 +0200

    add license

--- http-server/src/main/java/io/micronaut/http/server/util/NettyJsonFilterBodyParser.java
+++ http-server/src/main/java/io/micronaut/http/server/util/NettyJsonFilterBodyParser.java
@@ -1,19 +1,29 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.http.server.util;
 
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.core.async.annotation.SingleResult;
 import io.micronaut.core.async.publisher.Publishers;
-import io.micronaut.core.reflect.GenericTypeUtils;
-import io.micronaut.core.util.ArrayUtils;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.http.MediaType;
 import io.micronaut.http.ServerHttpRequest;
 import io.micronaut.http.body.ByteBody;
 import io.micronaut.http.body.CloseableByteBody;
 import io.micronaut.http.filter.bodyparser.JsonFilterBodyParser;
 import io.micronaut.json.JsonMapper;
-import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
@@ -21,7 +31,6 @@ import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
-import java.util.Objects;
 
 @Singleton
 @Requires(bean = JsonMapper.class)
