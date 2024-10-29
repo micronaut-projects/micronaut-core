d70ee12c4cf9ff20a5c11b4bd258b47b3ea7db3a
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-29 09:34:38 +0100

    added CompletionStage as a possible return type

---
+++ http-server-tck/src/main/java/io/micronaut/http/server/tck/tests/filter/RequestFilterCompletionStageFutureProceedTest.java
@@ -0,0 +1,77 @@
+package io.micronaut.http.server.tck.tests.filter;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.*;
+import io.micronaut.http.tck.AssertionUtils;
+import io.micronaut.http.tck.HttpResponseAssertion;
+import io.micronaut.http.tck.TestScenario;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+@SuppressWarnings({
+        "java:S5960", // We're allowed assertions, as these are used in tests only
+        "checkstyle:MissingJavadocType",
+        "checkstyle:DesignForExtension"
+})
+public class RequestFilterCompletionStageFutureProceedTest {
+    public static final String SPEC_NAME = "RequestFilterCompletionStageFutureProceedTest";
+
+    @Test
+    public void requestFilterProceedWithCompletableFuture() throws IOException {
+        TestScenario.builder()
+                .specName(SPEC_NAME)
+                .request(HttpRequest.GET("/foobar").header("X-FOOBAR", "123"))
+                .assertion((server, request) -> AssertionUtils.assertDoesNotThrow(server, request, HttpResponseAssertion.builder()
+                        .status(HttpStatus.ACCEPTED)
+                        .build()))
+                .run();
+
+        TestScenario.builder()
+                .specName(SPEC_NAME)
+                .request(HttpRequest.GET("/foobar"))
+                .assertion((server, request) -> AssertionUtils.assertThrows(server, request, HttpResponseAssertion.builder()
+                        .status(HttpStatus.UNAUTHORIZED)
+                        .build()))
+                .run();
+    }
+
+    /*
+    //tag::clazz[]
+    @ServerFilter(ServerFilter.MATCH_ALL_PATTERN)
+    class FooBarFilter {
+    //end::clazz[]
+    */
+    @Requires(property = "spec.name", value = SPEC_NAME)
+    @ServerFilter(ServerFilter.MATCH_ALL_PATTERN)
+    static class FooBarFilter {
+    //tag::methods[]
+        @RequestFilter
+        CompletionStage<@Nullable HttpResponse<?>> filter(@NonNull HttpRequest<?> request) {
+            if (request.getHeaders().contains("X-FOOBAR")) {
+                // proceed
+                return CompletableFuture.completedFuture(null);
+            } else {
+                return CompletableFuture.completedFuture(HttpResponse.unauthorized());
+            }
+        }
+    }
+    //end::methods[]
+
+    @Requires(property = "spec.name", value = SPEC_NAME)
+    @Controller("/foobar")
+    static class FooBarController {
+        @Get
+        @Status(HttpStatus.ACCEPTED)
+        void index() {
+            // no-op
+        }
+    }
+}
