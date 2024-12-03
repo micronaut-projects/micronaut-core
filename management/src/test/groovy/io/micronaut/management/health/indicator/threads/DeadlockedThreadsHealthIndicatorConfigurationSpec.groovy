9118dde6660a682ceb84a90198edc774c34f1119
Sergio del Amo <sergio.delamo@softamo.com>
2024-12-03 12:17:33 +0100

    extract configuration

--- management/src/test/groovy/io/micronaut/management/health/indicator/threads/DeadlockedThreadsHealthIndicatorConfigurationSpec.groovy
+++ management/src/test/groovy/io/micronaut/management/health/indicator/threads/DeadlockedThreadsHealthIndicatorConfigurationSpec.groovy
@@ -1,6 +1,7 @@
 package io.micronaut.management.health.indicator.threads
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.core.util.StringUtils
 import io.micronaut.management.health.indicator.discovery.DiscoveryClientHealthIndicator
 import io.micronaut.management.health.indicator.discovery.DiscoveryClientHealthIndicatorConfiguration
 import spock.lang.Specification
@@ -9,7 +10,8 @@ class DeadlockedThreadsHealthIndicatorConfigurationSpec extends Specification {
 
     void "bean of type DeadlockedThreadsHealthIndicator does not exist if you set endpoints.health.deadlocked-threads.enabled=false"() {
         given:
-        ApplicationContext applicationContext = ApplicationContext.run(['endpoints.health.deadlocked-threads.enabled': 'false'])
+        Map<String, Object> conf = ['endpoints.health.deadlocked-threads.enabled': StringUtils.FALSE]
+        ApplicationContext applicationContext = ApplicationContext.run(conf)
 
         expect:
         !applicationContext.containsBean(DeadlockedThreadsHealthIndicator)
