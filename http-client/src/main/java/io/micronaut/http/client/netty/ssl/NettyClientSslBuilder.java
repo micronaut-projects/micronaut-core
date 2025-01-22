f39deb16464db64e14893856cbb58855b8d57745
Sergio del Amo <sergio.delamo@softamo.com>
2025-01-22 18:11:22 +0100

    doc: document how to Disable hostname verification in ssl config

--- http-client/src/main/java/io/micronaut/http/client/netty/ssl/NettyClientSslBuilder.java
+++ http-client/src/main/java/io/micronaut/http/client/netty/ssl/NettyClientSslBuilder.java
@@ -88,6 +88,14 @@ public class NettyClientSslBuilder extends SslBuilder<SslContext> implements Cli
     @NonNull
     @Override
     public final SslContext build(SslConfiguration ssl, HttpVersionSelection versionSelection) {
+        try {
+            return createSslContextBuilder(ssl, versionSelection).build();
+        } catch (SSLException ex) {
+            throw new SslConfigurationException("An error occurred while setting up SSL", ex);
+        }
+    }
+
+    protected  SslContextBuilder createSslContextBuilder(SslConfiguration ssl, HttpVersionSelection versionSelection) {
         SslContextBuilder sslBuilder = SslContextBuilder
             .forClient()
             .keyManager(getKeyManagerFactory(ssl))
@@ -122,12 +130,7 @@ public class NettyClientSslBuilder extends SslBuilder<SslContext> implements Cli
                 versionSelection.getAlpnSupportedProtocols()
             ));
         }
-
-        try {
-            return sslBuilder.build();
-        } catch (SSLException ex) {
-            throw new SslConfigurationException("An error occurred while setting up SSL", ex);
-        }
+        return sslBuilder;
     }
 
     @Override
