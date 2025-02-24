08e32d573bfa6151ffbe7edf3cf7bef0191644b3
yawkat <jonas.konrad@oracle.com>
2025-02-24 16:13:20 +0100

    graceful shutdown for scheduler

--- context/src/main/java/io/micronaut/scheduling/executor/ExecutorFactory.java
+++ context/src/main/java/io/micronaut/scheduling/executor/ExecutorFactory.java
@@ -19,11 +19,18 @@ import io.micronaut.context.BeanLocator;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.EachBean;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.reflect.InstantiationUtils;
 import io.micronaut.inject.qualifiers.Qualifiers;
+import io.micronaut.runtime.server.GracefulShutdownCapable;
 import io.micronaut.scheduling.LoomSupport;
 import jakarta.inject.Inject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalLong;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
@@ -35,10 +42,11 @@ import java.util.concurrent.ThreadFactory;
  * @since 1.0
  */
 @Factory
-public class ExecutorFactory {
+public class ExecutorFactory implements GracefulShutdownCapable {
 
     private final BeanLocator beanLocator;
     private final ThreadFactory threadFactory;
+    private List<GracefulShutdownCapable> gracefulShutdownCapable;
 
     /**
      *
@@ -89,7 +97,14 @@ public class ExecutorFactory {
             case CACHED:
                 return Executors.newCachedThreadPool(getThreadFactory(executorConfiguration));
             case SCHEDULED:
-                return Executors.newScheduledThreadPool(executorConfiguration.getCorePoolSize(), getThreadFactory(executorConfiguration));
+                var exec = new GracefulShutdownCapableScheduledThreadPoolExecutor(executorConfiguration.getCorePoolSize(), getThreadFactory(executorConfiguration));
+                synchronized (this) {
+                    if (gracefulShutdownCapable == null) {
+                        gracefulShutdownCapable = new ArrayList<>();
+                    }
+                    gracefulShutdownCapable.add(exec);
+                }
+                return exec;
             case WORK_STEALING:
                 return Executors.newWorkStealingPool(executorConfiguration.getParallelism());
             case THREAD_PER_TASK:
@@ -117,4 +132,27 @@ public class ExecutorFactory {
                 });
     }
 
+    @Override
+    public @NonNull CompletionStage<?> shutdownGracefully() {
+        List<GracefulShutdownCapable> copy;
+        synchronized (this) {
+            if (gracefulShutdownCapable == null) {
+                return CompletableFuture.completedFuture(null);
+            }
+            copy = new ArrayList<>(gracefulShutdownCapable);
+        }
+        return GracefulShutdownCapable.shutdownAll(copy.stream());
+    }
+
+    @Override
+    public OptionalLong reportActiveTasks() {
+        List<GracefulShutdownCapable> copy;
+        synchronized (this) {
+            if (gracefulShutdownCapable == null) {
+                return OptionalLong.empty();
+            }
+            copy = new ArrayList<>(gracefulShutdownCapable);
+        }
+        return GracefulShutdownCapable.combineActiveTasks(copy);
+    }
 }
