fd49a5f56b8c821bb160b78d05b6fd133cc9f33f
yawkat <jonas.konrad@oracle.com>
2025-02-25 08:29:44 +0100

    fix test

--- management/src/test/groovy/io/micronaut/management/health/monitor/HealthMonitorTaskSpec.groovy
+++ management/src/test/groovy/io/micronaut/management/health/monitor/HealthMonitorTaskSpec.groovy
@@ -60,7 +60,7 @@ class HealthMonitorTaskSpec extends Specification {
                     assert appender.events.size() == 7
                     assert appender.events[0].contains('Health monitor result for compositeDiscoveryClient(): status UP, details {')
                     assert appender.events[1].contains('Health monitor result for diskSpace: status UP, details {')
-                    assert appender.events[2] == 'Health monitor result for gracefulShutdown: status UP, details {}'
+                    assert appender.events[2] == 'Health monitor result for gracefulShutdown: status UP, details {activeTasks=0}'
                     assert appender.events[3] == 'Health monitor result for liveness: status UP, details {}'
                     assert appender.events[4] == 'Health monitor result for readiness: status UP, details {}'
                     assert appender.events[5] == 'Health monitor result for service: status UP, details {}'
