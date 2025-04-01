2ba564602fa186d4f9b9fe61332be595bde01b09
Sergio del Amo <sergio.delamo@softamo.com>
2025-04-01 11:26:10 +0200

    add missing license

--- http-server-tck/src/main/java/io/micronaut/http/server/tck/tests/filter/CacheControlTest.java
+++ http-server-tck/src/main/java/io/micronaut/http/server/tck/tests/filter/CacheControlTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.http.server.tck.tests.filter;
 
 import io.micronaut.context.annotation.Requires;
@@ -12,7 +27,6 @@ import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.annotation.ResponseFilter;
 import io.micronaut.http.annotation.ServerFilter;
 import io.micronaut.http.cachecontrol.CacheControl;
-import io.micronaut.http.cachecontrol.ResponseDirective;
 import io.micronaut.http.filter.FilterPatternStyle;
 import io.micronaut.http.tck.AssertionUtils;
 import io.micronaut.http.tck.HttpResponseAssertion;
