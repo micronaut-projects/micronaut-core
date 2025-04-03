a13b606a13a5fab2dec23e1c66689074f241a0c0
Graeme Rocher <graeme.rocher@oracle.com>
2025-04-03 09:06:03 +0200

    reproducer verifier error

--- test-suite/src/test/java/io/micronaut/test/lombok/LombokIntrospectedBuilderTest.java
+++ test-suite/src/test/java/io/micronaut/test/lombok/LombokIntrospectedBuilderTest.java
@@ -1,8 +1,10 @@
 package io.micronaut.test.lombok;
 
+import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.beans.BeanConstructor;
 import io.micronaut.core.beans.BeanIntrospection;
 import io.micronaut.core.beans.BeanProperty;
+import io.micronaut.test.lombok.importtest.VersionManifest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +15,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Introspected(classes = VersionManifest.VersionManifestEntry.class)
 public class LombokIntrospectedBuilderTest {
+
+    @Test
+    void testImportedIntrospection() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        getClass().getClassLoader().loadClass("io.micronaut.test.lombok.$io_micronaut_test_lombok_importtest_VersionManifest$VersionManifestEntry$Introspection").newInstance();
+        BeanIntrospection<VersionManifest.VersionManifestEntry> introspection = BeanIntrospection.getIntrospection(VersionManifest.VersionManifestEntry.class);
+        VersionManifest.VersionManifestEntry entry = introspection.instantiate("test", 1, 2);
+        assertNotNull(entry);
+        assertEquals("test", entry.getName());
+        assertEquals(1, entry.getMajorVersion());
+    }
 
     @Test
     void testLombokInheritance() {
