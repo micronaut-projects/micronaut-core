85a80830a091df6d0d7b0ebc7460083fd04db530
Patrick Ziegler <patrick.ziegler@oracle.com>
2025-04-16 16:57:33 +0200

    Include Argument in BeanCreatedEvent

---
+++ inject-java/src/test/groovy/io/micronaut/inject/lifecycle/beancreationeventlistener/JCreatedListener.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.inject.lifecycle.beancreationeventlistener;
+
+import io.micronaut.context.event.BeanCreatedEvent;
+import io.micronaut.context.event.BeanCreatedEventListener;
+import io.micronaut.core.type.Argument;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class JCreatedListener implements BeanCreatedEventListener<J> {
+    boolean executed;
+    Argument<J> beanType;
+
+    @Override
+    public J onCreated(BeanCreatedEvent<J> event) {
+        assert !executed : "JCreatedListener is being triggered more than once";
+        executed = true;
+        beanType = event.getBeanType();
+        return event.getBean();
+    }
+}
