787f09fd240bfa5f755a81126cfda7849e5e39d2
Brane F. Gračnar <bfg@users.noreply.github.com>
2025-05-08 13:49:43 +0200

    Added factory class for `InstantSource` bean creation
    For motivation and rationale see #11655

---
+++ context/src/main/java/io/micronaut/context/time/InstantSourceFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017-2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.context.time;
+
+import io.micronaut.context.annotation.Bean;
+import io.micronaut.context.annotation.Factory;
+import jakarta.inject.Singleton;
+
+import java.time.Clock;
+import java.time.InstantSource;
+
+/**
+ * Factory class that creates {@link InstantSource} bean.
+ */
+@Factory
+public final class InstantSourceFactory {
+    /**
+     * Creates {@link InstantSource} from a system clock.
+     *
+     * @return The system clock
+     */
+    @Bean
+    @Singleton
+    public InstantSource systemClock() {
+        return Clock.systemDefaultZone();
+    }
+}
