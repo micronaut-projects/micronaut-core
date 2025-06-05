4e0d182f34e7fb4ecd9852bbc9f95bfb6701799d
yawkat <jonas.konrad@oracle.com>
2025-06-05 16:05:08 +0200

    workaround for MemorySegment issue

---
+++ http-netty/src/main/java/io/micronaut/http/netty/deletions.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017-2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.http.netty;
+
+import com.oracle.svm.core.annotate.Delete;
+import com.oracle.svm.core.annotate.TargetClass;
+
+// Netty CleanerJava24 references these two classes, and they reference ScopedMemoryAccess, which
+// is not implemented in native image. That leads to a link error even on JVMs where MemorySegment
+// is still in preview. We need to delete these APIs entirely to make the build work, and suppress
+// the build time error with --report-unsupported-elements-at-runtime
+
+@TargetClass(className = "java.lang.foreign.MemorySegment")
+@Delete
+final class MemorySegmentDeletion {
+}
+
+@TargetClass(className = "java.lang.foreign.Arena")
+@Delete
+final class ArenaDeletion {
+}
