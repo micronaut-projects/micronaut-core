0f885ca555e641581b2c71fd2b91150156759fa9
Graeme Rocher <graeme.rocher@oracle.com>
2025-10-31 12:45:51 +0100

    tests for field injection not yet working

---
+++ inject-python-test/src/test/groovy/io/micronaut/python/annotation/processing/test/inject/FieldInjectionSpec.groovy
@@ -0,0 +1,43 @@
+package io.micronaut.python.annotation.processing.test.inject
+
+import io.micronaut.python.annotation.processing.test.AbstractPythonTypeElementSpec
+import spock.lang.PendingFeature
+
+class FieldInjectionSpec extends AbstractPythonTypeElementSpec {
+
+    @PendingFeature(reason = "Compiler needs to produce correct metadata for Annotated[..]")
+    void "test field injection with Annotated[Type, Inject] syntax"() {
+        given: "Python code with field injection"
+        def pythonCode = '''
+from typing import Annotated
+from jakarta.inject import Singleton, Inject
+from io.micronaut.context.annotation import Executable
+
+@Singleton
+class HelperService:
+    @Executable
+    def get_help(self) -> str:
+        return "I am helping!"
+
+@Singleton
+class MainService:
+    helper: Annotated[HelperService, Inject] = None
+
+    @Executable
+    def get_combined_message(self) -> str:
+        if self.helper is None:
+            return "No helper available"
+        return "Main service with: " + self.helper.get_help()
+'''
+
+        when: "Building ApplicationContext and getting the main service"
+        def context = buildContext(pythonCode)
+        def mainService = getBean(context, "python.MainService")
+
+        then: "Field injection should work"
+        mainService.get_combined_message() == "Main service with: I am helping!"
+
+        cleanup: "Ensure context is properly closed"
+        context?.close()
+    }
+}
