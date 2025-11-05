e24e678098791fc872ef0c5bae066243e12b8655
Graeme Rocher <graeme.rocher@oracle.com>
2025-11-05 11:15:05 -0800

    more tests for controllers

--- inject-python-test/src/test/groovy/io/micronaut/python/annotation/processing/test/web/PythonControllerSpec.groovy
+++ inject-python-test/src/test/groovy/io/micronaut/python/annotation/processing/test/web/PythonControllerSpec.groovy
@@ -1,9 +1,12 @@
 package io.micronaut.python.annotation.processing.test.web
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.core.beans.BeanIntrospection
 import io.micronaut.http.client.HttpClient
 import io.micronaut.python.annotation.processing.test.AbstractPythonTypeElementSpec
 import io.micronaut.runtime.server.EmbeddedServer
+import jakarta.inject.Singleton
 
 class PythonControllerSpec extends AbstractPythonTypeElementSpec {
 
@@ -40,4 +43,83 @@ class HelloController:
         client.close()
         context?.close()
     }
+
+    void "test python controller map response"() {
+        given:
+        def context = buildContext('''
+from jakarta.inject import Singleton as S
+from io.micronaut.http.annotation import Controller, Get
+
+@S
+class MessageService:
+    def say_hello(self, name : str) -> dict[str, str]:
+        return {
+            "message": f"Hello {name}"
+        }
+
+@Controller("/hello")
+class HelloController:
+    def __init__(self, messageService: MessageService):
+        self.messageService = messageService
+
+    @Get("/{name}")
+    def say_hello(self, name : str) -> dict[str, str]:
+        return self.messageService.say_hello(name)
+
+''', true)
+
+        def embeddedServer = context.getBean(EmbeddedServer)
+        embeddedServer.start()
+        def client = context.createBean(HttpClient, embeddedServer.URL)
+
+        expect:
+        client.toBlocking().retrieve("/hello/John") == "{\"message\":\"Hello John\"}"
+
+        cleanup:
+        client.close()
+        context?.close()
+    }
+
+    void "test python controller JSON response"() {
+        given:
+        def context = buildContext('''
+from jakarta.inject import Singleton as S
+from io.micronaut.http.annotation import Controller, Get
+from io.micronaut.core.annotation import Introspected
+from dataclasses import dataclass
+
+@Introspected
+@dataclass
+class Message:
+    message: str = "Who are you?"
+
+@S
+class MessageService:
+    def say_hello(self, name : str) -> Message:
+        text = f"Hello {name}"
+        return Message(text)
+
+@Controller("/hello")
+class HelloController:
+    def __init__(self, messageService: MessageService):
+        self.messageService = messageService
+
+    @Get("/{name}")
+    def say_hello(self, name : str) -> Message:
+        return self.messageService.say_hello(name)
+
+''', true)
+
+        def embeddedServer = context.getBean(EmbeddedServer)
+        embeddedServer.start()
+        def client = context.createBean(HttpClient, embeddedServer.URL)
+
+        expect:
+        client.toBlocking().retrieve("/hello/John") == "{\"message\":\"Hello John\"}"
+
+        cleanup:
+        client.close()
+        context?.close()
+    }
+
 }
