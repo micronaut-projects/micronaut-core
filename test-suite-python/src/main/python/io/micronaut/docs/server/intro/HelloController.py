4b070af3729836d0052a75659cdb201f33c2c9c9
Graeme Rocher <graeme.rocher@oracle.com>
2025-11-12 17:45:47 +0100

    add hello controller

---
+++ test-suite-python/src/main/python/io/micronaut/docs/server/intro/HelloController.py
@@ -0,0 +1,12 @@
+# tag::imports[]
+from micronaut.http import MediaType
+from micronaut.http.annotation import Controller, Get
+# end::imports[]
+
+# tag::class[]
+@Controller("/hello") # <1>
+class HelloController:
+    @Get(produces = MediaType.TEXT_PLAIN) # <2>
+    def index(self) -> str:
+        return "Hello World" # <3>
+# end::class[]
