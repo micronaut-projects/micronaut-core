9f49b42f3cc0f7e305dab21110715781a2af8b4e
Graeme Rocher <graeme.rocher@oracle.com>
2025-11-19 11:21:55 +0100

    add working factory example for Python

---
+++ test-suite-python/src/test/python/micronaut/docs/factories/EngineSpec.py
@@ -0,0 +1,21 @@
+from org.junit.jupiter.api import Test
+from micronaut.test.extensions.junit5.annotation import MicronautTest
+from micronaut.context import ApplicationContext
+from micronaut.context.exceptions import NoSuchBeanException
+from jakarta.inject import Inject
+from typing import Annotated
+import java
+
+# tag::class[]
+@MicronautTest
+class EngineSpec:
+    context : Annotated[ApplicationContext, Inject] = None
+
+    @Test
+    def test_engine(self) -> None:
+        # tag::start[]
+        Engine = java.type("micronaut.docs.factories.Engine")
+        assert self.context.getBean(Engine) is not None
+        # end::start[]
+
+# end::class[]
