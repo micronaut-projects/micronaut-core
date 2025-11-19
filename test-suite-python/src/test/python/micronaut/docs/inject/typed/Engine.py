13691a12c066f10d0f07ea4df3b8ff259943ce8c
Graeme Rocher <graeme.rocher@oracle.com>
2025-11-19 11:07:01 +0100

    docs for typed injection for Python

---
+++ test-suite-python/src/test/python/micronaut/docs/inject/typed/Engine.py
@@ -0,0 +1,12 @@
+# tag::class[]
+from abc import ABC, abstractmethod
+
+class Engine(ABC): # <1>
+    @abstractmethod
+    def get_cylinders(self) -> int:
+        pass
+
+    @abstractmethod
+    def start(self) -> str:
+        pass
+# end::class[]
---
+++ test-suite-python/src/test/python/micronaut/docs/inject/typed/EngineSpec.py
@@ -0,0 +1,28 @@
+from org.junit.jupiter.api import Test
+from micronaut.test.extensions.junit5.annotation import MicronautTest
+from micronaut.context import ApplicationContext
+from micronaut.context.exceptions import NoSuchBeanException
+from jakarta.inject import Inject
+from typing import Annotated
+import java
+
+# tag::class[]
+@MicronautTest
+class EngineSpec:
+    context : Annotated[ApplicationContext, Inject] = None
+
+    @Test
+    def test_engine(self) -> None:
+        # tag::start[]
+        V8Engine = java.type("micronaut.docs.inject.typed.V8Engine")
+        Engine = java.type("micronaut.docs.inject.typed.Engine")
+        try:
+            self.context.getBean(V8Engine)
+            assert False # should not get here
+        except BaseException:
+            assert True
+
+        assert self.context.getBean(Engine) is not None
+        # end::start[]
+
+# end::class[]
