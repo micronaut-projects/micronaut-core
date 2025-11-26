ba6be9f703818b6c68739240c64dc97c823b6b43
Graeme Rocher <graeme.rocher@oracle.com>
2025-11-26 14:07:13 -0400

    add not working replaces example

---
+++ test-suite-python/src/test/python/micronaut/docs/replaces/BookServiceSpec.py
@@ -0,0 +1,18 @@
+from org.junit.jupiter.api import Test, Disabled
+from micronaut.test.extensions.junit5.annotation import MicronautTest
+from micronaut.context.annotation import Property
+from jakarta.inject import Inject
+from typing import Annotated
+from .BookService import BookService
+
+@Property(name = "datasource.url", value = "test")
+@MicronautTest
+class BookServiceSpec:
+    book_service : Annotated[BookService, Inject] = None
+
+    @Test
+    @Disabled("For some reason the ReplacesDefinition never loads")
+    def test_book_service(self):
+        assert self.book_service is not None, "should have a book service"
+        assert self.book_service.__class__.__name__ == 'MockBookService', "should be a JDBC book service"
+
