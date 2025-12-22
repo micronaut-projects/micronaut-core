6d86217ea26a8f867764de823dd1eefbedaf8bf5
Cedric Champeau <cedric.champeau@gmail.com>
2025-12-22 14:01:51 +0100

    Remove CLI module (which will be external)

--- pyronaut-cli/src/test/python/foo/listener.py
+++
@@ -1,9 +0,0 @@
-from micronaut.runtime.event.annotation import EventListener
-from micronaut.context.event import StartupEvent
-from jakarta.inject import Singleton
-
-@Singleton
-class MyListener:
-    @EventListener
-    def start(self, event: StartupEvent):
-        print("Hello from listener")
