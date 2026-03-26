ce6930c7b8c4ea04e4953134521707ea5bbc5b21
Sergio del Amo <sergio.delamo@softamo.com>
2026-03-26 12:15:42 +0100

    STATE_UPDATER

--- http/src/main/java/io/micronaut/http/reactive/execution/FlowAsMono.java
+++ http/src/main/java/io/micronaut/http/reactive/execution/FlowAsMono.java
@@ -61,11 +61,7 @@ final class FlowAsMono<T> extends Mono<T> implements Fuseable {
 
     private final class SubscriptionImpl implements QueueSubscription<T> {
         @SuppressWarnings("rawtypes")
-        private static final AtomicIntegerFieldUpdater<FlowAsMono.SubscriptionImpl> STATE_UPDATER
-            = AtomicIntegerFieldUpdater.newUpdater(FlowAsMono.SubscriptionImpl.class, "state");
-        @Deprecated
-        @SuppressWarnings("rawtypes")
-        private static final AtomicIntegerFieldUpdater<FlowAsMono.SubscriptionImpl> ATOMIC_INTEGER_FIELD_UPDATER_STATE = STATE_UPDATER;
+        private static final AtomicIntegerFieldUpdater<FlowAsMono.SubscriptionImpl> STATE_UPDATER = AtomicIntegerFieldUpdater.newUpdater(FlowAsMono.SubscriptionImpl.class, "state");
         private static final int STATE_WAITING = 0;
         private static final int STATE_SUBSCRIBING = 1;
         private static final int STATE_DONE = 2;
@@ -104,7 +100,7 @@ final class FlowAsMono<T> extends Mono<T> implements Fuseable {
         void callOnSubscribe() {
             state = STATE_SUBSCRIBING;
             actual.onSubscribe(this);
-            if (ATOMIC_INTEGER_FIELD_UPDATER_STATE.getAndSet(this, STATE_WAITING) == STATE_DONE) {
+            if (STATE_UPDATER.getAndSet(this, STATE_WAITING) == STATE_DONE) {
                 // onComplete was already called but result held back to avoid reentrancy, need to
                 // forward its result
                 forward(result, error);
@@ -118,7 +114,7 @@ final class FlowAsMono<T> extends Mono<T> implements Fuseable {
                 flow.onComplete((v, e) -> {
                     result = v;
                     error = e;
-                    if (ATOMIC_INTEGER_FIELD_UPDATER_STATE.getAndSet(this, STATE_DONE) == STATE_WAITING) {
+                    if (STATE_UPDATER.getAndSet(this, STATE_DONE) == STATE_WAITING) {
                         // onSubscribe is already done so we can forward immediately
                         forward(v, e);
                     }
