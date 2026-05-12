b035dfb9227e8fd435d8fbee769091b52f64e7d0
Graeme Rocher <graeme.rocher@oracle.com>
2026-05-12 18:50:41 +0200

    Add Python default implementation coverage

---
+++ inject-python-test/src/test/groovy/io/micronaut/python/annotation/processing/test/DefaultImplementationSpec.groovy
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017-2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.python.annotation.processing.test
+
+import spock.lang.PendingFeature
+
+class DefaultImplementationSpec extends AbstractPythonTypeElementSpec {
+
+    @PendingFeature(reason = "Tracked in inject-python-test/DISABLED_TESTS.md: PY-INJECT-0013")
+    void "test pick default implementation when multiple candidates"() {
+        given:
+        def context = buildContext('''\
+from abc import ABC
+from jakarta.inject import Singleton
+from micronaut.context.annotation import DefaultImplementation, Executable
+
+@DefaultImplementation(name="python.TestImpl")
+class Test(ABC):
+    def name(self) -> str:
+        return "base"
+
+@Singleton
+class TestImpl(Test):
+    @Executable
+    def name(self) -> str:
+        return "default"
+
+@Singleton
+class TestImpl2(Test):
+    @Executable
+    def name(self) -> str:
+        return "secondary"
+''')
+        def testClass = context.classLoader.loadClass("python.Test")
+
+        expect:
+        context.getBean(testClass).name() == "default"
+
+        cleanup:
+        context.close()
+    }
+}
