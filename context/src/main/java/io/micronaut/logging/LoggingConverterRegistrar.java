e38b0aa75bde92b5a777b1e0b6c16b69e9c2dbe7
dstepanov <denis.s.stepanov@oracle.com>
2026-09-11 18:11:21 +0300

    Address no-reflection review feedback

--- context/src/main/java/io/micronaut/logging/LoggingConverterRegistrar.java
+++ context/src/main/java/io/micronaut/logging/LoggingConverterRegistrar.java
@@ -39,13 +39,7 @@ public final class LoggingConverterRegistrar implements TypeConverterRegistrar {
                 return Optional.of(LogLevel.NOT_SPECIFIED);
             }
             try {
-                String name = object.toString().toUpperCase(Locale.ENGLISH);
-                for (LogLevel logLevel : LogLevel.values()) {
-                    if (logLevel.name().equals(name)) {
-                        return Optional.of(logLevel);
-                    }
-                }
-                throw new IllegalArgumentException("No enum constant " + LogLevel.class.getName() + "." + name);
+                return Optional.of(Enum.valueOf(LogLevel.class, object.toString().toUpperCase(Locale.ENGLISH)));
             } catch (IllegalArgumentException e) {
                 context.reject(object, e);
                 return Optional.empty();
