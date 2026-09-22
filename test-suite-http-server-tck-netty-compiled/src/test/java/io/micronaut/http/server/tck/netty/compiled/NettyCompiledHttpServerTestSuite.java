a9aea3713b1a902fdc1bed69d9b18d7b0a539765
dstepanov <denis.s.stepanov@oracle.com>
2026-09-22 17:56:58 +0300

    Run the Netty server TCK with the routes of its controllers linked into a route plan
    The suite is renamed test-suite-http-server-tck-netty-compiled. The TCK controllers are compiled
    without the route compiler, so the suite links their packages into one plan with the
    micronaut.router.link.packages and micronaut.router.link.class processor options.

--- test-suite-http-server-tck-netty-compiled/src/test/java/io/micronaut/http/server/tck/netty/compiled/NettyCompiledHttpServerTestSuite.java
+++ test-suite-http-server-tck-netty-compiled/src/test/java/io/micronaut/http/server/tck/netty/compiled/NettyCompiledHttpServerTestSuite.java
@@ -1,4 +1,4 @@
-package io.micronaut.http.server.tck.netty.precompiled;
+package io.micronaut.http.server.tck.netty.compiled;
 
 import org.junit.platform.suite.api.ExcludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
@@ -7,8 +7,8 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
-@SuiteDisplayName("HTTP Server TCK for Netty with precompiled routes")
+@SuiteDisplayName("HTTP Server TCK for Netty with compiled routes")
 // fails on native
 @ExcludeClassNamePatterns("io.micronaut.http.server.tck.tests.staticresources.StaticResourceTest")
-public class NettyPrecompiledHttpServerTestSuite {
+public class NettyCompiledHttpServerTestSuite {
 }
