47bd615837dfda8b33272846938b3d75b4c5c337
dstepanov <denis.s.stepanov@oracle.com>
2026-09-23 13:31:11 +0300

    Resolve a relative raw request URI against the Netty client URL
    A raw exchange of the Netty client sent the request URI as it was, so a relative URI of a
    client with a base URL (created, or injected with @Client) failed with a NullPointerException
    on the missing scheme. It is resolved like the URI of any other request now.

--- http-client/src/test/groovy/io/micronaut/http/client/RawHttpClientSpec.groovy
+++ http-client/src/test/groovy/io/micronaut/http/client/RawHttpClientSpec.groovy
@@ -7,7 +7,9 @@ import io.micronaut.http.HttpRequest
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.core.io.buffer.ByteArrayBufferFactory
 import io.micronaut.http.body.ByteBody
+import io.micronaut.http.body.ByteBodyFactory
 import io.micronaut.http.netty.body.NettyByteBodyFactory
 import io.micronaut.runtime.server.EmbeddedServer
 import io.netty.buffer.Unpooled
@@ -45,6 +47,30 @@ class RawHttpClientSpec extends Specification {
 
         cleanup:
         resp.close()
+        server.close()
+        ctx.close()
+    }
+
+    def 'relative request URI is resolved against the client URL'() {
+        given:
+        def ctx = ApplicationContext.run(['spec.name': 'RawHttpClientSpec'])
+        def server = ctx.getBean(EmbeddedServer)
+        server.start()
+        def client = ctx.createBean(RawHttpClient, server.URI)
+
+        when:
+        def resp = Mono.from(client.exchange(
+                HttpRequest.POST("/raw/echo", null),
+                ByteBodyFactory.createDefault(ByteArrayBufferFactory.INSTANCE).adapt("foo".getBytes(StandardCharsets.UTF_8)),
+                null
+        )).block()
+
+        then:
+        resp.byteBody().buffer().get().toString(StandardCharsets.UTF_8) == "foo"
+
+        cleanup:
+        resp?.close()
+        client.close()
         server.close()
         ctx.close()
     }
